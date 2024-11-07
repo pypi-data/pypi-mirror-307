921aff70bfe7791bcebf2e3442752f6e973cc655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 15:04:46 +0000

    Add package huggingleg/0.1/huggingleg-0.1.tar.gz

---
+++ packages/huggingleg/huggingleg-0.1.tar.gz/huggingleg-0.1/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='huggingleg',  # How you named your package folder (MyLib)
+    packages=['huggingleg'],  # Chose the same as "name"
+    version='0.1',  # Start with a small number and increase it with every change you make
+)
