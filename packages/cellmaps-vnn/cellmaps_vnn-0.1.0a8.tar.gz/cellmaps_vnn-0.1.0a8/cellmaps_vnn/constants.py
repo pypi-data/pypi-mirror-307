d55bd9c7ce77091a799ea62aacbfec63d285f5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:08:28 +0000

    Add package cellmaps-vnn/0.1.0a8/cellmaps_vnn-0.1.0a8.tar.gz

---
+++ packages/cellmaps-vnn/cellmaps_vnn-0.1.0a8.tar.gz/cellmaps_vnn-0.1.0a8/cellmaps_vnn/constants.py
@@ -0,0 +1,33 @@
+"""
+Contains constants used by cellmaps vnn
+"""
+
+PRHO_SCORE = 'P_rho'
+"""
+P rho score
+"""
+
+RLIPP_OUTPUT_FILE = 'rlipp.out'
+"""
+Output file from rlipp algorithm
+"""
+
+HIERARCHY_FILENAME = 'hierarchy.cx2'
+"""
+Hierarchy filename.
+"""
+
+ORIGINAL_HIERARCHY_FILENAME = 'original_hierarchy.cx2'
+"""
+Original hierarchy filename.
+"""
+
+PARENT_NETWORK_NAME = 'hierarchy_parent.cx2'
+"""
+Parent network of hierarchy filename.
+"""
+
+GENE_SET_COLUMN_NAME = 'CD_MemberList'
+"""
+Name of the node attribute of the hierarchy with list of genes/ proteins of this node.
+"""
