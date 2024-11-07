c40418307ed2fe1409ded45a50d34b11cf24e9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 14:04:14 +0000

    Add package piwiPre/0.19.3/piwipre-0.19.3.tar.gz

---
+++ packages/piwiPre/piwipre-0.19.3.tar.gz/piwipre-0.19.3/piwiPre/pwpVersion.py
@@ -0,0 +1,21 @@
+# ---------------------------------------------------------------------------------------------------------------
+# piwiPre project
+# This program and library is licenced under the European Union Public Licence v1.2 (see LICENCE)
+# developed by fabien.battini(at)gmail.com
+# ---------------------------------------------------------------------------------------------------------------
+
+
+class PwpVersion:
+    help = '0.19.3 at 11/07/2024 00:23:53'
+    spec = '0.19.3'
+    release = 0
+    version = 19
+    patch = 3
+    date = '11/07/2024 00:23:53'
+    url = 'https://gitlab.com/api/v4/projects/48915444/packages/generic/piwiPre/0.19.3/piwiPre-0.19.3.exe'
+    installer = 'https://gitlab.com/api/v4/projects/48915444/packages/generic/piwiPre/0.19.3/pwpInstaller-0.19.3.exe'
+    # noinspection PyPep8
+    configurator = 'https://gitlab.com/api/v4/projects/48915444/packages/generic/piwiPre/0.19.3/pwpConfigurator-0.19.3.exe'
+
+    def __init__(self):
+        pass
