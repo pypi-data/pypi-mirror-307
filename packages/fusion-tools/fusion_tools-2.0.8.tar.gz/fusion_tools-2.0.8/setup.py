67704354aa96e7f1dc6671b2def3fedf5e553ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 04:03:29 +0000

    Add package fusion-tools/2.0.8/fusion_tools-2.0.8.tar.gz

---
+++ packages/fusion-tools/fusion_tools-2.0.8.tar.gz/fusion_tools-2.0.8/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fusion-tools",
+    version="2.0.8",
+    author="Sam Border",
+    author_email="sam.border2256@gmail.com",
+    description="Modular visualization and analysis dashboard creation for high-resolution microscopy images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spborder/fusion-tools",
+    install_requires=[
+        "lxml>=4.9.2",
+        "geojson>=3.0.1",
+        "shapely>=2.0.1",
+        "geopandas",
+        "girder-client",
+        "numpy>=1.20.0",
+        "large-image[common]",
+        "wsi-annotations-kit",
+        "dash-leaflet[all]>=1.0.15",
+        "dash-extensions",
+        "dash-bootstrap-components",
+        "dash-mantine-components>=0.14.4",
+        "dash-treeview-antd",
+        "requests",
+        "uuid",
+        "scikit-image",
+        "umap-learn",
+        "fastapi>=0.103.2",
+        "uvicorn",
+        "statsmodels",
+        "typing-extensions>=4.8.0"
+    ],
+    packages=setuptools.find_packages(where = 'src',include=["fusion_tools*"]),
+    package_dir={"":"src"},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
