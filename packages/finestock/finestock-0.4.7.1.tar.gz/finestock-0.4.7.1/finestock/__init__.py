e7eaf680277501399da33bf462569f9da7841481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 15:04:34 +0000

    Add package finestock/0.4.7.1/finestock-0.4.7.1.tar.gz

---
+++ packages/finestock/finestock-0.4.7.1.tar.gz/finestock-0.4.7.1/finestock/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '0.4.7.1'
+
+from .api_fcatory import APIFactory
+from .model import *
+from .ebest import *
+from .kis import *
+
+__all__ = ['APIFactory', 'model',]
+
+
+APIS = ["EBEST", "KIS", "LS"]
+
+def print_version_info():
+    print(f"The version of this stock finance API is {__version__}.")
+
+def create_api(api_type):
+    return APIFactory.create_api(api_type)
