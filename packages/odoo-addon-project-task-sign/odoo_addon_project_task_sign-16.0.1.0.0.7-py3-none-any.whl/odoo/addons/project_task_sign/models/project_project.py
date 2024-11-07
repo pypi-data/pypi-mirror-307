7ee1364aadf31ab570a560a8d34a3fb8e8315e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:07:46 +0000

    Add package odoo-addon-project-task-sign/16.0.1.0.0.7/odoo_addon_project_task_sign-16.0.1.0.0.7-py3-none-any.whl

---
+++ packages/odoo-addon-project-task-sign/odoo_addon_project_task_sign-16.0.1.0.0.7-py3-none-any.whl/odoo/addons/project_task_sign/models/project_project.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProjectProject(models.Model):
+    _inherit = "project.project"
+
+    allow_customer_signature = fields.Boolean()
+    send_automatic_signature_mail = fields.Boolean()
