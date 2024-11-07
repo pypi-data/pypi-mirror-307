bfd91ae90e03d4ef290fa473c7f5eaf53b206210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 14:04:55 +0000

    Add package ikl-dsnd-probability/0.1/ikl_dsnd_probability-0.1.tar.gz

---
+++ packages/ikl-dsnd-probability/ikl_dsnd_probability-0.1.tar.gz/ikl_dsnd_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ikl_dsnd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ikl_dsnd_probability'],
+      zip_safe=False)
