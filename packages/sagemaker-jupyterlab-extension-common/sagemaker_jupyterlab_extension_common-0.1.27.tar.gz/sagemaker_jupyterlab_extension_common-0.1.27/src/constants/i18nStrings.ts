54bca6d2eb806c9dca5aed058d28af0788089b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 14:03:47 +0000

    Add package sagemaker-jupyterlab-extension-common/0.1.27/sagemaker_jupyterlab_extension_common-0.1.27.tar.gz

---
+++ packages/sagemaker-jupyterlab-extension-common/sagemaker_jupyterlab_extension_common-0.1.27.tar.gz/sagemaker_jupyterlab_extension_common-0.1.27/src/constants/i18nStrings.ts
@@ -0,0 +1,17 @@
+const i18nStrings = {
+  ShortBread: {
+    CookiePreferences: 'Cookie Preferences',
+  },
+  JumpStartDeeplinking: {
+    errorDialog: {
+      errorTitle: 'Unable to open JumpStart notebook',
+      defaultErrorMessage: 'Something went wrong when opening the JumpStart notebook.',
+      notebookNotFoundErrorMessage: 'The JumpStart notebook does not exist or is not supported in the current region.',
+      notebookSizeTooLargeErrorMessage: 'The notebook size is too large.',
+      invalidRequestErrorMessage: 'A request to open the JumpStart notebook is invalid.',
+      downLoadDirectoryNotFoundMessage: 'Requested download directory for JumpStart notebook does not exist.',
+    },
+  },
+};
+
+export { i18nStrings };
