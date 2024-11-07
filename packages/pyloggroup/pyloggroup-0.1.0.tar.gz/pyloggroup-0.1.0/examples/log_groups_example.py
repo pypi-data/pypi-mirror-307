fe343455086726efe2cc82659d0c6e721e6ba101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 15:04:02 +0000

    Add package pyloggroup/0.1.0/pyloggroup-0.1.0.tar.gz

---
+++ packages/pyloggroup/pyloggroup-0.1.0.tar.gz/pyloggroup-0.1.0/examples/log_groups_example.py
@@ -0,0 +1,30 @@
+import logging
+
+from pyloggroup import log_group
+
+
+@log_group("MainGroup", include_prints=True)
+def main_function():
+    print("====1Inside main function===")
+    print("*" * 20)
+    logging.info("Inside main function")
+    inner_function()
+
+
+@log_group("InnerGroup")
+def inner_function():
+    print("====2Inside inner function====")
+    print("*" * 20)
+    logging.info("Inside inner function")
+    innermost_function()
+
+
+@log_group("InnermostGroup")
+def innermost_function():
+    print("====3Inside innermost function====")
+    print("*" * 40)
+    logging.info("Inside innermost function")
+    print("Innermost function executed")
+
+
+main_function()
