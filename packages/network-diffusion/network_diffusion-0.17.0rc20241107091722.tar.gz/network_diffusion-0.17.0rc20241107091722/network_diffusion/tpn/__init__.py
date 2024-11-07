c2391592c7ffb7db11115fe4faec331942af2036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 16:04:07 +0000

    Add package network-diffusion/0.17.0rc20241107091722/network_diffusion-0.17.0rc20241107091722.tar.gz

---
+++ packages/network-diffusion/network_diffusion-0.17.0rc20241107091722.tar.gz/network_diffusion-0.17.0rc20241107091722/network_diffusion/tpn/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2023 by Mateusz Nurek, Radosław Michalski, Michał Czuba.
+#
+# This file is a part of Network Diffusion.
+#
+# Network Diffusion is licensed under the MIT License. You may obtain a copy
+# of the License at https://opensource.org/licenses/MIT
+# =============================================================================
+
+"""Modules to handle operations on temporal networks."""
+
+# flake8: noqa
+
+from network_diffusion.tpn.l2_course_net import get_l2_course_net
+from network_diffusion.tpn.tpnetwork import TemporalNetwork
