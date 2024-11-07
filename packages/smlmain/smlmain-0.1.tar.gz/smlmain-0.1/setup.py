81bdbba932ba1165a41bcc0d74111f28f4c8ef90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:08:49 +0000

    Add package smlmain/0.1/smlmain-0.1.tar.gz

---
+++ packages/smlmain/smlmain-0.1.tar.gz/smlmain-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smlmain',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies here, e.g., 'numpy', 'pandas'
+    ],
+    author='Priyanshi Mehta',
+    author_email='dhwanimehta2003@gmail.com',
+    description='A description of your package',
+    url='https://github.com/priyanshi58/smlmain',  # Optional
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
+ 
