4efb12c185ba08df00d7360ae8ad522f58edf2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:07:51 +0000

    Add package vital-ai-haley-kg/0.1.19/vital_ai_haley_kg-0.1.19.tar.gz

---
+++ packages/vital-ai-haley-kg/vital_ai_haley_kg-0.1.19.tar.gz/vital_ai_haley_kg-0.1.19/ai_haley_kg_domain/model/KGNode.pyi
@@ -0,0 +1,23 @@
+
+import datetime
+from vital_ai_vitalsigns.model.VITAL_Node import VITAL_Node
+
+
+class KGNode(VITAL_Node):
+        kGActionTypeList: str
+        kGActionTypeSummaryDateTime: datetime
+        kGChatInteractionGraphURI: str
+        kGDataHash: str
+        kGGraphAssertionDateTime: datetime
+        kGIdentifier: str
+        kGIndexDateTime: datetime
+        kGIndexStatusURI: str
+        kGJSON: str
+        kGModelVersion: str
+        kGNodeCacheDateTime: datetime
+        kGTenantIdentifier: str
+        kGValidTypeURI: str
+        kGVersion: str
+        kGVisualStyleJSON: str
+        kGraphDescription: str
+
