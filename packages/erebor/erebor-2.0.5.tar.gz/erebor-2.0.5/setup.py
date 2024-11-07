f4894a11556bf57711c958d7e0d6dc9872625f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:08:28 +0000

    Add package erebor/2.0.5/erebor-2.0.5.tar.gz

---
+++ packages/erebor/erebor-2.0.5.tar.gz/erebor-2.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="erebor",
+    version="2.0.5",
+    author="Chris Varga",
+    author_email="",
+    description="key-value store",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    scripts=["erebor"],
+    keywords="erebor persistent key-value store",
+)
