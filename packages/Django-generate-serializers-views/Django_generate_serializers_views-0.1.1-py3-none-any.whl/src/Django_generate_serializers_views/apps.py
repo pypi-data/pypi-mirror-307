fb04ce0428cdb759cda507d5cdffb9976733c0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 15:04:37 +0000

    Add package Django-generate-serializers-views/0.1.1/Django_generate_serializers_views-0.1.1-py3-none-any.whl

---
+++ packages/Django-generate-serializers-views/Django_generate_serializers_views-0.1.1-py3-none-any.whl/src/Django_generate_serializers_views/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+class Django_generate_serializers_viewsConfig(AppConfig):
+    name = 'Django_generate_serializers_views'
+
+    def ready(self):
+        # Code to run when the app is ready
+        pass
