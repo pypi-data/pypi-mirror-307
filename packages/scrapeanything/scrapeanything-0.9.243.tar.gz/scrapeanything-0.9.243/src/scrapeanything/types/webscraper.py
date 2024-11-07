ed196303080f41e622e48eaf1c051d7370e85637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:08:08 +0000

    Add package scrapeanything/0.9.243/scrapeanything-0.9.243.tar.gz

---
+++ packages/scrapeanything/scrapeanything-0.9.243.tar.gz/scrapeanything-0.9.243/src/scrapeanything/types/webscraper.py
@@ -0,0 +1,23 @@
+class Functions:
+    SUBSTRING = 'substring'
+    EXPLODE = 'explode'
+    REPLACE = 'join'
+    JOIN = 'join'
+
+class Types:
+    PERCENTAGE = 'PERCENTAGE'
+    BOOL = 'BOOL'
+    MONEY = 'MONEY'
+    NUMBER = 'NUMBER'
+    INTEGER = 'INTEGER'
+    GEO = 'GEO'
+    CHAR = 'CHAR'
+    URL = 'URL'
+    LIST = 'LIST'
+    DATE = 'DATE'
+    JSON = 'JSON'
+    STRING = 'STRING'
+
+class Props:
+    TEXT = 'text()'
+    HTML = 'html()'
