1c7143e9dd32c4fe27de69bd82b1234a91e3ff85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:07:39 +0000

    Add package thinkup-cli/1.1.0/thinkup_cli-1.1.0.tar.gz

---
+++ packages/thinkup-cli/thinkup_cli-1.1.0.tar.gz/thinkup_cli-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="thinkup-cli",
+    version="1.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "questionary",
+        "requests",
+        "prompt_toolkit",
+    ],
+    entry_points={
+        'console_scripts': [
+            'tkup.cli=thinkup_cli.main:main_function',
+        ],
+    },
+)
