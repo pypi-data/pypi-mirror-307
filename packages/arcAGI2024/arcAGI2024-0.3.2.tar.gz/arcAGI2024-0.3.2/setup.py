ddf9c9749756269951c9ace8f3f99fa9f3955e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 14:04:50 +0000

    Add package arcAGI2024/0.3.2/arcAGI2024-0.3.2.tar.gz

---
+++ packages/arcAGI2024/arcAGI2024-0.3.2.tar.gz/arcAGI2024-0.3.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+print("PACKAGES", find_packages(where="src/main"))
+setup(
+    name='arcAGI2024',
+    version='0.3.2',
+    description='Support mechanism for the arc agi project',
+    author='Chris',
+    author_email='your.email@example.com',
+    packages=find_packages(where="src/main"),
+    package_dir={'': 'src/main'},
+    install_requires=[
+        'torch',
+        'tokenizers', # add other dependencies here
+        'transformers',
+        'typeguard',
+        'numpy'
+    ],
+    python_requires='>=3.10', # adjust based on your requirements
+)
