a75635f3278cf7d40af2f50beec983a2c90b8218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 15:04:43 +0000

    Add package drf-json-api-atomic-operations/0.4.0/drf_json_api_atomic_operations-0.4.0-py3-none-any.whl

---
+++ packages/drf-json-api-atomic-operations/drf_json_api_atomic_operations-0.4.0-py3-none-any.whl/atomic_operations/exceptions.py
@@ -0,0 +1,49 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import status
+from rest_framework.exceptions import APIException, ParseError
+
+from atomic_operations.consts import ATOMIC_OPERATIONS
+
+
+class UnprocessableEntity(APIException):
+    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+    default_detail = _('Unprocessable entity.')
+    default_code = 'unprocessable_entity'
+
+
+class JsonApiParseError(ParseError):
+
+    def __init__(self, id, detail, pointer, status=status.HTTP_400_BAD_REQUEST, code=None):
+        self.status_code = status
+
+        _detail = {
+            "id": id,
+            "detail": detail,
+            "source": {
+                "pointer": pointer,
+            },
+            "status": f"{self.status_code}"
+        }
+
+        # there is a bug described by https://github.com/django-json-api/django-rest-framework-json-api/issues/1140
+        # thats why we pass in the details as array
+        super().__init__([_detail], code)
+
+
+class MissingPrimaryData(JsonApiParseError):
+
+    def __init__(self, idx: int):
+        super().__init__(
+            "missing-primary-data",
+            "primary data object musst be present",
+            f"/{ATOMIC_OPERATIONS}/{idx}"
+        )
+
+
+class InvalidPrimaryDataType(JsonApiParseError):
+    def __init__(self, idx, data_type: str):
+        super().__init__(
+            id="invalid-primary-data-type",
+            detail=f"primary data object musst be an {data_type}",
+            pointer=f"/{ATOMIC_OPERATIONS}/{idx}/data"
+        )
---
+++ packages/drf-json-api-atomic-operations/drf_json_api_atomic_operations-0.4.0-py3-none-any.whl/atomic_operations/renderers.py
@@ -0,0 +1,76 @@
+"""
+Renderers
+"""
+import json
+from typing import List, OrderedDict
+
+from rest_framework_json_api.renderers import JSONRenderer
+from rest_framework_json_api.utils import get_resource_type_from_serializer
+
+from atomic_operations.consts import (
+    ATOMIC_CONTENT_TYPE,
+    ATOMIC_MEDIA_TYPE,
+    ATOMIC_RESULTS,
+)
+
+
+class AtomicResultRenderer(JSONRenderer):
+    """
+    The `JSONRenderer` exposes a number of methods that you may override if you need highly
+    custom rendering control.
+
+    Render a JSON response per the JSON:API spec:
+
+    .. code-block:: json
+
+        {
+          "atomic:results": [{
+            "data": {
+              "links": {
+                "self": "http://example.com/blogPosts/13"
+              },
+              "type": "articles",
+              "id": "13",
+              "attributes": {
+                "title": "JSON API paints my bikeshed!"
+              }
+            }
+          }]
+        }
+    """
+
+    media_type = ATOMIC_CONTENT_TYPE
+    format = ATOMIC_MEDIA_TYPE
+
+    def check_error(self, operation_result_data, accepted_media_type, renderer_context):
+        # primitive check if any operation has errors while parsing
+        status = operation_result_data.get("status")
+        try:
+            status = int(status)
+            if status >= 400 and status <= 600:
+                return self.render_errors([operation_result_data], accepted_media_type, renderer_context)
+        except Exception:
+            pass
+
+    def render(self, data: List[OrderedDict], accepted_media_type=None, renderer_context=None):
+        renderer_context = renderer_context or {"view": {}}
+
+        atomic_results = []
+        for operation_result_data in data:
+            has_error = self.check_error(
+                operation_result_data, accepted_media_type, renderer_context)
+            if has_error:
+                return has_error
+
+            # pass in the resource name
+            renderer_context["view"].resource_name = get_resource_type_from_serializer(
+                operation_result_data.serializer)
+            rendered_primary_data = super().render(
+                operation_result_data, accepted_media_type, renderer_context)
+            atomic_results.append(rendered_primary_data.decode("UTF-8"))
+
+        atomic_results_str = f"[{','.join(atomic_results)}]"
+
+        rendered_content = '{"' + ATOMIC_RESULTS + '":' + atomic_results_str + '}'
+
+        return rendered_content.encode()
