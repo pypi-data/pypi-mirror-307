b15a92bb3807577b67786deafbc6fe9409b5f702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 15:04:33 +0000

    Add package echor-logger/0.1.3/echor_logger-0.1.3.tar.gz

---
+++ packages/echor-logger/echor_logger-0.1.3.tar.gz/echor_logger-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="echor-logger",
+    version="0.1.3",
+    packages=find_packages(),
+    description="A simple Python logging utility",
+    author="Pradeep",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
