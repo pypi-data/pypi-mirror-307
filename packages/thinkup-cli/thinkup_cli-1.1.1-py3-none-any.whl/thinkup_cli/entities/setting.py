fcf7af4757bf92648263703db4a2b92e33b5999f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:08:32 +0000

    Add package thinkup-cli/1.1.1/thinkup_cli-1.1.1-py3-none-any.whl

---
+++ packages/thinkup-cli/thinkup_cli-1.1.1-py3-none-any.whl/thinkup_cli/entities/setting.py
@@ -0,0 +1,38 @@
+import os
+import json
+
+from cli.utils.singleton import singleton
+
+DIR_NAME = '.thinkupcli'
+DEFAULT_FILE_NAME = 'thinkup-cli-config.json'
+DEFAULT_SETTING = {
+    "gemini_api_key": ""
+}
+
+
+@singleton
+class Settings:
+    directory = os.path.join(os.path.expanduser('~'), DIR_NAME)
+    file_path = os.path.join(directory, DEFAULT_FILE_NAME)
+    use_custom = False
+    config = None
+
+    def init(self):
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
+
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, "w") as config_file:
+                json.dump(DEFAULT_SETTING, config_file, indent=4)
+
+        self.load()
+
+    def load(self):
+        with open(self.file_path, "r") as config_file:
+            self.config = json.load(config_file)
+        return self.config
+
+    def save(self):
+        with open(self.file_path, "w") as config_file:
+            json.dump(self.config, config_file, indent=4)
+        return self.config
