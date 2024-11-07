1c9a967d550f9534fb842a29e2ef1b0b4fc96353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 14:03:40 +0000

    Add package testing-step-generator/0.1.2/testing_step_generator-0.1.2.tar.gz

---
+++ packages/testing-step-generator/testing_step_generator-0.1.2.tar.gz/testing_step_generator-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testing_step_generator',
+    version='0.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A CLI tool for initializing and generating output.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='kel89@cornell.edu',
+    url='https://github.com/kel89/testing-step-generator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'openai',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tsg=tsg.cli:main',
+        ],
+    },
+    python_requires='>=3.11',
+)
