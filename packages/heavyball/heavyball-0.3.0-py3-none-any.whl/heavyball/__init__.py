42170ed7c02a29ce0e3a6f91e264a4ba184afd49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:08:18 +0000

    Add package heavyball/0.3.0/heavyball-0.3.0-py3-none-any.whl

---
+++ packages/heavyball/heavyball-0.3.0-py3-none-any.whl/heavyball/__init__.py
@@ -0,0 +1,7 @@
+from .palm_foreach_sfadamw import PaLMForeachSFAdamW
+from .palm_foreach_soap import PaLMForeachSOAP
+from .schedule_free_palm_foreach_soap import SFPaLMForeachSOAP
+
+PalmForEachSoap = PaLMForeachSOAP
+
+__all__ = ['PalmForEachSoap', 'PaLMForeachSFAdamW', 'PaLMForeachSOAP', 'SFPaLMForeachSOAP']
