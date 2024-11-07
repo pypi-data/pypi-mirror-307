9d04d0d0b0efd0ca1e22d3b9a6d97ce8fd58dc84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:08:29 +0000

    Add package thinkup-cli/1.1.3/thinkup_cli-1.1.3.tar.gz

---
+++ packages/thinkup-cli/thinkup_cli-1.1.3.tar.gz/thinkup_cli-1.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="thinkup-cli",
+    version="1.1.3",
+    packages=find_packages(),
+    install_requires=[
+        "questionary",
+        "requests",
+        "prompt_toolkit",
+        "oauth2client"
+    ],
+    entry_points={
+        'console_scripts': [
+            'tkup.cli=thinkup_cli.main:main_function',
+        ],
+    },
+)
---
+++ packages/thinkup-cli/thinkup_cli-1.1.3.tar.gz/thinkup_cli-1.1.3/thinkup_cli/entities/app.py
@@ -0,0 +1,37 @@
+import os
+
+import questionary
+from thinkup_cli.entities.setting import Settings
+from thinkup_cli.utils.singleton import singleton
+
+
+@singleton
+class App:
+    version: str = "1.1.3"
+
+    def init(self):
+        os.system('printf "\033c"')
+        os.system('cls' if os.name == 'nt' else 'clear')
+        Settings().init()
+        self.init_install()
+
+    @staticmethod
+    def close():
+        os.system('printf "\033c"')
+        os.system('cls' if os.name == 'nt' else 'clear')
+        exit()
+    
+    def init_install(self):
+        gemini_api_key = Settings().config['gemini_api_key']
+        if not gemini_api_key or gemini_api_key == "":
+            try:
+                input = questionary.unsafe_prompt([
+                        {
+                            'type': 'input',
+                            'name': 'gemini_api_key',
+                            'message': f'Enter your Gemini API key or empty to skip (https://aistudio.google.com/app/apikey)',
+                        }])
+                Settings().config['gemini_api_key'] = input['gemini_api_key']
+                Settings().save()
+            except KeyboardInterrupt:
+                self.close()
