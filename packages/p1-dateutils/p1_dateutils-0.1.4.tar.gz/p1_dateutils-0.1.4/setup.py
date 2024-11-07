31c86e1d92ebaf191d0a1805163f0aa709ea89e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 14:04:01 +0000

    Add package p1-dateutils/0.1.4/p1_dateutils-0.1.4.tar.gz

---
+++ packages/p1-dateutils/p1_dateutils-0.1.4.tar.gz/p1_dateutils-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='p1-dateutils',
+    version='0.1.4',
+    description='Timezone Wrapper for Dekoruma Internal Use',
+    long_description='Timezone Wrapper for Dekoruma Internal Use',
+    url='',
+    author='Engineering',
+    author_email='engineering@dekoruma.com',
+    license='MIT',
+    packages=['dateutils'],
+    install_requires=[''],
+    zip_safe=False
+)
