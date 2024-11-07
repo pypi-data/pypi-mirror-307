855ee411cc21c81175681d415c3f3334eb704f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:08:52 +0000

    Add package python-kadmin-rs/0.1.0/python_kadmin_rs-0.1.0-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/python-kadmin-rs/python_kadmin_rs-0.1.0-cp312-cp312-manylinux_2_28_x86_64.whl/kadmin_local/exceptions/__init__.py
@@ -0,0 +1,19 @@
+from kadmin_local._lib import exceptions
+
+PyKAdminException = exceptions.PyKAdminException
+KAdminException = exceptions.KAdminException
+KerberosException = exceptions.KerberosException
+NullPointerDereference = exceptions.NullPointerDereference
+CStringConversion = exceptions.CStringConversion
+CStringImportFromVec = exceptions.CStringImportFromVec
+StringConversion = exceptions.StringConversion
+
+__all__ = (
+    "PyKAdminException",
+    "KAdminException",
+    "KerberosException",
+    "NullPointerDereference",
+    "CStringConversion",
+    "CStringImportFromVec",
+    "StringConversion",
+)
