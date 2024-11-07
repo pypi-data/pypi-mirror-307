22049cc98b5e1dcd22c4bfa880eaf548b635e519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 03:08:26 +0000

    Add package SigProfilerMatrixGenerator/1.2.31/SigProfilerMatrixGenerator-1.2.31.tar.gz

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.31.tar.gz/SigProfilerMatrixGenerator-1.2.31/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.31'
+version = '1.2.31'
+Update = 'v1.2.31: Resolve issue of ID and DBS matrices not being returned in the output matrices when chrom_based is set to True.'
+
+	
---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.31.tar.gz/SigProfilerMatrixGenerator-1.2.31/setup.py
@@ -0,0 +1,74 @@
+import os
+import shutil
+
+from setuptools import setup
+
+VERSION = "1.2.31"
+
+# remove the dist folder first if exists
+if os.path.exists("dist"):
+    shutil.rmtree("dist")
+
+
+def readme():
+    this_directory = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(this_directory, "README.md"), encoding="latin-1") as f:
+        long_description = f.read()
+        return long_description
+
+
+def write_version_py(filename="SigProfilerMatrixGenerator/version.py"):
+    # Copied from numpy setup.py
+    cnt = """
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+Update = 'v1.2.31: Resolve issue of ID and DBS matrices not being returned in the output matrices when chrom_based is set to True.'
+
+	"""
+    fh = open(filename, "w")
+    fh.write(
+        cnt
+        % {
+            "version": VERSION,
+        }
+    )
+    fh.close()
+
+
+write_version_py()
+
+setup(
+    name="SigProfilerMatrixGenerator",
+    version=VERSION,
+    description="SigProfiler matrix generator tool",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexandrovLab/SigProfilerMatrixGenerator.git",
+    author="Erik Bergstrom",
+    author_email="ebergstr@eng.ucsd.edu",
+    license="UCSD",
+    packages=["SigProfilerMatrixGenerator"],
+    python_requires=">=3.8",
+    install_requires=[
+        "matplotlib>=2.2.2",
+        "sigProfilerPlotting>=1.3.24",
+        "statsmodels>=0.9.0",
+        "numpy>=1.18.5,<2.0.0",
+        "pandas>=0.23.4,<2.0.0",
+        "scipy>=1.12.0; python_version>='3.9'",
+        "scipy>=1.1.0, <1.12.0; python_version=='3.8'",
+    ],
+    entry_points={
+        "console_scripts": [
+            "SigProfilerMatrixGenerator=SigProfilerMatrixGenerator.scripts.SigProfilerMatrixGenerator_CLI:main_function",
+        ],
+    },
+    extras_require={
+        "tests": [
+            "pytest",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
