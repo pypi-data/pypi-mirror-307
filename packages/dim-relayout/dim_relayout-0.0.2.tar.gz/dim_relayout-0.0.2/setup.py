b0e50d897ba72390fb598e5a47cf2eddd1037e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 15:04:27 +0000

    Add package dim-relayout/0.0.2/dim_relayout-0.0.2.tar.gz

---
+++ packages/dim-relayout/dim_relayout-0.0.2.tar.gz/dim_relayout-0.0.2/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r") as f:
+  long_description = f.read()
+
+setup(
+
+    name="dim_relayout",
+    version="0.0.2",
+    packages=find_packages(),
+    author="Yingke Huang",
+    author_email="ykhuang@ihep.ac.cn",
+    description="dim relayout",
+    install_requires=[],	# 依赖包会同时被安装
+    license='GPL',
+    #packages=find_packages())
+)
