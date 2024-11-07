a223ee89b7a32ba1ae1082c8078c11adbe0b4325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 15:04:37 +0000

    Add package Django-generate-serializers-views/0.1.1/django_generate_serializers_views-0.1.1.tar.gz

---
+++ packages/Django-generate-serializers-views/django_generate_serializers_views-0.1.1.tar.gz/django_generate_serializers_views-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Django_generate_serializers_views",
+    version="0.1.1",
+    description="This package helps in generate the views.py and serializers.py with the auto-generated code.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="ramesh patil",
+    author_email="rrp24999@gmail.com",
+    url="",
+    packages=find_packages(),
+    install_requires=[],  # add dependencies here
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
