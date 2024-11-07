f6aa8e9fc7c47e2291ffe83bf1290e38522399bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 04:03:29 +0000

    Add package odoo-addon-base-confirmation-requirement-rules/15.0.1.0.0.1/odoo_addon_base_confirmation_requirement_rules-15.0.1.0.0.1-py3-none-any.whl

---
+++ packages/odoo-addon-base-confirmation-requirement-rules/odoo_addon_base_confirmation_requirement_rules-15.0.1.0.0.1-py3-none-any.whl/odoo/addons/base_confirmation_requirement_rules/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Base Confirmation Requirement Rules",
+    "version": "15.0.1.0.0",
+    "summary": "Base module to set confirmation requirement rules.",
+    "website": "https://github.com/sygel-technology/sy-server-tools",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Base",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": ["data/confirmation_requirement_data.xml"],
+    "installable": True,
+    "auto_install": False,
+}
