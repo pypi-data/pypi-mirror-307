3081ad1d29e103c75166edd7479a9005601d809d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 04:03:20 +0000

    Add package odoo14-addon-avoid-exception-activity-notification/14.0.1.0.0/odoo14_addon_avoid_exception_activity_notification-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-avoid-exception-activity-notification/odoo14_addon_avoid_exception_activity_notification-14.0.1.0.0-py3-none-any.whl/odoo/addons/avoid_exception_activity_notification/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Avoid Exception Activity Notification",
+    "summary": "Avoid Exception Activity Notification",
+    "version": "14.0.1.0.0",
+    "category": "Mail",
+    "website": "https://www.sygel.es",
+    "author": "Sygel",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'mail',
+    ],
+}
