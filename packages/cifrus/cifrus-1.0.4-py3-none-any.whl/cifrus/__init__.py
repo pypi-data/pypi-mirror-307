9304f2c61ad474f4a703cf33f1b4682ef439d039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-07 14:04:14 +0000

    Add package cifrus/1.0.4/cifrus-1.0.4-py3-none-any.whl

---
+++ packages/cifrus/cifrus-1.0.4-py3-none-any.whl/cifrus/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 10:45:12 2024
+
+@author: Sakhawat
+"""
+
+from importlib.metadata import version
+__version__ = version("cifrus")
