97f190b8735a271b1b50eeeabce5ce81a1258b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:04:44 +0000

    Add package pypechain/0.0.47/pypechain-0.0.47.tar.gz

---
+++ packages/pypechain/pypechain-0.0.47.tar.gz/pypechain-0.0.47/pypechain/utilities/format.py
@@ -0,0 +1,130 @@
+"""Formatting utilities."""
+
+import keyword
+import subprocess
+from pathlib import Path
+
+import isort
+
+
+def avoid_python_keywords(name: str) -> str:
+    """Make sure the variable name is not a reserved Python word.  If it is, prepend with an
+    underscore.
+
+    Parameters
+    ----------
+    name : str
+       unsafe variable name.
+
+    Returns
+    -------
+    str
+        A string prepended with an underscore if it was a python reserved word.
+    """
+    if keyword.iskeyword(name) or keyword.issoftkeyword(name) or isbuiltin(name):
+        return "_" + name
+
+    return name
+
+
+builtin_function_names = [
+    "abs",
+    "aiter",
+    "all",
+    "any",
+    "ascii",
+    "bin",
+    "bool",
+    "breakpoint",
+    "bytearray",
+    "bytes",
+    "callable",
+    "chr",
+    "classmethod",
+    "compile",
+    "complex",
+    "delattr",
+    "dict",
+    "dir",
+    "divmod",
+    "enumerate",
+    "eval",
+    "exec",
+    "filter",
+    "float",
+    "format",
+    "frozenset",
+    "getattr",
+    "globals",
+    "hasattr",
+    "hash",
+    "help",
+    "hex",
+    "id",
+    "input",
+    "int",
+    "isinstance",
+    "issubclass",
+    "iter",
+    "len",
+    "list",
+    "locals",
+    "map",
+    "max",
+    "min",
+    "next",
+    "object",
+    "oct",
+    "open",
+    "ord",
+    "pow",
+    "print",
+    "property",
+    "repr",
+    "reversed",
+    "round",
+    "set",
+    "setattr",
+    "slice",
+    "sorted",
+    "staticmethod",
+    "str",
+    "sum",
+    "super",
+    "tuple",
+    "type",
+    "vars",
+    "zip",
+    "import",
+]
+
+isbuiltin = frozenset(builtin_function_names).__contains__
+
+
+def capitalize_first_letter_only(string: str) -> str:
+    """Capitalizes the first letter of a string without affecting the rest of the string.
+    capitalize() will lowercase the rest of the letters."""
+
+    if len(string) < 2:
+        return string
+    return string[0].upper() + string[1:]
+
+
+def format_file(file_path: Path, line_length: int = 120, quiet=True, remove_unused_imports=True) -> None:
+    """Formats a file with isort and black.
+
+    Parameters
+    ----------
+    file_path : Path
+        The file to be formatted.
+    line_length : int, optional
+        Black's line-length config option.
+    """
+
+    quiet_flag = "--quiet" if quiet else ""
+    if remove_unused_imports:
+        subprocess.run(
+            f"autoflake --in-place {quiet_flag} --remove-all-unused-imports {file_path}", shell=True, check=True
+        )
+    isort.file(file_path, config=isort.Config(quiet=quiet))
+    subprocess.run(f"black {quiet_flag} --line-length={line_length} {file_path}", shell=True, check=True)
---
+++ packages/pypechain/pypechain-0.0.47.tar.gz/pypechain-0.0.47/snapshots/expected_overloading.py
@@ -0,0 +1,181 @@
+
+
+
+
+
+class OverloadedBalanceOfContractFunction0(PypechainContractFunction):
+    """ContractFunction for the balanceOf() method."""
+
+    _function_name = "balanceOf"
+    _type_signature = expand_struct_type_str(
+        tuple([]),
+        structs
+    )
+    _error_class = OverloadedContractErrors
+
+    
+    
+    
+
+    
+
+    def call(
+        self,
+        transaction: TxParams | None = None,
+        block_identifier: BlockIdentifier | None = None,
+        state_override: StateOverride | None = None,
+        ccip_read_enabled: bool | None = None,
+    ) -> int:
+        """returns int."""
+        # We handle the block identifier = None case here for typing.
+        if block_identifier is None:
+            block_identifier = self.w3.eth.default_block
+
+        
+        # Define the expected return types from the smart contract call
+        return_types = int
+        
+
+        # Call the function
+        raw_values = self._call(transaction, block_identifier, state_override, ccip_read_enabled)
+
+        
+        return cast(int, rename_returned_types(structs, return_types, raw_values))
+        
+
+
+
+class OverloadedBalanceOfContractFunction1(PypechainContractFunction):
+    """ContractFunction for the balanceOf(str) method."""
+
+    _function_name = "balanceOf"
+    _type_signature = expand_struct_type_str(
+        tuple(['str']),
+        structs
+    )
+    _error_class = OverloadedContractErrors
+
+    
+    
+    
+
+    
+
+    def call(
+        self,
+        transaction: TxParams | None = None,
+        block_identifier: BlockIdentifier | None = None,
+        state_override: StateOverride | None = None,
+        ccip_read_enabled: bool | None = None,
+    ) -> int:
+        """returns int."""
+        # We handle the block identifier = None case here for typing.
+        if block_identifier is None:
+            block_identifier = self.w3.eth.default_block
+
+        
+        # Define the expected return types from the smart contract call
+        return_types = int
+        
+
+        # Call the function
+        raw_values = self._call(transaction, block_identifier, state_override, ccip_read_enabled)
+
+        
+        return cast(int, rename_returned_types(structs, return_types, raw_values))
+        
+
+
+class OverloadedBalanceOfContractFunction(PypechainContractFunction):
+    """ContractFunction for the balanceOf method."""
+    # super() call methods are generic, while our version adds values & types
+    # pylint: disable=arguments-differ# disable this warning when there is overloading
+    # pylint: disable=function-redefined
+
+    _function_name = "balanceOf"
+
+
+    # Make lookup for function signature -> overloaded function
+    # The function signatures are python types, as we need to do a
+    # lookup of arguments passed in to contract function
+    _functions: dict[str, PypechainContractFunction]
+
+
+
+    @overload
+    def __call__(self) -> OverloadedBalanceOfContractFunction0:  # type: ignore
+        ...
+
+    @overload
+    def __call__(self, who: str) -> OverloadedBalanceOfContractFunction1:  # type: ignore
+        ...
+
+
+    def __call__(self, *args, **kwargs) -> OverloadedBalanceOfContractFunction:  # type: ignore
+        clone = super().__call__(*(dataclass_to_tuple(arg) for arg in args), **{key: dataclass_to_tuple(arg) for key, arg in kwargs.items()})
+
+        # Arguments is the flattened set of arguments from args and kwargs, ordered by the abi
+        # We get the python types of the args passed in, but remapped from tuples -> dataclasses
+        arg_types = get_arg_type_names(clone.arguments)
+
+        # Look up the function class based on arg types.
+        # We ensure we use a copy of the original object.
+        function_obj = copy.copy(self._functions[arg_types])
+
+        function_obj.args = clone.args
+        function_obj.kwargs = clone.kwargs
+
+        # The `@overload` of `__call__` takes care of setting the type of this object correctly
+        return function_obj # type: ignore
+    
+    @classmethod
+    def factory(
+        cls, class_name: str, **kwargs: Any
+    ) -> Self:
+        out = super().factory(class_name, **kwargs)
+
+        # We initialize our overridden functions here.
+        # Note that we use the initialized object to ensure each function
+        # is attached to the instanciated object 
+        # (attached to a specific web3 and contract address)
+        out._functions = {
+        
+            OverloadedBalanceOfContractFunction0._type_signature: OverloadedBalanceOfContractFunction0.factory(
+                "OverloadedBalanceOfContractFunction0",
+                **kwargs
+            ),
+        
+            OverloadedBalanceOfContractFunction1._type_signature: OverloadedBalanceOfContractFunction1.factory(
+                "OverloadedBalanceOfContractFunction1",
+                **kwargs
+            ),
+        
+        }
+        return out
+
+
+
+
+
+class OverloadedContractFunctions(ContractFunctions):
+    """ContractFunctions for the Overloaded contract."""
+
+    balanceOf: OverloadedBalanceOfContractFunction
+
+    def __init__(
+         self,
+         abi: ABI,
+         w3: "Web3",
+         address: ChecksumAddress | None = None,
+         decode_tuples: bool | None = False,
+     ) -> None:
+         super().__init__(abi, w3, address, decode_tuples)
+         self.balanceOf = OverloadedBalanceOfContractFunction.factory(
+             "balanceOf",
+             w3=w3,
+             contract_abi=abi,
+             address=address,
+             decode_tuples=decode_tuples,
+             abi_element_identifier="balanceOf",
+         )
+         
