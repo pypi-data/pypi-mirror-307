69029b73f974aa30dc2b6159b02798d35ade3d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 03:07:01 +0000

    Add package yta-multimedia/0.0.165/yta_multimedia-0.0.165-py3-none-any.whl

---
+++ packages/yta-multimedia/yta_multimedia-0.0.165-py3-none-any.whl/yta_multimedia/video/edition/effect/zoom/linear_zoom_video_effect.py
@@ -0,0 +1,44 @@
+from yta_multimedia.video.edition.effect.video_effect import VideoEffect
+from yta_multimedia.video.edition.effect.utils.resize_t_functions import linear_zoom_transition_t_func
+from moviepy.editor import CompositeVideoClip, VideoFileClip, ImageClip, VideoClip, ColorClip
+from typing import Union
+
+
+class LinearZoomVideoEffect(VideoEffect):
+    """
+    Creates a linear Zoom effect in the provided video.
+    """
+
+    @classmethod
+    def apply(cls, video: Union[str, VideoFileClip, VideoClip, CompositeVideoClip, ImageClip, ColorClip], zoom_start: float, zoom_end: float):
+        """
+        Apply the effect on the provided 'video'.
+
+        :param float zoom_start: The zoom at the start of the video, where a 1 is no zoom, a 0.8 is a zoom out of 20% and 1.1 is a zoom in of a 10%.
+
+        :param float zoom_end: The zoom at the end of the video, where a 1 is no zoom, a 0.8 is a zoom out of 20% and 1.1 is a zoom in of a 10%.
+        """
+        video = VideoEffect.parse_moviepy_video(video)
+        # We reset the mask to avoid problems with zoom
+        video = video.add_mask()
+
+        if not zoom_start or not zoom_end:
+            raise Exception('No "zoom_start" or "zoom_end" provided.')
+
+        fps = video.fps
+        duration = video.duration
+        screensize = video.size
+
+        effected_video = (
+            video
+            .resize(screensize)
+            .resize(lambda t: linear_zoom_transition_t_func(t, duration, zoom_start, zoom_end))
+            .set_position(('center', 'center'))
+            .set_duration(duration)
+            .set_fps(fps)
+        )
+
+        return CompositeVideoClip([effected_video], size = screensize)
+
+
+    
