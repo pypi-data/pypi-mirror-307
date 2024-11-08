ddbcd159d80b4a2128bd80f64970dc370a9cae79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:04:47 +0000

    Add package hippo-db/0.3.30/hippo_db-0.3.30.tar.gz

---
+++ packages/hippo-db/hippo_db-0.3.30.tar.gz/hippo_db-0.3.30/hippo/feature.py
@@ -0,0 +1,54 @@
+import mcol
+
+from dataclasses import dataclass
+
+from .target import Target
+
+
+@dataclass
+class Feature:
+    """Pharmocophoric feature in a protein
+
+    attributes:
+            id: Database ID
+            family: Feature family
+            chain_name: Protein chain name/letter
+            residue_name: Protein residue name
+            residue_number: Protein residue number
+            atom_names: Protein atom names (whitespace-delimited)
+
+    """
+
+    id: int
+    family: Target
+    target: int
+    chain_name: str
+    residue_name: str
+    residue_number: int
+    atom_names: str
+
+    def __str__(self) -> str:
+        """Unformatted string representation"""
+        return f"F{self.id}"
+
+    def __repr__(self) -> str:
+        """ANSI Formatted string representation"""
+        return f"{mcol.bold}{mcol.underline}{self.family} {self.chain_name} {self.residue_name} {self.residue_number} [{self.atom_names}]{mcol.unbold}{mcol.ununderline}"
+
+    def __rich__(self) -> str:
+        """Representation for mrich"""
+        return f"[bold underline]{self.family} {self.chain_name} {self.residue_name} {self.residue_number} [{self.atom_names}]"
+
+    @property
+    def chain_res_name_number_str(self) -> str:
+        """Return a string representation of the feature"""
+        return f"{self.chain_name} {self.residue_name} {self.residue_number}"
+
+    @property
+    def res_name_number_str(self) -> str:
+        """Return a string representation of the feature"""
+        return f"{self.residue_name} {self.residue_number}"
+
+    @property
+    def res_name_number_family_str(self) -> str:
+        return f"{self.residue_name} {self.residue_number} {self.family}"
