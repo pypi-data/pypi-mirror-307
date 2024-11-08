d763d80bf048f5cb5363d0ce7d3bf4899dea9fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 03:07:10 +0000

    Add package naeural-core/7.0.1/naeural_core-7.0.1-py3-none-any.whl

---
+++ packages/naeural-core/naeural_core-7.0.1-py3-none-any.whl/naeural_core/utils/mixins/code_executor.py
@@ -0,0 +1,10 @@
+from PyE2 import BaseCodeChecker
+
+class _CodeExecutorMixin(BaseCodeChecker):
+  """
+  IMPORTANT: this class must be a mixin 
+  """
+  def __init__(self):
+    super(_CodeExecutorMixin, self).__init__()
+    return
+  
