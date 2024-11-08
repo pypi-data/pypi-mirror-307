560c0575cc5d15d4a061fe53989fd50965473404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 07:04:59 +0000

    Add package bugsink/0.1.16/bugsink-0.1.16.tar.gz

---
+++ packages/bugsink/bugsink-0.1.16.tar.gz/bugsink-0.1.16/bugsink/context_processors.py
@@ -0,0 +1,15 @@
+from bugsink.app_settings import get_settings, CB_ANYBODY
+
+
+def useful_settings_processor(request):
+    return {
+        'site_title': get_settings().SITE_TITLE,
+        'registration_enabled': get_settings().USER_REGISTRATION == CB_ANYBODY,
+        'app_settings': get_settings(),
+    }
+
+
+def logged_in_user_processor(request):
+    return {
+        'logged_in_user': request.user,
+    }
