cae2af4a0ed5f1268de57863761b7e420e47ac63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:03:53 +0000

    Add package scicat-sdk-py/4.6.6/scicat_sdk_py-4.6.6-py3-none-any.whl

---
+++ packages/scicat-sdk-py/scicat_sdk_py-4.6.6-py3-none-any.whl/scicat-sdk-py/api/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import apis into api package
+from scicat-sdk-py.api.admin_api import AdminApi
+from scicat-sdk-py.api.auth_api import AuthApi
+from scicat-sdk-py.api.datasets_api import DatasetsApi
+from scicat-sdk-py.api.default_api import DefaultApi
+from scicat-sdk-py.api.elastic_search_api import ElasticSearchApi
+from scicat-sdk-py.api.instruments_api import InstrumentsApi
+from scicat-sdk-py.api.jobs_api import JobsApi
+from scicat-sdk-py.api.logbooks_api import LogbooksApi
+from scicat-sdk-py.api.origdatablocks_api import OrigdatablocksApi
+from scicat-sdk-py.api.policies_api import PoliciesApi
+from scicat-sdk-py.api.proposals_api import ProposalsApi
+from scicat-sdk-py.api.published_data_api import PublishedDataApi
+from scicat-sdk-py.api.samples_api import SamplesApi
+from scicat-sdk-py.api.user_identities_api import UserIdentitiesApi
+from scicat-sdk-py.api.users_api import UsersApi
+
