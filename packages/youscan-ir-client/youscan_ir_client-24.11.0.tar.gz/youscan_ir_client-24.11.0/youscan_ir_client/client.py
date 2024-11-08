b5fe571767f3d6b0df4e44e8ce2ef221913f75ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:04:08 +0000

    Add package youscan-ir-client/24.11.0/youscan_ir_client-24.11.0.tar.gz

---
+++ packages/youscan-ir-client/youscan_ir_client-24.11.0.tar.gz/youscan_ir_client-24.11.0/youscan_ir_client/client.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+from typing import Any, AsyncIterator
+from types import TracebackType
+from contextlib import asynccontextmanager
+from logging import getLogger
+import uuid
+
+import asyncio
+import aiohttp
+from yarl import URL
+
+from .config import YouScanHeaderNames, YouScanAPIAddr
+from .factories import PayloadFactory, EntityFactory
+from .entities import ImageDetectReqParams, ImageDetectResponse
+
+
+LOGGER = getLogger(__name__)
+
+
+class YouScanIRClient:
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        base_url: URL | str | None = None,
+        timeout: aiohttp.ClientTimeout = aiohttp.client.DEFAULT_TIMEOUT,
+    ) -> None:
+        assert client_id, "Client ID was not provided"
+        assert client_secret, "Client secret key was not provided"
+        self._base_url = URL(base_url) if base_url else URL(YouScanAPIAddr.base_url)
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._timeout = timeout
+        self._client: aiohttp.ClientSession | None = None
+        self._payload_factory = PayloadFactory()
+        self._entity_factory = EntityFactory()
+
+    @asynccontextmanager
+    async def _request(
+        self,
+        method: str,
+        path: str,
+        **kwargs: Any,
+    ) -> AsyncIterator[aiohttp.ClientResponse]:
+        assert self._client
+        assert self._base_url
+        url = self._base_url / path
+        async with self._client.request(method, url, **kwargs) as resp:
+            resp.raise_for_status()
+            yield resp
+
+    def _create_headers(self) -> dict[str, str]:
+        headers = {
+            YouScanHeaderNames.client_id: self._client_id,
+            YouScanHeaderNames.client_secret: self._client_secret,
+        }
+        return headers
+
+    async def analyse(
+        self,
+        params: ImageDetectReqParams,
+        retries: int = 3,
+    ) -> ImageDetectResponse:
+        path = YouScanAPIAddr.img_detect_endpoint
+        req_payload = self._payload_factory.create_image_detect(params)
+        assert retries >= 1
+        for i in range(1, retries + 1):
+            try:
+                uid = uuid.uuid1()
+                LOGGER.debug(f"POST >>> {path} ({uid.hex}):\n{req_payload}")
+
+                async with self._request("POST", path, json=req_payload) as response:
+                    resp_payload = await response.json()
+                    LOGGER.debug(f"POST <<< {path} ({uid.hex}):\n{resp_payload}")
+
+                    resp_entity = self._entity_factory.create_detect_response(
+                        resp_payload
+                    )
+                    return resp_entity
+
+            except Exception as e:
+                if isinstance(e, aiohttp.ClientError):
+                    LOGGER.warning(f"Analyse request failed for '{params}'")
+                else:
+                    LOGGER.error(f"Error while parsing response for '{params}'")
+                    LOGGER.warning(resp_payload)
+                if i >= retries:
+                    raise
+                sleep = 2**i
+                LOGGER.info(f"{i}/{retries} retry in {sleep} sec...")
+                await asyncio.sleep(sleep)
+
+        raise RuntimeError("This should not happen")
+
+    async def __aenter__(self) -> YouScanIRClient:
+        self._client = await self._create_http_client()
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        await self.aclose()
+
+    async def aclose(self) -> None:
+        assert self._client
+        await self._client.close()
+
+    async def _create_http_client(self) -> aiohttp.ClientSession:
+        client = aiohttp.ClientSession(
+            headers=self._create_headers(),
+            timeout=self._timeout,
+        )
+        return await client.__aenter__()
---
+++ packages/youscan-ir-client/youscan_ir_client-24.11.0.tar.gz/youscan_ir_client-24.11.0/youscan_ir_client/entities.py
@@ -0,0 +1,95 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Sequence
+import enum
+
+
+class AnalysisAttributes(str, enum.Enum):
+    LOGOS = "logos"
+    OBJECTS = "objects"
+    SCENES = "scenes"
+    PEOPLE = "people"
+    ACTIVITIES = "activities"
+    TYPE = "type"
+    SUBTYPE = "subtype"
+    CONTENT_SENSITIVITY = "content_sensitivity"
+    TEXTS = "texts"
+    EMBEDDING = "embedding"
+    COLORS = "colors"
+
+
+@dataclass(frozen=True)
+class Image:
+    url: str = ""
+    b64_content: str = field(repr=False, default="")
+
+    def __post_init__(self) -> None:
+        if not self.url and not self.b64_content:
+            raise ValueError("Image URL or its base64-encoded content required")
+
+
+@dataclass(frozen=True)
+class ImageDetectReqParams:
+    images: Sequence[Image]
+    optimize_throughput: bool = False
+    analyse_attributes: Sequence[AnalysisAttributes] = field(default_factory=tuple)
+
+
+@dataclass(frozen=True)
+class FoundAttribute:
+    label: str
+    confidence: float
+
+
+@dataclass(frozen=True)
+class Point:  # X, Y coords
+    x: int
+    y: int
+
+
+@dataclass(frozen=True)
+class FoundText(FoundAttribute):
+    # bounding box coordinates with text found on image
+    topleft: Point
+    bottomright: Point
+
+
+@dataclass(frozen=True)
+class FoundColor:
+    color: str
+    shade: str
+    percentage: float
+
+
+@dataclass(frozen=True)
+class ImageAnalysisFailedResult:
+    status: str
+    error_text: str
+
+
+@dataclass(frozen=True)
+class ImageAnalysisResult:
+    version: str | None
+    cached: bool | None
+    cached_attributes: Sequence[AnalysisAttributes] | None
+    hash: str | None
+    elapsed: float | None
+    cache_origin: str | None = None
+    # Those are returned if ImageDetectReqParams.analyse_attributes contains them
+    logos: Sequence[FoundAttribute] = field(default_factory=list)
+    objects: Sequence[FoundAttribute] = field(default_factory=list)
+    scenes: Sequence[FoundAttribute] = field(default_factory=list)
+    people: Sequence[FoundAttribute] = field(default_factory=list)
+    activities: Sequence[FoundAttribute] = field(default_factory=list)
+    type: str | None = None
+    subtype: str | None = None
+    content_sensitivity: Sequence[FoundAttribute] = field(default_factory=list)
+    texts: Sequence[FoundText] = field(default_factory=list)
+    embedding: Sequence[float] = field(default_factory=list)
+    colors: Sequence[FoundColor] = field(default_factory=list)
+
+
+@dataclass(frozen=True)
+class ImageDetectResponse:
+    results: Sequence[ImageAnalysisResult | ImageAnalysisFailedResult]
