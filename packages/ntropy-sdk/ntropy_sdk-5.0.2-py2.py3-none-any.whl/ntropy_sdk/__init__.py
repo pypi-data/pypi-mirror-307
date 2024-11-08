bc4e63e8b51fa42cd5f50de4fa9e47b65c6aa7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:04:24 +0000

    Add package ntropy-sdk/5.0.2/ntropy_sdk-5.0.2-py2.py3-none-any.whl

---
+++ packages/ntropy-sdk/ntropy_sdk-5.0.2-py2.py3-none-any.whl/ntropy_sdk/__init__.py
@@ -0,0 +1,57 @@
+__version__ = "5.0.2"
+
+from typing import TYPE_CHECKING, Optional
+import requests
+
+
+if TYPE_CHECKING:
+    from typing_extensions import TypedDict
+
+    class ExtraKwargs(TypedDict, total=False):
+        request_id: Optional[str]
+        api_key: Optional[str]
+        session: Optional[requests.Session]
+        retries: int
+        timeout: int
+        retry_on_unhandled_exception: bool
+        extra_headers: Optional[dict]
+
+
+from .sdk import SDK
+from .v2.errors import (
+    NtropyError,
+    NtropyBatchError,
+    NtropyDatasourceError,
+    NtropyTimeoutError,
+    NtropyHTTPError,
+    NtropyValidationError,
+    NtropyQuotaExceededError,
+    NtropyNotSupportedError,
+    NtropyResourceOccupiedError,
+    NtropyServerConnectionError,
+    NtropyRateLimitError,
+    NtropyNotFoundError,
+    NtropyNotAuthorizedError,
+    NtropyValueError,
+    NtropyRuntimeError,
+)
+
+
+__all__ = (
+    "SDK",
+    "NtropyError",
+    "NtropyBatchError",
+    "NtropyDatasourceError",
+    "NtropyTimeoutError",
+    "NtropyHTTPError",
+    "NtropyValidationError",
+    "NtropyQuotaExceededError",
+    "NtropyNotSupportedError",
+    "NtropyResourceOccupiedError",
+    "NtropyServerConnectionError",
+    "NtropyRateLimitError",
+    "NtropyNotFoundError",
+    "NtropyNotAuthorizedError",
+    "NtropyValueError",
+    "NtropyRuntimeError",
+)
