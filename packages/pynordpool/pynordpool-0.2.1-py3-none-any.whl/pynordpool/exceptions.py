91848f71d75c5447c6c751875f39e13e9aa61db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 07:04:50 +0000

    Add package pynordpool/0.2.1/pynordpool-0.2.1-py3-none-any.whl

---
+++ packages/pynordpool/pynordpool-0.2.1-py3-none-any.whl/pynordpool/exceptions.py
@@ -0,0 +1,23 @@
+"""Exceptions for Nord Pool."""
+
+from typing import Any
+
+
+class NordPoolError(Exception):
+    """Error from Nord Pool api."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
+
+
+class NordPoolConnectionError(NordPoolError):
+    """Connection error from Nord Pool api."""
+
+
+class NordPoolResponseError(NordPoolError):
+    """Response error from Nord Pool api."""
+
+
+class NordPoolAuthenticationError(NordPoolError):
+    """Response error from Nord Pool api."""
