9e6b79d8648895e097aff7531e7eaabf5b5e749e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 14:05:23 +0000

    Add package visvalingamwyatt/0.3.0/visvalingamwyatt-0.3.0.tar.gz

---
+++ packages/visvalingamwyatt/visvalingamwyatt-0.3.0.tar.gz/visvalingamwyatt-0.3.0/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of visvalingamwyatt.
+# https://github.com/fitnr/visvalingamwyatt
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2015, fitnr <contact@fakeisthenewreal.org>
+
+from setuptools import setup
+
+setup()
