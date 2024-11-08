38aed3652489161ab5b3598e87b9776788e651d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 07:04:29 +0000

    Add package p1-dateutils/0.1.7/p1_dateutils-0.1.7.tar.gz

---
+++ packages/p1-dateutils/p1_dateutils-0.1.7.tar.gz/p1_dateutils-0.1.7/README.md
@@ -0,0 +1,9 @@
+# Python: Date Utils
+
+Python datetime wrapper to always return datetime with Asia/Jakarta timezone
+
+## Setup
+
+```sh
+$ pipenv install p1-dateutils
+```
---
+++ packages/p1-dateutils/p1_dateutils-0.1.7.tar.gz/p1_dateutils-0.1.7/dateutils/types.py
@@ -0,0 +1,7 @@
+from datetime import date  # noqa
+from datetime import datetime  # noqa
+from datetime import time  # noqa
+from datetime import timedelta  # noqa
+from datetime import tzinfo  # noqa
+
+from dateutil import relativedelta  # noqa
