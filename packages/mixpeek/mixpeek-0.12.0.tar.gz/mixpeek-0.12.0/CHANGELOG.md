06e99666c709fe77dbdda98cfce67ab75dc8d495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:03:56 +0000

    Add package mixpeek/0.12.0/mixpeek-0.12.0.tar.gz

---
+++ packages/mixpeek/mixpeek-0.12.0.tar.gz/mixpeek-0.12.0/CHANGELOG.md
@@ -0,0 +1,72 @@
+# Changelog
+
+## 0.12.0 (2024-11-07)
+
+Full Changelog: [v0.11.0...v0.12.0](https://github.com/mixpeek/python-client/compare/v0.11.0...v0.12.0)
+
+### Features
+
+* **api:** api update ([#20](https://github.com/mixpeek/python-client/issues/20)) ([db2e6da](https://github.com/mixpeek/python-client/commit/db2e6daf9557d9abf4b9a277406de6a8faf389b1))
+* **api:** api update ([#22](https://github.com/mixpeek/python-client/issues/22)) ([0a1e8b6](https://github.com/mixpeek/python-client/commit/0a1e8b687755a374928842f750f09a8d6be8214c))
+* **api:** api update ([#23](https://github.com/mixpeek/python-client/issues/23)) ([4136eed](https://github.com/mixpeek/python-client/commit/4136eed5afd041e521cfd4c4de8670fd65847ab9))
+* **api:** api update ([#24](https://github.com/mixpeek/python-client/issues/24)) ([ccd3506](https://github.com/mixpeek/python-client/commit/ccd3506617ed1acebb8f67bd7000ce3a1ab37815))
+* **api:** api update ([#25](https://github.com/mixpeek/python-client/issues/25)) ([a49b288](https://github.com/mixpeek/python-client/commit/a49b2889fcb0fc49972d1a1debe23b1380831b9d))
+* **api:** api update ([#26](https://github.com/mixpeek/python-client/issues/26)) ([333a7be](https://github.com/mixpeek/python-client/commit/333a7befe193832c411b6fce0db2902f3f2f3d27))
+* **api:** api update ([#27](https://github.com/mixpeek/python-client/issues/27)) ([0dd1ce5](https://github.com/mixpeek/python-client/commit/0dd1ce5ece27d0462256d285a9e4e37d410a73fb))
+* **api:** api update ([#28](https://github.com/mixpeek/python-client/issues/28)) ([b8e47eb](https://github.com/mixpeek/python-client/commit/b8e47ebbc34842590adf1f2c7d250fb7ae069c81))
+* **api:** api update ([#29](https://github.com/mixpeek/python-client/issues/29)) ([a3280d7](https://github.com/mixpeek/python-client/commit/a3280d79f4db6fe4d65107c163cfe514c3a55b83))
+* **api:** api update ([#30](https://github.com/mixpeek/python-client/issues/30)) ([a5b00d9](https://github.com/mixpeek/python-client/commit/a5b00d983236b099ce122ff8ab362cc445c3c84c))
+* **api:** api update ([#31](https://github.com/mixpeek/python-client/issues/31)) ([d3460c8](https://github.com/mixpeek/python-client/commit/d3460c83602410b76d076a23597ca048813bdddf))
+* **api:** api update ([#32](https://github.com/mixpeek/python-client/issues/32)) ([2bd1b48](https://github.com/mixpeek/python-client/commit/2bd1b484697aaf309c90e140d63db61fa9f1964f))
+* **api:** api update ([#33](https://github.com/mixpeek/python-client/issues/33)) ([9c887ca](https://github.com/mixpeek/python-client/commit/9c887ca01189997aa6785dc474f78d05efb9139d))
+* **api:** api update ([#34](https://github.com/mixpeek/python-client/issues/34)) ([dfbb1d5](https://github.com/mixpeek/python-client/commit/dfbb1d518c3c50bb91d2609261d81ce4cacb3e02))
+* **api:** api update ([#35](https://github.com/mixpeek/python-client/issues/35)) ([0b619d1](https://github.com/mixpeek/python-client/commit/0b619d1610a16427cf1470b7eea1675b772a2680))
+* **api:** api update ([#36](https://github.com/mixpeek/python-client/issues/36)) ([1093571](https://github.com/mixpeek/python-client/commit/1093571f6b4b9f641b9472cb66bfefba0ef0ce5e))
+* **api:** api update ([#37](https://github.com/mixpeek/python-client/issues/37)) ([7e2edde](https://github.com/mixpeek/python-client/commit/7e2edde6f6a06ef6f1c18116c706ec73ce24fac2))
+* **api:** api update ([#38](https://github.com/mixpeek/python-client/issues/38)) ([dfd6c76](https://github.com/mixpeek/python-client/commit/dfd6c76694c9b64ebd058d1b134aee56c727b3cf))
+* **api:** api update ([#39](https://github.com/mixpeek/python-client/issues/39)) ([3b31836](https://github.com/mixpeek/python-client/commit/3b31836c4947d76233de3d21b6d787b74af733fb))
+* **api:** api update ([#40](https://github.com/mixpeek/python-client/issues/40)) ([f73be23](https://github.com/mixpeek/python-client/commit/f73be238151f6449185c5fd501a7476c76b43564))
+* **api:** api update ([#42](https://github.com/mixpeek/python-client/issues/42)) ([06b52f1](https://github.com/mixpeek/python-client/commit/06b52f1bf1a2f2e641c15e3483758b84462ac4bb))
+* **api:** api update ([#43](https://github.com/mixpeek/python-client/issues/43)) ([22b9754](https://github.com/mixpeek/python-client/commit/22b9754904399f180aad28ef9445325a339464ab))
+* **api:** update via SDK Studio ([#41](https://github.com/mixpeek/python-client/issues/41)) ([e4f8efe](https://github.com/mixpeek/python-client/commit/e4f8efe736f339dc747f0d2a4f00095fa4af8021))
+* **api:** update via SDK Studio ([#44](https://github.com/mixpeek/python-client/issues/44)) ([0aa9e75](https://github.com/mixpeek/python-client/commit/0aa9e75be8c10c24c6a36fa90cd4bd0610a28bb2))
+
+
+### Bug Fixes
+
+* remove outdated custom code ([5a954c3](https://github.com/mixpeek/python-client/commit/5a954c3ba9a539e76dd847dda4d705aaf58faf24))
+* remove outdated examples ([c834bf7](https://github.com/mixpeek/python-client/commit/c834bf7aef2d0feaa123add9abe11c90ace2074b))
+
+## 0.11.0 (2024-10-08)
+
+Full Changelog: [v0.10.0...v0.11.0](https://github.com/mixpeek/python-client/compare/v0.10.0...v0.11.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#17](https://github.com/mixpeek/python-client/issues/17)) ([2b56978](https://github.com/mixpeek/python-client/commit/2b569781f873fb5756939bfbdecb1bed118afc6d))
+
+## 0.10.0 (2024-10-08)
+
+Full Changelog: [v0.9.0...v0.10.0](https://github.com/mixpeek/python-client/compare/v0.9.0...v0.10.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#13](https://github.com/mixpeek/python-client/issues/13)) ([c9b8748](https://github.com/mixpeek/python-client/commit/c9b87481c88b33e2667591a03aa73948720abd56))
+
+## 0.9.0 (2024-10-07)
+
+Full Changelog: [v0.8.3...v0.9.0](https://github.com/mixpeek/python-client/compare/v0.8.3...v0.9.0)
+
+### Features
+
+* **api:** update via SDK Studio ([#10](https://github.com/mixpeek/python-client/issues/10)) ([268489b](https://github.com/mixpeek/python-client/commit/268489b0f36b472d695bc368bf37225726b31c4d))
+
+## 0.8.3 (2024-10-07)
+
+Full Changelog: [v0.0.1-alpha.0...v0.8.3](https://github.com/mixpeek/python-client/compare/v0.0.1-alpha.0...v0.8.3)
+
+### Chores
+
+* add repr to PageInfo class ([#8](https://github.com/mixpeek/python-client/issues/8)) ([04d2ab3](https://github.com/mixpeek/python-client/commit/04d2ab37813edc8445f3597feeb95bb3e76b6fda))
+* go live ([#6](https://github.com/mixpeek/python-client/issues/6)) ([d3ff4b5](https://github.com/mixpeek/python-client/commit/d3ff4b53231cc878afce6fa350c1650feb68c0e7))
+* update SDK settings ([1f99fa6](https://github.com/mixpeek/python-client/commit/1f99fa6a533584eca8b7e5639c3e7106e1f4042a))
---
+++ packages/mixpeek/mixpeek-0.12.0.tar.gz/mixpeek-0.12.0/src/mixpeek/_client.py
@@ -0,0 +1,444 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+import os
+from typing import Any, Union, Mapping
+from typing_extensions import Self, override
+
+import httpx
+
+from . import resources, _exceptions
+from ._qs import Querystring
+from ._types import (
+    NOT_GIVEN,
+    Omit,
+    Timeout,
+    NotGiven,
+    Transport,
+    ProxiesTypes,
+    RequestOptions,
+)
+from ._utils import (
+    is_given,
+    get_async_library,
+)
+from ._version import __version__
+from ._streaming import Stream as Stream, AsyncStream as AsyncStream
+from ._exceptions import APIStatusError
+from ._base_client import (
+    DEFAULT_MAX_RETRIES,
+    SyncAPIClient,
+    AsyncAPIClient,
+)
+
+__all__ = [
+    "Timeout",
+    "Transport",
+    "ProxiesTypes",
+    "RequestOptions",
+    "resources",
+    "Mixpeek",
+    "AsyncMixpeek",
+    "Client",
+    "AsyncClient",
+]
+
+
+class Mixpeek(SyncAPIClient):
+    accounts: resources.AccountsResource
+    features: resources.FeaturesResource
+    index: resources.IndexResource
+    entities: resources.EntitiesResource
+    assets: resources.AssetsResource
+    collections: resources.CollectionsResource
+    tasks: resources.TasksResource
+    healthcheck: resources.HealthcheckResource
+    with_raw_response: MixpeekWithRawResponse
+    with_streaming_response: MixpeekWithStreamedResponse
+
+    # client options
+    api_key: str
+
+    def __init__(
+        self,
+        *,
+        api_key: str,
+        base_url: str | httpx.URL | None = None,
+        timeout: Union[float, Timeout, None, NotGiven] = NOT_GIVEN,
+        max_retries: int = DEFAULT_MAX_RETRIES,
+        default_headers: Mapping[str, str] | None = None,
+        default_query: Mapping[str, object] | None = None,
+        # Configure a custom httpx client.
+        # We provide a `DefaultHttpxClient` class that you can pass to retain the default values we use for `limits`, `timeout` & `follow_redirects`.
+        # See the [httpx documentation](https://www.python-httpx.org/api/#client) for more details.
+        http_client: httpx.Client | None = None,
+        # Enable or disable schema validation for data returned by the API.
+        # When enabled an error APIResponseValidationError is raised
+        # if the API responds with invalid data for the expected schema.
+        #
+        # This parameter may be removed or changed in the future.
+        # If you rely on this feature, please open a GitHub issue
+        # outlining your use-case to help us decide if it should be
+        # part of our public interface in the future.
+        _strict_response_validation: bool = False,
+    ) -> None:
+        """Construct a new synchronous mixpeek client instance."""
+        self.api_key = api_key
+
+        if base_url is None:
+            base_url = os.environ.get("MIXPEEK_BASE_URL")
+        if base_url is None:
+            base_url = f"https://api.mixpeek.com/"
+
+        super().__init__(
+            version=__version__,
+            base_url=base_url,
+            max_retries=max_retries,
+            timeout=timeout,
+            http_client=http_client,
+            custom_headers=default_headers,
+            custom_query=default_query,
+            _strict_response_validation=_strict_response_validation,
+        )
+
+        self.accounts = resources.AccountsResource(self)
+        self.features = resources.FeaturesResource(self)
+        self.index = resources.IndexResource(self)
+        self.entities = resources.EntitiesResource(self)
+        self.assets = resources.AssetsResource(self)
+        self.collections = resources.CollectionsResource(self)
+        self.tasks = resources.TasksResource(self)
+        self.healthcheck = resources.HealthcheckResource(self)
+        self.with_raw_response = MixpeekWithRawResponse(self)
+        self.with_streaming_response = MixpeekWithStreamedResponse(self)
+
+    @property
+    @override
+    def qs(self) -> Querystring:
+        return Querystring(array_format="comma")
+
+    @property
+    @override
+    def auth_headers(self) -> dict[str, str]:
+        api_key = self.api_key
+        return {"Authorization": f"Bearer {api_key}"}
+
+    @property
+    @override
+    def default_headers(self) -> dict[str, str | Omit]:
+        return {
+            **super().default_headers,
+            "X-Stainless-Async": "false",
+            **self._custom_headers,
+        }
+
+    def copy(
+        self,
+        *,
+        api_key: str | None = None,
+        base_url: str | httpx.URL | None = None,
+        timeout: float | Timeout | None | NotGiven = NOT_GIVEN,
+        http_client: httpx.Client | None = None,
+        max_retries: int | NotGiven = NOT_GIVEN,
+        default_headers: Mapping[str, str] | None = None,
+        set_default_headers: Mapping[str, str] | None = None,
+        default_query: Mapping[str, object] | None = None,
+        set_default_query: Mapping[str, object] | None = None,
+        _extra_kwargs: Mapping[str, Any] = {},
+    ) -> Self:
+        """
+        Create a new client instance re-using the same options given to the current client with optional overriding.
+        """
+        if default_headers is not None and set_default_headers is not None:
+            raise ValueError("The `default_headers` and `set_default_headers` arguments are mutually exclusive")
+
+        if default_query is not None and set_default_query is not None:
+            raise ValueError("The `default_query` and `set_default_query` arguments are mutually exclusive")
+
+        headers = self._custom_headers
+        if default_headers is not None:
+            headers = {**headers, **default_headers}
+        elif set_default_headers is not None:
+            headers = set_default_headers
+
+        params = self._custom_query
+        if default_query is not None:
+            params = {**params, **default_query}
+        elif set_default_query is not None:
+            params = set_default_query
+
+        http_client = http_client or self._client
+        return self.__class__(
+            api_key=api_key or self.api_key,
+            base_url=base_url or self.base_url,
+            timeout=self.timeout if isinstance(timeout, NotGiven) else timeout,
+            http_client=http_client,
+            max_retries=max_retries if is_given(max_retries) else self.max_retries,
+            default_headers=headers,
+            default_query=params,
+            **_extra_kwargs,
+        )
+
+    # Alias for `copy` for nicer inline usage, e.g.
+    # client.with_options(timeout=10).foo.create(...)
+    with_options = copy
+
+    @override
+    def _make_status_error(
+        self,
+        err_msg: str,
+        *,
+        body: object,
+        response: httpx.Response,
+    ) -> APIStatusError:
+        if response.status_code == 400:
+            return _exceptions.BadRequestError(err_msg, response=response, body=body)
+
+        if response.status_code == 401:
+            return _exceptions.AuthenticationError(err_msg, response=response, body=body)
+
+        if response.status_code == 403:
+            return _exceptions.PermissionDeniedError(err_msg, response=response, body=body)
+
+        if response.status_code == 404:
+            return _exceptions.NotFoundError(err_msg, response=response, body=body)
+
+        if response.status_code == 409:
+            return _exceptions.ConflictError(err_msg, response=response, body=body)
+
+        if response.status_code == 422:
+            return _exceptions.UnprocessableEntityError(err_msg, response=response, body=body)
+
+        if response.status_code == 429:
+            return _exceptions.RateLimitError(err_msg, response=response, body=body)
+
+        if response.status_code >= 500:
+            return _exceptions.InternalServerError(err_msg, response=response, body=body)
+        return APIStatusError(err_msg, response=response, body=body)
+
+
+class AsyncMixpeek(AsyncAPIClient):
+    accounts: resources.AsyncAccountsResource
+    features: resources.AsyncFeaturesResource
+    index: resources.AsyncIndexResource
+    entities: resources.AsyncEntitiesResource
+    assets: resources.AsyncAssetsResource
+    collections: resources.AsyncCollectionsResource
+    tasks: resources.AsyncTasksResource
+    healthcheck: resources.AsyncHealthcheckResource
+    with_raw_response: AsyncMixpeekWithRawResponse
+    with_streaming_response: AsyncMixpeekWithStreamedResponse
+
+    # client options
+    api_key: str
+
+    def __init__(
+        self,
+        *,
+        api_key: str,
+        base_url: str | httpx.URL | None = None,
+        timeout: Union[float, Timeout, None, NotGiven] = NOT_GIVEN,
+        max_retries: int = DEFAULT_MAX_RETRIES,
+        default_headers: Mapping[str, str] | None = None,
+        default_query: Mapping[str, object] | None = None,
+        # Configure a custom httpx client.
+        # We provide a `DefaultAsyncHttpxClient` class that you can pass to retain the default values we use for `limits`, `timeout` & `follow_redirects`.
+        # See the [httpx documentation](https://www.python-httpx.org/api/#asyncclient) for more details.
+        http_client: httpx.AsyncClient | None = None,
+        # Enable or disable schema validation for data returned by the API.
+        # When enabled an error APIResponseValidationError is raised
+        # if the API responds with invalid data for the expected schema.
+        #
+        # This parameter may be removed or changed in the future.
+        # If you rely on this feature, please open a GitHub issue
+        # outlining your use-case to help us decide if it should be
+        # part of our public interface in the future.
+        _strict_response_validation: bool = False,
+    ) -> None:
+        """Construct a new async mixpeek client instance."""
+        self.api_key = api_key
+
+        if base_url is None:
+            base_url = os.environ.get("MIXPEEK_BASE_URL")
+        if base_url is None:
+            base_url = f"https://api.mixpeek.com/"
+
+        super().__init__(
+            version=__version__,
+            base_url=base_url,
+            max_retries=max_retries,
+            timeout=timeout,
+            http_client=http_client,
+            custom_headers=default_headers,
+            custom_query=default_query,
+            _strict_response_validation=_strict_response_validation,
+        )
+
+        self.accounts = resources.AsyncAccountsResource(self)
+        self.features = resources.AsyncFeaturesResource(self)
+        self.index = resources.AsyncIndexResource(self)
+        self.entities = resources.AsyncEntitiesResource(self)
+        self.assets = resources.AsyncAssetsResource(self)
+        self.collections = resources.AsyncCollectionsResource(self)
+        self.tasks = resources.AsyncTasksResource(self)
+        self.healthcheck = resources.AsyncHealthcheckResource(self)
+        self.with_raw_response = AsyncMixpeekWithRawResponse(self)
+        self.with_streaming_response = AsyncMixpeekWithStreamedResponse(self)
+
+    @property
+    @override
+    def qs(self) -> Querystring:
+        return Querystring(array_format="comma")
+
+    @property
+    @override
+    def auth_headers(self) -> dict[str, str]:
+        api_key = self.api_key
+        return {"Authorization": f"Bearer {api_key}"}
+
+    @property
+    @override
+    def default_headers(self) -> dict[str, str | Omit]:
+        return {
+            **super().default_headers,
+            "X-Stainless-Async": f"async:{get_async_library()}",
+            **self._custom_headers,
+        }
+
+    def copy(
+        self,
+        *,
+        api_key: str | None = None,
+        base_url: str | httpx.URL | None = None,
+        timeout: float | Timeout | None | NotGiven = NOT_GIVEN,
+        http_client: httpx.AsyncClient | None = None,
+        max_retries: int | NotGiven = NOT_GIVEN,
+        default_headers: Mapping[str, str] | None = None,
+        set_default_headers: Mapping[str, str] | None = None,
+        default_query: Mapping[str, object] | None = None,
+        set_default_query: Mapping[str, object] | None = None,
+        _extra_kwargs: Mapping[str, Any] = {},
+    ) -> Self:
+        """
+        Create a new client instance re-using the same options given to the current client with optional overriding.
+        """
+        if default_headers is not None and set_default_headers is not None:
+            raise ValueError("The `default_headers` and `set_default_headers` arguments are mutually exclusive")
+
+        if default_query is not None and set_default_query is not None:
+            raise ValueError("The `default_query` and `set_default_query` arguments are mutually exclusive")
+
+        headers = self._custom_headers
+        if default_headers is not None:
+            headers = {**headers, **default_headers}
+        elif set_default_headers is not None:
+            headers = set_default_headers
+
+        params = self._custom_query
+        if default_query is not None:
+            params = {**params, **default_query}
+        elif set_default_query is not None:
+            params = set_default_query
+
+        http_client = http_client or self._client
+        return self.__class__(
+            api_key=api_key or self.api_key,
+            base_url=base_url or self.base_url,
+            timeout=self.timeout if isinstance(timeout, NotGiven) else timeout,
+            http_client=http_client,
+            max_retries=max_retries if is_given(max_retries) else self.max_retries,
+            default_headers=headers,
+            default_query=params,
+            **_extra_kwargs,
+        )
+
+    # Alias for `copy` for nicer inline usage, e.g.
+    # client.with_options(timeout=10).foo.create(...)
+    with_options = copy
+
+    @override
+    def _make_status_error(
+        self,
+        err_msg: str,
+        *,
+        body: object,
+        response: httpx.Response,
+    ) -> APIStatusError:
+        if response.status_code == 400:
+            return _exceptions.BadRequestError(err_msg, response=response, body=body)
+
+        if response.status_code == 401:
+            return _exceptions.AuthenticationError(err_msg, response=response, body=body)
+
+        if response.status_code == 403:
+            return _exceptions.PermissionDeniedError(err_msg, response=response, body=body)
+
+        if response.status_code == 404:
+            return _exceptions.NotFoundError(err_msg, response=response, body=body)
+
+        if response.status_code == 409:
+            return _exceptions.ConflictError(err_msg, response=response, body=body)
+
+        if response.status_code == 422:
+            return _exceptions.UnprocessableEntityError(err_msg, response=response, body=body)
+
+        if response.status_code == 429:
+            return _exceptions.RateLimitError(err_msg, response=response, body=body)
+
+        if response.status_code >= 500:
+            return _exceptions.InternalServerError(err_msg, response=response, body=body)
+        return APIStatusError(err_msg, response=response, body=body)
+
+
+class MixpeekWithRawResponse:
+    def __init__(self, client: Mixpeek) -> None:
+        self.accounts = resources.AccountsResourceWithRawResponse(client.accounts)
+        self.features = resources.FeaturesResourceWithRawResponse(client.features)
+        self.index = resources.IndexResourceWithRawResponse(client.index)
+        self.entities = resources.EntitiesResourceWithRawResponse(client.entities)
+        self.assets = resources.AssetsResourceWithRawResponse(client.assets)
+        self.collections = resources.CollectionsResourceWithRawResponse(client.collections)
+        self.tasks = resources.TasksResourceWithRawResponse(client.tasks)
+        self.healthcheck = resources.HealthcheckResourceWithRawResponse(client.healthcheck)
+
+
+class AsyncMixpeekWithRawResponse:
+    def __init__(self, client: AsyncMixpeek) -> None:
+        self.accounts = resources.AsyncAccountsResourceWithRawResponse(client.accounts)
+        self.features = resources.AsyncFeaturesResourceWithRawResponse(client.features)
+        self.index = resources.AsyncIndexResourceWithRawResponse(client.index)
+        self.entities = resources.AsyncEntitiesResourceWithRawResponse(client.entities)
+        self.assets = resources.AsyncAssetsResourceWithRawResponse(client.assets)
+        self.collections = resources.AsyncCollectionsResourceWithRawResponse(client.collections)
+        self.tasks = resources.AsyncTasksResourceWithRawResponse(client.tasks)
+        self.healthcheck = resources.AsyncHealthcheckResourceWithRawResponse(client.healthcheck)
+
+
+class MixpeekWithStreamedResponse:
+    def __init__(self, client: Mixpeek) -> None:
+        self.accounts = resources.AccountsResourceWithStreamingResponse(client.accounts)
+        self.features = resources.FeaturesResourceWithStreamingResponse(client.features)
+        self.index = resources.IndexResourceWithStreamingResponse(client.index)
+        self.entities = resources.EntitiesResourceWithStreamingResponse(client.entities)
+        self.assets = resources.AssetsResourceWithStreamingResponse(client.assets)
+        self.collections = resources.CollectionsResourceWithStreamingResponse(client.collections)
+        self.tasks = resources.TasksResourceWithStreamingResponse(client.tasks)
+        self.healthcheck = resources.HealthcheckResourceWithStreamingResponse(client.healthcheck)
+
+
+class AsyncMixpeekWithStreamedResponse:
+    def __init__(self, client: AsyncMixpeek) -> None:
+        self.accounts = resources.AsyncAccountsResourceWithStreamingResponse(client.accounts)
+        self.features = resources.AsyncFeaturesResourceWithStreamingResponse(client.features)
+        self.index = resources.AsyncIndexResourceWithStreamingResponse(client.index)
+        self.entities = resources.AsyncEntitiesResourceWithStreamingResponse(client.entities)
+        self.assets = resources.AsyncAssetsResourceWithStreamingResponse(client.assets)
+        self.collections = resources.AsyncCollectionsResourceWithStreamingResponse(client.collections)
+        self.tasks = resources.AsyncTasksResourceWithStreamingResponse(client.tasks)
+        self.healthcheck = resources.AsyncHealthcheckResourceWithStreamingResponse(client.healthcheck)
+
+
+Client = Mixpeek
+
+AsyncClient = AsyncMixpeek
