74474e598e9181b0b9d0009ad430cdf026e74f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 07:04:33 +0000

    Add package edx-username-changer/0.3.2/edx_username_changer-0.3.2.tar.gz

---
+++ packages/edx-username-changer/edx_username_changer-0.3.2.tar.gz/edx_username_changer-0.3.2/edx_username_changer/apps.py
@@ -0,0 +1,36 @@
+"""
+App configuration for edx-username-changer plugin
+"""
+
+from django.apps import AppConfig
+
+# pylint: disable=import-error, unused-import
+from edx_django_utils.plugins.constants import PluginSignals, PluginSettings
+from openedx.core.djangoapps.plugins.constants import (
+    ProjectType,
+    SettingsType,
+)
+
+
+class EdxUsernameChangerConfig(AppConfig):
+    name = "edx_username_changer"
+    verbose_name = "Open edX Username Changer"
+
+    plugin_app = {
+        PluginSignals.CONFIG: {
+            ProjectType.LMS: {
+                PluginSignals.RECEIVERS: [
+                    {
+                        PluginSignals.RECEIVER_FUNC_NAME: "user_pre_save_callback",
+                        PluginSignals.SIGNAL_PATH: "django.db.models.signals.pre_save",
+                        PluginSignals.SENDER_PATH: "django.contrib.auth.models.User",
+                    },
+                    {
+                        PluginSignals.RECEIVER_FUNC_NAME: "user_post_save_callback",
+                        PluginSignals.SIGNAL_PATH: "django.db.models.signals.post_save",
+                        PluginSignals.SENDER_PATH: "django.contrib.auth.models.User",
+                    },
+                ],
+            },
+        },
+    }
---
+++ packages/edx-username-changer/edx_username_changer-0.3.2.tar.gz/edx_username_changer-0.3.2/edx_username_changer/signals.py
@@ -0,0 +1,46 @@
+"""
+Signals and Signal Handlers for edx-username-changer plugin
+"""
+
+from django.conf import settings
+from django.db import transaction
+
+from common.djangoapps.util.model_utils import (  # pylint: disable=import-error
+    get_changed_fields_dict,
+)
+
+from edx_username_changer.utils import update_user_social_auth_uid
+from edx_username_changer.tasks import task_update_username_in_forum
+
+
+def user_pre_save_callback(sender, **kwargs):
+    """
+    Pre-save signal handler of User model to store changed fields to be used later
+    """
+    if settings.FEATURES.get("ENABLE_EDX_USERNAME_CHANGER"):
+        user = kwargs["instance"]
+        fields_to_update = get_changed_fields_dict(user, sender)
+        if "username" in fields_to_update:
+            fields_to_update.update({"new_username": user.username})
+            user._updated_fields = fields_to_update  # pylint: disable=protected-access
+
+
+# pylint: disable=protected-access, unused-argument
+def user_post_save_callback(sender, **kwargs):
+    """
+    Post-save signal handler of User model to update username throughout the application
+    """
+    if settings.FEATURES.get("ENABLE_EDX_USERNAME_CHANGER"):
+        user = kwargs["instance"]
+        if (
+            hasattr(user, "_updated_fields")
+            and user._updated_fields
+            and {"username", "new_username"}.issubset(user._updated_fields)
+        ):
+            new_username = user._updated_fields["new_username"]
+            transaction.on_commit(
+                lambda: task_update_username_in_forum.delay(new_username)
+            )
+            update_user_social_auth_uid(user._updated_fields["username"], new_username)
+            # pylint: disable=literal-used-as-attribute
+            delattr(user, "_updated_fields")
