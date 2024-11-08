349a3eaf44fcf9a12078f39349c1558353c788d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 15:05:32 +0000

    Add package fun-things/0.39.6/fun_things-0.39.6.tar.gz

---
+++ packages/fun-things/fun_things-0.39.6.tar.gz/fun_things-0.39.6/fun_things/cli/__init__.py
@@ -0,0 +1,29 @@
+import argparse
+from .freeze import Freeze
+from .install import Install
+
+
+def cli():
+    parser = argparse.ArgumentParser(
+        description="Pub/Sub consumer common.",
+    )
+
+    Freeze().run(
+        parser.add_subparsers(
+            dest="command",
+        )
+    )
+
+    Install().run(
+        parser.add_subparsers(
+            dest="command",
+        )
+    )
+
+    args = parser.parse_args()
+
+    if hasattr(args, "func"):
+        args.func(args)
+        return
+
+    parser.print_help()
