2d5ab0f675e045ee0ee0f36995d9fe3b80e83ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:05:01 +0000

    Add package types-lxml-multi-subclass/2024.11.8/types_lxml_multi_subclass-2024.11.8.tar.gz

---
+++ packages/types-lxml-multi-subclass/types_lxml_multi_subclass-2024.11.8.tar.gz/types_lxml_multi_subclass-2024.11.8/test-rt/_testutils/__init__.py
@@ -0,0 +1,8 @@
+__all__ = (
+    "empty_signature_tester",
+    "reveal_type_wrapper",
+    "signature_tester",
+)
+
+from .decorator import empty_signature_tester, signature_tester
+from .rt_wrapper import reveal_type_wrapper
