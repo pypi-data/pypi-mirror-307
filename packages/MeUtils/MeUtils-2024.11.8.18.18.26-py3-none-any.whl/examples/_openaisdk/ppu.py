1cb66e036f956885b620d82c44e077aea08b096a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:05:04 +0000

    Add package MeUtils/2024.11.8.18.18.26/MeUtils-2024.11.8.18.18.26-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2024.11.8.18.18.26-py3-none-any.whl/examples/_openaisdk/ppu.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : ppu
+# @Time         : 2024/1/9 17:37
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from meutils.pipe import *
+
+from openai import OpenAI
+
+base_url = "https://api.chatfire.cc/v1"
+base_url = "https://api.chatfire.cn/v1"
+
+client = OpenAI(base_url=base_url)
+
+q = ""
+data = {
+    'model': 'ppu-1',
+    'messages': [
+        {'role': 'user', 'content': q}
+    ],
+    'stream': False
+}
+
+print(client.chat.completions.create(**data))
