d89bc7241323985645dda42dd83c13482478c0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 15:04:46 +0000

    Add package scratchcon/1.2.4b0/scratchcon-1.2.4b0-py3-none-any.whl

---
+++ packages/scratchcon/scratchcon-1.2.4b0-py3-none-any.whl/scratchcon/actions/conn.py
@@ -0,0 +1,9 @@
+from .common import public
+
+
+def connect_project(proj_id):
+    public.project_id = proj_id
+
+
+def connect_studio(studio_id):
+    public.studio_id = studio_id
---
+++ packages/scratchcon/scratchcon-1.2.4b0-py3-none-any.whl/scratchcon/common.py
@@ -0,0 +1,8 @@
+class Public:
+    user_link = None
+    project_link = None
+    studio_link = None
+    login = None
+
+
+public = Public()
