165b8314fc5478a63241e4cda818d4731a2d58f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 05:04:30 +0000

    Add package network-diffusion/0.17.0/network_diffusion-0.17.0-cp312-cp312-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/network-diffusion/network_diffusion-0.17.0-cp312-cp312-manylinux_2_5_x86_64.manylinux1_x86_64.whl/network_diffusion/mln/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2023 by Michał Czuba, Piotr Bródka.
+#
+# This file is a part of Network Diffusion.
+#
+# Network Diffusion is licensed under the MIT License. You may obtain a copy
+# of the License at https://opensource.org/licenses/MIT
+# =============================================================================
+
+"""Modules that handle operations on multilayer networks."""
+
+# flake8: noqa
+
+from network_diffusion.mln import (
+    cbim,
+    centrality_discount,
+    driver_actors,
+    functions,
+    kppshell,
+)
+from network_diffusion.mln.actor import MLNetworkActor
+from network_diffusion.mln.mlnetwork import MultilayerNetwork
+from network_diffusion.mln.mlnetwork_torch import MultilayerNetworkTorch
