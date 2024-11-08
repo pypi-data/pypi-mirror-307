4cf9c450c1c16e46c3478db8442c0a8175ca170c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:04:04 +0000

    Add package ultra-framework/0.0.2/ultra_framework-0.0.2-py3-none-any.whl

---
+++ packages/ultra-framework/ultra_framework-0.0.2-py3-none-any.whl/ultra_framework/database/session_factory.py
@@ -0,0 +1,21 @@
+from typing import Iterator
+
+from sqlalchemy.orm import sessionmaker, Session
+
+
+class SessionFactory:
+
+    def __init__(self, session_maker: sessionmaker):
+        self.session_maker = session_maker
+
+    def create_session(self) -> Iterator[Session]:
+        session = self.session_maker()
+
+        try:
+            yield session
+            session.commit()
+        except Exception:
+            session.rollback()
+            raise
+        finally:
+            session.close()
