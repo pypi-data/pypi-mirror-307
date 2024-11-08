280bf0eee9fb1f82ceb29ba32e9d714664897b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:03:52 +0000

    Add package nixm/45/nixm-45.tar.gz

---
+++ packages/nixm/nixm-45.tar.gz/nixm-45/setup.py
@@ -0,0 +1,16 @@
+# This file is placed in the Public Domain.
+
+
+"stub"
+
+
+import setuptools
+
+
+if __name__ == "__main__":
+    setuptools.setup(scripts=[
+        'bin/nixm',
+        'bin/nixmctl',
+        'bin/nixmd',
+        'bin/nixms'
+    ])
