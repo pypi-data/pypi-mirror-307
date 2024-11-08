6a53f684e6fdd77a3a8ec7b7fec4fd44719d165f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 15:04:37 +0000

    Add package keytopPyUtils/1.0.3/keytoppyutils-1.0.3.tar.gz

---
+++ packages/keytopPyUtils/keytoppyutils-1.0.3.tar.gz/keytoppyutils-1.0.3/keytopPyUtils/FileUtils.py
@@ -0,0 +1,18 @@
+import os.path
+
+from weasyprint.css.computed_values import content
+
+
+class FileUtils:
+    @staticmethod
+    def create_paths(path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    @staticmethod
+    def read_file(path):
+        content = ''
+        with open(path,'r',encoding='utf-8') as file:
+            content = file.read()
+        return content
+#FileUtils.create_paths("D:/home/data/mdconverter/")
