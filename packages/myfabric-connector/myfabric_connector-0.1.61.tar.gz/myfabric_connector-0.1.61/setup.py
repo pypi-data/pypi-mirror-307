2125781c5b37d9ed2f13feeffb2c1750dc04a2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 05:04:52 +0000

    Add package myfabric-connector/0.1.61/myfabric_connector-0.1.61.tar.gz

---
+++ packages/myfabric-connector/myfabric_connector-0.1.61.tar.gz/myfabric_connector-0.1.61/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+
+from setuptools import setup, find_packages
+from myfabric.__version__ import __version__
+
+setup(
+    name='myfabric-connector',
+    version=__version__,
+    packages=find_packages(),
+    install_requires=[
+        'websockets',
+        'requests',
+        'pysher',
+    ],
+    entry_points={
+        'console_scripts': [
+            'myfabric-connector start = myfabric.main:main',
+            'myfabric-connector install = myfabric.install_runner:run_install',
+        ],
+    },
+    author='Khonik',
+    author_email='khonikdev@gmail.com',
+    description='Программа для взаимодействия 3D принтеров и  CRM MyFabric',
+    long_description=open('readme.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/myfabric-ru/ws-connector',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+)
