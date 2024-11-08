13710688abb5ff19bc968430ac094c47d4ef5387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:03:47 +0000

    Add package gammarers.aws-resource-naming/0.2.4/gammarers_aws_resource_naming-0.2.4.tar.gz

---
+++ packages/gammarers.aws-resource-naming/gammarers_aws_resource_naming-0.2.4.tar.gz/gammarers_aws_resource_naming-0.2.4/src/gammarers/aws_resource_naming/resource_naming/__init__.py
@@ -0,0 +1,66 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import typeguard
+from importlib.metadata import version as _metadata_package_version
+TYPEGUARD_MAJOR_VERSION = int(_metadata_package_version('typeguard').split('.')[0])
+
+def check_type(argname: str, value: object, expected_type: typing.Any) -> typing.Any:
+    if TYPEGUARD_MAJOR_VERSION <= 2:
+        return typeguard.check_type(argname=argname, value=value, expected_type=expected_type) # type:ignore
+    else:
+        if isinstance(value, jsii._reference_map.InterfaceDynamicProxy): # pyright: ignore [reportAttributeAccessIssue]
+           pass
+        else:
+            if TYPEGUARD_MAJOR_VERSION == 3:
+                typeguard.config.collection_check_strategy = typeguard.CollectionCheckStrategy.ALL_ITEMS # type:ignore
+                typeguard.check_type(value=value, expected_type=expected_type) # type:ignore
+            else:
+                typeguard.check_type(value=value, expected_type=expected_type, collection_check_strategy=typeguard.CollectionCheckStrategy.ALL_ITEMS) # type:ignore
+
+from .._jsii import *
+
+
+@jsii.data_type(
+    jsii_type="@gammarers/aws-resource-naming.ResourceNaming.Naming",
+    jsii_struct_bases=[],
+    name_mapping={},
+)
+class Naming:
+    def __init__(self) -> None:
+        self._values: typing.Dict[builtins.str, typing.Any] = {}
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "Naming(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+@jsii.enum(jsii_type="@gammarers/aws-resource-naming.ResourceNaming.NamingType")
+class NamingType(enum.Enum):
+    DEFAULT = "DEFAULT"
+    NONE = "NONE"
+
+
+__all__ = [
+    "Naming",
+    "NamingType",
+]
+
+publication.publish()
