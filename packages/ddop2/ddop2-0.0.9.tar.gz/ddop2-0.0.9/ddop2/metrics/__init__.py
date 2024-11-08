35fd2486f4999346b8ff22941f6f00ad1d94c9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:04:18 +0000

    Add package ddop2/0.0.9/ddop2-0.0.9.tar.gz

---
+++ packages/ddop2/ddop2-0.0.9.tar.gz/ddop2-0.0.9/ddop2/metrics/__init__.py
@@ -0,0 +1,4 @@
+from ._costs import pairwise_costs, average_costs, total_costs, prescriptiveness_score
+from ._scorer import make_scorer
+
+__all__ = ["pairwise_costs", "total_costs", "average_costs"]
