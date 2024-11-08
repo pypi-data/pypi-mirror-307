ac368454f0b881075ce5846ebc6ce3a4306846fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:04:38 +0000

    Add package package-publishing/0.1/package_publishing-0.1.tar.gz

---
+++ packages/package-publishing/package_publishing-0.1.tar.gz/package_publishing-0.1/package_publishing/__init__.py
@@ -0,0 +1,4 @@
+# package_publishing/__init__.py
+
+from .module1 import add, subtract
+from .module2 import multiply, divide
---
+++ packages/package-publishing/package_publishing-0.1.tar.gz/package_publishing-0.1/package_publishing/module1.py
@@ -0,0 +1,7 @@
+# package_publishing/module1.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
