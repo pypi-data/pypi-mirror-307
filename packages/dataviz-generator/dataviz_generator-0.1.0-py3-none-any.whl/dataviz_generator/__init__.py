e2676ea33df8631628edf0ff1d0c6d77afbf9929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 05:04:41 +0000

    Add package dataviz-generator/0.1.0/dataviz_generator-0.1.0-py3-none-any.whl

---
+++ packages/dataviz-generator/dataviz_generator-0.1.0-py3-none-any.whl/dataviz_generator/__init__.py
@@ -0,0 +1,11 @@
+"""
+DataVizGenerator - An AI-powered data visualization generator
+"""
+
+from .generator import DataVizGenerator
+
+__version__ = "0.1.0"
+__author__ = "Rio Nur Arifin"
+__email__ = "arifinrio95@gmail.com"
+
+__all__ = ["DataVizGenerator"]
