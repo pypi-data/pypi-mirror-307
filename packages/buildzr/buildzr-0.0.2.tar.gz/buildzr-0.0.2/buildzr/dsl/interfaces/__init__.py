546e1f826beb6bcbeb5e3e9fc00e98b9eda9c109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:03:57 +0000

    Add package buildzr/0.0.2/buildzr-0.0.2.tar.gz

---
+++ packages/buildzr/buildzr-0.0.2.tar.gz/buildzr-0.0.2/buildzr/dsl/interfaces/__init__.py
@@ -0,0 +1,14 @@
+from .interfaces import (
+    DslElement,
+    DslFluentRelationship,
+    DslRelationship,
+    DslWorkspaceElement,
+    DslViewsElement,
+    BindLeft,
+    BindRight,
+    BindLeftLate,
+    TSrc,
+    TDst,
+    TParent,
+    TChild,
+)
