723b4d1bb0313b1ba66b6d69c67ba7f42b82bf12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 05:04:05 +0000

    Add package sherbystats/0.0.18/sherbystats-0.0.18-py3-none-any.whl

---
+++ packages/sherbystats/sherbystats-0.0.18-py3-none-any.whl/sherbystats/__init__.py
@@ -0,0 +1,22 @@
+from sherbystats.anova import *
+from sherbystats.doe import *
+from sherbystats.mlr import *
+from sherbystats.lda import *
+from sherbystats.normplot import *
+from sherbystats.pca import *
+from sherbystats.pca_trou import *
+from sherbystats.xlsread import *
+
+from sherbystats.ternaire import *
+from sherbystats.binaire import *
+from sherbystats.binaire3D import *
+
+
+
+
+
+
+
+
+
+
