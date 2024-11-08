bd6f5296b149afd48f03d2ead5a54531369fa1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 03:07:17 +0000

    Add package letschatty/0.2.1/letschatty-0.2.1.tar.gz

---
+++ packages/letschatty/letschatty-0.2.1.tar.gz/letschatty-0.2.1/src/letschatty/services/factories/__init__.py
@@ -0,0 +1,3 @@
+from .chatty_responses.chatty_response_factory import ChattyFastAnswersFactory
+from .company.empresa_factory import EmpresaFactory
+from .messages import *
---
+++ packages/letschatty/letschatty-0.2.1.tar.gz/letschatty-0.2.1/src/letschatty/services/factories/chatty_responses/chatty_response_factory.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from pydantic import BaseModel, Field
+from typing import List, Dict, Any
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from bson import ObjectId
+from ....models.messages.chatty_messages import MessageRequest
+from ....models.company.assets import ChattyFastAnswer
+from ....models.messages.chatty_messages.schema import ChattyContext
+from ....models.utils import MessageSubtype
+
+class ChattyFastAnswersFactory:
+    
+    @staticmethod
+    def from_json(json_data: Dict[str, Any]) -> ChattyFastAnswer:
+        """This method is used to create a ChattyResponse from a JSON object"""
+        return ChattyFastAnswer(**json_data)
+    
+    @staticmethod
+    def create(messages: List[Dict[str, Any]]) -> ChattyFastAnswer:
+        """This method is used to create a ChattyResponse from a JSON object"""
+        id = str(ObjectId())
+        updated_at = datetime.now(tz=ZoneInfo("UTC"))
+        created_at = datetime.now(tz=ZoneInfo("UTC"))
+        chatty_context = ChattyContext(response_id=id)
+        subtype = MessageSubtype.CHATTY_RESPONSE
+        messages = [MessageRequest.from_response(data=message, context=chatty_context, subtype=subtype) for message in messages]
+        return ChattyFastAnswer(id=id, updated_at=updated_at, created_at=created_at, messages=messages)
+    
+    @staticmethod
+    def update(chatty_response: ChattyFastAnswer, messages: List[Dict[str, Any]]) -> ChattyFastAnswer:
+        """This method is used to update a ChattyResponse with a new list of messages"""
+        chatty_context = ChattyContext(response_id=chatty_response.id)
+        subtype = MessageSubtype.CHATTY_RESPONSE
+        messages = [MessageRequest.from_response(data=message, context=chatty_context, subtype=subtype) for message in messages]
+        chatty_response.updated_at = datetime.now(tz=ZoneInfo("UTC"))
+        chatty_response.messages = messages
+        return chatty_response
