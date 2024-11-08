bda2427c77e4fffe64aeec33001f31579c0d3aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 03:08:21 +0000

    Add package echor-logger/0.2.2/echor_logger-0.2.2-py3-none-any.whl

---
+++ packages/echor-logger/echor_logger-0.2.2-py3-none-any.whl/echor_logger/__init__.py
@@ -0,0 +1,7 @@
+from .index import EchorLogger, echorLogger
+
+# You can specify which symbols should be exposed when users do "from package import *"
+__all__ = ['EchorLogger', 'echorLogger']
+
+# Optionally, you can add package metadata
+__version__ = '0.1.0'
