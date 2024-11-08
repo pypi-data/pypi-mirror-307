bbd3354565b92f355f1025046c4287fe685c0635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 03:07:23 +0000

    Add package bestrag/0.1.1/bestrag-0.1.1.tar.gz

---
+++ packages/bestrag/bestrag-0.1.1.tar.gz/bestrag-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bestrag",
+    version="0.1.1",
+    description="BestRAG (Best Retrieval Augmented by Qdrant) is a library that provides "
+                "functionality for storing and searching document embeddings in a Qdrant vector database.",
+    author="samadpls",
+    author_email="abdulsamadsid1@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samadpls/bestrag",
+    packages=find_packages(),
+    install_requires=[
+        "fastembed==0.4.1",
+        "streamlit",
+        "pytest",
+        "flake8",
+        "PyPDF2",
+        "qdrant-client",
+        "onnxruntime==1.19.2",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
