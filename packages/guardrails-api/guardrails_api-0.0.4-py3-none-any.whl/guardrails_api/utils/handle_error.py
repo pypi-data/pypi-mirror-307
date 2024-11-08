841badd5b73f72673886b1279fcb07db64f71118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:04:44 +0000

    Add package guardrails-api/0.0.4/guardrails_api-0.0.4-py3-none-any.whl

---
+++ packages/guardrails-api/guardrails_api-0.0.4-py3-none-any.whl/guardrails_api/utils/handle_error.py
@@ -0,0 +1,32 @@
+from functools import wraps
+import traceback
+from werkzeug.exceptions import HTTPException
+from guardrails_api.classes.http_error import HttpError
+from guardrails_api.utils.logger import logger
+from guardrails.errors import ValidationError
+
+
+def handle_error(fn):
+    @wraps(fn)
+    def decorator(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except ValidationError as validation_error:
+            logger.error(validation_error)
+            traceback.print_exception(type(validation_error), validation_error, validation_error.__traceback__)
+            return str(validation_error), 400
+        except HttpError as http_error:
+            logger.error(http_error)
+            traceback.print_exception(type(http_error), http_error, http_error.__traceback__)
+            return http_error.to_dict(), http_error.status
+        except HTTPException as http_exception:
+            logger.error(http_exception)
+            traceback.print_exception(http_exception)
+            http_error = HttpError(http_exception.code, http_exception.description)
+            return http_error.to_dict(), http_error.status
+        except Exception as e:
+            logger.error(e)
+            traceback.print_exception(e)
+            return HttpError(500, "Internal Server Error").to_dict(), 500
+
+    return decorator
