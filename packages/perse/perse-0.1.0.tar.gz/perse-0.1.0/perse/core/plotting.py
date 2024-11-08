63b93f48963190b025060a3d8aca964346e210b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 03:07:05 +0000

    Add package perse/0.1.0/perse-0.1.0.tar.gz

---
+++ packages/perse/perse-0.1.0.tar.gz/perse-0.1.0/perse/core/plotting.py
@@ -0,0 +1,15 @@
+# plotting.py
+import matplotlib.pyplot as plt
+from .base import BaseDataFrame
+
+
+
+class PlottingDataFrame(BaseDataFrame):
+    """PlottingDataFrame"""
+    def plot(self, *args, show=True, **kwargs):
+        """Plot data using Pandas' plot method."""
+        self.refresh_pandas()
+        ax = self._df.plot(*args, **kwargs)
+        if show:
+            plt.show()
+        return ax
