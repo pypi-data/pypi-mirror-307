ef80147bf0f48103ca0d7ca4409637e60f3f6754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:04:35 +0000

    Add package torchutil/0.0.14/torchutil-0.0.14.tar.gz

---
+++ packages/torchutil/torchutil-0.0.14.tar.gz/torchutil-0.0.14/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+
+with open('README.md') as file:
+    long_description = file.read()
+
+
+setup(
+    name='torchutil',
+    description='PyTorch utilities for developing deep learning frameworks',
+    version='0.0.14',
+    author='Max Morrison',
+    author_email='maxrmorrison@gmail.com',
+    url='https://github.com/maxrmorrison/torchutil',
+    install_requires=['apprise', 'tensorboard', 'torch'],
+    packages=find_packages(),
+    package_data={'torchutil': ['assets/*', 'assets/*/*']},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['pytorch', 'utility', 'training'],
+    classifiers=['License :: OSI Approved :: MIT License'],
+    license='MIT')
---
+++ packages/torchutil/torchutil-0.0.14.tar.gz/torchutil-0.0.14/torchutil/download.py
@@ -0,0 +1,64 @@
+import os
+import requests
+import tarfile
+import tempfile
+import zipfile
+from typing import Union
+
+
+###############################################################################
+# Data download utilities
+###############################################################################
+
+
+def file(url: 'str', path: Union[str, bytes, os.PathLike]):
+    """Download file from url
+
+    Arguments
+        url - The URL to download
+        path - The location to save results
+    """
+    with requests.get(url, stream=True) as rstream:
+        rstream.raise_for_status()
+        with open(path, 'wb') as fstream:
+            for chunk in rstream.iter_content(chunk_size=128):
+                fstream.write(chunk)
+
+
+def tarbz2(url: 'str', path: Union[str, bytes, os.PathLike]):
+    """Download and extract tar bz2 file to location
+
+    Arguments
+        url - The URL to download
+        path - The location to save results
+    """
+    with requests.get(url, stream=True) as rstream:
+        rstream.raise_for_status()
+        with tarfile.open(fileobj=rstream.raw, mode='r|bz2') as tstream:
+            tstream.extractall(path)
+
+
+def targz(url: 'str', path: Union[str, bytes, os.PathLike]):
+    """Download and extract tar gz file to location
+
+    Arguments
+        url - The URL to download
+        path - The location to save results
+    """
+    with requests.get(url, stream=True) as rstream:
+        rstream.raise_for_status()
+        with tarfile.open(fileobj=rstream.raw, mode='r|gz') as tstream:
+            tstream.extractall(path)
+
+
+def zip(url: 'str', path: Union[str, bytes, os.PathLike]):
+    """Download and extract zip file to location
+
+    Arguments
+        url - The URL to download
+        path - The location to save results
+    """
+    with tempfile.NamedTemporaryFile() as tmp:
+        file(url, tmp.name)
+        with zipfile.ZipFile(tmp, 'r') as zfile:
+            zfile.extractall(path)
