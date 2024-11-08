8e48f1d37e7c1b5cf9a050439e27d14c63695a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:04:19 +0000

    Add package spool/1.0.0/spool-1.0.0.tar.gz

---
+++ packages/spool/spool-1.0.0.tar.gz/spool-1.0.0/tests/test_integration.py
@@ -0,0 +1,119 @@
+from pathlib import Path
+from unittest import mock
+
+import pytest
+
+from spool import main
+
+SIMPLE = '''\
+---
+mails:
+  - name: simple
+    description: A simple text message
+    sender: sender@example.org
+    recipients: recipient@example.org
+    subject: Simple Text Message
+    text_body: |
+        Just a simple text message.
+'''
+
+WITH_VARS = '''\
+---
+defaults:
+  sender: sender@example.org
+  recipients: recipient@example.org, other@example.com
+
+vars:
+  subject: Simple Text Message
+
+mails:
+  - name: with-vars
+    subject: '{{ subject|upper }}'
+    text_body: |
+        Just a simple text message.
+'''
+
+WITH_LOOP = '''\
+---
+defaults:
+  sender: sender@example.org
+
+vars:
+  friends:
+    - Ben
+    - Karol
+    - Steve
+
+mails:
+  - name: with-loop
+    subject: Simple Text Message
+    recipients: '{{ item }}'
+    text_body: |
+        Just a simple text message.
+
+    loop: '{{ friends }}'
+'''
+
+EXAMPLE_DIR = Path(__file__).parent / '../examples'
+
+
+@mock.patch('sys.argv', ['spool', '--help'])
+def test_help_flag():
+    with pytest.raises(SystemExit) as error:
+        main.cli()
+
+    assert error.type == SystemExit
+    assert error.value.code == 0
+
+
+@mock.patch('sys.argv', ['spool', ])
+def test_fail_with_no_config():
+    with pytest.raises(SystemExit) as error:
+        main.cli()
+
+    assert error.type == SystemExit
+    assert error.value.code == 2
+
+
+def test_success_with_simple_config(smtp_server, tmp_path):
+
+    config_1 = tmp_path / 'simple.yml'
+    config_1.write_text(SIMPLE)
+
+    config_2 = tmp_path / 'with_vars.yml'
+    config_2.write_text(WITH_VARS)
+
+    with mock.patch('sys.argv', [
+        'spool', '--relay', smtp_server.host, '--port', str(smtp_server.port),
+        str(config_1), str(config_2)
+    ]):
+        main.cli()
+
+    assert len(smtp_server.messages) == 2
+
+
+def test_success_with_loop(smtp_server, tmp_path):
+
+    config = tmp_path / 'with_vars.yml'
+    config.write_text(WITH_LOOP)
+
+    with mock.patch('sys.argv', [
+        'spool', '--relay', smtp_server.host, '--port', str(smtp_server.port),
+        str(config)
+    ]):
+        main.cli()
+
+    assert len(smtp_server.messages) == 3
+
+
+@pytest.mark.parametrize(
+    'config', EXAMPLE_DIR.glob('*.yml'), ids=lambda f: f.name)
+def test_examples(smtp_server, tmp_path, caplog, config):
+
+    args = ['spool', '--relay', smtp_server.host,
+            '--port', str(smtp_server.port), f'{config}']
+
+    with mock.patch('sys.argv', args):
+        main.cli()
+        for record in caplog.records:
+            assert record.levelname not in ['ERROR', 'CRITICAL']
---
+++ packages/spool/spool-1.0.0.tar.gz/spool-1.0.0/tests/test_mailer.py
@@ -0,0 +1,182 @@
+import logging
+import smtplib
+from unittest.mock import Mock, patch
+
+import dns
+import pytest
+
+from spool.mailer import MAIL_OUT_PREFIX, MAIL_OUT_SUFFIX, Mailer
+from spool.message import Message
+
+
+@pytest.fixture()
+def message():
+    return Message(
+        name='test', sender='sender@example.org',
+        recipients='recipient@example.org, noreply@example.org',
+        # Set Message-Id header to None to prevent a dns lookup
+        headers={'Message-ID': None},
+    )
+
+
+@pytest.fixture()
+def mailer(smtp_server):
+    return Mailer(relay=smtp_server.host,
+                  port=smtp_server.port, helo='mail.example.com')
+
+
+@patch.object(smtplib.SMTP, 'sendmail')
+def test_message_sent(mock_send, mailer, message, caplog):
+
+    caplog.set_level(logging.INFO)
+    mailer.send(message)
+
+    assert len(caplog.record_tuples) == 2
+    _, severity, msg = caplog.record_tuples[0]
+    assert severity == logging.INFO
+    assert 'Connecting to remote server.' in msg
+
+    _, severity, msg = caplog.record_tuples[1]
+    assert severity == logging.INFO
+    assert 'Message sent.' in msg
+
+
+@patch.object(smtplib.SMTP, 'sendmail')
+def test_remote_refused_sender(mock_send, mailer, message, caplog):
+    mock_send.side_effect = smtplib.SMTPSenderRefused(
+        550, b'5.1.0 Address rejected.', 'recipient@example.org')
+    mailer.send(message)
+
+    assert len(caplog.record_tuples) == 1
+    _, severity, msg = caplog.record_tuples[0]
+    assert severity == logging.ERROR
+    assert 'Failed to send message: Sender rejected.' in msg
+
+
+@patch.object(smtplib.SMTP, 'sendmail')
+def test_remote_refused_recipient(mock_send, mailer, message, caplog):
+
+    err_code, err_msg = 550, b'5.1.0 Address rejected.'
+    mock_send.return_value = {message.recipients[1][1]: (err_code, err_msg)}
+    mailer.send(message)
+
+    assert len(caplog.record_tuples) == 1
+    _, severity, msg = caplog.record_tuples[0]
+    assert severity == logging.WARNING
+    assert f'Remote refused recipient: {message.recipients[1][1]}' in msg
+
+
+@patch.object(smtplib.SMTP, 'sendmail')
+def test_remote_refused_all_recipients(mock_send, mailer, message, caplog):
+
+    recipients = [rcpt[1] for rcpt in message.recipients]
+    mock_send.side_effect = smtplib.SMTPRecipientsRefused(recipients)
+    mailer.send(message)
+
+    assert len(caplog.record_tuples) == 1
+    _, severity, msg = caplog.record_tuples[0]
+    assert severity == logging.ERROR
+    assert 'Failed to send message: Remote refused all recipients.' in msg
+
+
+@patch.object(smtplib.SMTP, 'sendmail')
+def test_remote_dropped_connection(mock_send, mailer, message, caplog):
+
+    mock_send.side_effect = smtplib.SMTPServerDisconnected()
+    mailer.send(message)
+
+    assert len(caplog.record_tuples) == 1
+    _, severity, msg = caplog.record_tuples[0]
+    assert severity == logging.ERROR
+    assert 'Failed to send message: Connection closed by remote host.' in msg
+
+
+@pytest.mark.parametrize('error', [
+    (554, b'5.7.1 Spam message rejected', 'permanent'),
+    (451, b'4.7.1 Try again later', 'temporary'),
+], ids=lambda error: error[2])
+@patch.object(smtplib.SMTP, 'sendmail')
+def test_remote_response_error(mock_send, error, mailer, message, caplog):
+
+    mock_send.side_effect = smtplib.SMTPResponseException(error[0], error[1])
+    mailer.send(message)
+
+    assert len(caplog.record_tuples) == 1
+    _, severity, msg = caplog.record_tuples[0]
+    assert severity == logging.ERROR
+    assert f'Error while sending message: {error[0]} - {error[1].decode()}' in msg
+
+
+class MockResourceRecord:
+
+    class DNSName:
+
+        def __init__(self, name):
+            self.name = name
+
+        def to_text(self):
+            return str(self.name)
+
+    def __init__(self, name, preference):
+        self.exchange = self.DNSName(name)
+        self.preference = preference
+
+
+dns_data = [
+    {
+        'domain': 'example.org',
+        'response': [
+            MockResourceRecord('.', 0)
+        ],
+        'expected': '.',
+        'case': 'is-root-server',
+    },
+    {
+        'domain': 'example.org',
+        'response': [
+            MockResourceRecord('mail.example.org.', 0)
+        ],
+        'expected': 'mail.example.org',
+        'case': 'is-non-root-server',
+    },
+    {
+        'domain': 'example.org',
+        'response': [
+            MockResourceRecord('mail1.example.org.', 10),
+            MockResourceRecord('mail2.example.org.', 10),
+        ],
+        'expected': 'mail1.example.org',
+        'case': 'ordered-equal-priority',
+    },
+    {
+        'domain': 'example.org',
+        'response': [
+            MockResourceRecord('mail1.example.org.', 20),
+            MockResourceRecord('mail2.example.org.', 10),
+        ],
+        'expected': 'mail2.example.org',
+        'case': 'has-priority'
+    },
+]
+
+
+@pytest.mark.parametrize('data', dns_data, ids=lambda data: data['case'])
+def test_get_remote(mailer, data):
+    with patch.object(dns.resolver.Resolver, 'query',
+                      return_value=data['response']) as mock_query:
+        assert mailer.get_remote(data['domain']) == data['expected']
+
+
+@patch.object(smtplib.SMTP, 'sendmail')
+def test_dump_to_console(mock_send, mailer, message, capsys):
+
+    mailer.send(message, print_only=True)
+    out, err = capsys.readouterr()
+
+    assert err == ''
+
+    assert out.startswith(MAIL_OUT_PREFIX)
+    assert message.as_string() in out
+    assert out.endswith(MAIL_OUT_SUFFIX + '\n')
+
+    mock_send.assert_not_called()
