d0e1991fcf32af0ec689fd0b526da98443eefbcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:04:54 +0000

    Add package pypowsybl/1.8.1/pypowsybl-1.8.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pypowsybl/pypowsybl-1.8.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pypowsybl/dynamic/impl/event_mapping.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2023, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+from typing import List
+from pypowsybl import _pypowsybl as _pp
+from pypowsybl._pypowsybl import Side
+from .util import EventType
+
+
+class EventMapping:
+    """
+    Class to map events
+    """
+
+    def __init__(self) -> None:
+        self._handle = _pp.create_event_mapping()
+
+    def add_disconnection(self, static_id: str, event_time: float, disconnect_only: Side) -> None:
+        """ Creates a equipment disconnection event
+
+        Args:
+            static_id (str): network element to disconnect
+            event_time (float): timestep at which the event happens
+            disconnect_only (BranchSide): the disconnection is made on the provided side only
+        """
+        _pp.add_event_disconnection(
+            self._handle, static_id, event_time, disconnect_only)
+
+    @staticmethod
+    def get_possible_events() -> List[EventType]:
+        return list(EventType)
