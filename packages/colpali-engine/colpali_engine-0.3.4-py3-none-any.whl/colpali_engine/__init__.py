dcc00f1acd89d5ce67d20b167a99864f67e5b784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 05:04:07 +0000

    Add package colpali-engine/0.3.4/colpali_engine-0.3.4-py3-none-any.whl

---
+++ packages/colpali-engine/colpali_engine-0.3.4-py3-none-any.whl/colpali_engine/__init__.py
@@ -0,0 +1,13 @@
+from .models import (
+    BiIdefics2,
+    BiPali,
+    BiPaliProj,
+    BiQwen2,
+    BiQwen2Processor,
+    ColIdefics2,
+    ColIdefics2Processor,
+    ColPali,
+    ColPaliProcessor,
+    ColQwen2,
+    ColQwen2Processor,
+)
