eca9c3c97a346516c1867fab14698771ec62b51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 03:07:18 +0000

    Add package swak/0.2.4/swak-0.2.4-py3-none-any.whl

---
+++ packages/swak/swak-0.2.4-py3-none-any.whl/swak/dictionary/__init__.py
@@ -0,0 +1,15 @@
+"""Convenient partials of functions for manipulating dictionaries.
+
+Parameters that are known at program start are used to initialize the classes
+so that, at runtime, dictionaries can flow through a preconfigured processing
+pipe of callable objects.
+
+"""
+
+from .valuesgetter import ValuesGetter
+from .nonedropper import NoneDropper
+
+__all__ = [
+    'ValuesGetter',
+    'NoneDropper'
+]
