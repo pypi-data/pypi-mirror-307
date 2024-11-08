dc85f8f6eb744006cf4170789e0775898dd5cd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 05:04:34 +0000

    Add package codenellypdf/1.0.2/codenellypdf-1.0.2.tar.gz

---
+++ packages/codenellypdf/codenellypdf-1.0.2.tar.gz/codenellypdf-1.0.2/codenellypdf/pdf2text.py
@@ -0,0 +1,7 @@
+
+def convert():
+    print("pdf2Text")
+
+
+if __name__ == "__main__":
+    convert()
---
+++ packages/codenellypdf/codenellypdf-1.0.2.tar.gz/codenellypdf-1.0.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="codenellypdf",
+    version="1.0.2" ,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+    
+)
