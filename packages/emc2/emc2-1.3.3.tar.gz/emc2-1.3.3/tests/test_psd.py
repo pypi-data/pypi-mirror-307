36275ee06a3b37297b6038b8696e5f2bf2b2da9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:03:57 +0000

    Add package emc2/1.3.3/emc2-1.3.3.tar.gz

---
+++ packages/emc2/emc2-1.3.3.tar.gz/emc2-1.3.3/tests/test_psd.py
@@ -0,0 +1,20 @@
+import emc2
+import xarray as xr
+import numpy as np
+
+
+def test_lambda_mu():
+    # We have a cloud with a constant N, increasing LWC
+    # Therefore, if dispersion is fixed, slope should decrease with LWC
+    # N_0 will also increases since it is directly proportional to lambda
+
+    my_model = emc2.core.model.TestConvection()
+    my_model = emc2.simulator.psd.calc_mu_lambda(my_model, hyd_type="cl", calc_dispersion=False)
+    my_ds = my_model.ds
+    assert np.all(my_ds["mu"] == 1 / 0.09)
+    diffs = np.diff(my_ds["lambda"])
+    diffs = diffs[np.isfinite(diffs)]
+    assert np.all(diffs < 0)
+    diffs = np.diff(my_ds["N_0"])
+    diffs = diffs[np.isfinite(diffs)]
+    assert np.all(diffs < 0)
