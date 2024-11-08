ea69522abac447f04c4dda4730f3fbc71f6c0f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:04:25 +0000

    Add package mozilla-nimbus-schemas/2024.11.3/mozilla_nimbus_schemas-2024.11.3.tar.gz

---
+++ packages/mozilla-nimbus-schemas/mozilla_nimbus_schemas-2024.11.3.tar.gz/mozilla_nimbus_schemas-2024.11.3/mozilla_nimbus_schemas/experiments/__init__.py
@@ -0,0 +1,11 @@
+from .experiments import DesktopNimbusExperiment, RandomizationUnit, SdkNimbusExperiment
+from .feature_manifests import DesktopFeature, DesktopFeatureManifest, SdkFeatureManifest
+
+__all__ = (
+    "DesktopFeature",
+    "DesktopFeatureManifest",
+    "DesktopNimbusExperiment",
+    "SdkNimbusExperiment",
+    "RandomizationUnit",
+    "SdkFeatureManifest",
+)
