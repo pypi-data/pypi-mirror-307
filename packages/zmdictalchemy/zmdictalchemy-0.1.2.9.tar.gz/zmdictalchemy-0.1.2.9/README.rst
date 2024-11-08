124a0a8cf3a14376c7b7c89949f931ec65793fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:04:42 +0000

    Add package zmdictalchemy/0.1.2.9/zmdictalchemy-0.1.2.9.tar.gz

---
+++ packages/zmdictalchemy/zmdictalchemy-0.1.2.9.tar.gz/zmdictalchemy-0.1.2.9/README.rst
@@ -0,0 +1,19 @@
+Zmdictalchemy
+===========
+
+Zmdictalchemy adds asdict() and fromdict() methods to SQLAlchemy declarative models.
+
+Documentation
+-------------
+
+Documentation can be found at `http://pythonhosted.org/zmdictalchemy/ <http://pythonhosted.org/zmdictalchemy/>`_
+
+Source
+------
+
+The source is hosted at `http://github.com/dafu-wu/zmdictalchemy <http://github.com/dafu-wu/zmdictalchemy>`_.
+
+Licence
+-------
+
+Zmdictalchemy is MIT licenced.
---
+++ packages/zmdictalchemy/zmdictalchemy-0.1.2.9.tar.gz/zmdictalchemy-0.1.2.9/setup.py
@@ -0,0 +1,58 @@
+"""
+~~~~~~~~~~~
+Zmdictalchemy
+~~~~~~~~~~~
+
+Contains asdict() and fromdict() methods that will work on SQLAlchemy
+declarative models.
+
+Read more in the source or on github
+<https://github.com/dafu-wu/zmdictalchemy>.
+"""
+
+import os
+import sys
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+# Requirements for the package
+install_requires = [
+    'SQLAlchemy>=0.9.4',
+]
+
+# Requirement for running tests
+test_requires = install_requires
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+setup(name='zmdictalchemy',
+      version='0.1.2.9',
+      description="Contains asdict and fromdict methods for SQL-Alchemy "
+      "declarative models",
+      long_description=README,
+      long_description_content_type='text/markdown',
+      url='http://github.com/dafu-wu/zmdictalchemy/',
+      license='MIT',
+      author='Dadu Wu',
+      author_email='wucy4328@gmail.com',
+      platforms='any',
+      classifiers=['Development Status :: 4 - Beta',
+                   'License :: OSI Approved :: MIT License',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                   'Topic :: Software Development :: '
+                   'Libraries :: Python Modules'],
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=test_requires,
+      test_suite='zmdictalchemy',
+      **extra)
