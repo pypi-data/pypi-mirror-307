1e920de61aa41e20dd0b7ec69ebc0c7f9b5705a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:04:20 +0000

    Add package group-action/0.2.18/group_action-0.2.18.tar.gz

---
+++ packages/group-action/group_action-0.2.18.tar.gz/group_action-0.2.18/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="group_action",
+	version="0.2.18",
+	author="ant1sir",
+	author_email="antoine@sirianni.ai",
+	description="A Python package for group action.",
+	long_description=open('README.md').read(),
+	long_description_content_type='text/markdown',
+	url="https://github.com/ant1sir/group_action",  # replace with your repo URL if you have one
+	packages=find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.10',
+	install_requires=[
+		'joblib>=1.4.2',
+		'tqdm>=4.66.4'
+	],
+	entry_points={
+		'console_scripts': [
+			'orbit = group_action.orbit:main',
+			'orbit_random = group_action.orbit_random:main',
+			'orbits = group_action.orbits:main',
+			'conjugacy_classes = group_action.conjugacy_classes:main',
+			'burnside = group_action.burnside:main',
+			'powerset = group_action.powerset:main',
+			'symmetric_functions = group_action.symmetric_functions:main',
+		],
+	},
+
+)
+
