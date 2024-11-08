027876e4638400dedc97ab339b1d2fccfb05b396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 14:05:17 +0000

    Add package bespokelabs-curator/0.1.3/bespokelabs_curator-0.1.3-py3-none-any.whl

---
+++ packages/bespokelabs-curator/bespokelabs_curator-0.1.3-py3-none-any.whl/bespokelabs/curator/request_processor/generic_response.py
@@ -0,0 +1,12 @@
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel
+
+
+class GenericResponse(BaseModel):
+    response: Optional[Dict[str, Any]] | str = None
+    request: Optional[Dict[str, Any]] = None
+    errors: Optional[List[str]] = None
+    row: Dict[str, Any]
+    row_idx: int
+    raw_response: Optional[Dict[str, Any]] = None
