9d514a0b63f61e436f2264fa365da68757796e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 14:04:51 +0000

    Add package FD-codepy/0.1.1/fd_codepy-0.1.1.tar.gz

---
+++ packages/FD-codepy/fd_codepy-0.1.1.tar.gz/fd_codepy-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FD_codepy",           # Project name
+    version="0.1.1",                   # Initial version
+    description="Codebook and flexibility distance solutions for energy time series analysis",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Rui Yuan",
+    author_email="123abcyuanrui@gmail.com",
+    url="https://github.com/abc123yuanrui/FD_codepy",  # Optional
+    packages=find_packages(),          # Automatically find packages in subfolders
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=["numpy",
+        "pandas",
+        "plotly",
+        "tslearn",
+        "statsmodels",
+        "stumpy",
+        "numba"
+        ],               # List of dependencies if any
+)
