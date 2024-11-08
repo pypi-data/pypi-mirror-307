8f92fd6c7442608cae56f1e83c0d1a25135d4425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:04:42 +0000

    Add package zmdictalchemy/0.1.2.9/zmdictalchemy-0.1.2.9-py3-none-any.whl

---
+++ packages/zmdictalchemy/zmdictalchemy-0.1.2.9-py3-none-any.whl/zmdictalchemy/__init__.py
@@ -0,0 +1,21 @@
+# vim: set fileencoding=utf-8 :
+"""
+
+~~~~~~~~~~~
+Zmdictalchemy
+~~~~~~~~~~~
+
+"""
+
+
+from zmdictalchemy.classes import DictableModel
+from zmdictalchemy.utils import make_class_dictable, asdict
+from zmdictalchemy.errors import (ZmdictalchemyError, UnsupportedRelationError,
+                                MissingRelationError)
+
+__all__ = [DictableModel,
+           make_class_dictable,
+           asdict,
+           ZmdictalchemyError,
+           UnsupportedRelationError,
+           MissingRelationError]
---
+++ packages/zmdictalchemy/zmdictalchemy-0.1.2.9-py3-none-any.whl/zmdictalchemy/classes.py
@@ -0,0 +1,40 @@
+# vim: set fileencoding=utf-8 :
+"""
+~~~~~~~
+Classes
+~~~~~~~
+
+Contains :class:`DictableModel` that can be used as a base class for
+:meth:`sqlalchemy.ext.declarative_base`.
+
+"""
+
+
+
+from zmdictalchemy import utils
+
+
+class DictableModel(object):
+    """Can be used as a base class for :meth:`sqlalchemy.ext.declarative`
+
+    Contains the methods :meth:`DictableModel.__iter__`,
+    :meth:`DictableModel.asdict` and :meth:`DictableModel.fromdict`.
+
+    :ivar zmdictalchemy_exclude: List of properties that should always be \
+            excluded.
+    :ivar zmdictalchemy_exclude_underscore: If True properties starting with an \
+            underscore will always be excluded.
+    :ivar zmdictalchemy_fromdict_allow_pk: If True the primary key can be \
+            updated by :meth:`DictableModel.fromdict`.
+    :ivar zmdictalchemy_asdict_include: List of properties that should always \
+            be included when calling :meth:`DictableModel.asdict`
+    :ivar zmdictalchemy_fromdict_include: List of properties that should always \
+            be included when calling :meth:`DictableModel.fromdict`
+
+    """
+
+    asdict = utils.asdict
+
+    fromdict = utils.fromdict
+
+    __iter__ = utils.iter
