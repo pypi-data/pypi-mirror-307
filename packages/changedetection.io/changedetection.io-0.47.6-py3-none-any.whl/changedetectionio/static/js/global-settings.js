98fd43d079da1372549bb9c57ea5d199be84c1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:04:36 +0000

    Add package changedetection.io/0.47.6/changedetection.io-0.47.6-py3-none-any.whl

---
+++ packages/changedetection.io/changedetection.io-0.47.6-py3-none-any.whl/changedetectionio/static/js/global-settings.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("#api-key").hover(
+        function () {
+            $("#api-key-copy").html('copy').fadeIn();
+        },
+        function () {
+            $("#api-key-copy").hide();
+        }
+    ).click(function (e) {
+        $("#api-key-copy").html('copied');
+        var range = document.createRange();
+        var n = $("#api-key")[0];
+        range.selectNode(n);
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();
+
+    });
+
+    $(".toggle-show").click(function (e) {
+        e.preventDefault();
+        let target = $(this).data('target');
+        $(target).toggle();
+    });
+
+});
+
