e192233d18ca92184de50997dd9678303d6ee2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:04:20 +0000

    Add package zmdictalchemy/0.1.2.8/zmdictalchemy-0.1.2.8.tar.gz

---
+++ packages/zmdictalchemy/zmdictalchemy-0.1.2.8.tar.gz/zmdictalchemy-0.1.2.8/setup.py
@@ -0,0 +1,57 @@
+"""
+~~~~~~~~~~~
+Dictalchemy
+~~~~~~~~~~~
+
+Contains asdict() and fromdict() methods that will work on SQLAlchemy
+declarative models.
+
+Read more in the source or on github
+<https://github.com/dafu-wu/dictalchemy>.
+"""
+
+import os
+import sys
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+# Requirements for the package
+install_requires = [
+    'SQLAlchemy>=0.9.4',
+]
+
+# Requirement for running tests
+test_requires = install_requires
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+setup(name='zmdictalchemy',
+      version='0.1.2.8',
+      description="Contains asdict and fromdict methods for SQL-Alchemy "
+      "declarative models",
+      long_description=README,
+      url='http://github.com/dafu-wu/dictalchemy/',
+      license='MIT',
+      author='Dadu Wu',
+      author_email='wucy4328@gmail.com',
+      platforms='any',
+      classifiers=['Development Status :: 4 - Beta',
+                   'License :: OSI Approved :: MIT License',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                   'Topic :: Software Development :: '
+                   'Libraries :: Python Modules'],
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=test_requires,
+      test_suite='dictalchemy',
+      **extra)
