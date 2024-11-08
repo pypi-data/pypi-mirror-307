8d7ab9629af371d772733ea5489af5d8fdb7ae4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:03:57 +0000

    Add package tower-cli/0.1.2/tower_cli-0.1.2.tar.gz

---
+++ packages/tower-cli/tower_cli-0.1.2.tar.gz/tower_cli-0.1.2/crates/tower-api/src/lib.rs
@@ -0,0 +1,152 @@
+use serde::{Deserialize, Serialize};
+use url::Url;
+use reqwest::{
+    Client as ReqwestClient,
+    StatusCode,
+    Method,
+};
+use serde_json::Value;
+use std::convert::From;
+use config::Config;
+
+mod types;
+mod error;
+
+// TowerError is the main error type of Tower errors. We export it here for convenience to crate
+// users.
+pub use error::TowerError;
+
+// All types get exported to make our lives easier, too.
+pub use types::*;
+
+#[derive(Serialize, Deserialize)]
+struct LoginRequest {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ListAppsResponse {
+    apps: Vec<AppSummary>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CreateAppRequest {
+    name: String,
+    short_description: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CreateAppResponse {
+    app: App,
+}
+
+#[derive(Serialize, Deserialize)]
+struct DeleteAppResponse {
+    app: App,
+}
+
+pub type Result<T> = std::result::Result<T, TowerError>;
+
+pub struct Client {
+    // domain is the URL that we use to connect to the client.
+    domain: Url,
+
+    // session is the current session that will be used for making subsequent requests.
+    session: Option<Session>,
+}
+
+impl Client {
+    pub fn default() -> Self {
+        Self {
+            domain: Url::parse("https://services.tower.dev").unwrap(),
+            session: None,
+        }
+    }
+
+    pub fn new(domain: Url) -> Self {
+        Self {
+            domain,
+            session: None,
+        }
+    }
+
+    pub fn from_config(config: &Config) -> Self {
+        Self {
+            session: None,
+            domain: Url::parse(&config.tower_url).unwrap(),
+        }
+    }
+
+    pub fn with_optional_session(&self, sess: Option<Session>) -> Self {
+        Self {
+            domain: self.domain.clone(),
+            session: sess,
+        }
+    }
+
+    pub async fn login(&self, username: &str, password: &str) -> Result<Session> {
+        let data = LoginRequest { 
+            username: String::from(username),
+            password: String::from(password),
+        };
+
+        let body = serde_json::to_value(data).unwrap();
+        self.request(Method::POST, "/api/session", Some(body)).await
+    }
+
+    pub async fn list_apps(&self) -> Result<Vec<AppSummary>> {
+        let res = self.request::<ListAppsResponse>(Method::GET, "/api/apps", None).await?;
+        Ok(res.apps)
+    }
+
+    pub async fn delete_app(&self, name: &str) -> Result<App> {
+        let path = format!("/api/apps/{}", name);
+        let res = self.request::<DeleteAppResponse>(Method::DELETE, &path, None).await?;
+        Ok(res.app)
+    }
+
+    pub async fn create_app(&self, name: &str, description: &str) -> Result<App> {
+        let data = CreateAppRequest {
+            name: String::from(name),
+            short_description: String::from(description),
+        };
+
+        let body = serde_json::to_value(data).unwrap();
+        let res = self.request::<CreateAppResponse>(Method::POST, "/api/apps", Some(body)).await?;
+        Ok(res.app)
+    }
+
+    async fn request<T>(&self, method: Method, path: &str, body: Option<Value>) -> Result<T>
+    where
+        T: for<'de> Deserialize<'de>,
+    {
+        let client = ReqwestClient::new();
+        let url = self.url_from_path(path);
+        let mut req = client.request(method, url);
+
+        if let Some(obj) = body {
+            req = req.json(&obj);
+        }
+
+        if let Some(sess) = &self.session {
+            req = req.header("Authorization", format!("Bearer {}", sess.token.jwt))
+        }
+
+        let res = req.send().await?;
+
+        match res.status() {
+            StatusCode::OK | StatusCode::CREATED => {
+                res.json::<T>().await.map_err(Into::into)
+            }
+            _ => {
+                Err(res.json::<TowerError>().await?)
+            }
+        }
+    }
+
+
+    fn url_from_path(&self, path: &str) -> Url {
+        self.domain.join(path).unwrap()
+    }
+}
---
+++ packages/tower-cli/tower_cli-0.1.2.tar.gz/tower_cli-0.1.2/crates/tower-api/src/types.rs
@@ -0,0 +1,46 @@
+use serde::{self, Serialize, Deserialize, Deserializer};
+pub use chrono::{DateTime, Utc};
+
+pub use config::{
+    User,
+    Token,
+    Session,
+};
+
+#[derive(Serialize, Deserialize)]
+pub struct App{
+    pub name: String,
+    pub short_description: String,
+    pub owner: String,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Run {
+    pub number: i32,
+    pub app_name: String,
+    pub status: String,
+    pub created_at: DateTime<Utc>,
+    pub started_at: Option<DateTime<Utc>>,
+    pub ended_at: Option<DateTime<Utc>>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AppSummary {
+    pub app: App,
+
+    #[serde(deserialize_with="parse_nullable_sequence")]
+    pub runs: Vec<Run>,
+}
+
+/// parse_nullable_sequence is a helper function that deserializes a sequence of items that may be
+/// null in the underlying data. This is useful for parsing content coming from the API that may or
+/// may not be null if the resultant data is empty.
+fn parse_nullable_sequence<'de, D, T>(deserializer: D) -> Result<Vec<T>, D::Error>
+where
+    D: Deserializer<'de>,
+    T: Deserialize<'de>,
+{
+    let opt = Option::deserialize(deserializer)?;
+    Ok(opt.unwrap_or_else(Vec::new))
+}
