8d250c09a4e018278737c1ddc9f47e20c96352bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 03:07:28 +0000

    Add package perse/0.1.1/perse-0.1.1.tar.gz

---
+++ packages/perse/perse-0.1.1.tar.gz/perse-0.1.1/perse/core/dataframe.py
@@ -0,0 +1,17 @@
+from .base import BaseDataFrame
+from .sql_operations import SQLDataFrame
+from .plotting import PlottingDataFrame
+from .df_utilities import UtilitiesDataFrame
+from .copy import CopyMixin
+
+class DataFrame(SQLDataFrame, PlottingDataFrame, UtilitiesDataFrame, CopyMixin):
+    def final_init(self, *args, **kwargs):
+        """final_init"""
+        super().__init__(*args, **kwargs)
+        _ = self.get_table_name()
+        return self
+
+
+    def final_copy(self, *args, **kwargs):
+        """final_init"""
+        return self.__class__(dl= kwargs['dl'])
