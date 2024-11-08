f564960cb9cc9dee8ac919143990fc57e8673714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 07:04:58 +0000

    Add package noteflow/0.1.2/noteflow-0.1.2.tar.gz

---
+++ packages/noteflow/noteflow-0.1.2.tar.gz/noteflow-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='noteflow',
+    version='0.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'fastapi',
+        'uvicorn',
+        'markdown-it-py',
+        'requests>=2.31.0',
+        'beautifulsoup4>=4.12.0',
+        'pydantic',
+        'python-multipart',
+        'jinja2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'noteflow=noteflow.noteflow:main',
+        ],
+    },
+    package_data={
+        'noteflow': ['fonts/*', 'static/*'],
+    },
+) 
