17fb469d12470d072a8c8e11fb221bcedf50c1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:04:47 +0000

    Add package doc-master/0.0.1/doc_master-0.0.1.tar.gz

---
+++ packages/doc-master/doc_master-0.0.1.tar.gz/doc_master-0.0.1/doc_master/__init__.py
@@ -0,0 +1,11 @@
+from doc_master.main import (
+    AutoFileReader,
+    read_single_file,
+    read_all_files_in_folder,
+)
+
+__all__ = [
+    "AutoFileReader",
+    "read_single_file",
+    "read_all_files_in_folder",
+]
