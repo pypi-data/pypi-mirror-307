fb49070778fc872385bd46fc6039bd4114f46a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 07:04:30 +0000

    Add package cloud-composer-json-log-formatter/0.1.4/cloud_composer_json_log_formatter-0.1.4.tar.gz

---
+++ packages/cloud-composer-json-log-formatter/cloud_composer_json_log_formatter-0.1.4.tar.gz/cloud_composer_json_log_formatter-0.1.4/cloud_composer_json_log_formatter/__init__.py
@@ -0,0 +1,5 @@
+# cloud_composer_json_log_formatter/__init__.py
+
+from .formatter import JSONFormatter, ComposerFormatter
+
+__all__ = ['JSONFormatter', 'ComposerFormatter']
---
+++ packages/cloud-composer-json-log-formatter/cloud_composer_json_log_formatter-0.1.4.tar.gz/cloud_composer_json_log_formatter-0.1.4/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+# Read the contents of README.md for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cloud-composer-json-log-formatter', 
+    version='0.1.4',
+    author='Anton',
+    #author_email='your.email@example.com',  # Replace with your email
+    description='A JSON formatter for Composer logging.',  # Short description
+    long_description=long_description,  # Long description from README
+    long_description_content_type='text/markdown',
+    #url='https://github.com/yourusername/json-log-formatter',  # Replace with your repo URL
+    packages=find_packages(),  # Automatically find packages in the directory
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Choose your license
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify the Python versions you support
+    install_requires=[],  # List dependencies if any
+)
