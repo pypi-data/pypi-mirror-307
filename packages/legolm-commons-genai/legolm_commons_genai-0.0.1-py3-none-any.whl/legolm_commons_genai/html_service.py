301f643e6fcd277a3542c5382896f28e86beb184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:04:43 +0000

    Add package legolm-commons-genai/0.0.1/legolm_commons_genai-0.0.1-py3-none-any.whl

---
+++ packages/legolm-commons-genai/legolm_commons_genai-0.0.1-py3-none-any.whl/legolm_commons_genai/html_service.py
@@ -0,0 +1,41 @@
+import logging
+import requests
+import random
+
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+]
+
+request_headers = {
+    'referer': 'https://www.google.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'pragma': 'no-cache',
+    'priority': 'u=0, i',
+    'sec-ch-ua': '"Not?A_Brand";v="99", "Chromium";v="130"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+}
+
+class HtmlService:
+    def __init__(self) -> None:
+        self.logger = logging.getLogger(__name__)
+        self.session = requests.Session()
+
+    def get_url(self, url: str):
+        random_ua = random.choice(user_agents)
+        headers = {**request_headers, 'user-agent': random_ua}
+
+        response = self.session.get(url, headers=headers)
+        response.raise_for_status()
+        return response
