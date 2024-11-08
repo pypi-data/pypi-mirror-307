5db831269a5d09bf74b54d4d97d4f0f28ea8f51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 03:07:17 +0000

    Add package xi-mzidentml-converter/0.3.2/xi_mzidentml_converter-0.3.2.tar.gz

---
+++ packages/xi-mzidentml-converter/xi_mzidentml_converter-0.3.2.tar.gz/xi_mzidentml_converter-0.3.2/models/dbsequence.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey, Text, Integer
+from models.base import Base
+
+
+class DBSequence(Base):
+    __tablename__ = "dbsequence"
+    upload_id: Mapped[str] = mapped_column(Integer, ForeignKey("upload.id"), primary_key=True, nullable=False)
+    id: Mapped[str] = mapped_column(Text, primary_key=True, nullable=False)
+    accession: Mapped[str] = mapped_column(Text, nullable=False)
+    name: Mapped[str] = mapped_column(Text, nullable=True)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    sequence: Mapped[str] = mapped_column(Text, nullable=True)
