671291714e48696afba44bad3ad12072188ca6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 15:05:00 +0000

    Add package nca-first-package/0.1.0/nca_first_package-0.1.0.tar.gz

---
+++ packages/nca-first-package/nca_first_package-0.1.0.tar.gz/nca_first_package-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nca_first_package',
+    version='0.1.0',
+    author='Your Name',
+    author_email='edwardaung.nlp@gmail.com',
+    description='First package. just division',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
