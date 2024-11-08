6d6bf85721abc135e09fa805c31b1c53abaf2980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:03:46 +0000

    Add package MarinetteSEFAZ/1.1.0/marinettesefaz-1.1.0.tar.gz

---
+++ packages/MarinetteSEFAZ/marinettesefaz-1.1.0.tar.gz/marinettesefaz-1.1.0/marinetteSEFAZ/__init__.py
@@ -0,0 +1,4 @@
+from .seiRJ import loginSEI,buscarNumeroDocumento,incluirEmBlocoDeAssinatura,incluirDocumentoExterno, escreverAcompanhamentoEspecial,pesquisarProcesso, baixarArquivos, procurarArquivos,obterProcessosDeBloco,buscarProcessoEmBloco,limparAnotacao, escreverAnotacao,buscarInformacaoEmDocumento, incluirDocumento,inserirHyperlinkSEI, incluirProcessoEmBloco,removerProcessoDoBloco,buscarInformacaoEmDocumento
+from .siafeRIO import loginSIAFE
+from .excel import salvarPlanilha
+
