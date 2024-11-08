8164445828064e65bc588e6c265e940a6d14b19f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:04:17 +0000

    Add package ailite/0.5.0/ailite-0.5.0-py3-none-any.whl

---
+++ packages/ailite/ailite-0.5.0-py3-none-any.whl/ailite/__init__.py
@@ -0,0 +1,9 @@
+from ._model._api import HUGPiLLM,HUGPIClient
+from src.ailite.main._ailite import ai,ClaudeEngine
+
+__all__ = [
+    'ai',
+    'HUGPIClient',
+    'HUGPiLLM',
+    'ClaudeEngine'
+]
