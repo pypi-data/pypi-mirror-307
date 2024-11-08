4b93d455b2bd2a6cb6efb18d4720943f2626fdb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 14:05:03 +0000

    Add package blue-geo/4.736.1/blue_geo-4.736.1.tar.gz

---
+++ packages/blue-geo/blue_geo-4.736.1.tar.gz/blue_geo-4.736.1/blue_geo/.abcli/QGIS/expressions.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+function blue_geo_QGIS_expressions() {
+    local task=$1
+
+    if [[ "$task" == pull ]]; then
+        rsync -av \
+            "$abcli_QGIS_path_expressions_git/" \
+            "$abcli_QGIS_path_expressions/"
+        return
+    fi
+
+    if [[ "$task" == push ]]; then
+        local options=$2
+        local do_push=$(abcli_option_int "$options" push 0)
+
+        rsync -av \
+            --exclude='__pycache__' \
+            --exclude='default.py' \
+            --exclude='__init__.py' \
+            "$abcli_QGIS_path_expressions/" \
+            "$abcli_QGIS_path_expressions_git/"
+
+        if [[ "$do_push" == 1 ]]; then
+            abcli_git blue-geo push \
+                "$(python3 -m blue_geo version) QGIS expressions"
+        else
+            abcli_git blue-geo status ~all
+        fi
+
+        return
+    fi
+
+    abcli_log_error "QGIS: expressions: $task: command not found."
+    return 1
+}
