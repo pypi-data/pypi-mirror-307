c584b3b59d672d4eed6dcb21e386581d043a2c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:04:44 +0000

    Add package kpi-formula-jun/0.2.2/kpi_formula_jun-0.2.2.tar.gz

---
+++ packages/kpi-formula-jun/kpi_formula_jun-0.2.2.tar.gz/kpi_formula_jun-0.2.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kpi-formula-jun",
+    version="0.2.2",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'statistics',
+    ],
+    author="Jun Ren",
+    author_email="leoren1314@gmail.com",
+    description="A package for KPI calculations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/kpi-formula",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
