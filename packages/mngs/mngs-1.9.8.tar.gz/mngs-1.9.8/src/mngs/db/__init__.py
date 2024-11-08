2cd38b1dba8e9d46e8396018a64e9ad93212cc45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:03:53 +0000

    Add package mngs/1.9.8/mngs-1.9.8.tar.gz

---
+++ packages/mngs/mngs-1.9.8.tar.gz/mngs-1.9.8/src/mngs/db/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: "2024-10-24 23:06:23 (ywatanabe)"
+# File: 2024-10-15 00:35:40
+
+from ._inspect import inspect, Inspector
+from ._delete_duplicates import delete_duplicates
+from ._BaseSQLiteDB import BaseSQLiteDB
