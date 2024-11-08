d8b43f3cb532c42e2446b9cd3499b585dd9e42f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:04:05 +0000

    Add package kpi-formula-jun/0.2.3/kpi_formula_jun-0.2.3.tar.gz

---
+++ packages/kpi-formula-jun/kpi_formula_jun-0.2.3.tar.gz/kpi_formula_jun-0.2.3/kpi_formula/__init__.py
@@ -0,0 +1,11 @@
+from .core import operations, models
+from .advanced import data_processor, data_validator, kpi_calculator, time_series
+
+__all__ = [
+    'operations',
+    'models',
+    'data_processor',
+    'data_validator',
+    'kpi_calculator',
+    'time_series'
+]
