9260100302a5362958fafe62aa868c6b4c8b0a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:04:52 +0000

    Add package ultrasound-acquisition-configuration/1.0.2/ultrasound_acquisition_configuration-1.0.2-cp310-cp310-manylinux_2_27_x86_64.whl

---
+++ packages/ultrasound-acquisition-configuration/ultrasound_acquisition_configuration-1.0.2-cp310-cp310-manylinux_2_27_x86_64.whl/ultrasound_acquisition_configuration/include/Uac-hotfix_1.0.2_static/uac/uac.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>  // IWYU pragma: keep
+
+namespace uac {
+
+constexpr uint16_t UAC_VERSION_MAJOR = 1;
+constexpr uint16_t UAC_VERSION_MINOR = 0;
+constexpr uint16_t UAC_VERSION_PATCH = 0;
+
+constexpr uint16_t MAX_RECURSION = 100;
+
+}  // namespace uac
