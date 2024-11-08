3ab3bdeae0f05aec3aa0026a7cc485c17bfc98aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 06:03:49 +0000

    Add package neuralneuron/21.5/neuralneuron-21.5.tar.gz

---
+++ packages/neuralneuron/neuralneuron-21.5.tar.gz/neuralneuron-21.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neuralneuron',
+    version='21.5',
+    packages=find_packages(),
+    description='A simple practical library',
+    author='noobstain',
+    author_email='noobstain2@gmail.com',
+    url='https://github.com/sumeet-darekar/ann',
+)
