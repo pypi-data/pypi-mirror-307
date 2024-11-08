b92428d1a4569042f10b0c308a75962882ddcd10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 15:04:55 +0000

    Add package sifty/1.0.5/sifty-1.0.5-py3-none-any.whl

---
+++ packages/sifty/sifty-1.0.5-py3-none-any.whl/sifty/__init__.py
@@ -0,0 +1,7 @@
+"""
+Sifty is a lightweight Python library with a flexible, easy-to-use API for diverse programming tasks. It 
+enables quick, reliable operations, helping streamline workflows and enhance productivity. Perfect 
+for everything from data manipulation to automation.
+"""
+
+from .typing.typing import *
---
+++ packages/sifty/sifty-1.0.5-py3-none-any.whl/sifty/filesystem.pyi
@@ -0,0 +1,9 @@
+from .typing.typing import String
+
+def getfilesize(base_dir: String = ".", filename: String = "") -> int | None:
+    """Get the size of a file in bytes."""
+    ...
+
+def search_files(base_dir, search_term: String) -> list[String]:
+    """Get the size of a file in bytes."""
+    ...
