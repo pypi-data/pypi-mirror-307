b82b0344ba1477c07bb1947a2256017ba0a25df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 03:07:57 +0000

    Add package InfraFair/1.2.0/InfraFair-1.2.0-py2.py3-none-any.whl

---
+++ packages/InfraFair/InfraFair-1.2.0-py2.py3-none-any.whl/InfraFair/__init__.py
@@ -0,0 +1,9 @@
+"""
+InfraFair.
+
+'Fairness in allocating infrastructure cost'
+"""
+
+__version__ = "1.1.0"
+__author__ = 'Mohamed A.Eltahir Elabbas'
+__credits__ = 'Universidad Pontificia Comillas'
