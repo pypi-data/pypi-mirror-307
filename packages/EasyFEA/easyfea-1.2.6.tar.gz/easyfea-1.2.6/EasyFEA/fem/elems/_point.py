488a86ad4df7559007ff7091537541197e2ac481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 05:04:50 +0000

    Add package EasyFEA/1.2.6/easyfea-1.2.6.tar.gz

---
+++ packages/EasyFEA/easyfea-1.2.6.tar.gz/easyfea-1.2.6/EasyFEA/fem/elems/_point.py
@@ -0,0 +1,42 @@
+# Copyright (C) 2021-2024 Université Gustave Eiffel.
+# This file is part of the EasyFEA project.
+# EasyFEA is distributed under the terms of the GNU General Public License v3 or later, see LICENSE.txt and CREDITS.md for more information.
+
+"""Point element module."""
+
+import numpy as np
+
+from .._group_elems import _GroupElem
+
+class POINT(_GroupElem):
+    
+    def __init__(self, gmshId: int, connect: np.ndarray, coordoGlob: np.ndarray, nodes: np.ndarray):
+
+        super().__init__(gmshId, connect, coordoGlob, nodes)
+
+    @property
+    def origin(self) -> list[int]:
+        return super().origin
+
+    @property
+    def triangles(self) -> list[int]:
+        return super().triangles
+
+    @property
+    def faces(self) -> list[int]:
+        return [0]
+
+    def _Ntild(self) -> np.ndarray:
+        pass
+
+    def _dNtild(self) -> np.ndarray:
+        pass
+
+    def _ddNtild(self) -> np.ndarray:
+        pass
+    
+    def _dddNtild(self) -> np.ndarray:
+        pass
+
+    def _ddddNtild(self) -> np.ndarray:
+        pass
