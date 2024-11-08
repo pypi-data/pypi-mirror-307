5ef90a30535f51fcef8d32ec7d0fa3da700d0c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 16:04:53 +0000

    Add package torch-npu/2.5.1rc1/torch_npu-2.5.1rc1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torch-npu/torch_npu-2.5.1rc1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torch_npu/utils/utils.py
@@ -0,0 +1,51 @@
+import os
+import time
+import warnings
+from warnings import _showwarnmsg_impl
+
+__all__ = ["print_info_log", "print_warn_log", "print_error_log", "should_print_warning"]
+
+
+class _LogLevel:
+    ERROR = "ERROR"
+    WARNING = "WARNING"
+    INFO = "INFO"
+
+
+def _print_log(level: str, msg: str):
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(time.time())))
+    pid = os.getpid()
+    print(f"{current_time}({pid})-[{level}] {msg}")
+
+
+def print_info_log(info_msg: str):
+    _print_log(_LogLevel.INFO, info_msg)
+
+
+def print_warn_log(warn_msg: str):
+    _print_log(_LogLevel.WARNING, warn_msg)
+
+
+def print_error_log(error_msg: str):
+    _print_log(_LogLevel.ERROR, error_msg)
+
+
+def should_print_warning():
+    disabled_warning = os.environ.get("TORCH_NPU_DISABLED_WARNING", "0")
+    if disabled_warning == "1":
+        return False
+    rank = os.environ.get("RANK", None)
+    if rank is None or rank == "0":
+        return True
+    return False
+
+
+def _apply_npu_show_warning():
+    def npu_show_warning(message, category, filename, lineno, file=None, line=None):
+        npu_path = os.path.dirname(os.path.dirname(__file__))
+        if not should_print_warning() and npu_path in filename:
+            return
+        msg = warnings.WarningMessage(message, category, filename, lineno, file, line)
+        _showwarnmsg_impl(msg)
+
+    warnings.showwarning = npu_show_warning
