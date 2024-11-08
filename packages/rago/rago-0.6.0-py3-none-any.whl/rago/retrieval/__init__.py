54809efdc50a3cc355be2ae9e76989179e65aaac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 07:05:01 +0000

    Add package rago/0.6.0/rago-0.6.0-py3-none-any.whl

---
+++ packages/rago/rago-0.6.0-py3-none-any.whl/rago/retrieval/__init__.py
@@ -0,0 +1,10 @@
+"""RAG Retrieval package."""
+
+from __future__ import annotations
+
+from rago.retrieval.base import RetrievalBase, StringRet
+
+__all__ = [
+    'RetrievalBase',
+    'StringRet',
+]
