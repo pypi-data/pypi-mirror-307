67983ee57fdd65cfb62c29fc8ac19d218f5c9fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:04:27 +0000

    Add package hats/0.4.3/hats-0.4.3-py3-none-any.whl

---
+++ packages/hats/hats-0.4.3-py3-none-any.whl/hats/pixel_math/healpix_shim.py
@@ -0,0 +1,213 @@
+from __future__ import annotations
+
+import healpy as hp
+import numpy as np
+
+# pylint: disable=missing-function-docstring
+
+## Arithmetic conversions
+
+
+def nside2order(param):
+    return hp.nside2order(param)
+
+
+def order2nside(param):
+    return hp.order2nside(param)
+
+
+def npix2nside(param):
+    return hp.npix2nside(param)
+
+
+def nside2npix(param):
+    return hp.nside2npix(param)
+
+
+def order2npix(param):
+    return hp.order2npix(param)
+
+
+def npix2order(param):
+    return hp.npix2order(param)
+
+
+def nside2resol(*args, **kwargs):
+    return hp.nside2resol(*args, **kwargs)
+
+
+def nside2pixarea(*args, **kwargs):
+    return hp.nside2pixarea(*args, **kwargs)
+
+
+def ang2pix(*args, **kwargs):
+    return hp.ang2pix(*args, **kwargs)
+
+
+def ring2nest(*args, **kwargs):
+    return hp.ring2nest(*args, **kwargs)
+
+
+def unseen_pixel():
+    return hp.pixelfunc.UNSEEN
+
+
+## Query
+
+
+def query_strip(*args, **kwargs):
+    return hp.query_strip(*args, **kwargs)
+
+
+def query_polygon(*args, **kwargs):
+    return hp.query_polygon(*args, **kwargs)
+
+
+def boundaries(*args, **kwargs):
+    return hp.boundaries(*args, **kwargs)
+
+
+def get_all_neighbours(*args, **kwargs):
+    return hp.get_all_neighbours(*args, **kwargs)
+
+
+## Coordinate conversion
+
+
+def ang2vec(*args, **kwargs):
+    return hp.ang2vec(*args, **kwargs)
+
+
+def pix2ang(*args, **kwargs):
+    return hp.pix2ang(*args, **kwargs)
+
+
+def vec2dir(*args, **kwargs):
+    return hp.vec2dir(*args, **kwargs)
+
+
+def pix2xyf(*args, **kwargs):
+    return hp.pix2xyf(*args, **kwargs)
+
+
+## FITS
+def read_map(*args, **kwargs):
+    return hp.read_map(*args, **kwargs)
+
+
+def write_map(*args, **kwargs):
+    return hp.write_map(*args, **kwargs)
+
+
+## Custom functions
+
+
+def avgsize2mindist(avg_size: np.ndarray) -> np.ndarray:
+    """Get the minimum distance between pixels for a given average size
+
+    Average size is a "resolution" in healpy terms, i.e. given by
+    `healpy.nside2resol`.
+
+    We don't have the precise geometry of the healpix grid yet,
+    so we are using average_size / mininimum_distance = 1.6
+    as a rough estimate.
+
+    Parameters
+    ----------
+    avg_size : np.ndarray of float
+        The average size of a healpix pixel
+
+    Returns
+    -------
+    np.ndarray of float
+        The minimum distance between pixels for the given average size
+    """
+    return avg_size / 1.6
+
+
+def mindist2avgsize(mindist: np.ndarray) -> np.ndarray:
+    """Get the average size for a given minimum distance between pixels
+
+    Average size is a "resolution" in healpy terms, i.e. given by
+    `healpy.nside2resol`.
+
+    We don't have the precise geometry of the healpix grid yet,
+    so we are using average_size / mininimum_distance = 1.6
+    as a rough estimate.
+
+    Parameters
+    ----------
+    mindist : np.ndarray of float
+        The minimum distance between pixels
+
+    Returns
+    -------
+    np.ndarray of float
+        The average size of a healpix pixel for the given minimum distance
+        between pixels.
+    """
+    return mindist * 1.6
+
+
+def avgsize2order(avg_size_arcmin: np.ndarray) -> np.ndarray:
+    """Get the largest order with average healpix size larger than avg_size_arcmin
+
+    "Average" size is healpy's "resolution", so this function is
+    reverse to healpy.nside2resol(healpy.order2nside(order))..
+
+    Parameters
+    ----------
+    avg_size_arcmin : np.ndarray of float
+        The average size of a healpix pixel in arcminutes
+
+    Returns
+    -------
+    np.ndarray of int
+        The largest healpix order for which the average size is larger than avg_size_arcmin
+    """
+    # resolution = sqrt(4pi / (12 * 4**order)) => order = log2(sqrt(4pi / 12) / resolution)
+    order_float = np.log2(np.sqrt(np.pi / 3) / np.radians(avg_size_arcmin / 60.0))
+    return np.array(order_float, dtype=int)
+
+
+def margin2order(margin_thr_arcmin: np.ndarray) -> np.ndarray:
+    """Get the largest order for which distance between pixels is less than margin_thr_arcmin
+
+    We don't have the precise geometry of the healpix grid yet,
+    we are using average_size / mininimum_distance = 1.6
+    as a rough estimate.
+
+    Parameters
+    ----------
+    margin_thr_arcmin : np.ndarray of float
+        The minimum distance between pixels in arcminutes
+
+    Returns
+    -------
+    np.ndarray of int
+        The largest healpix order for which the distance between pixels is less than margin_thr_arcmin
+    """
+    avg_size_arcmin = mindist2avgsize(margin_thr_arcmin)
+    return avgsize2order(avg_size_arcmin)
+
+
+def order2mindist(order: np.ndarray | int) -> np.ndarray | float:
+    """Get the estimated minimum distance between pixels at a given order.
+
+    We don't have the precise geometry of the healpix grid yet,
+    we are using average_size / mininimum_distance = 1.6
+    as a rough estimate.
+
+    Parameters
+    ----------
+    order : np.ndarray of int or a single scalar int
+        The healpix order
+
+    Returns
+    -------
+    np.ndarray of float or a single scalar float
+        The minimum distance between pixels in arcminutes
+    """
+    pixel_nside = order2nside(order)
+    pixel_avgsize = nside2resol(pixel_nside, arcmin=True)
+    return avgsize2mindist(pixel_avgsize)
---
+++ packages/hats/hats-0.4.3-py3-none-any.whl/hats/pixel_math/validators.py
@@ -0,0 +1,113 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Tuple
+
+import numpy as np
+
+
+class ValidatorsErrors(str, Enum):
+    """Error messages for the coordinate validators"""
+
+    INVALID_DEC = "declination must be in the -90.0 to 90.0 degree range"
+    INVALID_RADIUS = "cone radius must be positive"
+    INVALID_NUM_VERTICES = "polygon must contain a minimum of 3 vertices"
+    DUPLICATE_VERTICES = "polygon has duplicated vertices"
+    DEGENERATE_POLYGON = "polygon is degenerate"
+    INVALID_RADEC_RANGE = "invalid ra or dec range"
+
+
+def validate_radius(radius_arcsec: float):
+    """Validates that a cone search radius is positive
+
+    Args:
+        radius_arcsec (float): The cone radius, in arcseconds
+
+    Raises:
+        ValueError: if radius is non-positive
+    """
+    if radius_arcsec <= 0:
+        raise ValueError(ValidatorsErrors.INVALID_RADIUS.value)
+
+
+def validate_declination_values(dec: float | List[float]):
+    """Validates that declination values are in the [-90,90] degree range
+
+    Args:
+        dec (float | List[float]): The declination values to be validated
+
+    Raises:
+        ValueError: if declination values are not in the [-90,90] degree range
+    """
+    dec_values = np.array(dec)
+    lower_bound, upper_bound = -90.0, 90.0
+    if not np.all((dec_values >= lower_bound) & (dec_values <= upper_bound)):
+        raise ValueError(ValidatorsErrors.INVALID_DEC.value)
+
+
+def validate_polygon(vertices: np.ndarray):
+    """Checks if the polygon contain a minimum of three vertices, that they are
+    unique and that the polygon does not fall on a great circle.
+
+    Args:
+        vertices (np.ndarray): The polygon vertices, in cartesian coordinates
+
+    Raises:
+        ValueError: exception if the polygon is invalid.
+    """
+    if len(vertices) < 3:
+        raise ValueError(ValidatorsErrors.INVALID_NUM_VERTICES.value)
+    if len(vertices) != len(np.unique(vertices, axis=0)):
+        raise ValueError(ValidatorsErrors.DUPLICATE_VERTICES.value)
+    if is_polygon_degenerate(vertices):
+        raise ValueError(ValidatorsErrors.DEGENERATE_POLYGON.value)
+
+
+def is_polygon_degenerate(vertices: np.ndarray) -> bool:
+    """Checks if all the vertices of the polygon are contained in a same plane.
+    If the plane intersects the center of the sphere, the polygon is degenerate.
+
+    Args:
+        vertices (np.ndarray): The polygon vertices, in cartesian coordinates
+
+    Returns:
+        A boolean, which is True if the polygon is degenerate, i.e. if it falls
+        on a great circle, False otherwise.
+    """
+    # Calculate the normal vector of the plane using three of the vertices
+    normal_vector = np.cross(vertices[1] - vertices[0], vertices[2] - vertices[0])
+
+    # Check if the other vertices lie on the same plane
+    for vertex in vertices[3:]:
+        dot_product = np.dot(normal_vector, vertex - vertices[0])
+        if not np.isclose(dot_product, 0):
+            return False
+
+    # Check if the plane intersects the sphere's center. If it does,
+    # the polygon is degenerate and therefore, invalid.
+    center_distance = np.dot(normal_vector, vertices[0])
+    return bool(np.isclose(center_distance, 0))
+
+
+def validate_box_search(ra: Tuple[float, float] | None, dec: Tuple[float, float] | None):
+    """Checks if ra and dec values are valid for the box search.
+
+    - At least one range of ra or dec must have been provided
+    - Ranges must be pairs of non-duplicate minimum and maximum values, in degrees
+    - Declination values, if existing, must be in ascending order
+    - Declination values, if existing, must be in the [-90,90] degree range
+
+    Args:
+        ra (Tuple[float, float]): Right ascension range, in degrees
+        dec (Tuple[float, float]): Declination range, in degrees
+    """
+    invalid_range = False
+    if ra is not None:
+        if len(ra) != 2 or len(ra) != len(set(ra)):
+            invalid_range = True
+    if dec is not None:
+        if len(dec) != 2 or dec[0] >= dec[1]:
+            invalid_range = True
+        validate_declination_values(list(dec))
+    if (ra is None and dec is None) or invalid_range:
+        raise ValueError(ValidatorsErrors.INVALID_RADEC_RANGE.value)
