a091fe53d1c7f9f960433335d393627422515383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 05:04:38 +0000

    Add package auth-middleware/0.1.23/auth_middleware-0.1.23.tar.gz

---
+++ packages/auth-middleware/auth_middleware-0.1.23.tar.gz/auth_middleware-0.1.23/src/auth_middleware/functions.py
@@ -0,0 +1,56 @@
+from typing import List
+
+from fastapi import HTTPException, Request
+
+from auth_middleware.group_checker import GroupChecker
+from auth_middleware.settings import settings
+
+
+def require_groups(allowed_groups: List[str]):
+    """Check if the user has the required groups
+
+    Args:
+        allowed_groups (List[str]): a list of required groups
+    """
+
+    def _group_checker(request: Request):
+        """Calls the GroupChecker class to check if
+        the user has the required groups
+
+        Args:
+            request (Request): FastAPI request object
+
+        Returns:
+            GroupChecker: group checker object
+        """
+        return GroupChecker(allowed_groups)(request)
+
+    return _group_checker
+
+
+def require_user():
+    """Check if the user is authenticated"""
+
+    def _user_checker(request: Request):
+
+        if settings.AUTH_MIDDLEWARE_DISABLED:
+            return
+
+        if not hasattr(request.state, "current_user") or not request.state.current_user:
+            raise HTTPException(status_code=401, detail="Authentication required")
+
+    return _user_checker
+
+
+def get_current_user():
+    """Returns the current user object if it exists"""
+
+    def _get_user(request: Request):
+
+        return (
+            request.state.current_user
+            if hasattr(request.state, "current_user")
+            else None
+        )
+
+    return _get_user
