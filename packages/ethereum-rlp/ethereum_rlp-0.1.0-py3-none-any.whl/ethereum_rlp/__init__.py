128d1802eb273bce741d1f868cc80996520292c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 07:04:29 +0000

    Add package ethereum-rlp/0.1.0/ethereum_rlp-0.1.0-py3-none-any.whl

---
+++ packages/ethereum-rlp/ethereum_rlp-0.1.0-py3-none-any.whl/ethereum_rlp/__init__.py
@@ -0,0 +1,7 @@
+"""
+Defines the serialization and deserialization format used throughout Ethereum.
+"""
+
+from .rlp import RLP, Extended, Simple, decode, decode_to, encode  # noqa: F401
+
+__version__ = "0.1.0"
