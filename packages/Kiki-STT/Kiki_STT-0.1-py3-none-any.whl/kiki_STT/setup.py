06be308e7f31dedc89c51441b3835ac78a7c60ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-08 04:03:52 +0000

    Add package Kiki-STT/0.1/Kiki_STT-0.1-py3-none-any.whl

---
+++ packages/Kiki-STT/Kiki_STT-0.1-py3-none-any.whl/kiki_STT/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Kiki-STT',
+    version='0.1',
+    author='Muzamil Kiki',
+    author_email='kingmuzamil156@gmail.com',
+    description='I am making a bot for trading community created by Muzamil',
+)
+packages = find_packages(),
+install_requirment = [
+    'selenium',
+    'webdriver_manager'
+]
