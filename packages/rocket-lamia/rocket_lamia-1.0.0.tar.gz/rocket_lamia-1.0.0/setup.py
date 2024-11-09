f2b571657b28394db58a263d1478f485685e7fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:04:42 +0000

    Add package rocket-lamia/1.0.0/rocket_lamia-1.0.0.tar.gz

---
+++ packages/rocket-lamia/rocket_lamia-1.0.0.tar.gz/rocket_lamia-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='rocket_lamia',
+   version='1.0.0',
+   author='lamia',
+   author_email='l.abdelmalek@esi-sba.dz',
+   packages=['rocket_lamia'],
+   url='http://pypi.python.org/pypi/rocket_lamia/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['rocket_lamia']
+)
