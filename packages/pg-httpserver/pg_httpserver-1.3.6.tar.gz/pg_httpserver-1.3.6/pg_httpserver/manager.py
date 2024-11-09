9a7b648494a7f97ed2c74f6b500c087a3da92dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:05:24 +0000

    Add package pg-httpserver/1.3.6/pg_httpserver-1.3.6.tar.gz

---
+++ packages/pg-httpserver/pg_httpserver-1.3.6.tar.gz/pg_httpserver-1.3.6/pg_httpserver/manager.py
@@ -0,0 +1,79 @@
+from pg_common import SingletonBase, log_error, log_info
+from pg_environment import config
+from pg_redis import RedisManager
+from .define import ExCodeCfg
+import json
+
+
+__all__ = ("GameConfigManager", "GamePropertyManager")
+
+
+GAME_CONFIG_REDIS_KEY = "__GAME_CONFIG__"
+GAME_PROPERTY_REDIS_KEY = "__GAME_PROPERTY__"
+
+
+class _GamePropertyManager(SingletonBase):
+    def __init__(self):
+        self._cfg: dict[str, dict] = {}
+
+    async def reload(self):
+        _r = await RedisManager.get_redis()
+        if _r:
+            _prop = await _r.get(GAME_PROPERTY_REDIS_KEY)
+            if _prop:
+                self._cfg = json.loads(_prop)
+                log_info("load game property success.")
+            else:
+                log_error(f"!!!can not get key {GAME_PROPERTY_REDIS_KEY} in redis.")
+        else:
+            log_error("!!!!can not get redis client.")
+
+    def get_config(self):
+        if self._cfg:
+            return self._cfg
+        else:
+            return config.get_conf("game_property", {})
+
+
+class _GameExCodeManager(SingletonBase):
+    def __init__(self):
+        self._ex_cfg:dict[int, ExCodeCfg] = {}
+
+    def reload(self, data:[]):
+        for d in data:
+            self._ex_cfg[d['id']] = ExCodeCfg(**d)
+
+    def get_by_id(self, _id)->ExCodeCfg:
+        return self._ex_cfg[_id]
+
+class _GameConfigManager(SingletonBase):
+    def __init__(self):
+        self._cfg: dict[str, dict] = {}
+
+    async def reload(self):
+        _r = await RedisManager.get_redis()
+        if _r:
+            _games = await _r.smembers(GAME_CONFIG_REDIS_KEY)
+            for _g in _games:
+                _json = await _r.get("%s:%s" % (GAME_CONFIG_REDIS_KEY, _g))
+                if _json:
+                    self._cfg[_g] = json.loads(_json)
+                    log_info(f"===[{_g}]:[{self._cfg[_g]['version']}]===")
+
+                    if "excodes" in self._cfg[_g]:
+                        GameExCodeManager.reload(self._cfg[_g])
+                    else:
+                        log_info(f"---[{_g}]:has no ex code config---")
+
+        else:
+            log_error("!!!!!!!can not get redis client.")
+
+    def get_config(self, game: str) -> dict:
+        if game in self._cfg:
+            return self._cfg[game]
+        return None
+
+
+GameConfigManager = _GameConfigManager()
+GamePropertyManager = _GamePropertyManager()
+GameExCodeManager = _GameExCodeManager()
