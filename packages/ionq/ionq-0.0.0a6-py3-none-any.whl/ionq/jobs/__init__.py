d1f78684291c6f91cd1eabd98b25b0da2c78fdb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 07:04:51 +0000

    Add package ionq/0.0.0a6/ionq-0.0.0a6-py3-none-any.whl

---
+++ packages/ionq/ionq-0.0.0a6-py3-none-any.whl/ionq/jobs/__init__.py
@@ -0,0 +1,8 @@
+from .jobs import (
+    Circuit,
+    QuantumFunction,
+    Optimization,
+    OptimizationMethod,
+)  # todo specific classes to export
+
+__all__ = ["Circuit", "QuantumFunction", "Optimization", "OptimizationMethod"]
