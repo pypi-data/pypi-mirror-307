03945ce780cc5e12fb43ba223750e80d194ab1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:05:32 +0000

    Add package pbatoolkit-gpu/0.0.3/pbatoolkit_gpu-0.0.3-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pbatoolkit-gpu/pbatoolkit_gpu-0.0.3-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/lib64/cmake/PhysicsBasedAnimationToolkit/PhysicsBasedAnimationToolkit_Targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "PhysicsBasedAnimationToolkit::PhysicsBasedAnimationToolkit" for configuration "Release"
+set_property(TARGET PhysicsBasedAnimationToolkit::PhysicsBasedAnimationToolkit APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(PhysicsBasedAnimationToolkit::PhysicsBasedAnimationToolkit PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CUDA;CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libPhysicsBasedAnimationToolkit.a"
+  )
+
+list(APPEND _cmake_import_check_targets PhysicsBasedAnimationToolkit::PhysicsBasedAnimationToolkit )
+list(APPEND _cmake_import_check_files_for_PhysicsBasedAnimationToolkit::PhysicsBasedAnimationToolkit "${_IMPORT_PREFIX}/lib64/libPhysicsBasedAnimationToolkit.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
