b937251ffebd7f90e034d1ef80aed6998a4efffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 06:05:11 +0000

    Add package yta-multimedia/0.0.168/yta-multimedia-0.0.168.tar.gz

---
+++ packages/yta-multimedia/yta-multimedia-0.0.168.tar.gz/yta-multimedia-0.0.168/yta_multimedia/video/edition/effect/zoom/linear_zoom_out_video_effect.py
@@ -0,0 +1,43 @@
+from yta_multimedia.video.edition.effect.video_effect import VideoEffect
+from yta_multimedia.video.edition.effect.utils.resize_t_functions import linear_zoom_out_t_func
+from moviepy.editor import CompositeVideoClip, VideoFileClip, ImageClip, VideoClip, ColorClip
+from typing import Union
+
+
+class LinearZoomOutVideoEffect(VideoEffect):
+    """
+    Creates a linear Zoom out effect in the provided video.
+    """
+    
+    @classmethod
+    def apply(cls, video: Union[str, VideoFileClip, VideoClip, CompositeVideoClip, ImageClip, ColorClip], zoom_ratio = None):
+        """
+        Apply the effect on the provided 'video'.
+
+        :param float zoom_ratio: The zoom out movement effect
+        to apply. A value of 0.2 will be a 20% of zoom out.
+        """
+        video = VideoEffect.parse_moviepy_video(video)
+        # We reset the mask to avoid problems with zoom
+        video = video.add_mask()
+
+        if zoom_ratio is None:
+            zoom_ratio = 0.2
+
+        fps = video.fps
+        duration = video.duration
+        screensize = video.size
+
+        effected_video = (
+            video
+            .resize(screensize)
+            .resize(lambda t: linear_zoom_out_t_func(t, duration, zoom_ratio))
+            .set_position(('center', 'center'))
+            .set_duration(duration)
+            .set_fps(fps)
+        )
+
+        return CompositeVideoClip([effected_video], size = screensize)
+
+
+    
