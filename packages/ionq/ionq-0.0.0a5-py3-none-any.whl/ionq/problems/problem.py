f185ce65ef782e5eeb2ba70431513c75ae8475dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 07:04:45 +0000

    Add package ionq/0.0.0a5/ionq-0.0.0a5-py3-none-any.whl

---
+++ packages/ionq/ionq-0.0.0a5-py3-none-any.whl/ionq/problems/problem.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from abc import ABC, abstractmethod
+
+from qiskit import QuantumCircuit
+from ionq.jobs import QuantumFunction
+
+
+class Problem(ABC):
+    """
+    An abstract class to model an optimization problem.
+
+    The constructor, defined in a child class, ingests problem instance data.
+    Then the class's main method, :meth:`.quantum_function_objective`
+    constructs the problem's objective function as a :class:`.QuantumFunction`
+    object that can be passed to a hybrid solver for optimization.
+    """
+
+    @abstractmethod
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def quantum_function_objective(
+        self,
+        ansatz: Optional[QuantumCircuit] = None,
+        **kwargs,
+    ) -> QuantumFunction:
+        pass
