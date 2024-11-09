4adabf0a820c3626ef5e3701e11d9801acf53f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:27 +0000

    Add package ultimate-rvc/0.1.18/ultimate_rvc-0.1.18.tar.gz

---
+++ packages/ultimate-rvc/ultimate_rvc-0.1.18.tar.gz/ultimate_rvc-0.1.18/src/init.py
@@ -0,0 +1,6 @@
+from ultimate_rvc.core import initialize_models
+from ultimate_rvc.core.generate_song_cover import initialize_audio_separator
+
+if __name__ == "__main__":
+    initialize_models()
+    initialize_audio_separator()
