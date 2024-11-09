9d4b35dc85211429f2233add9c72ef9d7fc04f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 07:04:50 +0000

    Add package WITecSDK/1.4.1/WITecSDK-1.4.1-py3-none-any.whl

---
+++ packages/WITecSDK/WITecSDK-1.4.1-py3-none-any.whl/WITecSDK/Modules/StateManager.py
@@ -0,0 +1,21 @@
+﻿from typing import Callable
+from WITecSDK.Parameters import COMStringParameter, COMTriggerParameter
+
+parampath = "MultiComm|MicroscopeControl|StateManager|"
+
+class StateManager:
+
+    def __init__(self, aGetParameter: Callable):
+        self._stateNameCOM: COMStringParameter = aGetParameter(parampath + "StateName")
+        self._resetAllCOM: COMTriggerParameter = aGetParameter(parampath + "ResetAll")
+
+    @property
+    def State(self) -> str:
+        return self._stateNameCOM.Value
+    
+    @State.setter
+    def State(self, value: str):
+        self._stateNameCOM.Value = value
+
+    def ResetAll(self):
+        self._resetAllCOM.ExecuteTrigger()
