622ccacd994fce38c6f11e27a0add86808acddb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:05:36 +0000

    Add package zipinto/1.0.0/zipinto-1.0.0.tar.gz

---
+++ packages/zipinto/zipinto-1.0.0.tar.gz/zipinto-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='zipinto',
+    version='1.0.0',
+    author='ChewangNeko',
+    author_email='2745283841@qq.com',
+    description='Some operations for zip compression files',
+    py_modules=['zipinto'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
