9a21c96aba523cc9e04aaa59f1ec827c57813dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:04:58 +0000

    Add package py-smtper/1.0.0/py_smtper-1.0.0-py3-none-any.whl

---
+++ packages/py-smtper/py_smtper-1.0.0-py3-none-any.whl/py_smtper/__init__.py
@@ -0,0 +1,11 @@
+"""
+ * py-smtper OSS
+ * author: github.com/alisharify7
+ * email: alisharifyofficial@gmail.com
+ * license: see LICENSE for more details.
+ * Copyright (c) 2024 - ali sharifi
+ * https://github.com/alisharify7/py-smtper
+"""
+
+from .klass import MailManager
+from .utils import create_mail_manager
