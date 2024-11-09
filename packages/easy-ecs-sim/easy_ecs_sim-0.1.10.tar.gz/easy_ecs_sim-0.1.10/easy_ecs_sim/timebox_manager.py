a34db48cb94e480648a494d44df70e163f364fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 05:04:20 +0000

    Add package easy-ecs-sim/0.1.10/easy_ecs_sim-0.1.10.tar.gz

---
+++ packages/easy-ecs-sim/easy_ecs_sim-0.1.10.tar.gz/easy_ecs_sim-0.1.10/easy_ecs_sim/timebox_manager.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, field
+from typing import Type
+
+import time
+
+from easy_ecs_sim.storage.database import Database
+
+
+@dataclass
+class TimeboxManager[T]:
+    signature: Type[T]
+    max_time_per_step: float
+    items: list[T] = field(default_factory=list)
+    dt_bonus: float = 0
+
+    def iter(self, db: Database, dt: float):
+        if len(self.items) == 0:
+            self.items = list(db.get_table(self.signature).iter())
+            self.dt_bonus = 0
+
+        start = time.time()
+        while time.time() - start < self.max_time_per_step:
+            if len(self.items) == 0:
+                break
+            yield self.items.pop()
+        self.dt_bonus += dt
