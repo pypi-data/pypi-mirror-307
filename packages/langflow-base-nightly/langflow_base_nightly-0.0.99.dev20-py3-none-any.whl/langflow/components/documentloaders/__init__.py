c0a8b7b8a5471bf2993b17f2d9d129fad71b3c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:05:37 +0000

    Add package langflow-base-nightly/0.0.99.dev20/langflow_base_nightly-0.0.99.dev20-py3-none-any.whl

---
+++ packages/langflow-base-nightly/langflow_base_nightly-0.0.99.dev20-py3-none-any.whl/langflow/components/documentloaders/__init__.py
@@ -0,0 +1,5 @@
+from .confluence import ConfluenceComponent
+from .git import GitLoaderComponent
+from .unstructured import UnstructuredComponent
+
+__all__ = ["ConfluenceComponent", "GitLoaderComponent", "UnstructuredComponent"]
