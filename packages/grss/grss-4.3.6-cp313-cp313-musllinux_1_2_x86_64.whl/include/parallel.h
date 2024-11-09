2258c298683342f46dbcae4d796423f89da9edc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 05:05:06 +0000

    Add package grss/4.3.6/grss-4.3.6-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/grss/grss-4.3.6-cp313-cp313-musllinux_1_2_x86_64.whl/include/parallel.h
@@ -0,0 +1,15 @@
+#ifndef PARALLEL_H
+#define PARALLEL_H
+
+#include "gr15.h"
+#include <omp.h>
+
+/**
+ * @brief Propagate an array of bodies in parallel via OpenMP, using a reference
+ * simulation as a template.
+ */
+void propSim_parallel_omp(const PropSimulation refSim, const bool isCometary,
+                          const std::vector<std::vector<real> > &allBodies,
+                          const int &maxThreads = 128);
+
+#endif
