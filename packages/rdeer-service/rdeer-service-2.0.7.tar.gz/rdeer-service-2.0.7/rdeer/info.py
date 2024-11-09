e43f009764d2a347c425e31fd13dd897cd5f912d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:04:34 +0000

    Add package rdeer-service/2.0.7/rdeer-service-2.0.7.tar.gz

---
+++ packages/rdeer-service/rdeer-service-2.0.7.tar.gz/rdeer-service-2.0.7/rdeer/info.py
@@ -0,0 +1,6 @@
+APPNAME = "rdeer-service"
+SHORTDESC = "Client-server to handle Reindeer in query mode."
+LICENCE = "GPL3"
+VERSION = "2.0.7"
+AUTHOR = "Benoit Guibert"
+AUTHOR_EMAIL = "benoit.guibert@free.fr"
