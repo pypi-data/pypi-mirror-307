47f48f023c4412e37709eb0340fae621b4dc03cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 06:04:59 +0000

    Add package a-jupyterlab-session/0.1.1/a_jupyterlab_session-0.1.1.tar.gz

---
+++ packages/a-jupyterlab-session/a_jupyterlab_session-0.1.1.tar.gz/a_jupyterlab_session-0.1.1/src/index.ts
@@ -0,0 +1,54 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { PathExt } from '@jupyterlab/coreutils';
+import { Contents } from '@jupyterlab/services';
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * Initialization data for the a-jupyterlab-session extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'a-jupyterlab-session:plugin',
+  autoStart: true,
+  activate: async (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension a-jupyterlab-session is activated!');
+
+    // Generate a '%dd.%mm.%yyyy-%hh:%mm:%ss' timestamp
+    const timestamp = new Date()
+      .toLocaleDateString('en-us', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hourCycle: 'h24'
+      })
+      .replace(/\//g, '.')
+      .replace(', ', '-');
+    const salt = uuidv4().slice(0, 8);
+
+    // Generate a unique session directory name with the current time and a random suffix
+    const session = `${timestamp}-${salt}`;
+
+    // Create the new directory
+    await app.serviceManager.contents
+      .newUntitled({
+        type: 'directory' as Contents.ContentType,
+        path: PathExt.dirname(session)
+      })
+      .then(async directory => {
+        await app.serviceManager.contents.rename(directory.path, session);
+      });
+
+    // Navigate the filebrowser to the new session directory
+    const filePath = PathExt.join('tree/', session);
+    app.commands.execute('filebrowser:open-path', {
+      path: filePath
+    });
+  }
+};
+
+export default plugin;
