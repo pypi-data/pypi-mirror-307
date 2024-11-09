fe53201fb804e1a4a06fc144a81449b8b5655eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:07:16 +0000

    Add package elixir-dns/1.1.0/elixir_dns-1.1.0.tar.gz

---
+++ packages/elixir-dns/elixir_dns-1.1.0.tar.gz/elixir_dns-1.1.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+setup(
+    name='elixir-dns',
+    version='1.1.0',
+    description='Elixir is a fast multi-function DNS Enumeration, Subdomain Enumeration and Attack Surface Mapping tool.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/B0lg0r0v/Elixir',
+    author='B0lg0r0v',
+    author_email='contact@arthurminasyan.com',
+    maintainer='B0lg0r0v',
+    license='MIT',
+    install_requires=[
+        'beautifulsoup4',
+        'dnspython',
+        'requests',
+        'setuptools'
+    ],
+    packages=[
+        'list',
+        'core'
+    ],
+    py_modules=['entry'],
+    package_data={
+        'list': ['subdomains.txt'],
+    },
+    entry_points={
+        'console_scripts': [
+            'elixir-dns=entry:main',
+        ],
+
+    },
+    platforms=['Unix'],    
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    
+    ]   
+
+)
