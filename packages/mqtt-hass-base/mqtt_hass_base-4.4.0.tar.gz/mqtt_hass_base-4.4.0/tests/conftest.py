7721c7a7b5baf0794cb1fd677eecefa27c328208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:39 +0000

    Add package mqtt-hass-base/4.4.0/mqtt_hass_base-4.4.0.tar.gz

---
+++ packages/mqtt-hass-base/mqtt_hass_base-4.4.0.tar.gz/mqtt_hass_base-4.4.0/tests/conftest.py
@@ -0,0 +1,14 @@
+"""Fixture for tests."""
+
+import asyncio
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def event_loop():  # type: ignore
+    """Clean asyncio loop for each test."""
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
