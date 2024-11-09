93d1e980efb6f4d9978be3252322d32f7c8c7370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:05:28 +0000

    Add package paicorelib/1.3.1/paicorelib-1.3.1.tar.gz

---
+++ packages/paicorelib/paicorelib-1.3.1.tar.gz/paicorelib-1.3.1/README.md
@@ -0,0 +1,24 @@
+<div align="center">
+
+# Library of PAICORE 2.0
+
+</div>
+
+<p align="center">
+    <a href="https://github.com/PAICookers/PAIlib/blob/master/pyproject.toml">
+        <img alt="PyPI - Python Version" src="https://img.shields.io/pypi/pyversions/paicorelib">
+    </a>
+    <a href="https://pypi.org/project/paicorelib/">
+        <img alt="PyPI - Version" src="https://img.shields.io/pypi/v/paicorelib?color=pink">
+    </a>
+    <a href="https://www.codefactor.io/repository/github/PAICookers/PAIlib">
+        <img alt="CodeFactor Grade" src="https://img.shields.io/codefactor/grade/github/PAICookers/PAIlib?color=orange">
+    </a>
+        <a href="https://results.pre-commit.ci/latest/github/PAICookers/PAIlib/master">
+        <img alt="pre-commit.ci status" src="https://results.pre-commit.ci/badge/github/PAICookers/PAIlib/master.svg">
+    </a>
+</p>
+
+[参数术语文档](docs/Table-of-Terms.md)
+
+[Changelog](./CHANGELOG.md)
---
+++ packages/paicorelib/paicorelib-1.3.1.tar.gz/paicorelib-1.3.1/paicorelib/__init__.py
@@ -0,0 +1,30 @@
+from importlib.metadata import version
+
+from .coordinate import *
+from .framelib import *
+
+# In recent versions, `HwConfig` for external packages remain unchanged.
+from .hw_defs import HwParams as HwConfig
+from .ram_model import *
+from .ram_types import LeakComparisonMode as LCM
+from .ram_types import LeakDirectionMode as LDM
+from .ram_types import LeakIntegrationMode as LIM
+from .ram_types import NegativeThresholdMode as NTM
+from .ram_types import ResetMode as RM
+from .ram_types import SynapticIntegrationMode as SIM
+from .ram_types import ThresholdMode as TM
+from .reg_model import *
+from .reg_types import CoreMode, CoreType
+from .reg_types import InputWidthFormatType as InputWidthFormat
+from .reg_types import LCNExtensionType as LCN_EX
+from .reg_types import MaxPoolingEnableType as MaxPoolingEnable
+from .reg_types import SNNModeEnableType as SNNModeEnable
+from .reg_types import SpikeWidthFormatType as SpikeWidthFormat
+from .reg_types import WeightWidthType as WeightWidth
+from .reg_types import get_core_mode
+from .routing_defs import *
+
+try:
+    __version__ = version("paicorelib")
+except Exception:
+    __version__ = None
