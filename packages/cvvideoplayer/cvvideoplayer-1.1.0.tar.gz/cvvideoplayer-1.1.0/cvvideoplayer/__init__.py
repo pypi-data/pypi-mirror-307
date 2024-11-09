b699eac89bb79062e17b796be2f9c7f770d8030b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 06:05:11 +0000

    Add package cvvideoplayer/1.1.0/cvvideoplayer-1.1.0.tar.gz

---
+++ packages/cvvideoplayer/cvvideoplayer-1.1.0.tar.gz/cvvideoplayer-1.1.0/cvvideoplayer/__init__.py
@@ -0,0 +1,6 @@
+from .frame_reader import *
+from .recorder import SimpleRecorder, AbstractRecorder
+from .video_player import VideoPlayer
+from .utils.bbox_utils import Bbox
+from .utils.video_player_utils import KeyFunction
+from .input_management import InputParser, InputHandler
