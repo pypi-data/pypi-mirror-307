dd2f984c55aa67939f4ed926299d8feb436ae619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:47 +0000

    Add package newversion/3.0.0/newversion-3.0.0.tar.gz

---
+++ packages/newversion/newversion-3.0.0.tar.gz/newversion-3.0.0/newversion/constants.py
@@ -0,0 +1,50 @@
+"""
+Constants used in project.
+"""
+
+import enum
+from typing import Final
+
+PACKAGE_NAME: Final = "newversion"
+LOGGER_NAME: Final = "newversion"
+
+
+class VersionParts(enum.Enum):
+    """
+    Utility class with constants for version parts.
+    """
+
+    ALPHA = "alpha"
+    BETA = "beta"
+    RC = "rc"
+    PRE = "pre"
+    POST = "post"
+    DEV = "dev"
+    MAJOR = "major"
+    MINOR = "minor"
+    MICRO = "micro"
+    LOCAL = "local"
+    EPOCH = "epoch"
+    FULL = "full"
+
+
+class Commands(enum.Enum):
+    """
+    CLI commands.
+    """
+
+    LT = "lt"
+    LTE = "lte"
+    GT = "gt"
+    GTE = "gte"
+    EQ = "eq"
+    NE = "ne"
+
+    IS_STABLE = "is_stable"
+    SET = "set"
+    GET = "get"
+    BUMP = "bump"
+    STABLE = "stable"
+    PACKAGE = "package"
+    SET_PACKAGE = "set_package"
+    UNKNOWN = "unknown"
