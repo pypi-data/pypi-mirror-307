c737a9cebe6492e481350642a11095ed48b36dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:27 +0000

    Add package vivado-report-parser/0.5.0/vivado_report_parser-0.5.0-py3-none-any.whl

---
+++ packages/vivado-report-parser/vivado_report_parser-0.5.0-py3-none-any.whl/vivado_report_parser/metadata.py
@@ -0,0 +1,42 @@
+"""Provides functions for parsing the metadata headers of Vivado report files."""
+
+import re
+
+
+def parse_metadata(report_text: str) -> dict:
+    """Parses the metadata section at the top of Vivado report files.
+
+    Args:
+        report_text: The full text of a Vivado report file.
+    Returns:
+        A dictionary whose keys are the metadata properties described in the
+        table at the top of the file. For example,
+
+        ---------------------------------------------
+        | ...              : ...
+        | Date             : Wed Jul 10 12:18:20 2024
+        | ...              : ...
+        ---------------------------------------------
+
+        gets parsed as:
+
+        {
+            'Date': 'Wed Jul 10 12:18:20 2024',
+            ...
+        }
+    """
+    match = re.search(r"-+\r?\n(?P<metadata>(?:\|.*\r?\n)*)-+", report_text)
+    assert match is not None, "regex should match metadata section of report"
+    metadata_text = match["metadata"]
+    lines = metadata_text.strip().split("\n")
+    metadata = {}
+    for line in lines:
+        [key, value] = line.split(":", 1)  # Split at only the first ':'
+        metadata[key.strip(" |")] = value.strip()
+
+    return metadata
+
+
+def get_generating_command(report_text: str) -> str:
+    """Return the Tcl command used to generate the given report."""
+    return parse_metadata(report_text)["Command"].split()[0]
---
+++ packages/vivado-report-parser/vivado_report_parser-0.5.0-py3-none-any.whl/vivado_report_parser/parse_vivado_report.py
@@ -0,0 +1,65 @@
+"""This module provides a function for parsing an arbitrary Vivado report as well as a CLI."""
+
+import json
+import sys
+
+import argparse
+
+from vivado_report_parser.helpers import get_parser
+
+
+def parse_vivado_report(report_text: str) -> dict:
+    """Parses a Vivado report file as a dict.
+
+    Reads the text content of a Vivado report file and selects the appropriate
+    parser from this library. If no parser is available, returns None.
+    Otherwise, returns the result of applying the parser to the report_text.
+
+    Args:
+        report_text: The text content of a Vivado report file to parse.
+    Returns:
+        A dict containing the parsed report according to the specifications of
+        the parser used, or None if no parser is available.
+    Raises:
+        NotImplementedError: If this library does not implement a parser for the
+          kind of report given.
+    """
+    parse_report = get_parser(report_text)
+    return parse_report(report_text)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Parse Vivado report files.")
+    parser.add_argument("file", nargs="?", help="Vivado report file to parse")
+    parser.add_argument("-d", "--dest", help="file to which output should be written")
+
+    args = parser.parse_args()
+
+    if args.file is not None:
+        try:
+            # Read file without converting \r\n to \n
+            with open(args.file, "r", newline="") as f:
+                report_text = f.read()
+        except (FileNotFoundError, IsADirectoryError) as e:
+            print(e)
+            sys.exit(e.errno)
+    else:
+        try:
+            report_text = sys.stdin.read()
+        except KeyboardInterrupt as e:
+            print(e)
+            sys.exit(130)  # See https://tldp.org/LDP/abs/html/exitcodes.html.
+
+    report = parse_vivado_report(report_text)
+
+    if report is None:
+        print("Error: This type of report file is not currently supported.")
+        sys.exit(1)
+
+    output = json.dumps(report)
+
+    if args.dest:
+        with open(args.dest, "w") as f:
+            f.write(output)
+    else:
+        sys.stdout.write(output)
