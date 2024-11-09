98116a24d60e55d6bd08897b068b21570dec849c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:32 +0000

    Add package flowtask/5.5.18/flowtask-5.5.18-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/flowtask/flowtask-5.5.18-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/flowtask/interfaces/stat.py
@@ -0,0 +1,33 @@
+from abc import ABC
+from typing import Optional
+import traceback
+from ..utils.stats import TaskMonitor
+
+
+class StatSupport(ABC):
+    """StatSupport.
+
+    Adding Support for Task Monitor (Statistics Collector.)
+    """
+
+    def __init__(self, stat: Optional[TaskMonitor] = None, *args, **kwargs):
+        # stats object:
+        self._stat_: bool = True
+        if stat:
+            self.stat: Optional[TaskMonitor] = stat
+        else:
+            self.stat = None
+            self._stat_ = False
+        super().__init__(*args, **kwargs)
+
+    def save_traceback(self):
+        try:
+            self.stat.stacktrace(traceback.format_exc())
+        finally:
+            pass
+
+    def add_metric(self, name, value):
+        try:
+            self.stat.add_metric(name, value)
+        except AttributeError:
+            pass
