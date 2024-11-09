019fdc566b171229001b1513db01e939ec7d958c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 05:04:49 +0000

    Add package firebase-main/1.2.1/firebase-main-1.2.1.tar.gz

---
+++ packages/firebase-main/firebase-main-1.2.1.tar.gz/firebase-main-1.2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='firebase-main',
+    version='1.2.1',
+    packages=find_packages(),
+    install_requires=[
+        'colorama',
+        'trackdir',
+    ],
+    entry_points={
+        'console_scripts': [
+            'firebase-main=firebase_main.cli:main',
+        ],
+    },
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    author='MrFidal',
+    author_email='mrfidal@proton.me',
+    url='https://github.com/ByteBreach/firebase-main',
+)
