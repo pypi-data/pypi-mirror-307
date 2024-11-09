d086079afefb36443e88e208456db7de174bab02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:05:10 +0000

    Add package easy-fastapi/0.1.0/easy_fastapi-0.1.0-py3-none-any.whl

---
+++ packages/easy-fastapi/easy_fastapi-0.1.0-py3-none-any.whl/easy_fastapi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .core import execute_from_command_line
+
+
+__version__ = '0.1.0'
+__author__  = 'one-ccs'
+__email__   = 'one-ccs@foxmal.com'
+
+__all__ = ['execute_from_command_line']
