31253f3e8349f1d8bfd3ca057fbc7be1050358b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:31 +0000

    Add package cord64/0.1.4/cord64-0.1.4.tar.gz

---
+++ packages/cord64/cord64-0.1.4.tar.gz/cord64-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+version = '0.1.4'
+setup(
+    author='evergaster',
+    name='cord64',
+    version=version,
+    license='MIT License',
+    description='Convert files to Base64 and vice versa.',
+    long_description=open('README.md').read(),
+    keywords='base64 convert file',
+    long_description_content_type='text/markdown',
+    url='https://github.com/EverGasterXd/cord64',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    	"Development Status :: 4 - Beta",
+    	"Natural Language :: English",
+    	"Operating System :: Microsoft :: Windows :: Windows 10",
+    	"Programming Language :: Python :: 3",
+    	"Programming Language :: Python :: 3.9",
+    	"Topic :: Internet"
+    ]
+
+)
