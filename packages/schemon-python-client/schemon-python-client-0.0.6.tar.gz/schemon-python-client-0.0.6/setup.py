6a85ce2b46a568d775ffe30b5f1417a88e220fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:04:54 +0000

    Add package schemon-python-client/0.0.6/schemon-python-client-0.0.6.tar.gz

---
+++ packages/schemon-python-client/schemon-python-client-0.0.6.tar.gz/schemon-python-client-0.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+VERSION = "0.0.6"
+
+
+setup(
+    name="schemon-python-client",
+    version=VERSION,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    license="Apache License 2.0",
+    license_files=["LICENSE"],  # Specify the license file
+    install_requires=[
+        "pyspark==3.5.0",
+        "cryptography==43.0.3",
+        "delta-spark==3.2.0",
+        "boto3==1.35.18",
+        "mysql-connector-python==9.1.0",
+    ],
+    entry_points={},
+    python_requires=">=3.9",
+    include_package_data=True,  # Include package data specified in MANIFEST.in
+    package_data={},
+    exclude_package_data={},
+)
