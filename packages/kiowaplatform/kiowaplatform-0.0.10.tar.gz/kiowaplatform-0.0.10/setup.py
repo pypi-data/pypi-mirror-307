8f287a14fb145ce284d18f8efc9991e2d3665de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:37 +0000

    Add package kiowaplatform/0.0.10/kiowaplatform-0.0.10.tar.gz

---
+++ packages/kiowaplatform/kiowaplatform-0.0.10.tar.gz/kiowaplatform-0.0.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kiowaplatform',
+    version='0.0.10',
+    packages=find_packages(),
+    description='A collection of helper functions and utilities for the Kiowa Platform.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://your-pypi-url.org/lte-utils',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
