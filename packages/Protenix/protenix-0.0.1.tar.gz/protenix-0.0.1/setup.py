a9a8b6a544b163eb8f5761496ffd9390b605204c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 03:06:32 +0000

    Add package Protenix/0.0.1/protenix-0.0.1.tar.gz

---
+++ packages/Protenix/protenix-0.0.1.tar.gz/protenix-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages  
+  
+setup(
+    name = 'Protenix',  
+    version = '0.0.1',
+    keywords = [],
+    description = '',
+    license = 'Apache 2',
+    url = 'https://ai4s.bytedance.net',
+    author = 'Bytedance Inc.',
+    author_email = 'ai4s-bio@bytedance.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'manylinux1',
+    install_requires = []
+)
