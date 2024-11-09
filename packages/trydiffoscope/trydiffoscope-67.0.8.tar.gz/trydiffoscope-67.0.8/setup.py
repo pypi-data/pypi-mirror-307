0560323e76e8976477262eb8babbfde3954b3cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 06:05:36 +0000

    Add package trydiffoscope/67.0.8/trydiffoscope-67.0.8.tar.gz

---
+++ packages/trydiffoscope/trydiffoscope-67.0.8.tar.gz/trydiffoscope-67.0.8/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='trydiffoscope',
+    version='67.0.8',
+    author="Chris Lamb",
+    author_email="lamby@debian.org",
+    scripts=('trydiffoscope',),
+)
