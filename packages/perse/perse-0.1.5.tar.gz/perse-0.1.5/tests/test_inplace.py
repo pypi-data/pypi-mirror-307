af393dedad3937ac4837b1c082e826d7d3b9656f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 03:07:10 +0000

    Add package perse/0.1.5/perse-0.1.5.tar.gz

---
+++ packages/perse/perse-0.1.5.tar.gz/perse-0.1.5/tests/test_inplace.py
@@ -0,0 +1,30 @@
+import pytest
+import numpy as np
+import polars as pl
+from perse import DataFrame
+
+
+def get_dict():
+    return {'A': range(10), 'B': range(10)}
+
+
+def test_inplace():
+    data = get_dict()
+    df = DataFrame(data)
+    df2 = df.add_column('C', range(10))
+    assert df.shape == (10, 2)
+    assert df2.shape == (10, 3) and 'C' in df2.df.columns
+
+    assert 'C' in df2
+    assert 'C' not in df
+
+
+def test_inplace():
+    data = get_dict()
+    df = DataFrame(data)
+    df2 = df.add_column('C', range(10))
+    dfA = df2.loc[:, ['A']]
+    assert df.shape == (10, 2)
+    assert df2.shape == (10, 3) and 'C' in df2.df.columns
+    assert 'C' in df2
+    assert dfA.shape == (10, 1)
