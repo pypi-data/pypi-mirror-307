db43d3caf4ba9c6a64fe881885417bf202e15f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:05:35 +0000

    Add package abcli/9.407.1/abcli-9.407.1.tar.gz

---
+++ packages/abcli/abcli-9.407.1.tar.gz/abcli-9.407.1/abcli/.abcli/plugins/pypi.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+function abcli_pypi() {
+    local task=$(abcli_unpack_keyword $1 help)
+
+    local options=$2
+    local plugin_name=$(abcli_option "$options" plugin abcli)
+
+    if [[ "$task" == "help" ]]; then
+        abcli_pypi_browse "$1,$2"
+        abcli_pypi_build "$1,$2"
+        abcli_pypi_install "$1,$2"
+        return
+    fi
+
+    local function_name=abcli_pypi_$task
+    if [[ $(type -t $function_name) == "function" ]]; then
+        $function_name "${@:2}"
+        return
+    fi
+
+    abcli_log_error "-$plugin_name: pypi: $task: command not found."
+    return 1
+}
+
+abcli_source_caller_suffix_path /pypi
