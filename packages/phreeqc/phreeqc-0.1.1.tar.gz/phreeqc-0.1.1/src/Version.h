2764890ab49d738109e7213bef175eb33d17e2fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 05:04:57 +0000

    Add package phreeqc/0.1.1/phreeqc-0.1.1.tar.gz

---
+++ packages/phreeqc/phreeqc-0.1.1.tar.gz/phreeqc-0.1.1/src/Version.h
@@ -0,0 +1,36 @@
+#define NAME          "IPhreeqc"
+#define VER_MAJOR      3
+#define VER_MINOR      7
+#define VER_PATCH      3
+#define VER_REVISION   15968
+#define GIT_COMMIT     dcfca6c581d1d9ce4768a10d1a068dc0d3940994
+
+#define RELEASE_DATE           "December  2, 2021"
+
+#define APR_STRINGIFY(n) APR_STRINGIFY_HELPER(n)
+#define APR_STRINGIFY_HELPER(n) #n
+
+/** Version number */
+#define VER_NUM                APR_STRINGIFY(VER_MAJOR) \
+                           "." APR_STRINGIFY(VER_MINOR) \
+                           "." APR_STRINGIFY(VER_PATCH) \
+                           "." APR_STRINGIFY(VER_REVISION)
+
+
+
+#define PRODUCT_NAME   NAME \
+                       "-" APR_STRINGIFY(VER_MAJOR) \
+                       "." APR_STRINGIFY(VER_MINOR)
+
+#if defined(_WIN64)
+#define VERSION_STRING         APR_STRINGIFY(VER_MAJOR) \
+                           "." APR_STRINGIFY(VER_MINOR) \
+                           "." APR_STRINGIFY(VER_PATCH) \
+                           "-" APR_STRINGIFY(VER_REVISION) \
+                           "-x64"
+#else
+#define VERSION_STRING         APR_STRINGIFY(VER_MAJOR) \
+                           "." APR_STRINGIFY(VER_MINOR) \
+                           "." APR_STRINGIFY(VER_PATCH) \
+                           "-" APR_STRINGIFY(VER_REVISION)
+#endif
