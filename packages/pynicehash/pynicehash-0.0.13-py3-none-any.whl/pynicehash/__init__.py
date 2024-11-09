dd7ec18fb5b6f0348293f17c2c7f417098bd1c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 05:04:16 +0000

    Add package pynicehash/0.0.13/pynicehash-0.0.13-py3-none-any.whl

---
+++ packages/pynicehash/pynicehash-0.0.13-py3-none-any.whl/pynicehash/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.1"
+
+from pynicehash.miningdevice import MiningDevice, DeviceMiningStatusEnum
+from pynicehash.miningrig import MiningRig
+from pynicehash.nicehash import NiceHash
+from pynicehash.mining_status import MiningStatus
