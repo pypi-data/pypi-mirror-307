1259f319c491c98f0718041d9c3b0a0dfb0e6585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:04:56 +0000

    Add package pytest_tagging/1.6.0/pytest_tagging-1.6.0-py3-none-any.whl

---
+++ packages/pytest_tagging/pytest_tagging-1.6.0-py3-none-any.whl/pytest_tagging/utils.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from multiprocessing import Manager
+from typing import Any, Iterable
+
+from pytest_tagging.choices import OperandChoices
+
+
+class TagCounterThreadSafe:
+    def __init__(self) -> None:
+        self._manager = Manager()
+        self.counter = self._manager.dict()
+        self.lock = self._manager.Lock()
+
+    def update(self, tags: Iterable[str]) -> None:
+        with self.lock:
+            for tag in tags:
+                if tag in self.counter:
+                    self.counter[tag] += 1
+                else:
+                    self.counter[tag] = 1
+
+    def items(self) -> list[Any]:
+        return sorted(self.counter.items(), key=lambda x: (x[1], x[0]), reverse=True)
+
+    def __bool__(self) -> bool:
+        return bool(self.counter)
+
+
+@dataclass(frozen=True)
+class TaggingOptions:
+    tags: list[str] | None = None
+    exclude_tags: list[str] | None = None
+    operand: OperandChoices = OperandChoices.OR
