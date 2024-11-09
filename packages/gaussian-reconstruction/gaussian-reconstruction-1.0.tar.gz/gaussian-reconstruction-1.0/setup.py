f3332a9f09fa64404d13813432ae5299172a3bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:04:50 +0000

    Add package gaussian-reconstruction/1.0/gaussian-reconstruction-1.0.tar.gz

---
+++ packages/gaussian-reconstruction/gaussian-reconstruction-1.0.tar.gz/gaussian-reconstruction-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import  setup
+import setuptools
+packages = ['gaussian-reconstruction']
+setup(name='gaussian-reconstruction',
+	version='1.0',
+	author='BerryChen',
+    packages=packages,
+    package_dir={'requests': 'requests'},)
