373c4af9f6414e6369ebd5f2e7740721aa98b70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:06:20 +0000

    Add package dektools/0.1.161/dektools-0.1.161-py3-none-any.whl

---
+++ packages/dektools/dektools-0.1.161-py3-none-any.whl/dektools/web/mimetypes.py
@@ -0,0 +1,14 @@
+from mimetypes import MimeTypes
+
+mimetypes = MimeTypes()
+
+custom_types = {
+    'text/javascript': '.js'
+}
+
+for k, v in custom_types.items():
+    mimetypes.add_type(k, v)
+
+
+def ct_to_mt(content_type):
+    return content_type.partition(';')[0].strip()
