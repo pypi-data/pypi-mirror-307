4b70fe48881847410dd2942910c10ea1dd2501f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 16:04:30 +0000

    Add package xync-schema/0.6.35/xync_schema-0.6.35.tar.gz

---
+++ packages/xync-schema/xync_schema-0.6.35.tar.gz/xync_schema-0.6.35/xync_schema/pydantic.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class FiatUpd(BaseModel):
+    detail: str | None = None
+    name: str | None = None
+    amount: float | None = None
+    target: int | None = None
+
+
+class FiatNew(FiatUpd):
+    cur_id: int
+    pm_id: int
+    detail: str
+    amount: float = 0
+
+
+class Agent(BaseModel):
+    cur_id: int
+    pm_id: int
+    detail: str
+    amount: float = 0
