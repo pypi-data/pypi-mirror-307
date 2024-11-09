7a5ee9427d59a19442bcc8a05e88019afda0b4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 05:04:18 +0000

    Add package bbstrader/0.1.92/bbstrader-0.1.92-py3-none-any.whl

---
+++ packages/bbstrader/bbstrader-0.1.92-py3-none-any.whl/bbstrader/models/__init__.py
@@ -0,0 +1,6 @@
+"""
+The `models` module provides a foundational framework for implementing various quantitative finance models. 
+
+It is designed to be a versatile base module for different types of models used in financial analysis and trading. 
+"""
+from bbstrader.models.risk import *
