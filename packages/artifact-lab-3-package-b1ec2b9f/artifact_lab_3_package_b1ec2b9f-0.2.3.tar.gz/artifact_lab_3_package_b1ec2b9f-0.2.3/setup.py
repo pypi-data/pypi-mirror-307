62f78db3cc29aaeb3d22aa9fde41ea94dfbe228a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 16:04:35 +0000

    Add package artifact-lab-3-package-b1ec2b9f/0.2.3/artifact_lab_3_package_b1ec2b9f-0.2.3.tar.gz

---
+++ packages/artifact-lab-3-package-b1ec2b9f/artifact_lab_3_package_b1ec2b9f-0.2.3.tar.gz/artifact_lab_3_package_b1ec2b9f-0.2.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='artifact-lab-3-package-b1ec2b9f',
+        version='0.2.3',
+        packages=find_packages(),
+        install_requires=[
+            'requests>=2.32.3',
+            ],
+        )
