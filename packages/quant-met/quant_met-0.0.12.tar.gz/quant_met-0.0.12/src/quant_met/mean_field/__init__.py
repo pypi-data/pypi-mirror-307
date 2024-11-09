b3602f8a13382ca6e3b30062fc536cadc19b3c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 16:04:46 +0000

    Add package quant-met/0.0.12/quant_met-0.0.12.tar.gz

---
+++ packages/quant-met/quant_met-0.0.12.tar.gz/quant_met-0.0.12/src/quant_met/mean_field/__init__.py
@@ -0,0 +1,40 @@
+# SPDX-FileCopyrightText: 2024 Tjark Sievers
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Mean field
+==========
+
+Submodules
+----------
+
+.. autosummary::
+    :toctree: generated/
+
+    hamiltonians
+
+
+Functions
+---------
+
+.. autosummary::
+   :toctree: generated/
+
+   superfluid_weight
+   quantum_metric
+   self_consistency_loop
+"""  # noqa: D205, D400
+
+from quant_met.mean_field import hamiltonians
+
+from .quantum_metric import quantum_metric
+from .self_consistency import self_consistency_loop
+from .superfluid_weight import superfluid_weight
+
+__all__ = [
+    "superfluid_weight",
+    "quantum_metric",
+    "self_consistency_loop",
+    "hamiltonians",
+]
