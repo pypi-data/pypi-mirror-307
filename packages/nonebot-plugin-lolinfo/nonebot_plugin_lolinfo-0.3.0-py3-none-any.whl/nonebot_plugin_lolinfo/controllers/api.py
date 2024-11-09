de2df75b93ca295ccfc10441197be9cca9c741d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 16:04:38 +0000

    Add package nonebot-plugin-lolinfo/0.3.0/nonebot_plugin_lolinfo-0.3.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-lolinfo/nonebot_plugin_lolinfo-0.3.0-py3-none-any.whl/nonebot_plugin_lolinfo/controllers/api.py
@@ -0,0 +1,50 @@
+import json
+import httpx
+from nonebot import logger
+from ..config import config
+
+HTTPClient = httpx.AsyncClient(
+    verify=False, timeout=config.lol_httpx_timeout, headers=config.lol_httpx_headers
+)
+
+
+class LOLAPI:
+    """
+    [https://api-dev.shadow403.cn/docs/LOL/](https://api-dev.shadow403.cn/docs/LOL/)
+    """
+
+    def __init__(self):
+        self.url = config.lol_api_url
+
+    async def _get_Hinfo(self, HName: str):
+        try:
+            rURL = f"{self.url}/hero_info/{HName}?min=true"
+            logger.info(f"开始获取数据 {rURL}")
+            rDict = json.loads((await HTTPClient.get(rURL)).text)
+            logger.success(f"获取数据成功 {rURL}")
+            return rDict
+        except Exception as e:
+            logger.error(f"获取数据失败 {e}")
+            return {"code": 401, "message": e}
+
+    async def _get_Rinfo(self, HName: str, SPosi: str = "false"):
+        try:
+            rURL = f"{self.url}/hero_rank/{HName}?posi={SPosi}"
+            logger.info(f"开始获取数据 {rURL}")
+            rDict = json.loads((await HTTPClient.get(rURL)).text)
+            logger.success(f"获取数据成功 {rURL}")
+            return rDict
+        except Exception as e:
+            logger.error(f"获取数据失败 {e}")
+            return {"code": 401, "message": e}
+
+    async def _get_DList(self):
+        try:
+            rURL = f"{self.url}/originlist"
+            logger.info(f"开始获取数据 {rURL}")
+            rDict = json.loads((await HTTPClient.get(rURL)).text)
+            logger.success(f"获取数据成功 {rURL}")
+            return rDict
+        except Exception as e:
+            logger.error(f"获取数据失败 {e}")
+            return {"code": 401, "message": e}
