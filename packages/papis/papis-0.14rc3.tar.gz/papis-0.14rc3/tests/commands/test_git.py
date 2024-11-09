379c4fff93bb7618595347586ccea342c6f08ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 06:04:46 +0000

    Add package papis/0.14rc3/papis-0.14rc3.tar.gz

---
+++ packages/papis/papis-0.14rc3.tar.gz/papis-0.14rc3/tests/commands/test_git.py
@@ -0,0 +1,18 @@
+import os
+
+import papis.config
+
+from papis.testing import TemporaryLibrary, PapisRunner
+
+
+def test_git_cli(tmp_library: TemporaryLibrary) -> None:
+    from papis.commands.git import cli
+    cli_runner = PapisRunner()
+
+    result = cli_runner.invoke(
+        cli,
+        ["init"])
+    assert result.exit_code == 0
+
+    folder, = papis.config.get_lib_dirs()
+    assert os.path.exists(os.path.join(folder, ".git"))
