d643bd422798e09b4fe06675ba7388f1a8f855c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:06:05 +0000

    Add package rust_neotools/0.0.3/rust_neotools-0.0.3-cp313-none-win_amd64.whl

---
+++ packages/rust_neotools/rust_neotools-0.0.3-cp313-none-win_amd64.whl/rust_neotools/rust_neotools.pyi
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import Optional
+
+class Symol:
+    @staticmethod
+    def get_minute(date: datetime, /) -> int:
+        """Returns the Symol minute of the day for the given datetime."""
+        ...
+    @staticmethod
+    def get_window(date: datetime, /) -> list[int]:
+        """Returns the window of Symol minutes of the day for the given datetime."""
+        ...
+
+class IslandMystic:
+    @staticmethod
+    def check(date: datetime, username: str) -> bool:
+        """Checks if the username can get the Island Mystic avatar on this date."""
+        ...
+    @staticmethod
+    def check_non_english(date: datetime, username: str) -> bool:
+        """Checks if the username can get the Island Mystic avatar on this date."""
+        ...
+    @staticmethod
+    def brute_force_day(date: datetime, english: bool) -> list[str]:
+        """Brute forces all username prefixes for the given date."""
+        ...
+    @staticmethod
+    def brute_force_user(
+        date: datetime, username: str, step: int, english: bool
+    ) -> Optional[datetime]:
+        """Brute forces all dates for the given username, for the languange and step direction of time given."""
+        ...
