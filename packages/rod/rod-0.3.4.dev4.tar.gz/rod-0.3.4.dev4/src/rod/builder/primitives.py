c3be3cb7fa3f60cc838d6403b71d0c97d2a8df2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:05:02 +0000

    Add package rod/0.3.4.dev4/rod-0.3.4.dev4.tar.gz

---
+++ packages/rod/rod-0.3.4.dev4.tar.gz/rod-0.3.4.dev4/src/rod/builder/primitives.py
@@ -0,0 +1,107 @@
+import dataclasses
+import pathlib
+
+import trimesh
+from numpy.typing import NDArray
+
+import rod
+from rod.builder.primitive_builder import PrimitiveBuilder
+
+
+@dataclasses.dataclass
+class SphereBuilder(PrimitiveBuilder):
+    radius: float
+
+    def _inertia(self) -> rod.Inertia:
+        return rod.Inertia(
+            ixx=2 / 5 * self.mass * (self.radius) ** 2,
+            iyy=2 / 5 * self.mass * (self.radius) ** 2,
+            izz=2 / 5 * self.mass * (self.radius) ** 2,
+        )
+
+    def _geometry(self) -> rod.Geometry:
+        return rod.Geometry(sphere=rod.Sphere(radius=self.radius))
+
+
+@dataclasses.dataclass
+class BoxBuilder(PrimitiveBuilder):
+    x: float
+    y: float
+    z: float
+
+    def _inertia(self) -> rod.Inertia:
+        return rod.Inertia(
+            ixx=self.mass / 12 * (self.y**2 + self.z**2),
+            iyy=self.mass / 12 * (self.x**2 + self.z**2),
+            izz=self.mass / 12 * (self.x**2 + self.y**2),
+        )
+
+    def _geometry(self) -> rod.Geometry:
+        return rod.Geometry(box=rod.Box(size=[self.x, self.y, self.z]))
+
+
+@dataclasses.dataclass
+class CylinderBuilder(PrimitiveBuilder):
+    radius: float
+    length: float
+
+    def _inertia(self) -> rod.Inertia:
+        ixx_iyy = self.mass * (3 * self.radius**2 + self.length**2) / 12
+
+        return rod.Inertia(
+            ixx=ixx_iyy,
+            iyy=ixx_iyy,
+            izz=0.5 * self.mass * self.radius**2,
+        )
+
+    def _geometry(self) -> rod.Geometry:
+        return rod.Geometry(
+            cylinder=rod.Cylinder(radius=self.radius, length=self.length)
+        )
+
+
+@dataclasses.dataclass
+class MeshBuilder(PrimitiveBuilder):
+    mesh_path: str | pathlib.Path
+    scale: NDArray
+
+    def __post_init__(self) -> None:
+        """
+        Post-initialization method for the class.
+        Loads the mesh from the specified file path and performs necessary checks.
+
+        Raises:
+            AssertionError: If the scale is not a 3D vector.
+            TypeError: If the mesh_path is not a str or pathlib.Path.
+        """
+
+        mesh_path = (
+            self.mesh_path
+            if isinstance(self.mesh_path, pathlib.Path)
+            else pathlib.Path(self.mesh_path)
+        )
+
+        if not mesh_path.is_file():
+            raise FileNotFoundError(f"Mesh file not found at {self.mesh_path}")
+
+        self.mesh: trimesh.base.Trimesh = trimesh.load_mesh(
+            file_obj=mesh_path,
+        )
+
+        assert self.scale.shape == (
+            3,
+        ), f"Scale must be a 3D vector, got {self.scale.shape}"
+
+    def _inertia(self) -> rod.Inertia:
+        inertia = self.mesh.moment_inertia
+        return rod.Inertia(
+            ixx=inertia[0, 0],
+            ixy=inertia[0, 1],
+            ixz=inertia[0, 2],
+            iyy=inertia[1, 1],
+            iyz=inertia[1, 2],
+            izz=inertia[2, 2],
+        )
+
+    def _geometry(self) -> rod.Geometry:
+        return rod.Geometry(mesh=rod.Mesh(uri=str(self.mesh_path), scale=self.scale))
---
+++ packages/rod/rod-0.3.4.dev4.tar.gz/rod-0.3.4.dev4/src/rod/sdf/link.py
@@ -0,0 +1,160 @@
+import dataclasses
+
+import mashumaro
+import numpy as np
+import numpy.typing as npt
+
+from .collision import Collision
+from .common import Pose
+from .element import Element
+from .visual import Visual
+
+
+@dataclasses.dataclass
+class Inertia(Element):
+    ixx: float = dataclasses.field(
+        default=1.0,
+        metadata=mashumaro.field_options(serialize=Element.serialize_float),
+    )
+
+    iyy: float = dataclasses.field(
+        default=1.0,
+        metadata=mashumaro.field_options(serialize=Element.serialize_float),
+    )
+
+    izz: float = dataclasses.field(
+        default=1.0,
+        metadata=mashumaro.field_options(serialize=Element.serialize_float),
+    )
+
+    ixy: float = dataclasses.field(
+        default=0.0,
+        metadata=mashumaro.field_options(serialize=Element.serialize_float),
+    )
+
+    ixz: float = dataclasses.field(
+        default=0.0,
+        metadata=mashumaro.field_options(serialize=Element.serialize_float),
+    )
+
+    iyz: float = dataclasses.field(
+        default=0.0,
+        metadata=mashumaro.field_options(serialize=Element.serialize_float),
+    )
+
+    def matrix(self) -> npt.NDArray:
+        return np.array(
+            [
+                [self.ixx, self.ixy, self.ixz],
+                [self.ixy, self.iyy, self.iyz],
+                [self.ixz, self.iyz, self.izz],
+            ]
+        )
+
+
+@dataclasses.dataclass
+class Inertial(Element):
+    mass: float = dataclasses.field(
+        metadata=mashumaro.field_options(serialize=Element.serialize_float),
+    )
+
+    inertia: Inertia
+
+    name: str | None = dataclasses.field(default=None)
+    pose: Pose | None = dataclasses.field(default=None)
+
+
+@dataclasses.dataclass
+class Link(Element):
+    name: str = dataclasses.field(metadata=mashumaro.field_options(alias="@name"))
+
+    pose: Pose | None = dataclasses.field(default=None)
+
+    inertial: Inertial | None = dataclasses.field(default=None)
+
+    visual: Visual | list[Visual] | None = dataclasses.field(default=None)
+
+    collision: Collision | list[Collision] | None = dataclasses.field(default=None)
+
+    gravity: bool | None = dataclasses.field(
+        default=None,
+        metadata=mashumaro.field_options(
+            serialize=Element.serialize_bool, deserialize=Element.deserialize_bool
+        ),
+    )
+
+    enable_wind: bool | None = dataclasses.field(
+        default=None,
+        metadata=mashumaro.field_options(
+            serialize=Element.serialize_bool, deserialize=Element.deserialize_bool
+        ),
+    )
+
+    self_collide: bool | None = dataclasses.field(
+        default=None,
+        metadata=mashumaro.field_options(
+            serialize=Element.serialize_bool, deserialize=Element.deserialize_bool
+        ),
+    )
+
+    kinematic: bool | None = dataclasses.field(
+        default=None,
+        metadata=mashumaro.field_options(
+            serialize=Element.serialize_bool, deserialize=Element.deserialize_bool
+        ),
+    )
+
+    must_be_base_link: bool | None = dataclasses.field(
+        default=None,
+        metadata=mashumaro.field_options(
+            serialize=Element.serialize_bool, deserialize=Element.deserialize_bool
+        ),
+    )
+
+    def visuals(self) -> list[Visual]:
+        if self.visual is None:
+            return []
+
+        if isinstance(self.visual, Visual):
+            return [self.visual]
+
+        assert isinstance(self.visual, list), type(self.visual)
+        return self.visual
+
+    def collisions(self) -> list[Collision]:
+        if self.collision is None:
+            return []
+
+        if isinstance(self.collision, Collision):
+            return [self.collision]
+
+        assert isinstance(self.collision, list), type(self.collision)
+        return self.collision
+
+    def add_visual(self, visual: Visual) -> None:
+        if self.visual is None:
+            self.visual = visual
+            return
+
+        visuals = self.visual
+
+        if not isinstance(visuals, list):
+            assert isinstance(visuals, Visual), type(visuals)
+            visuals = [visuals]
+
+        visuals.append(visual)
+        self.visual = visuals
+
+    def add_collision(self, collision: Collision) -> None:
+        if self.collision is None:
+            self.collision = collision
+            return
+
+        collisions = self.collision
+
+        if not isinstance(collisions, list):
+            assert isinstance(collisions, Collision), type(collisions)
+            collisions = [collisions]
+
+        collisions.append(collision)
+        self.collision = collisions
