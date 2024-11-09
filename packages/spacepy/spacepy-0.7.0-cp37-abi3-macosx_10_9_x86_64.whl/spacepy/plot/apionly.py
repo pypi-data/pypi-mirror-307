4c59e0313b47e76ff2a73ca3220d8f10ea8fff48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 06:05:42 +0000

    Add package spacepy/0.7.0/spacepy-0.7.0-cp37-abi3-macosx_10_9_x86_64.whl

---
+++ packages/spacepy/spacepy-0.7.0-cp37-abi3-macosx_10_9_x86_64.whl/spacepy/plot/apionly.py
@@ -0,0 +1,10 @@
+"""
+Formerly used to import spacepy.plot without changing styles.
+
+.. versionchanged:: 0.5.0
+   Deprecated, as styles are no longer applied on import.
+"""
+import warnings
+
+warnings.warn('Plot styles no longer applied on import, apionly deprecated'
+              ' in SpacePy 0.5.0', DeprecationWarning, stacklevel=2)
