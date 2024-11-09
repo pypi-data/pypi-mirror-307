893486c7fe871dc0f3e6d4e80395e3e88c2846a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:05:01 +0000

    Add package nautobot-secrets-providers/3.2.0/nautobot_secrets_providers-3.2.0-py3-none-any.whl

---
+++ packages/nautobot-secrets-providers/nautobot_secrets_providers-3.2.0-py3-none-any.whl/nautobot_secrets_providers/urls.py
@@ -0,0 +1,14 @@
+"""Django urlpatterns declaration for nautobot_secrets_providers app."""
+
+from django.templatetags.static import static
+from django.urls import path
+from django.views.generic import RedirectView
+
+from nautobot_secrets_providers import views
+
+app_name = "nautobot_secrets_providers"
+
+urlpatterns = [
+    path("", views.SecretsProvidersHomeView.as_view(), name="home"),
+    path("docs/", RedirectView.as_view(url=static("nautobot_secrets_providers/docs/index.html")), name="docs"),
+]
