7b95a3fd3206f15bb0a6a709499d4791a07a1d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 05:04:24 +0000

    Add package pupilio/1.1.6/pupilio-1.1.6-py3-none-any.whl

---
+++ packages/pupilio/pupilio-1.1.6-py3-none-any.whl/pupilio/version.py
@@ -0,0 +1,18 @@
+_major_version = '1'
+_minor_version = '1'
+_patch_version = '6'
+
+__version__ = f'{_major_version}.{_minor_version}.{_patch_version}'
+
+__title__ = "Pupil.IO"
+__description__ = "Pupil.IO is a toolbox for using eye trackers. " \
+                  "The SDK has to be installed on the same computer as your software application."
+__url__ = "https://deep-gaze.com/sdk/python"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __url__ + ">"
+
+__author__ = "Pupil.IO"
+__email__ = "zhugc2016@gmail.com"
+
+__license__ = "Creative Commons Attribution 4.0 (CC BY 4.0)"
+__copyright__ = "Copyright (c) 2023-2024 " + __author__
