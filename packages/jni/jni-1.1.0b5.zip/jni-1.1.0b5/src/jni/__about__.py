2b805a07b23723c52ac7259c378b1c55c73ca36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:06:31 +0000

    Add package jni/1.1.0b5/jni-1.1.0b5.zip

---
+++ packages/jni/jni-1.1.0b5.zip/jni-1.1.0b5/src/jni/__about__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2004 Adam Karpierz
+# Licensed under CC BY-NC-ND 4.0
+# Licensed under proprietary License
+# Please refer to the accompanying LICENSE file.
+
+__import__("pkg_about").about()
+__copyright__ = f"Copyright (c) 2004-2024 {__author__}"  # noqa
