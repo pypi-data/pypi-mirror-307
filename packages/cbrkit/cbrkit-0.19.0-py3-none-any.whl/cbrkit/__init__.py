ca7ae98e63b1292f4c6da7c02550530d403656aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 06:04:56 +0000

    Add package cbrkit/0.19.0/cbrkit-0.19.0-py3-none-any.whl

---
+++ packages/cbrkit/cbrkit-0.19.0-py3-none-any.whl/cbrkit/__init__.py
@@ -0,0 +1,24 @@
+"""
+**[🌟 GitHub Project 🌟](https://github.com/wi2trier/cbrkit)**
+
+.. include:: ../README.md
+
+"""
+
+from . import (
+    eval,
+    helpers,
+    loaders,
+    retrieval,
+    sim,
+    typing,
+)
+
+__all__ = [
+    "loaders",
+    "sim",
+    "retrieval",
+    "eval",
+    "typing",
+    "helpers",
+]
