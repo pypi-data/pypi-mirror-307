c0492ab4ba502839ef8868f0eccb5239566b99a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:04:33 +0000

    Add package manatus/1.0.14/manatus-1.0.14-py3-none-any.whl

---
+++ packages/manatus/manatus-1.0.14-py3-none-any.whl/manatus/__init__.py
@@ -0,0 +1,9 @@
+import manatus.cli.harvest
+import manatus.cli.transform
+from .cli import argument_parser
+from .exceptions import *
+from .maps import *
+from .organizations import *
+from .scenarios import *
+from .source_resource import *
+from .tests import *
