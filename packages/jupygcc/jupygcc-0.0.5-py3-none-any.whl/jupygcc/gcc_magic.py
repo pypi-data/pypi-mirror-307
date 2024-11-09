7845a9b20003e8fd2b48865b2d40e3009f831458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 06:05:09 +0000

    Add package jupygcc/0.0.5/jupygcc-0.0.5-py3-none-any.whl

---
+++ packages/jupygcc/jupygcc-0.0.5-py3-none-any.whl/jupygcc/gcc_magic.py
@@ -0,0 +1,20 @@
+from IPython.core.magic import Magics, line_cell_magic, cell_magic, magics_class
+
+from .utils import compile_run_c, handle_metadata
+
+
+@magics_class
+class GccMagic(Magics):
+    @line_cell_magic
+    def gcc(self, line, cell=None):
+        """Compile and run C code using gcc."""
+        if cell is None:
+            with open(line)  as f:
+                metadata_dict, code = handle_metadata(f.read())
+                compile_run_c(code, metadata_dict)
+            #return line
+        else:
+            print("Called as cell magic")
+            metadata_dict, code = handle_metadata(cell)
+            compile_run_c(code, metadata_dict)
+            #return line, cell
