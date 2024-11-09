939226301a5eddaa0c86843d4cc042f35dc118e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 03:06:57 +0000

    Add package chap-core/0.0.17/chap_core-0.0.17.tar.gz

---
+++ packages/chap-core/chap_core-0.0.17.tar.gz/chap_core-0.0.17/tests/docker_flow.py
@@ -0,0 +1,79 @@
+"""
+This is meant to be a standalone python file for testing the flow with docker compose
+
+This file should be possible to run without chap installed, only with the necessary requirements
+"""
+import json
+import time
+
+import requests
+import logging
+logger = logging.getLogger(__name__)
+
+def dataset():
+    dataset = "../example_data/anonymous_chap_request.json"
+    data = json.load(open(dataset))
+    return data
+
+
+#hostname = "localhost"
+hostname = 'chap'
+chap_url = "http://%s:8000" % hostname
+#chap_with_r_inla_url = "http://localhost:8001"
+
+
+def main():
+    model_url = chap_url + "/v1/list-models"
+    ensure_up(chap_url)
+    models = requests.get(model_url)
+
+    for model in models.json():
+        evaluate_model(chap_url, dataset(), model)
+
+
+def ensure_up(chap_url):
+    for _ in range(5):
+        try:
+            requests.get(chap_url + "/v1/status")
+            break
+        except requests.exceptions.ConnectionError:
+            time.sleep(5)
+
+
+def evaluate_model(chap_url, data, model, timeout=600):
+    print("Evaluating", model)
+    ensure_up(chap_url)
+    model_name = model["name"]
+    data["estimator_id"] = model_name
+    logger.info(model_name)
+    evalute_url = chap_url + f"/v1/predict/"
+    response = requests.post(evalute_url, json=data)
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+
+    for _ in range(timeout // 5):
+        job_status = requests.get(chap_url + "/v1/status").json()
+        if job_status['status'] == "failed":
+            exception_info = requests.get(chap_url + "/v1/get-exception").json()
+            if "Earth Engine client library not initialized" in exception_info:
+                logger.warning("Earth Engine client library not initialized. Ignoring this test")
+                logger.warning("Exception: %s" % exception_info)
+                return
+
+            raise ValueError("Model evaluation failed. Exception: %s" % exception_info)
+
+        logger.info(job_status)
+
+        time.sleep(5)
+        if job_status['ready']:
+            break
+    else:
+        raise TimeoutError("Model evaluation took too long")
+    results = requests.get(chap_url + "/v1/get-results").json()
+    assert len(results['dataValues']) == 45, len(results['dataValues'])
+
+if __name__ == "__main__":
+    main()
+    #
+
+#evaluate_model(chap_url, dataset(), {"name": "naive_model"})
---
+++ packages/chap-core/chap_core-0.0.17.tar.gz/chap_core-0.0.17/tests/external/test_external_models.py
@@ -0,0 +1,64 @@
+import logging
+from pathlib import Path
+
+import pandas as pd
+import pytest
+import yaml
+
+from chap_core.spatio_temporal_data.temporal_dataclass import DataSet
+from chap_core.datatypes import ClimateHealthTimeSeries
+from chap_core.testing.external_model import sanity_check_external_model
+
+logging.basicConfig(level=logging.INFO)
+from chap_core.external.external_model import get_model_from_directory_or_github_url
+from chap_core.util import docker_available, pyenv_available
+
+
+@pytest.mark.skipif(not docker_available(), reason="Requires docker")
+def test_python_model_from_folder_with_mlproject_file(models_path):
+    path = models_path / "naive_python_model_with_mlproject_file"
+    model = get_model_from_directory_or_github_url(path)
+
+
+def get_dataset_from_yaml(yaml_path: Path, datatype=ClimateHealthTimeSeries):
+    specs = yaml.load(yaml_path.read_text(), Loader=yaml.FullLoader)
+    if "demo_data" in specs:
+        path = yaml_path.parent / specs["demo_data"]
+        df = pd.read_csv(path)
+    if "demo_data_adapter" in specs:
+        for to_name, from_name in specs["demo_data_adapter"].items():
+            if "{" in from_name:
+                new_col = [
+                    from_name.format(**df.iloc[i].to_dict()) for i in range(len(df))
+                ]
+                df[to_name] = new_col
+            else:
+                df[to_name] = df[from_name]
+    # df['disease_cases'] = np.arange(len(df))
+
+    return DataSet.from_pandas(df, datatype)
+
+
+@pytest.mark.skip(reason="This model does not have a mlproject file, using old yml spec")
+def test_get_model_from_github():
+    repo_url = "https://github.com/knutdrand/external_rmodel_example.git"
+    model = get_model_from_directory_or_github_url(repo_url)
+    assert model.name == "example_model"
+
+@pytest.mark.skip(reason="This model does not have a mlproject file, using old yml spec")
+def test_get_model_from_local_directory(models_path):
+    repo_url = models_path / "ewars_Plus"
+    model = get_model_from_directory_or_github_url(repo_url)
+    assert model.name == "ewars_Plus"
+
+
+@pytest.mark.skipif(not pyenv_available(), reason="requires pyenv")
+@pytest.mark.slow
+def test_external_sanity(models_path):
+    sanity_check_external_model(models_path / "naive_python_model_with_mlproject_file")
+
+
+@pytest.mark.skipif(not docker_available(), reason="requires pyenv")
+@pytest.mark.slow
+def test_external_sanity_deepar(models_path):
+    sanity_check_external_model("https://github.com/dhis2-chap/chap_auto_ewars")
