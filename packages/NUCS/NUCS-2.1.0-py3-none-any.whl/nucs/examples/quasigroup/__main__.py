6a59167f855b95048cdabf0e2b12e61cfabf45f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:05:37 +0000

    Add package NUCS/2.1.0/NUCS-2.1.0-py3-none-any.whl

---
+++ packages/NUCS/NUCS-2.1.0-py3-none-any.whl/nucs/examples/quasigroup/__main__.py
@@ -0,0 +1,34 @@
+###############################################################################
+# __   _            _____    _____
+# | \ | |          / ____|  / ____|
+# |  \| |  _   _  | |      | (___
+# | . ` | | | | | | |       \___ \
+# | |\  | | |_| | | |____   ____) |
+# |_| \_|  \__,_|  \_____| |_____/
+#
+# Fast constraint solving in Python  - https://github.com/yangeorget/nucs
+#
+# Copyright 2024 - Yan Georget
+###############################################################################
+import argparse
+
+from rich import print
+
+from nucs.examples.quasigroup.quasigroup_problem import Quasigroup5Problem
+from nucs.solvers.backtrack_solver import BacktrackSolver
+from nucs.solvers.heuristics import DOM_HEURISTIC_MIN_VALUE, VAR_HEURISTIC_SMALLEST_DOMAIN
+from nucs.statistics import get_statistics
+
+# Run with the following command (the second run is much faster because the code has been compiled):
+# NUMBA_CACHE_DIR=.numba/cache PYTHONPATH=. python -m nucs.examples.quasigroup -n 10 --symmetry_breaking
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", type=int, default=10)
+    parser.add_argument("--symmetry_breaking", action=argparse.BooleanOptionalAction, default=True)
+    args = parser.parse_args()
+    problem = Quasigroup5Problem(args.n, args.symmetry_breaking)
+    solver = BacktrackSolver(
+        problem, var_heuristic_idx=VAR_HEURISTIC_SMALLEST_DOMAIN, dom_heuristic_idx=DOM_HEURISTIC_MIN_VALUE
+    )
+    solver.solve_all()
+    print(get_statistics(solver.statistics))
