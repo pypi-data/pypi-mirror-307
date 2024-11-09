6ffe74c8ebca454301d27ec63fe2b21a02741219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 03:06:23 +0000

    Add package cosinorage/0.1.0/cosinorage-0.1.0.tar.gz

---
+++ packages/cosinorage/cosinorage-0.1.0.tar.gz/cosinorage-0.1.0/cosinorage/features/__init__.py
@@ -0,0 +1,9 @@
+# features/__init__.py
+
+'''
+This module provides the functionality to compute a wide range
+wearable-features based on minute-level ENMO data including
+- Circadian rhythm features, e.g., IV, IS, RA, M10, L5
+- Physical activity features, e.g., SB, LIPA, MVPA
+- Sleep features, e.g., TST, WASO, SE, SR
+'''
