f6777562107805ab68476f862379382abc1ee253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:20 +0000

    Add package fluidsim-core/0.8.4/fluidsim_core-0.8.4-py3-none-any.whl

---
+++ packages/fluidsim-core/fluidsim_core-0.8.4-py3-none-any.whl/fluidsim_core/__init__.py
@@ -0,0 +1,21 @@
+"""
+Pure-Python core library for FluidSim framework. This package provides
+generic base classes and utilities to build new solvers.
+
+.. autosummary::
+   :toctree:
+
+   params
+   info
+   solver
+   output
+   loader
+   magic
+   extend_simul
+   hexa_files
+   scripts
+   paths
+
+"""
+
+from ._version import __version__
