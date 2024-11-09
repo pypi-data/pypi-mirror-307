a1d81e83c3d6286177edb63482a8635a8ad1b13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:27 +0000

    Add package hg2-item-parser/0.5.4/hg2_item_parser-0.5.4-py3-none-any.whl

---
+++ packages/hg2-item-parser/hg2_item_parser-0.5.4-py3-none-any.whl/hg2_item_parser/cli.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+from typing import Annotated
+
+import typer
+
+from .item_parser import ItemParser
+
+app = typer.Typer()
+
+
+def ask_overwrite_if_exists(file_path: str | Path) -> bool:
+    if not isinstance(file_path, Path):
+        file_path = Path(file_path)
+    if file_path.is_file():
+        message = f"{file_path} is already exists, overwrite it? (y/n): "
+        overwrite = input(message).lower() == "y"
+    else:
+        overwrite = True
+
+    return overwrite
+
+
+@app.command(help="Prints an item to a console")
+def check(
+    item_id: int, data_dir_path: Annotated[str, typer.Argument()] = "extracted"
+) -> None:
+    parser = ItemParser(data_dir_path)
+    item = parser.parse_item(item_id)
+    typer.echo(item)
+
+
+@app.command(help="Parses items in range")
+def parse_from_to(
+    first_item_id: int,
+    last_item_id: int,
+    output_file_path: Annotated[str, typer.Argument()] = "parsed/items.txt",
+    data_dir_path: Annotated[str, typer.Argument()] = "extracted",
+) -> None:
+    if ask_overwrite_if_exists(output_file_path):
+        parser = ItemParser(data_dir_path)
+        parser.parse_and_write_items_from_to(
+            first_item_id, last_item_id, output_file_path
+        )
+
+
+@app.command(help="Parses an item")
+def parse(
+    item_id: int,
+    output_file_path: Annotated[str, typer.Argument()] = "parsed/items.txt",
+    data_dir_path: Annotated[str, typer.Argument()] = "extracted",
+) -> None:
+    if ask_overwrite_if_exists(output_file_path):
+        parser = ItemParser(data_dir_path)
+        parser.parse_and_write_item(item_id, output_file_path)
+
+
+def main() -> None:
+    app()
