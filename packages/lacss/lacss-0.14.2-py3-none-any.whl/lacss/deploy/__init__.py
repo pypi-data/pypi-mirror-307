81af75fdd7fd6341a14a043c7c5e4f4402b0ea1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:21 +0000

    Add package lacss/0.14.2/lacss-0.14.2-py3-none-any.whl

---
+++ packages/lacss/lacss-0.14.2-py3-none-any.whl/lacss/deploy/__init__.py
@@ -0,0 +1,13 @@
+from typing import Mapping
+# from .predict import Predictor
+
+model_urls: Mapping[str, str] = {
+    "lacss3-small": "https://huggingface.co/jiyuuchc/lacss3-small/resolve/main/lacss3-small",
+    "lacss3-base": "https://huggingface.co/jiyuuchc/lacss3-base/resolve/main/lacss3-base",
+    "lacss3-small-cellpose": "https://huggingface.co/jiyuuchc/lacss3-small-cellpose/resolve/main/lacss3-small-c",
+    "lacss3-small-livecell": "https://huggingface.co/jiyuuchc/lacss3-small-livecell/resolve/main/lacss3-small-l",
+    "lacss3-small-nips": "https://huggingface.co/jiyuuchc/lacss3-small-nips/resolve/main/lacss3-small-n",
+}
+
+model_urls["default"] = model_urls["lacss3-base"]
+
