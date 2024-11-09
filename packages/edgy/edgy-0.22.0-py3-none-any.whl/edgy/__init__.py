aadea07acf0bfa3e2ed1532d306b7b08a0da2bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 03:06:47 +0000

    Add package edgy/0.22.0/edgy-0.22.0-py3-none-any.whl

---
+++ packages/edgy/edgy-0.22.0-py3-none-any.whl/edgy/__init__.py
@@ -0,0 +1,110 @@
+__version__ = "0.22.0"
+
+from .cli.base import Migrate
+from .conf import settings
+from .conf.global_settings import EdgySettings
+from .core import files
+from .core.connection.database import Database, DatabaseURL
+from .core.connection.registry import Registry
+from .core.db import fields
+from .core.db.constants import CASCADE, RESTRICT, SET_NULL, ConditionalRedirect
+from .core.db.datastructures import Index, UniqueConstraint
+from .core.db.fields import (
+    BigIntegerField,
+    BinaryField,
+    BooleanField,
+    CharField,
+    ChoiceField,
+    CompositeField,
+    ComputedField,
+    DateField,
+    DateTimeField,
+    DecimalField,
+    DurationField,
+    EmailField,
+    ExcludeField,
+    FloatField,
+    IntegerField,
+    JSONField,
+    PasswordField,
+    PlaceholderField,
+    RefForeignKey,
+    SmallIntegerField,
+    TextField,
+    TimeField,
+    URLField,
+    UUIDField,
+)
+from .core.db.fields.foreign_keys import ForeignKey
+from .core.db.fields.many_to_many import ManyToMany, ManyToManyField
+from .core.db.fields.one_to_one_keys import OneToOne, OneToOneField
+from .core.db.models import Model, ModelRef, ReflectModel, StrictModel
+from .core.db.models.managers import Manager
+from .core.db.querysets import Prefetch, Q, QuerySet, and_, not_, or_
+from .core.extras import EdgyExtra
+from .core.signals import Signal
+from .core.utils.sync import run_sync
+from .exceptions import MultipleObjectsReturned, ObjectNotFound
+
+__all__ = [
+    "and_",
+    "not_",
+    "or_",
+    "Q",
+    "BigIntegerField",
+    "BinaryField",
+    "BooleanField",
+    "CASCADE",
+    "ConditionalRedirect",
+    "CharField",
+    "ChoiceField",
+    "ComputedField",
+    "CompositeField",
+    "Database",
+    "DatabaseURL",
+    "DateField",
+    "DateTimeField",
+    "DurationField",
+    "DecimalField",
+    "EdgyExtra",
+    "EdgySettings",
+    "EmailField",
+    "ExcludeField",
+    "fields",
+    "files",
+    "FloatField",
+    "FileField",
+    "ForeignKey",
+    "Index",
+    "IntegerField",
+    "JSONField",
+    "RefForeignKey",
+    "Manager",
+    "ManyToMany",
+    "ManyToManyField",
+    "Migrate",
+    "Model",
+    "ModelRef",
+    "MultipleObjectsReturned",
+    "ObjectNotFound",
+    "OneToOne",
+    "OneToOneField",
+    "PasswordField",
+    "PlaceholderField",
+    "Prefetch",
+    "QuerySet",
+    "ReflectModel",
+    "StrictModel",
+    "RESTRICT",
+    "Registry",
+    "SET_NULL",
+    "Signal",
+    "SmallIntegerField",
+    "TextField",
+    "TimeField",
+    "URLField",
+    "UUIDField",
+    "UniqueConstraint",
+    "settings",
+    "run_sync",
+]
---
+++ packages/edgy/edgy-0.22.0-py3-none-any.whl/edgy/core/db/models/__init__.py
@@ -0,0 +1,4 @@
+from .model import Model, ReflectModel, StrictModel
+from .model_reference import ModelRef
+
+__all__ = ["Model", "StrictModel", "ModelRef", "ReflectModel"]
