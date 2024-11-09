e72e6ad4fefe19cc88a07d0404ac9842ba8d7a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:06:01 +0000

    Add package pyeio/0.1.4/pyeio-0.1.4.tar.gz

---
+++ packages/pyeio/pyeio-0.1.4.tar.gz/pyeio-0.1.4/pyeio/__meta__.py
@@ -0,0 +1,7 @@
+from importlib.metadata import version
+
+__name__ = __package__ = __module__ = "pyeio"
+__version__ = version(__name__)
+__repository__ = "https://github.com/harttraveller/pyeio"
+__documentation__ = "https://harttraveller.github.io/pyeio/"
+__issues__ = "https://github.com/harttraveller/pyeio/issues/"
