f8f8846d5b180cad743962be593f6918231bffca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:05:49 +0000

    Add package pyeio/0.1.3/pyeio-0.1.3-cp310-abi3-macosx_11_0_arm64.whl

---
+++ packages/pyeio/pyeio-0.1.3-cp310-abi3-macosx_11_0_arm64.whl/pyeio/jsonl.py
@@ -0,0 +1,67 @@
+def parse(): ...
+
+
+def serialize(): ...
+
+
+def open(): ...
+
+
+def read(): ...
+
+
+def save(): ...
+
+
+def load(): ...
+
+
+def stream(): ...
+
+
+def download(): ...
+
+
+# def append(): ...
+
+
+# def prepend(): ...
+
+
+# def walk(): ...
+
+
+# import orjson
+# from pyeio.types import FilePath
+
+
+# def stream(): ...
+
+
+# # import json
+# # from pathlib import Path
+# # from .core import io
+
+
+# # def load(path: str | Path) -> list | dict:
+# #     return json.loads(io.load_text(path))
+
+
+# # def save(): ...
+
+
+# # todo
+# # def stream(
+# #     path: str | Path,
+# #     validator,
+# #     handler,
+# # ): ...
+
+
+# # # same as stream except operation is done on each element
+# # def stream_ingest(
+# #     path,
+# #     validator,
+# #     handler,
+# #     errors,  # raise or log
+# # ): ...
