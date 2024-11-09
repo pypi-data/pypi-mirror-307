218e8643727e48185abdcf64c830e1f7d5fe39f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 14:05:29 +0000

    Add package brunata-api/0.1.1/brunata_api-0.1.1.tar.gz

---
+++ packages/brunata-api/brunata_api-0.1.1.tar.gz/brunata_api-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brunata-api',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'httpx>=0.27.2',
+    ],
+    author='Yuki Electronics',
+    author_email='yuki@yukie.dev',
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://codeberg.org/YukiElectronics/brunata-api',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
