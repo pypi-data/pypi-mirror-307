d789544f725b72211be6b599dc5f62f09c008792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 05:04:51 +0000

    Add package PatelHet-STT/0.1/patelhet_stt-0.1.tar.gz

---
+++ packages/PatelHet-STT/patelhet_stt-0.1.tar.gz/patelhet_stt-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='PatelHet_STT',
+    version='0.1',
+    author='Patel Het',
+    author_email='patelhet8265@gmail.com',
+    description='this is speech to text packeg created by patel het'
+)
+packages = find_packages(),
+install_requirement = [
+    'selenium'
+    'webdriver_manager'
+]
+
