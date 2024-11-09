4a65d4347f64474673873a55e86f1874f66f6597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:04:31 +0000

    Add package regression-lamia/1.0.0/regression_lamia-1.0.0.tar.gz

---
+++ packages/regression-lamia/regression_lamia-1.0.0.tar.gz/regression_lamia-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='regression_lamia',
+   version='1.0.0',
+   author='lamia',
+   author_email='l.abdelmalek@esi-sba.dz',
+   packages=['regression_lamia'],
+   url='http://pypi.python.org/pypi/regression_lamia/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['regression_lamia']
+)
