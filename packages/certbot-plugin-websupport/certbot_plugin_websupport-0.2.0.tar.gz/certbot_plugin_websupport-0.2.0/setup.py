0b30e8ba45f63434c5a336767692f21fcba74b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 06:05:29 +0000

    Add package certbot-plugin-websupport/0.2.0/certbot_plugin_websupport-0.2.0.tar.gz

---
+++ packages/certbot-plugin-websupport/certbot_plugin_websupport-0.2.0.tar.gz/certbot_plugin_websupport-0.2.0/setup.py
@@ -0,0 +1,74 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = '0.2.0'
+
+# Remember to update local-oldest-requirements.txt when changing the minimum
+# acme/certbot version.
+install_requires = [
+    'certbot>=0.38.0',
+    'requests',
+    'setuptools',
+    'zope.interface',
+]
+
+docs_extras = [
+    'Sphinx>=1.0',  # autodoc_member_order = 'bysource', autodoc_default_flags
+    'sphinx_rtd_theme',
+]
+
+setup(
+    name='certbot-plugin-websupport',
+    version=version,
+    description="Websupport DNS Authenticator plugin for Certbot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Mordred/certbot-plugin-websupport',
+    author="Martin Jantosovic",
+    author_email='jantosovic.martin@gmail.com',
+    license='MIT',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        'docs': docs_extras,
+    },
+    entry_points={
+        'certbot.plugins': [
+            'dns = certbot_plugin_websupport.dns:Authenticator',
+            'dns-websupport = certbot_plugin_websupport.dns:Authenticator',
+        ],
+    },
+)
