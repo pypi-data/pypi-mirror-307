e3a7713c71e4f68c382b67c9c92fe2a55e9a44c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 04:04:40 +0000

    Add package fasttower/0.1.4a0/fasttower-0.1.4a0.tar.gz

---
+++ packages/fasttower/fasttower-0.1.4a0.tar.gz/fasttower-0.1.4a0/fasttower/utils/__init__.py
@@ -0,0 +1,64 @@
+from contextlib import asynccontextmanager, AsyncExitStack
+from importlib import import_module
+from typing import Type, List, Callable
+
+from fastapi import FastAPI
+from tortoise import timezone
+from tortoise.contrib.fastapi import register_tortoise
+
+
+def get_module(path: str):
+    try:
+        path, module = path.rsplit(':', maxsplit=1)
+        module = getattr(import_module(path), module)
+    except ModuleNotFoundError as e:
+        raise e
+    except ValueError:
+        raise ImportError('Module not be imported')
+    return module
+
+
+def get_user_model() -> Type['AbstractUser']:
+    from fasttower.conf import settings
+    return get_module(settings.USER_MODEL)
+
+
+def lifespans(lifespans_: List[Callable]):
+    @asynccontextmanager
+    async def _lifespan_manager(app: FastAPI):
+        exit_stack = AsyncExitStack()
+        async with exit_stack:
+            for lifespan_ in lifespans_:
+                await exit_stack.enter_async_context(lifespan_(app))
+            yield
+
+    return _lifespan_manager
+
+
+def setup():
+    from fasttower.conf import settings
+    for app in settings.INSTALLED_APPS:
+        try:
+            import_module(f'{app}.admin')
+        except ModuleNotFoundError:
+            pass
+
+
+def register_orm(app: FastAPI, **kwargs):
+    from fasttower.conf import settings
+    return register_tortoise(
+        app,
+        config=settings.DATABASES,
+        generate_schemas=True,
+        **kwargs
+    )
+
+
+__all__ = [
+    'timezone',
+    'lifespans',
+    'get_module',
+    'register_orm',
+    'get_user_model',
+    'setup',
+]
