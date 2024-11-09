4bfde0bad97ff28c92550bc5370e5348609074b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 06:05:18 +0000

    Add package utubes/0.0.52/utubes-0.0.52-py3-none-any.whl

---
+++ packages/utubes/utubes-0.0.52-py3-none-any.whl/Utubes/scripts/eo.py
@@ -0,0 +1,9 @@
+
+
+class FHttps(object):
+
+    DATA01 = r"^https://www\.instagram\.com/([A-Za-z0-9._]+/)?(p|tv|reel)/([A-Za-z0-9\-_]*)"
+
+class Okeys(object):
+
+    DATA01 = "%(title,fulltitle,alt_title)s%(season_number& |)s%(season_number&S|)s%(season_number|)02d%(episode_number&E|)s%(episode_number|)02d.%(ext)s"
