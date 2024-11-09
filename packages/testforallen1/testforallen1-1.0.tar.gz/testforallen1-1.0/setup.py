1fc445929fd6639d3efc6e89f19bd7055ef6324e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-09 15:06:08 +0000

    Add package testforallen1/1.0/testforallen1-1.0.tar.gz

---
+++ packages/testforallen1/testforallen1-1.0.tar.gz/testforallen1-1.0/setup.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+
+from setuptools import setup, find_packages
+
+with open('Readme.md') as f:
+    long_description = f.read()
+
+
+setup(
+        name='testforallen1',     # 包名字
+        version='1.0',   # 包版本
+        description='This is a test of the setup',   # 简单描述
+        author='allensrj',  # 作者
+        author_email='allensrj@qq.com',  # 作者邮箱
+        url='https://github/allensrj/',      # 包的主页
+        packages=find_packages(),
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        license='MIT',
+        package_dir={'testforallen1': 'src'},
+
+)
