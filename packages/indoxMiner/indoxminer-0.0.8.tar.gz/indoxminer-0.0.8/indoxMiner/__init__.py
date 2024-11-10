60e89aa6e442099a7910b0fe65b8def0c9b86955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:08:58 +0000

    Add package indoxMiner/0.0.8/indoxminer-0.0.8.tar.gz

---
+++ packages/indoxMiner/indoxminer-0.0.8.tar.gz/indoxminer-0.0.8/indoxMiner/__init__.py
@@ -0,0 +1,36 @@
+# __init__.py
+
+from .extractor import Extractor
+from .schema import ExtractorSchema, Schema
+from .extraction_results import ExtractionResult, ExtractionResults
+from .fields import Field, ValidationRule, FieldType
+
+from .loader import DocumentProcessor, ProcessingConfig
+
+from .llms import OpenAi, Anthropic, IndoxApi, AsyncIndoxApi, AsyncOpenAi
+
+__all__ = [
+    # Extractor and schema related
+    "Extractor",
+    "ExtractorSchema",
+    "Schema",  # For accessing predefined schemas like Passport, Invoice, etc.
+    "ExtractionResult",
+    "ExtractionResults",
+    "Field",
+    "ValidationRule",
+    "FieldType",
+    # Document processing related
+    "DocumentProcessor",
+    "ProcessingConfig",
+    # llms
+    "OpenAi",
+    "Anthropic",
+    "IndoxApi",
+    "AsyncIndoxApi",
+    "AsyncOpenAi",
+]
+
+# Package metadata
+__version__ = "0.0.0"
+__author__ = "IndoxMiner Team"
+__description__ = "A comprehensive document extraction and processing library"
