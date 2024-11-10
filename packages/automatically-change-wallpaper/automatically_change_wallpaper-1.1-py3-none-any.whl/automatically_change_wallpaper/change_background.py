c5b53359bc5b3aa1a40fced56967bf16fc465fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:33 +0000

    Add package automatically-change-wallpaper/1.1/automatically_change_wallpaper-1.1-py3-none-any.whl

---
+++ packages/automatically-change-wallpaper/automatically_change_wallpaper-1.1-py3-none-any.whl/automatically_change_wallpaper/change_background.py
@@ -0,0 +1,13 @@
+import ctypes
+from pathlib import Path
+
+def set_wallpaper(img):
+
+    # check if the image file exists
+    if not Path(img).exists():
+        print(f"Error: Video file '{img}' not found.")
+        return
+
+    # Set the image as the desktop wallpaper using Windows API
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, img, 0)
+    
