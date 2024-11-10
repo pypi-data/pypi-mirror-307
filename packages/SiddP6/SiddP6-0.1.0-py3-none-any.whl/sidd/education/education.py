d1c96a3b0c78e80b34325fe941c70bfb1dd1e731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 18:04:59 +0000

    Add package SiddP6/0.1.0/SiddP6-0.1.0-py3-none-any.whl

---
+++ packages/SiddP6/SiddP6-0.1.0-py3-none-any.whl/sidd/education/education.py
@@ -0,0 +1,18 @@
+def educations():
+    print(
+"""
+
++++++++++++++++++++++++++++++++++++++++EDUCATION+++++++++++++++++++++++++++++++++++++++
+
+*** VELLORE INSTITUTE OF TECHNOLOGY ***
+    • B.Tech in Computer Science with Business Systems
+    • May 2023 | Vellore, India
+    • Cum. GPA: 8.23 / 10.0
+
+*** AWS, UDACITY AND INTEL ***
+    • Nano-Degree in AI Programming with Python program
+    • Feb 2023 | Remote
+
+"""
+    )
+
