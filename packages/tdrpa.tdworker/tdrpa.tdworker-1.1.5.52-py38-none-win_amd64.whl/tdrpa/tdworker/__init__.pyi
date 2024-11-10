83564aaf24e820a2ce67a3c08fd4883a28bc2677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:36 +0000

    Add package tdrpa.tdworker/1.1.5.52/tdrpa.tdworker-1.1.5.52-py38-none-win_amd64.whl

---
+++ packages/tdrpa.tdworker/tdrpa.tdworker-1.1.5.52-py38-none-win_amd64.whl/tdrpa/tdworker/__init__.pyi
@@ -0,0 +1,7 @@
+from ._excel import Excel as Excel
+from ._os import App as App
+from ._w import Window as Window
+from ._web import Web as Web
+from ._winE import WinElement as WinElement
+from ._winK import WinKeyboard as WinKeyboard
+from ._winM import WinMouse as WinMouse
