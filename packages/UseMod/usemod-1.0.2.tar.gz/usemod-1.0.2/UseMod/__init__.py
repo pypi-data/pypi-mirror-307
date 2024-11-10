c1667c7f9d67bc2ee8eb8811d62f62ee31eab503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:28 +0000

    Add package UseMod/1.0.2/usemod-1.0.2.tar.gz

---
+++ packages/UseMod/usemod-1.0.2.tar.gz/usemod-1.0.2/UseMod/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+"""
+Simple_Dropbox est un module qui fournit un accès simplifié à Dropbox.
+Il contient des fonctions pour interagir avec l'API de Dropbox.
+"""
+from .Dropbox import *
+from .GitHub import *
