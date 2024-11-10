ec63112a08c5362cb69e6a5aa56635ab804a57f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 05:05:18 +0000

    Add package reactpy-html-string-compiler/0.2.0/reactpy_html_string_compiler-0.2.0.tar.gz

---
+++ packages/reactpy-html-string-compiler/reactpy_html_string_compiler-0.2.0.tar.gz/reactpy_html_string_compiler-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="reactpy_html_string_compiler",
+    version="0.2.0",
+    description="compiles reactpy components into html string",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),  
+    include_package_data=True,
+    install_requires=[
+        "reactpy>=1.0.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
