1ca1fb8f398ad62b0a366b4cbdc599558fb35571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:04 +0000

    Add package pyqubo/1.5.0/pyqubo-1.5.0-cp38-cp38-win_amd64.whl

---
+++ packages/pyqubo/pyqubo-1.5.0-cp38-cp38-win_amd64.whl/pyqubo/__init__.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_8_3():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, '.'))
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-pyqubo-1.5.0')
+        if os.path.isfile(load_order_filepath):
+            import ctypes.wintypes
+            with open(os.path.join(libs_dir, '.load-order-pyqubo-1.5.0')) as file:
+                load_order = file.read().split()
+            kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+            kernel32.LoadLibraryExW.restype = ctypes.wintypes.HMODULE
+            kernel32.LoadLibraryExW.argtypes = ctypes.wintypes.LPCWSTR, ctypes.wintypes.HANDLE, ctypes.wintypes.DWORD
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(lib_path, None, 8):
+                    raise OSError('Error loading {}; {}'.format(lib, ctypes.FormatError(ctypes.get_last_error())))
+
+
+_delvewheel_patch_1_8_3()
+del _delvewheel_patch_1_8_3
+# end delvewheel patch
+
+import cpp_pyqubo
+from cpp_pyqubo import *
+from pyqubo.utils.asserts import *
+from pyqubo.utils.solver import *
+from .array import *
+from .logical_constraint import *
+from .logic import *
+from pyqubo.integer.integer import *
+from pyqubo.integer.log_encoded_integer import *
+from pyqubo.integer.one_hot_enc_integer import *
+from pyqubo.integer.order_enc_integer import *
+from pyqubo.integer.unary_encoded_integer import *
