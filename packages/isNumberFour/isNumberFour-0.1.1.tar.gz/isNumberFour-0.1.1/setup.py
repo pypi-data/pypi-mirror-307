c2abe1e418b5495e34f6b11ee2f33c6e0236c7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:15 +0000

    Add package isNumberFour/0.1.1/isNumberFour-0.1.1.tar.gz

---
+++ packages/isNumberFour/isNumberFour-0.1.1.tar.gz/isNumberFour-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="isNumberFour",
+    version="0.1.1",
+    description="A Python library to Check whether the value is Number 4 or Not",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Yajith Vishwa",
+    author_email="yajithvishwa2001@example.com",
+    url="https://github.com/YajithVishwa/isNumberFour",
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
