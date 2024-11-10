9dcefd2ac6c7da22043389c7017be89cc46d9fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:31 +0000

    Add package blue-geo/4.739.1/blue_geo-4.739.1.tar.gz

---
+++ packages/blue-geo/blue_geo-4.739.1.tar.gz/blue_geo-4.739.1/blue_geo/.abcli/tests/help.sh
@@ -0,0 +1,59 @@
+#! /usr/bin/env bash
+
+function test_blue_geo_help() {
+    # TODO: enable
+    return 0
+
+    local options=$1
+
+    local module
+    for module in \
+        "blue_geo" \
+        \
+        "blue_geo catalog" \
+        "blue_geo catalog browse" \
+        "blue_geo catalog get" \
+        "blue_geo catalog list" \
+        "blue_geo catalog query" \
+        \
+        "blue_geo datacube" \
+        "blue_geo datacube crop" \
+        "blue_geo datacube generate" \
+        "blue_geo datacube get" \
+        "blue_geo datacube ingest" \
+        "blue_geo datacube list" \
+        \
+        "blue_geo gdal" \
+        \
+        "blue_geo QGIS" \
+        "blue_geo QGIS_expressions" \
+        "blue_geo QGIS_server" \
+        \
+        "blue_geo watch" \
+        "blue_geo watch batch" \
+        "blue_geo watch map" \
+        "blue_geo watch reduce" \
+        \
+        "blue_geo watch targets" \
+        "blue_geo watch targets cat" \
+        "blue_geo watch targets cp" \
+        "blue_geo watch targets copy" \
+        "blue_geo watch targets download" \
+        "blue_geo watch targets edit" \
+        "blue_geo watch targets get" \
+        "blue_geo watch targets list" \
+        "blue_geo watch targets open" \
+        "blue_geo watch targets publish" \
+        "blue_geo watch targets save" \
+        "blue_geo watch targets test" \
+        "blue_geo watch targets update_template" \
+        "blue_geo watch targets upload"; do
+        abcli_eval ,$options \
+            abcli_help $module
+        [[ $? -ne 0 ]] && return 1
+
+        abcli_hr
+    done
+
+    return 0
+}
