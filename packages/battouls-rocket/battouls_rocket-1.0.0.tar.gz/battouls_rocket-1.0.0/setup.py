2bbb2f18799c22f6fb7cf70bc973c9d97bc7e42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:31 +0000

    Add package battouls-rocket/1.0.0/battouls_rocket-1.0.0.tar.gz

---
+++ packages/battouls-rocket/battouls_rocket-1.0.0.tar.gz/battouls_rocket-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='battouls-rocket',  
+   version='1.0.0',
+   author='Elbatoul',  
+   author_email='e.zerga@esi-sba.dz',  
+   packages=['battouls-rocket'],  
+   url='http://pypi.python.org/pypi/your_package_name/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['battouls-rocket']
+)
