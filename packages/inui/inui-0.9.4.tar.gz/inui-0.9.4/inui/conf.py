c07e21fceb4b05d77970d0487bab1b7bc0e2a1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:08:57 +0000

    Add package inui/0.9.4/inui-0.9.4.tar.gz

---
+++ packages/inui/inui-0.9.4.tar.gz/inui-0.9.4/inui/conf.py
@@ -0,0 +1,16 @@
+BOOTSTRAP_VERSION = "5.3.3"
+
+BOOTSTRAP_CSS_URL = (
+    "https://cdn.jsdelivr.net/npm/bootstrap@%s/dist/css/bootstrap.min.css"
+    % BOOTSTRAP_VERSION
+)
+BOOTSTRAP_JS_URL = (
+    "https://cdn.jsdelivr.net/npm/bootstrap@%s/dist/js/bootstrap.bundle.min.js"
+    % BOOTSTRAP_VERSION
+)
+
+TAILWINDCSS_URL = "https://cdn.tailwindcss.com"
+
+TAILWINDCSS_CONFIG = {}
+
+TAILWINDCSS_CUSTOM_CSS = ""
