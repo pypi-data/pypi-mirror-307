4700d10db7a4b6cfc8a82c2db5f0daf92aadd22d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:08:49 +0000

    Add package modern-di-litestar/0.2.2/modern_di_litestar-0.2.2.tar.gz

---
+++ packages/modern-di-litestar/modern_di_litestar-0.2.2.tar.gz/modern_di_litestar-0.2.2/modern_di_litestar/main.py
@@ -0,0 +1,44 @@
+import dataclasses
+import enum
+import typing
+
+import litestar
+from litestar.di import Provide
+from litestar.params import Dependency
+from modern_di import Container, providers
+from modern_di import Scope as DIScope
+
+
+T_co = typing.TypeVar("T_co", covariant=True)
+
+
+def setup_di(app: litestar.Litestar, scope: enum.IntEnum = DIScope.APP) -> Container:
+    app.state.di_container = Container(scope=scope)
+    app.dependencies = {"request_di_container": Provide(build_di_container), **app.dependencies}
+    return app.state.di_container
+
+
+def fetch_di_container(app: litestar.Litestar) -> Container:
+    return typing.cast(Container, app.state.di_container)
+
+
+async def build_di_container(
+    request: litestar.Request[typing.Any, typing.Any, typing.Any],
+) -> typing.AsyncIterator[Container]:
+    scope = DIScope.REQUEST
+    context = {"request": request}
+    container: Container = fetch_di_container(request.app)
+    async with container.build_child_container(context=context, scope=scope) as request_container:
+        yield request_container
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class _Dependency(typing.Generic[T_co]):
+    dependency: providers.AbstractProvider[T_co]
+
+    async def __call__(self, request_di_container: typing.Annotated[Container, Dependency()]) -> T_co:
+        return await self.dependency.async_resolve(request_di_container)
+
+
+def FromDI(dependency: providers.AbstractProvider[T_co], *, use_cache: bool = True) -> Provide:  # noqa: N802
+    return Provide(dependency=_Dependency(dependency), use_cache=use_cache)
