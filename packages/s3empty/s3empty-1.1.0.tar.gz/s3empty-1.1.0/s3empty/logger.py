3b4d742df46dffab0e58513c8dc6ae2003a3ff0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 17:04:30 +0000

    Add package s3empty/1.1.0/s3empty-1.1.0.tar.gz

---
+++ packages/s3empty/s3empty-1.1.0.tar.gz/s3empty-1.1.0/s3empty/logger.py
@@ -0,0 +1,15 @@
+"""Logger for S3 Empty."""
+from conflog import Conflog
+
+def init():
+    """Initialize logger.
+    """
+
+    cfl = Conflog(
+        conf_dict={
+            'level': 'info',
+            'format': '[s3empty] %(levelname)s %(message)s'
+        }
+    )
+
+    return cfl.get_logger(__name__)
