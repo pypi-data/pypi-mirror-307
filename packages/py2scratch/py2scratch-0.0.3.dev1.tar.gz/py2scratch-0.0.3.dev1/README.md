2ca350666acedc34df883985755b23644deb4c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:32 +0000

    Add package py2scratch/0.0.3.dev1/py2scratch-0.0.3.dev1.tar.gz

---
+++ packages/py2scratch/py2scratch-0.0.3.dev1.tar.gz/py2scratch-0.0.3.dev1/README.md
@@ -0,0 +1,12 @@
+<img align="center" src="./readme-assets/scratch.png">
+<h1 align="center">py2scratch</h1>
+
+<p align="center">A module that converts python into scratch projects<p>
+
+## Installation
+
+Just `pip install` this, Depencies are currently `pillow`, `mutagen` and `astroid`!
+
+```cmd
+pip install py2scratch
+```
---
+++ packages/py2scratch/py2scratch-0.0.3.dev1.tar.gz/py2scratch-0.0.3.dev1/tests/main.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.insert(0, '..')
+from src.scratch import Project, Stage, Sprite, Sound, Costume, RotationStyles
+
+proj = Project()
+
+Stage = Stage()
+Stage.costumes.append(Costume('crunchi', './assets/background.png'))
+Stage.sounds.append(Sound('blitz', './assets/hyper-velocity.mp3'))
+
+toilet = Sprite('skibidi')
+toilet.rotation = 180
+toilet.rotation_style = RotationStyles.LEFT_RIGHT
+toilet.costumes.append(Costume('toilet', './assets/toilet.svg'))
+
+def _flag_clicked(sprite):
+    print(4)
+
+toilet.funcs.append(_flag_clicked)
+
+proj.add(Stage)
+proj.add(toilet)
+proj.build()
