bb41ddfb6ddbd08e229ed19ffec68fe1527abd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:00 +0000

    Add package cinplus/1.0.1/cinplus-1.0.1.tar.gz

---
+++ packages/cinplus/cinplus-1.0.1.tar.gz/cinplus-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cinplus',
+    version='1.0.1',
+    author='woskethebot',
+    author_email='nushratakhter1999@gmail.com',
+    description='A Python module that extends the basic input function with 16 different parameters and many combinations.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/woskethebot/cinplus',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: Microsoft :: Windows',
+    ],
+    python_requires='>=3.6',
+)
