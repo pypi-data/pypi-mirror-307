541b1fe4f686d0a70591403b0cf342b3deca6fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:02 +0000

    Add package wizata-dsapi/1.0.45/wizata-dsapi-1.0.45.tar.gz

---
+++ packages/wizata-dsapi/wizata-dsapi-1.0.45.tar.gz/wizata-dsapi-1.0.45/wizata_dsapi/streamlit_utils.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def get_streamlit_token():
+    if 'streamlit' in sys.modules:
+        st = sys.modules['streamlit']
+        if st.query_params is not None and "dsapi" in st.query_params and "auth_token" in st.query_params:
+            domain = st.query_params["dsapi"]
+            auth_token = st.query_params["auth_token"]
+            return auth_token
+
+
+def get_streamlit_domain():
+    if 'streamlit' in sys.modules:
+        st = sys.modules['streamlit']
+        if st.query_params is not None and "dsapi" in st.query_params and "auth_token" in st.query_params:
+            domain = st.query_params["dsapi"]
+            auth_token = st.query_params["auth_token"]
+            return auth_token
+
