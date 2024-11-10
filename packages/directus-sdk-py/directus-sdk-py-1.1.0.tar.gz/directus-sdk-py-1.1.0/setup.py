ed4b2f4c47af54fe4b1ea0e1edfdb5274c682e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:00 +0000

    Add package directus-sdk-py/1.1.0/directus-sdk-py-1.1.0.tar.gz

---
+++ packages/directus-sdk-py/directus-sdk-py-1.1.0.tar.gz/directus-sdk-py-1.1.0/setup.py
@@ -0,0 +1,40 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="directus-sdk-py",
+    version="1.1.0",
+    description="Python SDK for interacting with Directus API (colletion, items, users, files)",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/reise69/directus-py-sdk",
+    author="Alban Lamberet",
+    author_email="contact@refbax.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "urllib3",
+        "sqlparse",
+    ],
+    entry_points={
+        "console_scripts": [
+            "directus-sdk-py=directus_sdk_py.__main__:main",
+        ]
+    },
+)
