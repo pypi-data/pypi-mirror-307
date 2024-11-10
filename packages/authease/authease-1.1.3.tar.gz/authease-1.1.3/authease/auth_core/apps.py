b08c06f69f9d3924974fcb73ea5b88cf770f5a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:04 +0000

    Add package authease/1.1.3/authease-1.1.3.tar.gz

---
+++ packages/authease/authease-1.1.3.tar.gz/authease-1.1.3/authease/auth_core/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AuthCoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'auth_ease.auth_core'
+    label = 'auth_core'
