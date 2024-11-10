346726bb0627a566ea4d5f1db8165fb9282727f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 14:04:50 +0000

    Add package topicgpt-python/0.1.2/topicgpt_python-0.1.2-py3-none-any.whl

---
+++ packages/topicgpt-python/topicgpt_python-0.1.2-py3-none-any.whl/topicgpt_python/__init__.py
@@ -0,0 +1,7 @@
+import sys
+
+from .generation_1 import generate_topic_lvl1
+from .generation_2 import generate_topic_lvl2
+from .refinement import refine_topics
+from .assignment import assign_topics
+from .correction import correct_topics
