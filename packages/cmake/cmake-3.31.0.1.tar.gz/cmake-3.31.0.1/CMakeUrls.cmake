9d7bd75992eae0ab09218344b8d0ef86b30e8f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 17:04:55 +0000

    Add package cmake/3.31.0.1/cmake-3.31.0.1.tar.gz

---
+++ packages/cmake/cmake-3.31.0.1.tar.gz/cmake-3.31.0.1/CMakeUrls.cmake
@@ -0,0 +1,29 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url          "https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0.tar.gz")
+set(unix_source_sha256       "300b71db6d69dcc1ab7c5aae61cbc1aa2778a3e00cbd918bc720203e311468c3")
+
+set(windows_source_url       "https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0.zip")
+set(windows_source_sha256    "ff6f76bef08286f3241302e519bb35d4219929ee191b92413bccedf606fbeab4")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url       "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256    "NA")
+
+set(linux64_binary_url       "https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-linux-x86_64.tar.gz")
+set(linux64_binary_sha256    "0fcb338b4515044f9ac77543550ac92c314c58f6f95aafcac5cd36aa75db6924")
+
+set(macos10_10_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-macos10.10-universal.tar.gz")
+set(macos10_10_binary_sha256 "58863bf65412e0fcaacd7dae2fcf0b5f33d78e46d0b84b547c946c59f4d19960")
+
+set(win32_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-windows-i386.zip")
+set(win32_binary_sha256      "62789a29b61c17a5da47a8fda10a4c553c891d4846310addc6b3a76631621026")
+
+set(win64_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-windows-x86_64.zip")
+set(win64_binary_sha256      "f4b499f71cf44115a1d711d4867da3c6bd2f3f4d7121c213ace979757640385c")
+
+set(winarm64_binary_url      "https://github.com/Kitware/CMake/releases/download/v3.31.0/cmake-3.31.0-windows-arm64.zip")
+set(winarm64_binary_sha256   "3bae17886ad47909aadd89e41932a109a8b6a708faef6f28de2d4554c7c08276")
