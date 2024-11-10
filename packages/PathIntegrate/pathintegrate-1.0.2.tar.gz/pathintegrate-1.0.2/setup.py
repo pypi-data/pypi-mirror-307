1d345afa22d4eb4fceb4e55ab4696f13b5546920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 05:05:14 +0000

    Add package PathIntegrate/1.0.2/pathintegrate-1.0.2.tar.gz

---
+++ packages/PathIntegrate/pathintegrate-1.0.2.tar.gz/pathintegrate-1.0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name='PathIntegrate',
+    version='1.0.2',
+    packages=['pathintegrate'],
+    package_dir={'':'src'},
+    url='https://github.com/cwieder/PathIntegrate',
+    license='GNU 3.0',
+    author='Cecilia Wieder',
+    author_email='cw2019@ic.ac.uk',
+    description='PathIntegrate: multivariate modelling approaches for pathway-based muti-omics integration',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_data={'pathintegrate': ['data/*']},
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+    install_requires=[
+        'cmcrameri',
+        'dash',
+        'dash-bootstrap-components',
+        'dash-cytoscape',
+        'datauri',
+        'mbpls',
+        'scikit-learn',
+        'networkx',
+        'setuptools',
+        'sspa>=1.0.1',
+        'statsmodels',
+        'svgwrite'
+    ],
+)
