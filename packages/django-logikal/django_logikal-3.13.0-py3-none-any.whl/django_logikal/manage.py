eb65462250924247101c1a783aa53c4f8c4bdb83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:04 +0000

    Add package django-logikal/3.13.0/django_logikal-3.13.0-py3-none-any.whl

---
+++ packages/django-logikal/django_logikal-3.13.0-py3-none-any.whl/django_logikal/manage.py
@@ -0,0 +1,19 @@
+"""
+Execute a Django management command.
+"""
+import sys
+from collections.abc import Sequence
+from os import environ
+
+from django.core.management import execute_from_command_line
+from logikal_utils.project import tool_config
+
+
+def main(args: Sequence[str] = tuple(sys.argv[1:])) -> int:
+    sys.path.insert(0, '.')
+    environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        tool_config('django_logikal')['DJANGO_SETTINGS_MODULE'],
+    )
+    execute_from_command_line(['manage'] + list(args))
+    return 0
