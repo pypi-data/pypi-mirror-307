7c0c3d428d018937b42bc3112b378ab0b6196194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 15:04:13 +0000

    Add package SquidLibs/0.0.5/squidlibs-0.0.5.tar.gz

---
+++ packages/SquidLibs/squidlibs-0.0.5.tar.gz/squidlibs-0.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# Read the version from the VERSION file
+with open("VERSION") as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name="SquidLibs",
+    version=version,
+    author="Squid Coder",
+    author_email="squid@squidcoder.com",
+    description="A Python library for translation, tkinter Windows, and basic file handling",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SquidCoderIndustries/SquidLibs",
+    packages=find_packages(),
+    package_data={
+        'SquidLibs': ['lang/*', 'lang/**/*'],  # Include lang directory and its contents
+    },
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
