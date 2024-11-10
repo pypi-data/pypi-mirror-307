35387ddf48486a14d9226ce8e653c5bc71efaa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:31 +0000

    Add package python-deprecation-dates/0.0.1/python_deprecation_dates-0.0.1.tar.gz

---
+++ packages/python-deprecation-dates/python_deprecation_dates-0.0.1.tar.gz/python_deprecation_dates-0.0.1/python-deprecation-dates/main.py
@@ -0,0 +1,15 @@
+import requests
+
+ENDOFLIFE_URL = "https://endoflife.date/api/python.json"
+
+def get_deprecation_dates():
+    """
+    Fetches Python version deprecation dates from endoflife.date and returns
+    a dictionary where the keys are the Python versions and values are the deprecation dates.
+    """
+    response = requests.get(ENDOFLIFE_URL)
+    response.raise_for_status()
+
+    data = response.json()
+    deprecation_dates = {item['cycle']: item['eol'] for item in data if item['eol']}
+    return deprecation_dates
