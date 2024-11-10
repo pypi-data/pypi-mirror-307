dd35369920dfcad6ca1cd9f5932c9c219bcb34bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 18:05:00 +0000

    Add package bridgtl-edm-dp-python-library/0.1.2/bridgtl_edm_dp_python_library-0.1.2.tar.gz

---
+++ packages/bridgtl-edm-dp-python-library/bridgtl_edm_dp_python_library-0.1.2.tar.gz/bridgtl_edm_dp_python_library-0.1.2/bridgtl_edm_dp_python_library/data_platform/pyspark/JupyterKubernetesSparkSession.py
@@ -0,0 +1,40 @@
+import os
+
+from .KubernetesSparkSession import KubernetesSparkSession
+
+
+class JupyterKubernetesSparkSession(KubernetesSparkSession):
+
+    def __init__(
+            self,
+            name: str,
+
+            executor_instances: int = 1,
+            executor_cores: int = 1,
+            executor_memory: str = "4g",
+
+            spark_ui_port: int = 4040,
+
+            config: dict | None = None,
+            **kwargs
+    ):
+        config = config or {}
+
+        if os.getenv("KUBERNETES_POD_NAME"):
+            config["spark.kubernetes.driver.pod.name"] = os.getenv("KUBERNETES_POD_NAME")
+
+        # Proxy configurations
+        config["spark.ui.proxyBase"] = f"/user/{os.getenv('JUPYTERHUB_USER')}/vscode/proxy/{spark_ui_port}"
+        config["spark.ui.proxyRedirectUri"] = "/"
+
+        super().__init__(
+            name=name,
+            spark_ui_port=spark_ui_port,
+
+            executor_instances=executor_instances,
+            executor_cores=executor_cores,
+            executor_memory=executor_memory,
+
+            config=config,
+            **kwargs
+        )
