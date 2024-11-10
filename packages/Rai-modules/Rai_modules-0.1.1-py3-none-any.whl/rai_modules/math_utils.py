a7e4cb7eed48cec8c4ac412a957000300890dec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:08:49 +0000

    Add package Rai-modules/0.1.1/Rai_modules-0.1.1-py3-none-any.whl

---
+++ packages/Rai-modules/Rai_modules-0.1.1-py3-none-any.whl/rai_modules/math_utils.py
@@ -0,0 +1,12 @@
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
+def divide_rest(n1, n2):
+    return n1 % n2
+def number(n):
+    return int(n)
