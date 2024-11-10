844aefd22650a89bde792342d3c268fee3f96695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 14:04:52 +0000

    Add package topicgpt-python/0.1.1/topicgpt_python-0.1.1.tar.gz

---
+++ packages/topicgpt-python/topicgpt_python-0.1.1.tar.gz/topicgpt_python-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="topicgpt_python",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=requirements,
+    author="Chau Minh Pham", 
+    author_email="chautm.pham@gmail.com", 
+    description="Official implementation of TopicGPT: A Prompt-based Topic Modeling Framework (NAACL'24)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://chtmp223.github.io/topicGPT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
