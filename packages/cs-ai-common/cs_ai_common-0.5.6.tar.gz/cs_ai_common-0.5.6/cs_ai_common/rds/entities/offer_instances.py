42b841c2a6cc2ad960ede0bfb24a13f573634712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:49 +0000

    Add package cs-ai-common/0.5.6/cs_ai_common-0.5.6.tar.gz

---
+++ packages/cs-ai-common/cs_ai_common-0.5.6.tar.gz/cs_ai_common-0.5.6/cs_ai_common/rds/entities/offer_instances.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timezone
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from cs_ai_common.rds.entities.base import Base
+
+
+class OfferInstance(Base):
+    __tablename__ = "offer_instances"
+
+    id = Column(Integer(), primary_key=True)
+    status = Column(String(50), nullable=False)
+    created_at = Column(DateTime(), default=datetime.now(timezone.utc))
+    external_url = Column(String(50), nullable=False)
+    views = Column(Integer(), default=0)
+    offer_id = Column(Integer(), ForeignKey('offers.id'))
+
+    offer = relationship('Offer', back_populates='offer_instances')
