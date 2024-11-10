1eb5cb57b23db391f48ce1f2540abbcf80ea44c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:01 +0000

    Add package chinu9653s-tool/0.0.1/chinu9653s_tool-0.0.1.tar.gz

---
+++ packages/chinu9653s-tool/chinu9653s_tool-0.0.1.tar.gz/chinu9653s_tool-0.0.1/chinus_tool/tools.py
@@ -0,0 +1,34 @@
+import json
+import json5
+
+
+def load_json(file_path: str, encoding='utf-8') -> dict:
+    with open(file_path, 'r', encoding=encoding) as f:
+        return json.load(f)
+
+
+def dump_json(
+        file_path: str,
+        data: dict,
+        encoding='utf-8',
+        ensure_ascii=False,
+        indent=4
+) -> None:
+    with open(file_path, 'w', encoding=encoding) as f:
+        json.dump(data, f, ensure_ascii=ensure_ascii, indent=indent)
+
+
+def load_json5(file_path: str, encoding='utf-8') -> dict:
+    with open(file_path, 'r', encoding=encoding) as f:
+        return json5.load(f)
+
+
+def dump_json5(
+        file_path: str,
+        data: dict,
+        encoding='utf-8',
+        ensure_ascii=False,
+        indent=4
+) -> None:
+    with open(file_path, 'w', encoding=encoding) as f:
+        json5.dump(data, f, ensure_ascii=ensure_ascii, indent=indent)
---
+++ packages/chinu9653s-tool/chinu9653s_tool-0.0.1.tar.gz/chinu9653s_tool-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import json5
+from setuptools import setup, find_packages
+
+setup(
+    name='chinu9653s_tool',  # 패키지 이름
+    version='0.0.1',  # 버전
+    packages=find_packages(),  # 패키지 자동 검색
+    install_requires=[  # 의존성 라이브러리
+        "json5"
+    ],
+    description='This is a sample Python package.',
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    author='Chinu9653',
+    author_email='juyoung9653@gamil.com',
+    url='https://github.com/juyoung9653/chinu9653s_tool',
+    classifiers=[  # 패키지 정보
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',  # 최소 파이썬 버전
+)
