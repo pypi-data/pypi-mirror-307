d1b6ab10d38e6b62d97a3952355819a11bbd31fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:08:49 +0000

    Add package toolmate/0.4.95/toolmate-0.4.95-py3-none-any.whl

---
+++ packages/toolmate/toolmate-0.4.95-py3-none-any.whl/toolmate/plugins/ask chatgpt.py
@@ -0,0 +1,46 @@
+"""
+ToolMate AI Plugin - ask chatgpt
+
+Ask ChatGPT for conversation only; no function calling
+
+[TOOL_CALL]
+"""
+
+
+from toolmate import config
+from toolmate.utils.call_chatgpt import CallChatGPT
+
+try:
+    CallChatGPT.checkCompletion()
+
+    def ask_chatgpt(function_args):
+        config.stopSpinning()
+        query = function_args.get("query") # required
+        config.currentMessages[-1] = {"role": "user", "content": query}
+        completion = CallChatGPT.regularCall(config.currentMessages)
+        config.toolmate.streamCompletion(completion, openai=True)
+        return ""
+
+    functionSignature = {
+        "examples": [
+            "Ask ChatGPT",
+        ],
+        "name": "ask_chatgpt",
+        "description": "Ask ChatGPT to chat or provide information",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "query": {
+                    "type": "string",
+                    "description": "The original request in detail, including any supplementary information",
+                },
+            },
+            "required": ["query"],
+        },
+    }
+
+    config.addFunctionCall(signature=functionSignature, method=ask_chatgpt)
+    config.inputSuggestions.append("Ask ChatGPT: ")
+
+except:
+    print("Plugin `ask chatgpt` not enabled! Check if your OpenAI API key is valid!")
