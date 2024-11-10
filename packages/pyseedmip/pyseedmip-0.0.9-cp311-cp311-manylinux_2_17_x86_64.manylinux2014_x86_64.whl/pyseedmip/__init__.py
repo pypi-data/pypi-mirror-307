bc99f05f62e295523ecaa6527d091117d2dba9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:09 +0000

    Add package pyseedmip/0.0.9/pyseedmip-0.0.9-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyseedmip/pyseedmip-0.0.9-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyseedmip/__init__.py
@@ -0,0 +1,9 @@
+# import os
+# import sys
+
+# Add the directory containing the .so file to the PATH
+# lib_dir = os.path.join(os.path.dirname(__file__), "..", "lib")
+# if lib_dir not in sys.path:
+#     sys.path.append(lib_dir)
+
+from .pyseedmip import *
