8fad5da5b5b98fe32e95dd896f9ebd2c8258e66d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:04 +0000

    Add package pyqubo/1.5.0/pyqubo-1.5.0-cp312-cp312-win_amd64.whl

---
+++ packages/pyqubo/pyqubo-1.5.0-cp312-cp312-win_amd64.whl/pyqubo/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_8_3():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, '.'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_8_3()
+del _delvewheel_patch_1_8_3
+# end delvewheel patch
+
+import cpp_pyqubo
+from cpp_pyqubo import *
+from pyqubo.utils.asserts import *
+from pyqubo.utils.solver import *
+from .array import *
+from .logical_constraint import *
+from .logic import *
+from pyqubo.integer.integer import *
+from pyqubo.integer.log_encoded_integer import *
+from pyqubo.integer.one_hot_enc_integer import *
+from pyqubo.integer.order_enc_integer import *
+from pyqubo.integer.unary_encoded_integer import *
