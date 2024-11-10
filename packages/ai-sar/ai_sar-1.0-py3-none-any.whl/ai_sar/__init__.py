f0f20ae71a6dadd031c2589020a5c0e5e85a9ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 17:04:36 +0000

    Add package ai-sar/1.0/ai_sar-1.0-py3-none-any.whl

---
+++ packages/ai-sar/ai_sar-1.0-py3-none-any.whl/ai_sar/__init__.py
@@ -0,0 +1,13 @@
+print("""Done!
+
+ai-sar package installed
+
+you can start using it by ai_sar.start()
+
+Project members - Shubham Rashmi Anand
+
+Created by and Owned by Shubham
+
+Year of publish 2024
+
+Thanks for Downloading.........""")
