71869b25d8d99c9ee31f8e4f173a5c663496dd72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:01 +0000

    Add package renardo-gatherer/0.1.4.dev1/renardo_gatherer-0.1.4.dev1.tar.gz

---
+++ packages/renardo-gatherer/renardo_gatherer-0.1.4.dev1.tar.gz/renardo_gatherer-0.1.4.dev1/renardo_gatherer/__init__.py
@@ -0,0 +1,13 @@
+from renardo_gatherer.config_dir import SAMPLES_DIR_PATH
+from renardo_gatherer.collections import DEFAULT_SAMPLES_PACK_NAME
+
+
+def main():
+    pass
+
+
+# def get_spack_dirname_from_num(spack_num):
+#     '''associate samples pack directory names in sample folder with pack numbers'''
+#     samples_pack_dirname = ('foxdot_default' if str(spack_num) == '0'
+#                             else str(spack_num))
+#     return samples_pack_dirname
