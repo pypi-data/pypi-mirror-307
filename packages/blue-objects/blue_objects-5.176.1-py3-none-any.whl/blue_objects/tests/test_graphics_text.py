e1c3d43c8b9c6a3f5bef630ade0d3ef42ec3e8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:35 +0000

    Add package blue-objects/5.176.1/blue_objects-5.176.1-py3-none-any.whl

---
+++ packages/blue-objects/blue_objects-5.176.1-py3-none-any.whl/blue_objects/tests/test_graphics_text.py
@@ -0,0 +1,16 @@
+import pytest
+import numpy as np
+
+from blue_objects import file, objects
+from blue_objects.graphics.text import render_text
+from blue_objects.env import VANWATCH_TEST_OBJECT, DUMMY_TEXT
+
+
+def test_graphics_text_render_text():
+    assert isinstance(
+        render_text(
+            text=[DUMMY_TEXT for _ in range(10)],
+            image_width=1024,
+        ),
+        np.ndarray,
+    )
