e26fe3e9ab5cda93d87b6ff7c5f90ef31d3df80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 05:05:21 +0000

    Add package armodel/1.6.0/armodel-1.6.0-py3-none-any.whl

---
+++ packages/armodel/armodel-1.6.0-py3-none-any.whl/armodel/models/datatype.py
@@ -0,0 +1,227 @@
+from abc import ABCMeta
+from typing import List
+
+from .ar_object import ARLiteral, ARNumerical
+from .ar_ref import RefType
+from .ar_package import Referrable
+from .general_structure import ARElement, ARObject
+from .data_prototype import ApplicationCompositeElementDataPrototype, ApplicationRecordElement
+from .common_structure import ImplementationDataTypeElement, ModeRequestTypeMap
+
+class ImplementationProps(Referrable, metaclass=ABCMeta):
+    def __init__(self, parent: ARObject, short_name: str):
+        if type(self) == ImplementationProps:
+            raise NotImplementedError("ImplementationProps is an abstract class.")
+
+        super().__init__(parent, short_name)
+        self.symbol = ""
+
+class SymbolProps(ImplementationProps):
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+
+class BaseTypeDefinition(ARObject):
+    def __init__(self):
+        super().__init__()
+    
+class BaseTypeDirectDefinition(BaseTypeDefinition):
+    def __init__(self):
+        super().__init__()
+
+        self.base_type_encoding = None
+        self.base_type_size = None                # type: ARNumerical
+        self.byteOrder = None                   # type: str
+        self.mem_alignment = None                # type: ARNumerical
+        self.native_declaration = None      
+
+class BaseType(ARElement, metaclass=ABCMeta):
+    def __init__(self, parent: ARObject, short_name: str):
+        if type(self) == BaseType:
+            raise NotImplementedError("BaseType is an abstract class.")
+
+        super().__init__(parent, short_name)
+
+        self.baseTypeDefinition = BaseTypeDirectDefinition()
+
+class SwBaseType(BaseType):
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+
+class AtpType(ARElement, metaclass=ABCMeta):
+    def __init__(self, parent: ARObject, short_name: str):
+        if type(self) == AtpType:
+            raise NotImplementedError("AtpType is an abstract class.")
+
+        super().__init__(parent, short_name)
+
+
+class AutosarDataType(AtpType, metaclass = ABCMeta):
+    def __init__(self, parent: ARObject, short_name: str):
+        if type(self) == AutosarDataType:
+            raise NotImplementedError("AutosarDataType is an abstract class.")
+
+        super().__init__(parent, short_name)
+
+        self.swDataDefProps = None                  # type: SwDataDefProps
+
+    def getSwDataDefProps(self):
+        return self.swDataDefProps
+
+    def setSwDataDefProps(self, value):
+        self.swDataDefProps = value
+        return self
+
+class ApplicationDataType(AutosarDataType, metaclass = ABCMeta):
+    def __init__(self, parent: ARObject, short_name: str):
+        if type(self) == ApplicationDataType:
+            raise NotImplementedError("ApplicationDataType is an abstract class.")
+
+        super().__init__(parent, short_name)
+
+
+class ApplicationPrimitiveDataType(ApplicationDataType):
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+
+
+class ApplicationCompositeDataType(ApplicationDataType, metaclass=ABCMeta):
+    def __init__(self, parent: ARObject, short_name: str):
+        if type(self) == ApplicationCompositeDataType:
+            raise NotImplementedError("ApplicationCompositeDataType is an abstract class.")
+
+        super().__init__(parent, short_name)
+
+class ApplicationArrayElement(ApplicationCompositeElementDataPrototype):
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+
+        self.arraySizeHandling = None               # type: str
+        self.arraySizeSemantics = None              # type: str
+        self.indexDataTypeRef = None                # type: RefType
+        self.maxNumberOfElements = None             # type: ARNumerical
+
+    def setArraySizeHandling(self, handling: str):
+        self.arraySizeHandling = handling
+        return self
+    
+    def setArraySizeSemantics(self, semantics: str):
+        self.arraySizeSemantics = semantics
+        return self
+    
+    def setIndexDataTypeRef(self, ref: RefType):
+        self.indexDataTypeRef = ref
+        return self
+    
+    def setMaxNumberOfElements(self, number: ARNumerical):
+        self.maxNumberOfElements = number
+        return self
+
+class ApplicationArrayDataType(ApplicationCompositeDataType):
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+
+        self.dynamic_array_size_profile = None      # type: ARLiteral
+        self.element = None                         # type: ApplicationArrayElement
+
+    def createApplicationArrayElement(self, short_name: str) -> ApplicationArrayElement:
+        if (short_name not in self.elements):
+            array_element = ApplicationArrayElement(self, short_name)
+            self.elements[short_name] = array_element
+            self.element = self.elements[short_name]
+        return self.elements[short_name]
+    
+class ApplicationRecordDataType(ApplicationCompositeDataType):
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+
+        self.record_elements = []
+
+    def createApplicationRecordElement(self, short_name: str) -> ApplicationRecordElement:
+        if (short_name not in self.elements):
+            record_element = ApplicationRecordElement(self, short_name)
+            self.elements[short_name] = record_element
+            self.record_elements.append(self.elements[short_name])
+        return self.elements[short_name]
+
+    def getApplicationRecordElements(self) -> List[ApplicationRecordElement]:
+        return self.record_elements
+
+class AbstractImplementationDataType(AutosarDataType, metaclass=ABCMeta):
+    def __init__(self, parent: ARObject, short_name: str):
+        if type(self) == AbstractImplementationDataType:
+            raise NotImplementedError("AbstractImplementationDataType is an abstract class.")
+
+        super().__init__(parent, short_name)
+
+
+class ImplementationDataType(AbstractImplementationDataType):
+    CATEGORY_TYPE_REFERENCE = "TYPE_REFERENCE"
+    CATEGORY_TYPE_VALUE = "VALUE"
+    CATEGORY_TYPE_STRUCTURE = "STRUCTURE"
+    CATEGORY_DATA_REFERENCE = "DATA_REFERENCE"
+    CATEGORY_ARRAY = "ARRAY"
+
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+        
+        self.sub_elements = []      # type: List[str]
+        self.symbol_props = None    # type: ARLiteral
+        self._type_emitter = None    # type: ARLiteral
+
+        self._array_type = None     # ImplementationDataType
+
+    #type:  ImplementationDataTypeElement
+    def createImplementationDataTypeElement(self, short_name: str):
+        self.sub_elements.append(short_name)
+        if (short_name not in self.elements):
+            event = ImplementationDataTypeElement(self, short_name)
+            self.elements[short_name] = event
+        return self.elements[short_name]
+
+    def getImplementationDataTypeElements(self) -> List[ImplementationDataTypeElement]:
+        elements = []
+        for sub_element in self.sub_elements:
+            elements.append(self.elements[sub_element])
+        return elements
+        # return filter(lambda c: isinstance(c, ImplementationDataTypeElement), self.elements.values())
+
+    def getArrayElementType(self) -> str:
+        return self._array_type
+
+    def setArrayElementType(self, type: str):
+        self._array_type = type
+        return self
+
+    def setTypeEmitter(self, emitter: str):
+        self._type_emitter = emitter
+        return self
+    
+    def getTypeEmitter(self) -> str:
+        return self._type_emitter
+
+
+class DataTypeMap(ARObject):
+    def __init__(self):
+        self.application_data_type_ref = None       # type: RefType
+        self.implementation_data_type_ref = None    # type: RefType
+
+class DataTypeMappingSet(ARElement):
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+        
+        self._dataTypeMaps = []                     # type: List[DataTypeMap]
+        self._modeRequestTypeMaps = []              # type: List[ModeRequestTypeMap]
+
+    def addDataTypeMap(self, type_map: DataTypeMap):
+        self._dataTypeMaps.append(type_map)
+
+    def getDataTypeMaps(self) -> List[DataTypeMap]:
+        return self._dataTypeMaps
+    
+    def addModeRequestTypeMap(self, map: ModeRequestTypeMap):
+        self._modeRequestTypeMaps.append(map)
+
+    def getModeRequestTypeMaps(self) -> List[ModeRequestTypeMap]:
+        return self._modeRequestTypeMaps
+    
+
---
+++ packages/armodel/armodel-1.6.0-py3-none-any.whl/armodel/tests/test_armodel/models/test_ar_ref.py
@@ -0,0 +1,74 @@
+""" Test ARRef """
+
+import pytest
+
+from ....models.ar_ref import ArVariableInImplementationDataInstanceRef, AutosarVariableRef, PPortInCompositionInstanceRef, RPortInCompositionInstanceRef
+from ....models.ar_ref import OperationInAtomicSwcInstanceRef, POperationInAtomicSwcInstanceRef, ROperationInAtomicSwcInstanceRef, RefType, TRefType, AtpInstanceRef
+from ....models.general_structure import ARObject
+
+
+class TestARRef:
+    def test_RefType(self):
+        ref_type = RefType()
+        assert(ref_type.value == "")
+        assert(ref_type.dest == "")
+
+    def test_TRefType(self):
+        ref_type = TRefType()
+        assert(ref_type.value == "")
+        assert(ref_type.dest == "")
+
+    def test_AutosarVariableRef(self):
+        ref_type = AutosarVariableRef()
+        assert(ref_type != None)
+        assert(ref_type.autosarVariableIRef == None)
+        assert(ref_type.autosarVariableInImplDatatype == None)
+        assert(ref_type.localVariableRef == None)
+
+    def test_AtpInstanceRef(self):
+        with pytest.raises(NotImplementedError) as err:
+            ref_type = AtpInstanceRef()
+        assert(str(err.value) == "AtpInstanceRef is an abstract class.")
+
+    def test_ProvidedPortPrototypeInstanceRef(self):
+        ref_type = PPortInCompositionInstanceRef()
+        assert(ref_type != None)
+        assert(ref_type.context_component_ref == None)
+        assert(ref_type.target_p_port_ref == None)
+
+    def test_RequiredPortPrototypeInstanceRef(self):
+        ref_type = RPortInCompositionInstanceRef()
+        assert(ref_type != None)
+        assert(ref_type.context_component_ref == None)
+        assert(ref_type.target_r_port_ref == None)
+
+    def test_ArVariableInImplementationDataInstanceRef(self):
+        ref_type = ArVariableInImplementationDataInstanceRef()
+        assert(ref_type != None)
+        assert(ref_type.port_prototype_ref == None)
+        assert(ref_type.target_data_prototype_ref == None)
+
+    def test_OperationInAtomicSwcInstanceRef(self):
+        with pytest.raises(NotImplementedError) as err:
+            ref_type = OperationInAtomicSwcInstanceRef()
+        assert(str(err.value) == "OperationInAtomicSwcInstanceRef is an abstract class.")
+
+    def test_POperationInAtomicSwcInstanceRef(self):
+        ref_type = POperationInAtomicSwcInstanceRef()
+        assert(isinstance(ref_type, ARObject))
+        assert(isinstance(ref_type, AtpInstanceRef))
+        assert(isinstance(ref_type, OperationInAtomicSwcInstanceRef))
+        assert(isinstance(ref_type, POperationInAtomicSwcInstanceRef))
+        assert(ref_type != None)
+        assert(ref_type.context_p_port_ref == None)
+        assert(ref_type.target_provided_operation_ref == None)
+
+    def test_ROperationInAtomicSwcInstanceRef(self):
+        ref_type = ROperationInAtomicSwcInstanceRef()
+        assert(isinstance(ref_type, ARObject))
+        assert(isinstance(ref_type, AtpInstanceRef))
+        assert(isinstance(ref_type, OperationInAtomicSwcInstanceRef))
+        assert(isinstance(ref_type, ROperationInAtomicSwcInstanceRef))
+        assert(ref_type != None)
+        assert(ref_type.context_r_port_ref == None)
+        assert(ref_type.target_required_operation_ref == None)
