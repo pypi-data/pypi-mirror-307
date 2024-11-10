01e5d93a61dbf538b476ae47e5077984206b3964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 14:04:40 +0000

    Add package packaging-version-increment/0.1.1/packaging_version_increment-0.1.1-py3-none-any.whl

---
+++ packages/packaging-version-increment/packaging_version_increment-0.1.1-py3-none-any.whl/packaging_version_increment/__init__.py
@@ -0,0 +1,9 @@
+from importlib import metadata as _metadata
+
+from packaging_version_increment.increment import IncrementEnum, increment_version  # noqa: F401
+
+try:
+    __version__ = _metadata.version('packaging-version-increment')
+
+except _metadata.PackageNotFoundError:  # pragma: no cover
+    __version__ = '0.0.0'
