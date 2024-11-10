8c06102c1a8a7b17e0c58fdb21d064164b01f873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 14:04:40 +0000

    Add package reworkd-platform/0.1.0/reworkd_platform-0.1.0-py3-none-any.whl

---
+++ packages/reworkd-platform/reworkd_platform-0.1.0-py3-none-any.whl/reworkd_platform/web/api/agent/tools/utils.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass
+from typing import List
+
+from fastapi.responses import StreamingResponse as FastAPIStreamingResponse
+from lanarky.responses import StreamingResponse
+from ollama import Ollama
+
+
+@dataclass
+class CitedSnippet:
+    index: int
+    text: str
+    url: str = ""
+
+    def __repr__(self) -> str:
+        """
+        The string representation the AI model will see
+        """
+        return f"{{i: {self.index}, text: {self.text}, url: {self.url}}}"
+
+
+@dataclass
+class Snippet:
+    text: str
+
+    def __repr__(self) -> str:
+        """
+        The string representation the AI model will see
+        """
+        return f"{{text: {self.text}}}"
+
+
+def summarize(
+    model: Ollama,
+    language: str,
+    goal: str,
+    text: str,
+) -> FastAPIStreamingResponse:
+    from reworkd_platform.web.api.agent.prompts import summarize_prompt
+
+    chain = model.create_chain(prompt=summarize_prompt)
+
+    return StreamingResponse.from_chain(
+        chain,
+        {
+            "goal": goal,
+            "language": language,
+            "text": text,
+        },
+        media_type="text/event-stream",
+    )
+
+
+def summarize_with_sources(
+    model: Ollama,
+    language: str,
+    goal: str,
+    query: str,
+    snippets: List[CitedSnippet],
+) -> FastAPIStreamingResponse:
+    from reworkd_platform.web.api.agent.prompts import summarize_with_sources_prompt
+
+    chain = model.create_chain(prompt=summarize_with_sources_prompt)
+
+    return StreamingResponse.from_chain(
+        chain,
+        {
+            "goal": goal,
+            "query": query,
+            "language": language,
+            "snippets": snippets,
+        },
+        media_type="text/event-stream",
+    )
+
+
+def summarize_sid(
+    model: Ollama,
+    language: str,
+    goal: str,
+    query: str,
+    snippets: List[Snippet],
+) -> FastAPIStreamingResponse:
+    from reworkd_platform.web.api.agent.prompts import summarize_sid_prompt
+
+    chain = model.create_chain(prompt=summarize_sid_prompt)
+
+    return StreamingResponse.from_chain(
+        chain,
+        {
+            "goal": goal,
+            "query": query,
+            "language": language,
+            "snippets": snippets,
+        },
+        media_type="text/event-stream",
+    )
