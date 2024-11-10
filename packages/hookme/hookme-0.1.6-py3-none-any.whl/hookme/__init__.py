8a9221a51cd2b64f41b74a56aa935826d0f157c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 14:04:48 +0000

    Add package hookme/0.1.6/hookme-0.1.6-py3-none-any.whl

---
+++ packages/hookme/hookme-0.1.6-py3-none-any.whl/hookme/__init__.py
@@ -0,0 +1,6 @@
+from .d_webhook import DiscordWebhook
+from .automated_webhook import AutomatedWebhook
+from .exceptions import WebhookError
+
+__version__ = "0.1.6" # want to remove this from the package
+__all__ = ["DiscordWebhook", "WebhookError", "AutomatedWebhook"] 
