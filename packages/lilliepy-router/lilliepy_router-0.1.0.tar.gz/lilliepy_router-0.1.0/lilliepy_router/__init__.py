8420503be23b1b42294607cd90e5b7f1b6505769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 14:04:42 +0000

    Add package lilliepy-router/0.1.0/lilliepy_router-0.1.0.tar.gz

---
+++ packages/lilliepy-router/lilliepy_router-0.1.0.tar.gz/lilliepy_router-0.1.0/lilliepy_router/__init__.py
@@ -0,0 +1,3 @@
+from .main import use_dynamic_router, use_router, returnurl, goto
+
+__all__ = ["use_dynamic_router", "use_router", "returnurl", "goto"]
---
+++ packages/lilliepy-router/lilliepy_router-0.1.0.tar.gz/lilliepy_router-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lilliepy-router",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "flask>=3.0.3"
+    ],
+)
