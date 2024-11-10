838f855dc93b9d14f25dde703c34e609cd6522c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:31 +0000

    Add package beametrics/0.2.3/beametrics-0.2.3-py3-none-any.whl

---
+++ packages/beametrics/beametrics-0.2.3-py3-none-any.whl/beametrics/metrics.py
@@ -0,0 +1,44 @@
+import json
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Dict, Optional, Union
+
+from apache_beam.options.value_provider import StaticValueProvider, ValueProvider
+
+
+class MetricType(Enum):
+    """Types of metrics that can be generated"""
+
+    COUNT = "count"
+    SUM = "sum"
+
+
+@dataclass
+class MetricDefinition:
+    name: str
+    type: Union[MetricType, ValueProvider]
+    field: Optional[str]
+    metric_labels: Dict[str, str]
+    dynamic_labels: Optional[Union[Dict[str, str], ValueProvider]] = None
+
+    def __post_init__(self):
+        if isinstance(self.type, ValueProvider):
+            if isinstance(self.type, StaticValueProvider):
+                type_value = self.type.get().lower()
+                if type_value == "sum" and self.field is None:
+                    raise ValueError(f"field is required for {type_value} metric type")
+
+        elif self.type in [MetricType.SUM] and self.field is None:
+            raise ValueError(f"field is required for {self.type.value} metric type")
+
+        if not isinstance(self.dynamic_labels, ValueProvider):
+            self.dynamic_labels = self.dynamic_labels or {}
+
+    def get_dynamic_labels(self) -> Dict[str, str]:
+        """Get resolved dynamic labels"""
+        if isinstance(self.dynamic_labels, ValueProvider):
+            try:
+                return json.loads(self.dynamic_labels.get())
+            except Exception:
+                return {}
+        return self.dynamic_labels or {}
