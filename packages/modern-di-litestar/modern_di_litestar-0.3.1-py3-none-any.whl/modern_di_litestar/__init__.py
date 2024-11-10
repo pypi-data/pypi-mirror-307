3339a2352bc2b0ed691068a58c97f89a9a0a20f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 03:09:12 +0000

    Add package modern-di-litestar/0.3.1/modern_di_litestar-0.3.1-py3-none-any.whl

---
+++ packages/modern-di-litestar/modern_di_litestar-0.3.1-py3-none-any.whl/modern_di_litestar/__init__.py
@@ -0,0 +1,9 @@
+from modern_di_litestar.main import FromDI, di_middleware_factory, fetch_di_container, setup_di
+
+
+__all__ = [
+    "FromDI",
+    "di_middleware_factory",
+    "fetch_di_container",
+    "setup_di",
+]
