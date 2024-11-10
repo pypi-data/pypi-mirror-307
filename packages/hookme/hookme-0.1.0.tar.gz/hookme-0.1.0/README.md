50743fa41d667942499507ca3e20f21e12231957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 05:05:24 +0000

    Add package hookme/0.1.0/hookme-0.1.0.tar.gz

---
+++ packages/hookme/hookme-0.1.0.tar.gz/hookme-0.1.0/README.md
@@ -0,0 +1,5 @@
+# HookMe
+
+A powerful and easy-to-use Python package for handling Discord and Slack webhooks.
+
+## Installation 
---
+++ packages/hookme/hookme-0.1.0.tar.gz/hookme-0.1.0/hookme/__init__.py
@@ -0,0 +1,5 @@
+from .d_webhook import DiscordWebhook
+from .exceptions import WebhookError
+
+__version__ = "0.1.0"
+__all__ = ["DiscordWebhook", "WebhookError"] 
