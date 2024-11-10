8517302964017d8b95d4c6d4aefa61379a003bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:33 +0000

    Add package automatically-change-wallpaper/1.1/automatically_change_wallpaper-1.1.tar.gz

---
+++ packages/automatically-change-wallpaper/automatically_change_wallpaper-1.1.tar.gz/automatically_change_wallpaper-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as txt:
+    description = txt.read()
+
+setup(
+    name='automatically_change_wallpaper',
+    version='1.1', 
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    entry_points={
+        "console_scripts": [
+            "acw = automatically_change_wallpaper:main",
+        ]
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
