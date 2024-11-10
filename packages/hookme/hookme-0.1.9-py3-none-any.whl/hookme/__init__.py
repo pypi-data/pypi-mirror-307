625a394c93e9941be04305d32cf6ce3dd34045f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 15:04:13 +0000

    Add package hookme/0.1.9/hookme-0.1.9-py3-none-any.whl

---
+++ packages/hookme/hookme-0.1.9-py3-none-any.whl/hookme/__init__.py
@@ -0,0 +1,5 @@
+from .d_webhook import DiscordWebhook
+from .automated_webhook import AutomatedWebhook
+from .exceptions import WebhookError
+
+__all__ = ["DiscordWebhook", "WebhookError", "AutomatedWebhook"] 
