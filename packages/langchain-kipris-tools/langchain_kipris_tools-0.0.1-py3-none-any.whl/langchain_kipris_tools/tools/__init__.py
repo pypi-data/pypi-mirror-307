8bbaceab32321dab71f865f8af22a554c3a37b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 18:04:59 +0000

    Add package langchain-kipris-tools/0.0.1/langchain_kipris_tools-0.0.1-py3-none-any.whl

---
+++ packages/langchain-kipris-tools/langchain_kipris_tools-0.0.1-py3-none-any.whl/langchain_kipris_tools/tools/__init__.py
@@ -0,0 +1,5 @@
+from .applicant_search_tool import ApplicantSearchTool
+from .patent_keyword_search_tool import PatentKeywordSearchTool
+from .patent_search_tool import PatentSearchTool
+
+tools = [ApplicantSearchTool, PatentKeywordSearchTool, PatentSearchTool]
