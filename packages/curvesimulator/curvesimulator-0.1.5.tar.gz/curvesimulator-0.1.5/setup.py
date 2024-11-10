6be49cd4c849156a3443f15487c1f10e3b163dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 04:04:47 +0000

    Add package curvesimulator/0.1.5/curvesimulator-0.1.5.tar.gz

---
+++ packages/curvesimulator/curvesimulator-0.1.5.tar.gz/curvesimulator-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="curvesimulator",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        "numpy",
+        "matplotlib",
+        "configparser",
+        # "math",
+        # "time",
+        # "sys",
+    ],
+    author="Uli Scheuss",
+    description="Curvesimulator calculates the movements and eclipses of celestial bodies and produces a video of this and of the resulting lightcurve.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lichtgestalter/curvesimulator",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
