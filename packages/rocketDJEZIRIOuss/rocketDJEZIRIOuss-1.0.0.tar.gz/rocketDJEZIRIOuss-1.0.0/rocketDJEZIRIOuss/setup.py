db83dc14b3b5d59cc75ae139ed0139231d028355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 14:04:39 +0000

    Add package rocketDJEZIRIOuss/1.0.0/rocketDJEZIRIOuss-1.0.0.tar.gz

---
+++ packages/rocketDJEZIRIOuss/rocketDJEZIRIOuss-1.0.0.tar.gz/rocketDJEZIRIOuss-1.0.0/rocketDJEZIRIOuss/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='OussDjeziri',
+   version='1.0.1',
+   author='OusDjeziri',
+   author_email='o.djeziri@esi-sba.dz',
+   packages=['rocket'],
+   url='http://pypi.python.org/pypi/rocket/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['rocket']
+)
---
+++ packages/rocketDJEZIRIOuss/rocketDJEZIRIOuss-1.0.0.tar.gz/rocketDJEZIRIOuss-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='rocketDJEZIRIOuss',
+   version='1.0.0',
+   author='OusDjeziri',
+   author_email='o.djeziri@esi-sba.dz',
+   packages=['rocketDJEZIRIOuss'],
+   url='http://pypi.python.org/pypi/rocketDJEZIRIOuss/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['rocketDJEZIRIOuss']
+)
