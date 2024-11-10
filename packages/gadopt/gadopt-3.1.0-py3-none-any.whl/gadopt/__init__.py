656b891345d993f91aed7abcb883b4a30449d923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 18:05:00 +0000

    Add package gadopt/3.1.0/gadopt-3.1.0-py3-none-any.whl

---
+++ packages/gadopt/gadopt-3.1.0-py3-none-any.whl/gadopt/__init__.py
@@ -0,0 +1,35 @@
+from firedrake import *
+from firedrake.output import VTKFile
+
+from .approximations import (
+    AnelasticLiquidApproximation,
+    BoussinesqApproximation,
+    ExtendedBoussinesqApproximation,
+    TruncatedAnelasticLiquidApproximation,
+    SmallDisplacementViscoelasticApproximation,
+)
+from .diagnostics import GeodynamicalDiagnostics
+from .energy_solver import EnergySolver
+from .level_set_tools import (
+    LevelSetSolver,
+    Material,
+    density_RaB,
+    entrainment,
+    field_interface,
+)
+from .limiter import VertexBasedP1DGLimiter
+from .preconditioners import FreeSurfaceMassInvPC, SPDAssembledPC
+from .stokes_integrators import StokesSolver, ViscoelasticStokesSolver, create_stokes_nullspace
+from .time_stepper import BackwardEuler, CrankNicolsonRK, ImplicitMidpoint, eSSPRKs3p3, eSSPRKs10p3
+from .utility import (
+    InteriorBC,
+    LayerAveraging,
+    ParameterLog,
+    TimestepAdaptor,
+    interpolate_1d_profile,
+    log,
+    node_coordinates,
+    timer_decorator,
+)
+
+PETSc.Sys.popErrorHandler()
