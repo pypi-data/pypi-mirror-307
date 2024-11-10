16b6f62e98ced952926ac98feec6572bd16b3225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 16:04:20 +0000

    Add package botasaurus/4.0.75/botasaurus-4.0.75.tar.gz

---
+++ packages/botasaurus/botasaurus-4.0.75.tar.gz/botasaurus-4.0.75/botasaurus/decorators_utils.py
@@ -0,0 +1,18 @@
+from os import path, makedirs
+import os
+
+def relative_path(path, goback=0):
+    levels = [".."] * (goback + -1)
+    return os.path.abspath(os.path.join(os.getcwd(), *levels, path.strip()))
+
+def create_directory_if_not_exists(passed_path):
+    dir_path =  relative_path(passed_path)
+    if not path.exists(dir_path):
+        makedirs(dir_path)
+
+output_check_done = False
+def create_output_directory_if_not_exists():
+    global output_check_done
+    if not output_check_done:
+        output_check_done = True
+        create_directory_if_not_exists('output/')
