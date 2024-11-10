86c8dbf642a4d22d3cda1de0a9d505b5537d3cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 16:04:16 +0000

    Add package aiwolf-nlp-common/0.0.3/aiwolf_nlp_common-0.0.3.tar.gz

---
+++ packages/aiwolf-nlp-common/aiwolf_nlp_common-0.0.3.tar.gz/aiwolf_nlp_common-0.0.3/tests/AIWolfNLAgentPython/player/seer.py
@@ -0,0 +1,69 @@
+import configparser
+
+from AIWolfNLAgentPython.player.agent import Agent
+
+from aiwolf_nlp_common import util
+
+
+class Seer(Agent):
+
+    def __init__(self, inifile: configparser.ConfigParser, name: str, is_hand_over: bool = False):
+        super().__init__(inifile, name, is_hand_over)
+
+    @Agent.with_timelimit
+    def parse_info(self, receive: str) -> None:
+        return super().parse_info(receive)
+
+    def get_info(self):
+        return super().get_info()
+
+    def initialize(self) -> None:
+        return super().initialize()
+
+    def daily_initialize(self) -> None:
+        return super().daily_initialize()
+
+    def daily_finish(self) -> None:
+        return super().daily_finish()
+
+    @Agent.with_timelimit
+    def get_name(self) -> str:
+        return super().get_name()
+
+    @Agent.with_timelimit
+    def get_role(self) -> str:
+        return super().get_role()
+
+    @Agent.with_timelimit
+    def talk(self) -> str:
+
+        if self.gameInfo is not None and self.gameInfo.get("divineResult") is not None:
+            divine_result:str = self.gameInfo["divineResult"]
+            self.logger.divine_result(divine_result=divine_result)
+            return divine_result.get("result")
+
+        return super().talk()
+
+    @Agent.with_timelimit
+    def vote(self) -> int:
+        return super().vote()
+
+    @Agent.with_timelimit
+    def whisper(self) -> None:
+        return super().whisper()
+
+    @Agent.with_timelimit
+    @Agent.send_agent_index
+    def divine(self) -> int:
+        divine_target:int = util.random_select(self.alive)
+        self.logger.divine(divine_target=divine_target)
+        return divine_target
+
+    def action(self) -> str:
+
+        if self.request == "DIVINE":
+            return self.divine()
+        return super().action()
+
+    def hand_over(self, new_agent) -> None:
+        return super().hand_over(new_agent)
