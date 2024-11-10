b8a821cf3685c4ea9efeaa7a10a3215028f5cbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 05:05:13 +0000

    Add package artifact-lab-3-package-736f752d/0.1.4/artifact_lab_3_package_736f752d-0.1.4.tar.gz

---
+++ packages/artifact-lab-3-package-736f752d/artifact_lab_3_package_736f752d-0.1.4.tar.gz/artifact_lab_3_package_736f752d-0.1.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='artifact-lab-3-package-736f752d',
+        version='0.1.4',
+        packages=find_packages(),
+        install_requires=[
+            'requests>=2.32.3',
+            ],
+        )
