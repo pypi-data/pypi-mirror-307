4a64f5403b32d5ae39617aea6d8b39b8cfe6b650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 14:04:40 +0000

    Add package qe-api-client/2.3.0/qe_api_client-2.3.0.tar.gz

---
+++ packages/qe-api-client/qe_api_client-2.3.0.tar.gz/qe_api_client-2.3.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qe-api-client",
+    version="2.3.0",
+    author="Rumen Vasilev",
+    author_email="R.Vasilev@LRWorld.com",
+    description="Python wrapper around Qlik Engine JSON API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lr-bicc/qe-api-client",
+    package_dir={'qe_api_client': 'qe_api_client'},
+    packages=setuptools.find_packages(exclude=["test", "examples", "docs"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'websocket-client>=0.47.0'
+    ],
+    python_requires='>=3.6',
+)
