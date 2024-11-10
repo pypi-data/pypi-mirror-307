1d2635351107d8742eb366425c52c0cced0a104d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 05:05:22 +0000

    Add package utz/0.11.1/utz-0.11.1.tar.gz

---
+++ packages/utz/utz-0.11.1.tar.gz/utz-0.11.1/utz/fn.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from inspect import getfullargspec
+from typing import Callable, Sequence
+
+Deco = Callable[[Callable], Callable]
+
+
+def decos(*args: Deco | Sequence[Deco]):
+    """Compose decorators."""
+    decos = [
+        deco
+        for decos in args
+        for deco in (decos if isinstance(decos, Sequence) else [decos])
+    ]
+
+    def _fn(fn):
+        for deco in reversed(decos):
+            fn = deco(fn)
+        return fn
+
+    return _fn
+
+
+def args(fn, kwargs):
+    """Filter kwargs to match function signature."""
+    spec = getfullargspec(fn)
+    args = spec.args
+    return { k: v for k, v in kwargs.items() if k in args }
+
+
+def call(fn, *_args, **kwargs):
+    """Call a function with only the kwargs that it is able to receive."""
+    return fn(*_args, args(fn, **kwargs))
