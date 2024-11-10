0dfa9b9e7040d826d71d6e51be12bbae053a388a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-10 15:04:17 +0000

    Add package free-range-rust/0.1.1/free_range_rust-0.1.1.tar.gz

---
+++ packages/free-range-rust/free_range_rust-0.1.1.tar.gz/free_range_rust-0.1.1/src/main.rs
@@ -0,0 +1,30 @@
+//pub mod bindings;
+//pub mod ipomcppf;
+
+use simple_logger::SimpleLogger;
+
+//use crate::bindings::spaces::Space;
+//use crate::ipomcppf::nodes::BeliefNode;
+
+fn main() {
+    let _ = SimpleLogger::new().without_timestamps().init().unwrap();
+
+    //let head = BeliefNode::new(0);
+    //let action_space = Space::new_one_of(vec![
+    //    Space::new_discrete(3, 0),
+    //    Space::new_discrete(3, 0),
+    //    Space::new_one_of(vec![
+    //        Space::new_discrete(2, 0),
+    //        Space::new_one_of(vec![Space::new_discrete(5, 0)]),
+    //    ]),
+    //]);
+
+    //BeliefNode::create_action_nodes(&head, action_space.clone(), vec![]);
+
+    //let head_borrow = head.borrow();
+    //head_borrow.show();
+    //head_borrow.show_with_depth(4);
+
+    //let pf = &head_borrow.data.as_belief().unwrap().particle_filter;
+    //println!("{}", pf);
+}
