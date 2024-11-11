b9fabe21f63a9585dd42bffe2244fcc70c850d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 16:06:06 +0000

    Add package django-design-system/0.0.2/django_design_system-0.0.2-py3-none-any.whl

---
+++ packages/django-design-system/django_design_system-0.0.2-py3-none-any.whl/django_design_system/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoDesignSystemConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_design_system"
+    verbose_name = "Système de design générique sur la base de celui de l’État français"
---
+++ packages/django-design-system/django_design_system-0.0.2-py3-none-any.whl/django_design_system/static/design-system/dist/component/button/button.module.js
@@ -0,0 +1,22 @@
+/*! DESIGN-SYSTEM v0.0.3 | SPDX-License-Identifier: MIT | License-Filename: LICENSE.md | restricted use (see terms and conditions) */
+
+const config = {
+  prefix: 'design-system',
+  namespace: 'design-system',
+  version: '0.0.3'
+};
+
+const api = window[config.namespace];
+
+const ButtonSelector = {
+  EQUISIZED_BUTTON: `${api.internals.ns.selector('btns-group--equisized')} ${api.internals.ns.selector('btn')}`,
+  EQUISIZED_GROUP: api.internals.ns.selector('btns-group--equisized')
+};
+
+api.button = {
+  ButtonSelector: ButtonSelector
+};
+
+api.internals.register(api.button.ButtonSelector.EQUISIZED_BUTTON, api.core.Equisized);
+api.internals.register(api.button.ButtonSelector.EQUISIZED_GROUP, api.core.EquisizedsGroup);
+//# sourceMappingURL=button.module.js.map
