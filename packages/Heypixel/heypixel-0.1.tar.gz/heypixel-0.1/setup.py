06b11999b7dcabdf13f0397c88d155b35cd5fde8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:06:10 +0000

    Add package Heypixel/0.1/heypixel-0.1.tar.gz

---
+++ packages/Heypixel/heypixel-0.1.tar.gz/heypixel-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='Heypixel',
+    version='0.1',
+    packages=find_packages(),
+    description='A third-party library for Python. To help YaoMao, the demon cat, achieve her BMW dream.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/fjxlol/Heypixel',
+    author='LoserShadow',
+    author_email='2463358929@qq.com',
+    license='MIT'
+)
