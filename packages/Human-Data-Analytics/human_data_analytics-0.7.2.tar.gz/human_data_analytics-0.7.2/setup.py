dea0d47f5850d2f316b42898f692d368548a0299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 05:05:51 +0000

    Add package Human-Data-Analytics/0.7.2/human_data_analytics-0.7.2.tar.gz

---
+++ packages/Human-Data-Analytics/human_data_analytics-0.7.2.tar.gz/human_data_analytics-0.7.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Human_Data_Analytics",                     
+    version="0.7.2",
+    author = "Cesare Bidini, Nicolò Rinaldi",                              
+    packages=find_packages(),
+    # install_requires=[                            
+    #     "somepackage>=1.0.0",
+    # ],
+    python_requires=">=3.6",                      
+    author_email="your.email@example.com",
+    description="A brief description of your package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cesarbid/Human-Data-Analytics",
+    classifiers=[                                 
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
