b6ddf335598aee83303b144162ddf8589f02defa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 14:05:18 +0000

    Add package bomiot/0.4.18/bomiot-0.4.18.tar.gz

---
+++ packages/bomiot/bomiot-0.4.18.tar.gz/bomiot-0.4.18/bomiot/server/core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+
+
+
+# def index(request):
+#     """
+#     render index page
+#     :param request: request object
+#     :return: page
+#     """
+#     return render(request, 'index.html')
+
