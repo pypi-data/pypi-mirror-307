afa3f9fda8962ffd770e2f3a99352aaa40d86d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:06:05 +0000

    Add package TextArena/0.1.1/textarena-0.1.1.tar.gz

---
+++ packages/TextArena/textarena-0.1.1.tar.gz/textarena-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="textarena",
+    version="0.1.1",
+    url="https://github.com/LeonGuertler/TextArena",
+    author="Leon Guertler",
+    author_email="Guertlerlo@cfar.a-star.edu.sg",
+    description="[WIP] A Collection of Competitive Text-Based Games for Language Model Evaluation and Reinforcement Learning",
+    packages=find_packages(),
+    install_requires=[],  # fill in later
+)
