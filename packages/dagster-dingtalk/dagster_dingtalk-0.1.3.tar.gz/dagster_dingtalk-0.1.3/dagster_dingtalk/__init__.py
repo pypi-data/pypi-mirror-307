32f170a5c0f3e8b49fe3dbf8834109cce1b15292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 05:05:51 +0000

    Add package dagster-dingtalk/0.1.3/dagster_dingtalk-0.1.3.tar.gz

---
+++ packages/dagster-dingtalk/dagster_dingtalk-0.1.3.tar.gz/dagster_dingtalk-0.1.3/dagster_dingtalk/__init__.py
@@ -0,0 +1,8 @@
+# noinspection PyProtectedMember
+from dagster._core.libraries import DagsterLibraryRegistry
+
+from dagster_dingtalk.resources import DingTalkAPIResource, DingTalkWebhookResource
+from dagster_dingtalk.operations import DingTalkWebhookOp
+from dagster_dingtalk.version import __version__
+
+DagsterLibraryRegistry.register("dagster-dingtalk", __version__)
