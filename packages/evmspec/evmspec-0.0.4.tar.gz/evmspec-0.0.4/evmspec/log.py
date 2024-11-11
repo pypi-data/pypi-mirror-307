fd536549b581cdbcc20b0e9d0758b4c6e841ba9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:01 +0000

    Add package evmspec/0.0.4/evmspec-0.0.4.tar.gz

---
+++ packages/evmspec/evmspec-0.0.4.tar.gz/evmspec-0.0.4/evmspec/log.py
@@ -0,0 +1,162 @@
+from typing import Optional, Tuple
+
+from dictstruct import LazyDictStruct
+from hexbytes import HexBytes
+
+from evmspec import uints
+from evmspec._ids import LogIndex, TransactionIndex
+from evmspec.data import (
+    Address,
+    BlockHash,
+    BlockNumber,
+    HexBytes32,
+    TransactionHash,
+    uint,
+)
+
+_ADDRESS_TOPIC_PREFIX = HexBytes("0") * 12
+
+
+class Data(HexBytes):
+    """
+    Represents data in Ethereum logs, providing utilities for interpreting the data as various types.
+    """
+
+    @property
+    def as_uint(self) -> uint:
+        return uint(self.hex(), 16)
+
+    @property
+    def as_address(self) -> Address:
+        if self[:12] != _ADDRESS_TOPIC_PREFIX:
+            raise ValueError(
+                f"This {type(self).__name__} does not represent an address", self
+            )
+        return Address.checksum(self[-20:].hex())
+
+    @property
+    def as_uint8(self) -> uints.uint8:
+        return uints.uint8(self)
+
+    @property
+    def as_uint64(self) -> uints.uint64:
+        return uints.uint64(self)
+
+    @property
+    def as_uint128(self) -> uints.uint128:
+        return uints.uint128(self)
+
+    @property
+    def as_uint256(self) -> uints.uint256:
+        return uints.uint256(self)
+
+
+class Topic(HexBytes32, Data):
+    """
+    Represents a topic in Ethereum logs, providing utilities for interpreting the topic as various evm types.
+    """
+
+
+# Dynamically define properties for all uint types.
+# Example:
+# ```
+# Topic(bytes_data).as_uint40
+# Topic(bytes_data).as_uint48
+# Topic(bytes_data).as_uint56
+# ```
+for i in range(2, 31):
+    # these commonly used ones are already defined above for better use with ides
+    if i in [1, 8, 16, 32]:
+        continue
+    bits = i * 8
+    uint_cls_name = f"uint{bits}"
+    uint_cls = getattr(uints, uint_cls_name)
+    setattr(Topic, f"as_{uint_cls_name}", property(uint_cls))
+
+
+class TinyLog(LazyDictStruct, frozen=True, kw_only=True):  # type: ignore [call-arg]
+    """
+    Represents a minimal log structure with topics.
+    """
+
+    topics: Tuple[Topic, ...]
+    """
+    An array of 0 to 4 32-byte topics. 
+    The first topic is the event signature and the others are indexed filters on the event return data.
+    """
+
+    @property
+    def topic0(self) -> Topic:
+        """Returns the first topic."""
+        return self.topics[0]
+
+    @property
+    def topic1(self) -> Topic:
+        """Returns the second topic if it exists, otherwise raises an AttributeError."""
+        try:
+            return self.topics[1]
+        except IndexError:
+            new_err = (
+                f"'this {type(self).__name__} object '{self}' has no attribute 'topic1'"
+            )
+            raise AttributeError(new_err) from None
+
+    @property
+    def topic2(self) -> Topic:
+        """Returns the third topic if it exists, otherwise raises an AttributeError."""
+        try:
+            return self.topics[2]
+        except IndexError:
+            new_err = (
+                f"this {type(self).__name__} object '{self}' has no attribute 'topic2'"
+            )
+            raise AttributeError(new_err) from None
+
+    @property
+    def topic3(self) -> Topic:
+        """Returns the fourth topic if it exists, otherwise raises an AttributeError."""
+        try:
+            return self.topics[3]
+        except IndexError:
+            new_err = (
+                f"this {type(self).__name__} object '{self}' has no attribute 'topic3'"
+            )
+            raise AttributeError(new_err) from None
+
+
+class SmallLog(TinyLog, frozen=True, kw_only=True):  # type: ignore [call-arg]
+
+    address: Optional[Address]
+    """The address of the contract that generated the log."""
+
+    data: Optional[Data]
+    """Array of 32-bytes non-indexed return data of the log."""
+
+
+class Log(SmallLog, frozen=True, kw_only=True):  # type: ignore [call-arg]
+
+    removed: Optional[bool]
+    """`True` when the log was removed, due to a chain reorganization. `False` if it's a valid log."""
+
+    blockNumber: Optional[BlockNumber]
+    """The block where the transaction was included where the log originated from. `None` for pending transactions."""
+
+    transactionHash: TransactionHash
+    """The hash of the transaction that generated the log. `None` for pending transactions."""
+
+    logIndex: LogIndex
+    """Index position of the log in the transaction. `None` for pending transactions."""
+
+    transactionIndex: TransactionIndex
+    """The index of the transaction in the block, where the log originated from."""
+
+    @property
+    def block(self) -> Optional[BlockNumber]:
+        """A shorthand getter for 'blockNumber'"""
+        return self.blockNumber
+
+
+class FullLog(Log, frozen=True, kw_only=True, forbid_unknown_fields=True):  # type: ignore [call-arg]
+
+    blockHash: Optional[BlockHash]
+    """The hash of the block where the transaction was included where the log originated from. `None` for pending transactions."""
---
+++ packages/evmspec/evmspec-0.0.4.tar.gz/evmspec-0.0.4/evmspec/receipt.py
@@ -0,0 +1,139 @@
+from decimal import Decimal
+from enum import Enum, EnumMeta
+from functools import cached_property
+from typing import Optional, Tuple
+
+from dictstruct import DictStruct, LazyDictStruct
+from hexbytes import HexBytes
+from msgspec import UNSET, Raw, field, json
+
+from evmspec._ids import TransactionIndex
+from evmspec.data import Address, BlockNumber, TransactionHash, Wei, uint, _decode_hook
+from evmspec.log import Log
+
+
+class FeeStats(DictStruct, frozen=True, forbid_unknown_fields=True):  # type: ignore [call-arg]
+    """Arbitrum includes this in the `feeStats` field of a tx receipt."""
+
+    l1Calldata: Wei
+    l2Storage: Wei
+    l1Transaction: Wei
+    l2Computation: Wei
+
+
+class ArbitrumFeeStats(DictStruct, frozen=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):  # type: ignore [call-arg]
+    """Arbitrum includes these with a tx receipt."""
+
+    paid: FeeStats
+    """
+    The breakdown of gas paid for the transaction.
+    
+    (price * unitsUsed)
+    """
+    # These 2 attributes do not always exist
+    unitsUsed: FeeStats = UNSET  # type: ignore [assignment]
+    """The breakdown of units of gas used for the transaction."""
+    prices: FeeStats = UNSET  # type: ignore [assignment]
+    """The breakdown of gas prices for the transaction."""
+
+
+class _HexStringToIntEnumMeta(EnumMeta):
+    """
+    A metaclass for Enums that allows conversion from hexadecimal string values to integer Enum members.
+    """
+
+    def __call__(cls, value: str, *args, **kw):
+        return super().__call__(int(value, 16), *args, **kw)
+
+
+class Status(Enum, metaclass=_HexStringToIntEnumMeta):
+    """
+    Enum representing the status of a transaction, indicating success or failure.
+    """
+
+    failure = 0
+    success = 1
+
+
+class TransactionReceipt(LazyDictStruct, frozen=True, kw_only=True, omit_defaults=True, repr_omit_defaults=True, dict=True):  # type: ignore [call-arg]
+
+    transactionHash: TransactionHash
+    """The unique hash of this transaction."""
+
+    blockNumber: BlockNumber
+    """The block number that contains the transaction."""
+
+    contractAddress: Optional[Address]
+    """The contract address created, if the transaction was a contract creation, otherwise `None`."""
+
+    transactionIndex: TransactionIndex
+    """The position of this transaction within the block."""
+
+    status: Status
+    """1 if the transaction succeeded, 0 if it failed."""
+
+    gasUsed: Wei
+    """The amount of gas used by this transaction, not counting internal transactions, calls or delegate calls."""
+
+    cumulativeGasUsed: Wei
+    """The total amount of gas used during this transaction."""
+
+    _logs: Raw = field(name="logs")
+    """The logs that were generated during this transaction."""
+
+    @cached_property
+    def logs(self) -> Tuple[Log, ...]:
+        """The logs that were generated during this transaction."""
+        return json.decode(self._logs, type=Tuple[Log, ...], dec_hook=_decode_hook)
+
+    # These fields are only present on Mainnet.
+    effectiveGasPrice: Wei = UNSET  # type: ignore [assignment]
+    """
+    The actual value per gas deducted from the sender's account.
+    
+    This field is only present on Mainnet.
+    """
+
+    type: uint = UNSET  # type: ignore [assignment]
+    """
+    The transaction type.
+
+    This field is only present on Mainnet.
+    """
+
+    blobGasUsed: Wei = UNSET  # type: ignore [assignment]
+    """This field is sometimes present, only on Mainnet."""
+
+    # These fields are only present on Optimism.
+    l1FeeScalar: Decimal = UNSET  # type: ignore [assignment]
+    """This field is only present on Optimism."""
+    l1GasUsed: Wei = UNSET  # type: ignore [assignment]
+    """This field is only present on Optimism."""
+    l1GasPrice: Wei = UNSET  # type: ignore [assignment]
+    """This field is only present on Optimism."""
+    l1Fee: Wei = UNSET  # type: ignore [assignment]
+    """This field is only present on Optimism."""
+
+    # These fields are only present on Arbitrum.
+    l1BlockNumber: BlockNumber = UNSET  # type: ignore [assignment]
+    """This field is only present on Arbitrum."""
+    l1InboxBatchInfo: Optional[HexBytes] = UNSET  # type: ignore [assignment]
+    """This field is only present on Arbitrum."""
+    _feeStats: Raw = field(name="feeStats", default=UNSET)  # type: ignore [assignment]
+    """This field is only present on Arbitrum."""
+
+    @cached_property
+    def feeStats(self) -> ArbitrumFeeStats:
+        """This field is only present on Arbitrum."""
+        return json.decode(
+            self._feeStats, type=ArbitrumFeeStats, dec_hook=Wei._decode_hook
+        )
+
+
+class FullTransactionReceipt(TransactionReceipt, frozen=True, kw_only=True, forbid_unknown_fields=True, omit_defaults=True, repr_omit_defaults=True):  # type: ignore [call-arg]
+
+    blockHash: HexBytes
+    """The hash of the block that contains the transaction."""
+
+    logsBloom: HexBytes
+    """The bloom filter for all logs in this block."""
