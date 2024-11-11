a6cb1226516379430672f3be94ab1648c2d00afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:42 +0000

    Add package opnsense-helper/0.1.4/opnsense_helper-0.1.4-py3-none-any.whl

---
+++ packages/opnsense-helper/opnsense_helper-0.1.4-py3-none-any.whl/opnsense_helper/utils.py
@@ -0,0 +1,118 @@
+import xml.etree.ElementTree as ET
+# import libraries
+import json
+import requests
+import os
+aliases={
+"parentinterface": "if",
+"_from":"from",
+"_to":"to"
+}
+
+
+def getRes(host, command, api_key, api_secret, timeout = 5):
+    
+    response = os.system(f"ping -c 1 {host}")
+    if response == 0:
+        print(f"IP {host} is reachable")
+        url = f"https://{host}/api/{command}"
+        r = requests.get(url, verify=False,  auth=(api_key, api_secret))
+        # verify='OPNsense.pem',
+        if r.status_code == 200:
+            response = json.loads(r.text)
+            with open('/home/ji/.ansible/collections/ansible_collections/ji_podhead/podnet/plugins/data.json', 'w') as f:
+                json.dump(response, f)
+            print (response)
+        else:
+            print ('Connection / Authentication issue, response received:')
+            print (r.text)
+    else:
+        print(f"IP {host} is not reachable")
+
+#curl -k -u $OPNS_KEY:$OPNS_SECRET https://$IPFW/api/diagnostics/interface/getinterfaceconfig
+
+def get_child(root,element, id, keys):
+    elements=[]
+    for parent in root.findall(element):
+        child= {}
+        for y in keys:
+            child[y]=None
+        for x in parent.findall(id):
+            for key in keys:
+                child[key]=parseChild(x, key)
+        elements.append(child)
+    return elements
+def parseChild(parent, tag):
+    result=parent.find(tag)
+
+    element=result.text if  result is not None else None
+    return element
+
+def get_child(root,element, id, keys):
+    elements=[]
+    for parent in root.findall(element):
+        child= {}
+        for y in keys:
+            child[y]=None
+        for x in parent.findall(id):
+            for key in keys:
+                child[key]=parseChild(x, key)
+        elements.append(child)
+    return elements
+def parseChild(parent, tag):
+    result=parent.find(tag)
+
+    element=result.text if  result is not None else None
+    return element
+def getRes(host, command, api_key, api_secret, timeout = 5):
+    host = '192.168.1.103'
+    response = os.system(f"ping -c 1 {host}")
+    if response == 0:
+        print(f"IP {host} is reachable")
+        url = f"https://{host}/api/{command}"
+        r = requests.get(url, verify=False,  auth=(api_key, api_secret))
+        # verify='OPNsense.pem',
+        if r.status_code == 200:
+            response = json.loads(r.text)
+            with open('/home/ji/.ansible/collections/ansible_collections/ji_podhead/podnet/plugins/data.json', 'w') as f:
+                json.dump(response, f)
+            print (response)
+        else:
+            print ('Connection / Authentication issue, response received:')
+            print (r.text)
+    else:
+        print(f"IP {host} is not reachable")
+def get_element(root,id, obj):
+    for x in root.findall(id):
+        for key in obj.__dict__.keys():
+#            if isinstance(obj.__dict__[key], dict):
+            if(key!="_to"and key!="_from"):
+                
+                if key in aliases.keys():
+                    key2 = aliases[key] 
+                    y=parseChild(x, key2)
+                else:
+                    y=parseChild(x, key)
+                setattr(obj, key, y)
+    return obj
+
+def recoursion(e, value):
+    for key2, value2 in value.items():
+        if(key2!="attr"):
+            if key2 in aliases.keys():
+                key2 = aliases[key2] 
+            x=ET.SubElement(e, key2)
+            if isinstance(value2, dict):
+                recoursion(x,value2)
+            else:
+                x.text = value2
+
+def update_xml_file(objects,root,type):
+    el = root.find(type)
+    el.clear()
+    for key, value in objects.items():
+        if value["attr"] is not None:
+             e = ET.SubElement(el, key,value["attr"])
+        else:
+            e=ET.SubElement(el,key)
+        recoursion(e,value)
