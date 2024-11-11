d39df3a11310631c9ca8741eb78911c0c820a215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 14:04:53 +0000

    Add package lavender-dir-tree/1.0.1/lavender-dir-tree-1.0.1.tar.gz

---
+++ packages/lavender-dir-tree/lavender-dir-tree-1.0.1.tar.gz/lavender-dir-tree-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lavender-dir-tree",
+    version="1.0.1",
+    description="A pip package for listing files.",
+    long_description=open(
+        "README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Zeyu Xie",
+    author_email="xie.zeyu20@gmail.com",
+    url="https://github.com/Zeyu-Xie/Lavender-Dir-Tree",
+    packages=find_packages(
+        include=["lavender_dir_tree.*"]),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
