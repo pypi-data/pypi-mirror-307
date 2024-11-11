322bc09286aa5e89bbf5ebafddac398b3680b9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:06:52 +0000

    Add package aseg-gdf2/0.7/aseg_gdf2-0.7.tar.gz

---
+++ packages/aseg-gdf2/aseg_gdf2-0.7.tar.gz/aseg_gdf2-0.7/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="aseg_gdf2",
+    packages=("aseg_gdf2", ),
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="Python package to help read ASEG GDF2 packages ",
+    long_description=open("README.md", mode="r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kinverarity1/aseg_gdf2",
+    author="Kent Inverarity",
+    author_email="kinverarity@hotmail.com",
+    license="MIT",
+    classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+    ),
+    keywords="python geophysics file-formats",
+    install_requires=("pandas", "dask"),
+)
