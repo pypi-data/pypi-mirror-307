9a3b130470cb0c9c127088474add831130827536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 14:05:23 +0000

    Add package pnadcontinua/0.0.6/pnadcontinua-0.0.6.tar.gz

---
+++ packages/pnadcontinua/pnadcontinua-0.0.6.tar.gz/pnadcontinua-0.0.6/pnadcontinua/gui/app.py
@@ -0,0 +1,47 @@
+import os
+import tkinter as tk
+from pnadcontinua.gui.frames import (
+    HomeFrame, MicrodataFrame, AggregationsFrame, TutorialFrame, VariablesFrame
+)
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.screen_width = self.winfo_screenwidth()
+        self.screen_height = self.winfo_screenheight()
+
+        self.window_size = 530
+
+        self.title("PNAD Contínua")
+
+        x_position = int((self.screen_width - self.window_size) / 2)
+        y_position = int((self.screen_height - self.window_size) / 2)
+
+        self.geometry(f"{self.window_size}x{self.window_size}+{x_position}+{y_position}")
+        self.resizable(False, False)
+
+        self.protocol("WM_DELETE_WINDOW", lambda: os._exit(0))
+
+        # Creation of the frames
+        self.home_frame = HomeFrame(self)
+        self.microdata_frame = MicrodataFrame(self)
+        self.aggregations_frame = AggregationsFrame(self)
+        self.variables_frame = VariablesFrame(self)
+        self.tutorial_frame = TutorialFrame(self)
+
+        # Place the frames on the window
+        self.home_frame.grid(row=0, column=0, sticky="nsew")
+        self.microdata_frame.grid(row=0, column=0, sticky="nsew")
+        self.aggregations_frame.grid(row=0, column=0, sticky="nsew")
+        self.variables_frame.grid(row=0, column=0, sticky="nsew")
+        self.tutorial_frame.grid(row=0, column=0, sticky="nsew")
+        # Display the home frame
+        self.show_frame(self.home_frame)
+
+    def show_frame(self, frame):
+        frame.tkraise()
---
+++ packages/pnadcontinua/pnadcontinua-0.0.6.tar.gz/pnadcontinua-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pnadcontinua",
+    version="0.0.6",
+    author="Cleomar Felipe Rabelo Antoszczyszyn",
+    author_email="cleomarfelipe@gmail.com",
+    description="A Python library designed to simplify the acquisition and analysis of PNADC microdata",
+    url="https://github.com/cleomarfelipe/pnadcontinua",
+    packages=find_packages(),
+    install_requires=[
+        "certifi==2024.8.30",
+        "charset-normalizer==3.4.0",
+        "fastexcel==0.12.0",
+        "idna==3.10",
+        "polars==1.12.0",
+        "pyarrow==18.0.0",
+        "requests==2.32.3",
+        "urllib3==2.2.3",
+    ],
+    include_package_data=True,
+    python_requires='>=3.7',
+)
