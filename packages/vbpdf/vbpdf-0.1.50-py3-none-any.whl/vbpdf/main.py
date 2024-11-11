120039b7b5309e1ced3c4a84c2a2a3a1f86a0b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 16:05:06 +0000

    Add package vbpdf/0.1.50/vbpdf-0.1.50-py3-none-any.whl

---
+++ packages/vbpdf/vbpdf-0.1.50-py3-none-any.whl/vbpdf/main.py
@@ -0,0 +1,35 @@
+import click
+
+from .split import split
+from .overlay import overlay
+# from .compress import compress
+from .topng import topng
+from .addbg import addbg
+from .instagram import instagram
+from .webpage import webpage
+# from .blur import blur
+# from .todocx import todocx
+
+
+CONTEXT_SETTINGS = dict(
+    help_option_names=[
+        '-h',
+        '--help'
+    ]
+)
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def main():
+    pass
+
+
+# main.add_command(blur)
+main.add_command(instagram)
+main.add_command(split)
+main.add_command(overlay)
+main.add_command(topng)
+main.add_command(addbg)
+main.add_command(webpage)
+# main.add_command(compress)
+# main.add_command(todocx)
