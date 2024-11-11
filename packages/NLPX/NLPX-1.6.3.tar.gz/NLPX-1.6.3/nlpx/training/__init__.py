a82925c55106bc204d44389f67dfd2ce428fcb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 14:05:15 +0000

    Add package NLPX/1.6.3/NLPX-1.6.3.tar.gz

---
+++ packages/NLPX/NLPX-1.6.3.tar.gz/NLPX-1.6.3/nlpx/training/__init__.py
@@ -0,0 +1,4 @@
+from .utils import evaluate, cal_correct, acc_evaluate, r2_evaluate, convert_data, convert_data_acc, convert_data_r2, is_float
+from ._trainer import Trainer, SimpleTrainer, EvalTrainer, SimpleEvalTrainer, \
+	ClassTrainer, SimpleClassTrainer, EvalClassTrainer, SimpleEvalClassTrainer, \
+	RegressTrainer, SimpleRegressTrainer, EvalRegressTrainer, SimpleEvalRegressTrainer
