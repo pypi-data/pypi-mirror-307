10b2ce223e373d0efd4b0c3e3821b0b7f09543f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:30 +0000

    Add package skitso/0.1.0/skitso-0.1.0.tar.gz

---
+++ packages/skitso/skitso-0.1.0.tar.gz/skitso-0.1.0/src/skitso/scene.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from PIL import Image, ImageDraw
+
+from skitso.atom import Container
+
+
+class Scene(Container):
+    DIRECT = False
+
+    def __init__(self, canvas_size, base_folder_path, color="black"):
+        self.color = color
+        self.width, self.height = canvas_size
+        self.create_canvas()
+        self.folder_path = (
+            Path(base_folder_path) / type(self).__name__.lower() / "frames"
+        )
+        self.folder_path.mkdir(parents=True, exist_ok=True)
+        self.next_tick_id = 1
+        self.children = []
+
+    def create_canvas(self):
+        self.image = Image.new("RGB", (self.width, self.height), self.color)
+        self.draw = ImageDraw.Draw(self.image)
+        self.draw.fontmode = "L"
+
+    def tick(self):
+        if not self.DIRECT:
+            # need to create the image from scratch
+            self.create_canvas()
+            for item in self.children:
+                item.draw_me(self.draw)
+        new_img_path = self.folder_path / f"{self.next_tick_id:08}.jpg"
+        im = self.image.resize(
+            (self.width * 2, self.height * 2), resample=Image.Resampling.LANCZOS
+        )
+        im = im.resize((self.width, self.height), resample=Image.Resampling.LANCZOS)
+        im.save(new_img_path, subsampling=0, quality=95)
+        self.next_tick_id += 1
+
+    def add(self, item):
+        if self.DIRECT:
+            item.draw_me(self.draw)
+        else:
+            self.children.append(item)
