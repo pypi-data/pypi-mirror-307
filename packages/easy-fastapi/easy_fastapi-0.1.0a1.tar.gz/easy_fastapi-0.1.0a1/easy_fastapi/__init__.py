31a34c63872cd0bbf866c799adae3bc731b96017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 16:05:18 +0000

    Add package easy-fastapi/0.1.0a1/easy_fastapi-0.1.0a1.tar.gz

---
+++ packages/easy-fastapi/easy_fastapi-0.1.0a1.tar.gz/easy_fastapi-0.1.0a1/easy_fastapi/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from .core import logger as logger
+from .core import execute_from_command_line as execute_from_command_line
+
+
+__version__ = '0.1.0-alpha.1'
+__author__  = 'one-ccs'
+__email__   = 'one-ccs@foxmal.com'
+
+__all__ = [
+    'logger',
+    'execute_from_command_line',
+]
