c6e92bcae1dabda0ceed93297f5f981716909816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 16:05:23 +0000

    Add package warpper-schnorr/0.1.4/warpper_schnorr-0.1.4.tar.gz

---
+++ packages/warpper-schnorr/warpper_schnorr-0.1.4.tar.gz/warpper_schnorr-0.1.4/warpper_schnorr.pyi
@@ -0,0 +1,14 @@
+from typing import Union
+
+
+def private_key_to_public_key(private_key: str) -> str : ...
+
+def generate_keys_with_seed(seed: str) -> Union[str, str] : ...
+
+
+def verify_signature(
+    message: str,
+    signature: str,
+    public_key: str,
+    context: str,
+) -> bool: ...
