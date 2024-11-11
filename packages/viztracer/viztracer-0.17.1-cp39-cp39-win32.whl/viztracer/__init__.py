99aee74c7a1ffcf76e69135ef2486b3f7b26830f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 05:05:11 +0000

    Add package viztracer/0.17.1/viztracer-0.17.1-cp39-cp39-win32.whl

---
+++ packages/viztracer/viztracer-0.17.1-cp39-cp39-win32.whl/viztracer/__init__.py
@@ -0,0 +1,29 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
+
+__version__ = "0.17.1"
+
+
+from .cellmagic import load_ipython_extension
+from .decorator import ignore_function, log_sparse, trace_and_save
+from .main import main
+from .viewer import viewer_main
+from .vizcounter import VizCounter
+from .vizlogging import VizLoggingHandler
+from .vizobject import VizObject
+from .viztracer import VizTracer, get_tracer
+
+__all__ = [
+    "__version__",
+    "main",
+    "viewer_main",
+    "VizTracer",
+    "ignore_function",
+    "trace_and_save",
+    "log_sparse",
+    "get_tracer",
+    "VizCounter",
+    "VizObject",
+    "VizLoggingHandler",
+    "load_ipython_extension",
+]
