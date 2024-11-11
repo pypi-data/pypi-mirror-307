45e61a261ea4c85d3491ae7fb599dcf653a1005c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:29 +0000

    Add package bridgtl-edm-dp-python-library/0.1.4/bridgtl_edm_dp_python_library-0.1.4-py3-none-any.whl

---
+++ packages/bridgtl-edm-dp-python-library/bridgtl_edm_dp_python_library-0.1.4-py3-none-any.whl/bridgtl_edm_dp_python_library/data_platform/pyspark/utility.py
@@ -0,0 +1,19 @@
+import os
+
+from pyspark.sql import SparkSession
+
+
+def copy_file_to_spark(session: SparkSession, path: str):
+    filename = os.path.basename(path)
+    content = open(path).read()
+
+    session.sparkContext.addFile(path)
+
+    def write_keyfile():
+        from pyspark import SparkFiles
+
+        keyfile_path = SparkFiles.get(filename)
+        with open(keyfile_path, 'w') as keyfile:
+            keyfile.write(content)
+
+    session.sparkContext.parallelize([1], 1).foreach(lambda _: write_keyfile())
