087829997a4da0fd32d48ac8d7ba0bd228999a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:43 +0000

    Add package SiddP6/0.4.1/SiddP6-0.4.1.tar.gz

---
+++ packages/SiddP6/SiddP6-0.4.1.tar.gz/SiddP6-0.4.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+# Read the README file for long description
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# # Read the requirements from the requirements.txt file
+# with open('requirements.txt', 'r') as f:
+#     install_requires = f.read().splitlines()
+
+# Setup function for the package
+setuptools.setup(
+    name="SiddP6",                     # This is the name of the package
+    version="0.4.1",                        # The initial release version
+    author="Siddhartha Purwar",                     # Full name of the author
+    description="Siddhartha Purwar portfolio pip package",
+    long_description=long_description,      # Long description read from the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all Python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["sidd"],             # Name of the python package
+    package_dir={'': '.'},     # Directory of the source code of the package
+    install_requires=['streamlit==1.40.0']                     # Install other dependencies from requirements.txt
+)
---
+++ packages/SiddP6/SiddP6-0.4.1.tar.gz/SiddP6-0.4.1/sidd/Experiences.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import json
+import os
+
+# Function to display experience details creatively
+def display_experience_details(experience_data):
+    st.markdown(f"# 🚀 **{experience_data['Title']}** at {experience_data['Company']}")
+
+    st.markdown(f"__💻 Skills:__ {experience_data['Skills']}")
+    st.markdown(f"__📅 Duration:__ {experience_data['start_date']} - {experience_data['end_date']}")
+
+    st.markdown("#### Description:")
+    for line in experience_data["description"]:
+        st.markdown(f" - {line}")
+
+# Hardcoded JSON file path
+json_file_path = os.path.join(os.path.dirname(__file__), 'src\experiences.json')
+print("\n\n\n\n", json_file_path, "\n\n\n\n")
+print(os.path.dirname(__file__))
+# Load JSON data
+try:
+    with open(json_file_path, "r") as file:
+        experiences_data = json.load(file)
+
+        # Get experience titles for the dropdown options
+        experience_titles = [f"{exp['Title']} at {exp['Company']}" for exp in experiences_data]
+
+        # Dropdown to select an experience
+        selected_experience = st.selectbox("Select an experience", experience_titles)
+
+        # Find the selected experience and display its details
+        for experience in experiences_data:
+            experience_label = f"{experience['Title']} at {experience['Company']}"
+            if experience_label == selected_experience:
+                display_experience_details(experience)
+                break
+
+except FileNotFoundError:
+    st.error(f"File not found at path: {json_file_path}")
+except Exception as e:
+    st.error(f"An error occurred: {e}")
