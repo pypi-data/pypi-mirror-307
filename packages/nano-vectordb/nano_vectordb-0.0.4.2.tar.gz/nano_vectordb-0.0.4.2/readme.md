176422eadcda32be43579379f71d14f70e41e85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:07:15 +0000

    Add package nano-vectordb/0.0.4.2/nano_vectordb-0.0.4.2.tar.gz

---
+++ packages/nano-vectordb/nano_vectordb-0.0.4.2.tar.gz/nano_vectordb-0.0.4.2/readme.md
@@ -0,0 +1,118 @@
+<div align="center">
+  <h1>nano-VectorDB</h1>
+  <p><strong>A simple, easy-to-hack Vector Database</strong></p>
+  <p>
+    <img src="https://img.shields.io/badge/python->=3.9.11-blue">
+    <a href="https://pypi.org/project/nano-vectordb/">
+      <img src="https://img.shields.io/pypi/v/nano-vectordb.svg">
+    </a>
+    <a href="https://codecov.io/github/gusye1234/nano-vectordb" > 
+ <img src="https://codecov.io/github/gusye1234/nano-vectordb/graph/badge.svg?token=3ACScwuv4h"/> 
+ </a>
+  </p>
+</div>
+
+
+
+
+🌬️ A vector database implementation with single-dependency (`numpy`).
+
+🎁 It can handle a query from `100,000` vectors and return in 100 milliseconds.
+
+🏃 It's okay for your prototypes, maybe even more.
+
+
+
+## Install
+
+**Install from PyPi**
+
+```shell
+pip install nano-vectordb
+```
+
+**Install from source**
+
+```shell
+# clone this repo first
+cd nano-vectordb
+pip install -e .
+```
+
+
+
+## Quick Start
+
+**Faking your data**:
+
+```python
+from nano_vectordb import NanoVectorDB
+import numpy as np
+
+data_len = 100_000
+fake_dim = 1024
+fake_embeds = np.random.rand(data_len, fake_dim)    
+
+fakes_data = [{"__vector__": fake_embeds[i], **ANYFIELDS} for i in range(data_len)]
+```
+
+You can add any fields to a data. But there are two keywords:
+
+- `__id__`: If passed, `NanoVectorDB` will use your id, otherwise a generated id will be used.
+- `__vector__`: must pass, your embedding `np.ndarray`.
+
+### Init a DB
+
+```python
+vdb = NanoVectorDB(fake_dim, storage_file="fool.json")
+```
+
+Next time you init `vdb` from `fool.json`, `NanoVectorDB` will load the index automatically.
+
+### Upsert
+
+```python
+r = vdb.upsert(fakes_data)
+print(r["update"], r["insert"])
+```
+
+### Query
+
+```python
+# query with embedding 
+vdb.query(np.random.rand(fake_dim))
+
+# arguments:
+vdb.query(np.random.rand(fake_dim), top_k=5, better_than_threshold=0.01)
+```
+
+#### Conditional filter
+
+```python
+vdb.query(np.random.rand(fake_dim), filter_lambda=lambda x: x["any_field"] == "any_value")
+```
+
+### Save
+
+```python
+# will create/overwrite 'fool.json'
+vdb.save()
+```
+
+### Get, Delete
+
+```python
+# get and delete the inserted data
+print(vdb.get(r["insert"]))
+vdb.delete(r["insert"])
+```
+
+
+
+## Benchmark
+
+> Embedding Dim: 1024. Device: MacBook M3 Pro
+
+- Save a index with `100,000` vectors will generate a roughly 520M json file.
+- Insert `100,000` vectors will cost roughly `2`s
+- Query from `100,000` vectors will cost roughly `0.1`s
---
+++ packages/nano-vectordb/nano_vectordb-0.0.4.2.tar.gz/nano_vectordb-0.0.4.2/tests/test_init.py
@@ -0,0 +1,103 @@
+import os
+import numpy as np
+from nano_vectordb import NanoVectorDB
+from nano_vectordb.dbs import f_METRICS, f_ID, f_VECTOR
+
+
+def test_init():
+    from time import time
+
+    data_len = 1000
+    fake_dim = 1024
+
+    start = time()
+    a = NanoVectorDB(fake_dim)
+    print("Load", time() - start)
+
+    fake_embeds = np.random.rand(data_len, fake_dim)
+    fakes_data = [{f_VECTOR: fake_embeds[i], f_ID: i} for i in range(data_len)]
+    query_data = fake_embeds[data_len // 2]
+    start = time()
+    r = a.upsert(fakes_data)
+    print("Upsert", time() - start)
+    a.save()
+
+    a = NanoVectorDB(fake_dim)
+
+    start = time()
+    r = a.query(query_data, 10, better_than_threshold=0.01)
+    assert r[0][f_ID] == data_len // 2
+    print(r)
+    assert len(r) <= 10
+    for d in r:
+        assert d[f_METRICS] >= 0.01
+    os.remove("nano-vectordb.json")
+
+
+def test_same_upsert():
+    from time import time
+
+    data_len = 1000
+    fake_dim = 1024
+
+    start = time()
+    a = NanoVectorDB(fake_dim)
+    print("Load", time() - start)
+
+    fake_embeds = np.random.rand(data_len, fake_dim)
+    fakes_data = [{f_VECTOR: fake_embeds[i]} for i in range(data_len)]
+    r1 = a.upsert(fakes_data)
+    assert len(r1["insert"]) == len(fakes_data)
+    fakes_data = [{f_VECTOR: fake_embeds[i]} for i in range(data_len)]
+    r2 = a.upsert(fakes_data)
+    assert r2["update"] == r1["insert"]
+
+
+def test_get():
+    a = NanoVectorDB(1024)
+    a.upsert(
+        [
+            {f_VECTOR: np.random.rand(1024), f_ID: str(i), "content": i}
+            for i in range(100)
+        ]
+    )
+    r = a.get(["0", "1", "2"])
+    assert len(r) == 3
+    assert r[0]["content"] == 0
+    assert r[1]["content"] == 1
+    assert r[2]["content"] == 2
+
+
+def test_delete():
+    a = NanoVectorDB(1024)
+    a.upsert(
+        [
+            {f_VECTOR: np.random.rand(1024), f_ID: str(i), "content": i}
+            for i in range(100)
+        ]
+    )
+    a.delete(["0", "50", "99"])
+
+    r = a.get(["0", "50", "99"])
+    assert len(r) == 0
+    assert len(a) == 97
+
+
+def test_cond_filter():
+    data_len = 10
+    fake_dim = 1024
+
+    a = NanoVectorDB(fake_dim)
+    fake_embeds = np.random.rand(data_len, fake_dim)
+    cond_filer = lambda x: x[f_ID] == 1
+
+    fakes_data = [{f_VECTOR: fake_embeds[i], f_ID: i} for i in range(data_len)]
+    query_data = fake_embeds[data_len // 2]
+    a.upsert(fakes_data)
+
+    assert len(a) == data_len
+    r = a.query(query_data, 10, better_than_threshold=0.01)
+    assert r[0][f_ID] == data_len // 2
+
+    r = a.query(query_data, 10, filter_lambda=cond_filer)
+    assert r[0][f_ID] == 1
