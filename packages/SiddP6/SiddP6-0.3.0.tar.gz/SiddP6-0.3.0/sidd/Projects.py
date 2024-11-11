2034426ca2611acbc324c1d98112c582bbf3b436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:29 +0000

    Add package SiddP6/0.3.0/SiddP6-0.3.0.tar.gz

---
+++ packages/SiddP6/SiddP6-0.3.0.tar.gz/SiddP6-0.3.0/sidd/Projects.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import json
+import requests
+
+
+
+def fetch_json_from_url(url):
+    try:
+        # Send GET request to the URL
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Return the parsed JSON data
+            return response.json()
+        else:
+            print(f"Failed to retrieve the file. Status code: {response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        # Catch any exceptions during the request
+        print(f"An error occurred: {e}")
+        return None
+
+
+
+# Function to display project details creatively
+def display_project_details(project_data):
+    st.markdown(f"# 🚀 **{project_data['project_name']}**")
+
+    st.markdown(f"__💻 Technology Used:__ {project_data['technology_used']}")
+
+    st.markdown(f"__💡Concepts:__ {project_data['concepts']}")
+
+    st.markdown(f"### 🌐 [Code Link]({project_data['code_link']})")
+
+    st.markdown("#### Description:")
+    for line in project_data["description"]:
+        st.markdown(f" - {line}")
+
+    st.markdown("#### Features:")
+    st.json(project_data["features"])
+
+
+
+# Load JSON data
+try:
+    url = "https://raw.githubusercontent.com/sidd6p/sidd-package/refs/heads/main/sidd/src/projects.json"
+    projects_data = fetch_json_from_url(url)
+
+    # Get project names for the dropdown options
+    project_names = [project["project_name"] for project in projects_data]
+
+    # Dropdown to select a project
+    selected_project = st.selectbox("Select a Project", project_names)
+
+    # Find the selected project and display its details
+    for project in projects_data:
+        if project["project_name"] == selected_project:
+            display_project_details(project)
+            break
+
+except Exception as e:
+    st.error(f"An error occurred: {e}")
+
+
+    # Add more projects as needed
