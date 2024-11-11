878b947c3a0380cfdf1f858d71cca759791f90fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 05:05:08 +0000

    Add package Organizze-Wrapper/1.2.2/organizze_wrapper-1.2.2.tar.gz

---
+++ packages/Organizze-Wrapper/organizze_wrapper-1.2.2.tar.gz/organizze_wrapper-1.2.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Organizze-Wrapper',
+    version='1.2.2',
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.32.3",
+        "PyMultiHelper>=1.1.11"
+    ],
+    description='Biblioteca Python de Wrapper para a API do Organizze.com.br',
+    author='Anderson',
+    author_email='anderbytes@gmail.com',
+    url='https://github.com/anderbytes/Organizze-Wrapper',
+
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read()
+)
