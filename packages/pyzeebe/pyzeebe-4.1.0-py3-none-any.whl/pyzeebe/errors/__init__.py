9dc519f8ad4284e94a1c6bc7436b106bc77d362f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:06:03 +0000

    Add package pyzeebe/4.1.0/pyzeebe-4.1.0-py3-none-any.whl

---
+++ packages/pyzeebe/pyzeebe-4.1.0-py3-none-any.whl/pyzeebe/errors/__init__.py
@@ -0,0 +1,53 @@
+from .credentials_errors import InvalidOAuthCredentialsError
+from .job_errors import (
+    ActivateJobsRequestInvalidError,
+    JobAlreadyDeactivatedError,
+    JobNotFoundError,
+)
+from .message_errors import MessageAlreadyExistsError
+from .process_errors import (
+    InvalidJSONError,
+    ProcessDefinitionHasNoStartEventError,
+    ProcessDefinitionNotFoundError,
+    ProcessInstanceNotFoundError,
+    ProcessInvalidError,
+    ProcessTimeoutError,
+)
+from .pyzeebe_errors import (
+    BusinessError,
+    DuplicateTaskTypeError,
+    NoVariableNameGivenError,
+    PyZeebeError,
+    TaskNotFoundError,
+)
+from .zeebe_errors import (
+    UnknownGrpcStatusCodeError,
+    ZeebeBackPressureError,
+    ZeebeDeadlineExceeded,
+    ZeebeGatewayUnavailableError,
+    ZeebeInternalError,
+)
+
+__all__ = (
+    "InvalidOAuthCredentialsError",
+    "ActivateJobsRequestInvalidError",
+    "JobAlreadyDeactivatedError",
+    "JobNotFoundError",
+    "MessageAlreadyExistsError",
+    "InvalidJSONError",
+    "ProcessDefinitionHasNoStartEventError",
+    "ProcessDefinitionNotFoundError",
+    "ProcessInstanceNotFoundError",
+    "ProcessInvalidError",
+    "ProcessTimeoutError",
+    "BusinessError",
+    "DuplicateTaskTypeError",
+    "NoVariableNameGivenError",
+    "PyZeebeError",
+    "TaskNotFoundError",
+    "UnknownGrpcStatusCodeError",
+    "ZeebeBackPressureError",
+    "ZeebeDeadlineExceeded",
+    "ZeebeGatewayUnavailableError",
+    "ZeebeInternalError",
+)
