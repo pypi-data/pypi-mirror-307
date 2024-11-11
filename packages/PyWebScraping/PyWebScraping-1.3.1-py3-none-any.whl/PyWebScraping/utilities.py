c5540efee0e1a84c8338a9d520db38d95f261248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 14:05:23 +0000

    Add package PyWebScraping/1.3.1/PyWebScraping-1.3.1-py3-none-any.whl

---
+++ packages/PyWebScraping/PyWebScraping-1.3.1-py3-none-any.whl/PyWebScraping/utilities.py
@@ -0,0 +1,89 @@
+from win32api import GetSystemMetrics
+#
+#
+#
+#
+class WindowRect:
+	"""
+	Represents a window rectangle with x, y, width, and height.
+
+	Attributes:
+		x (int): The x-coordinate of the top-left corner. Defaults to 1/4 of screen width.
+		y (int): The y-coordinate of the top-left corner. Defaults to 10% of screen height.
+		width (int): The width of the rectangle. Defaults to 1/2 of screen width.
+		height (int): The height of the rectangle. Defaults to 80% of screen height.
+
+	:Usage:
+		rect = WindowRect()
+		print(rect.x, rect.y, rect.width, rect.height)
+
+		rect = WindowRect(x=100, y=50, width=800, height=600)
+		print(rect.x, rect.y, rect.width, rect.height)
+	"""
+	x = GetSystemMetrics(0) // 4
+	y = int(GetSystemMetrics(1) * 0.1)
+	width = GetSystemMetrics(0) // 2
+	height = int(GetSystemMetrics(1) * 0.8)
+	#
+	#
+	#
+	#
+	def __init__(
+			self,
+			x: int = None,
+			y: int = None,
+			width: int = None,
+			height: int = None
+	):
+		"""
+		Initializes WindowRect with optional x, y, width, and height values.
+
+		Args:
+			x (int): The x-coordinate. Defaults to None.
+			y (int): The y-coordinate. Defaults to None.
+			width (int): The width. Defaults to None.
+			height (int): The height. Defaults to None.
+
+		:Usage:
+		   rect = WindowRect(0, 0, 300, 200)
+		"""
+		self.set_rect(x, y, width, height)
+	#
+	#
+	#
+	#
+	def set_rect(
+			self,
+			x: int = None,
+			y: int = None,
+			width: int = None,
+			height: int = None
+	):
+		"""
+		Sets the rectangle dimensions.
+
+		Args:
+			x (int): The x-coordinate. Defaults to None.
+			y (int): The y-coordinate. Defaults to None.
+			width (int): The width. Defaults to None.
+			height (int): The height. Defaults to None.
+
+		Returns:
+			self: Returns the instance for method chaining.
+
+		:Usage:
+			rect = WindowRect().set_rect(0, 0, 300, 200)
+		"""
+		if x is not None:
+			self.x = x
+
+		if y is not None:
+			self.y = y
+
+		if width is not None:
+			self.width = width
+
+		if height is not None:
+			self.height = height
+
+		return self
