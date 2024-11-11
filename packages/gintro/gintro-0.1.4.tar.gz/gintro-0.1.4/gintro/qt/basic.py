c7b20c053760df4503772c4a76d797a481fc69ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 03:09:14 +0000

    Add package gintro/0.1.4/gintro-0.1.4.tar.gz

---
+++ packages/gintro/gintro-0.1.4.tar.gz/gintro-0.1.4/gintro/qt/basic.py
@@ -0,0 +1,10 @@
+from chinese_calendar import is_workday
+from datetime import datetime
+
+def is_trade_day(date):
+    # 检查日期是否是工作日且不是周末
+    if is_workday(date) and date.weekday() < 5:  # 周一到周五
+        return True
+    return False
+
+
