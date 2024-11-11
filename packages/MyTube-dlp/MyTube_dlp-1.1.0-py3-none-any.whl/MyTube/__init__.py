5e9d9949890e3c745d86c292f63b43c81a31afe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 03:09:27 +0000

    Add package MyTube-dlp/1.1.0/MyTube_dlp-1.1.0-py3-none-any.whl

---
+++ packages/MyTube-dlp/MyTube_dlp-1.1.0-py3-none-any.whl/MyTube/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.1.0"
+
+from .youtube import YouTube
+from .downloader import Downloader
+from .stream import Stream
+from .streams_manager import StreamsManager
+from .utils import Channel, Thumbnail
