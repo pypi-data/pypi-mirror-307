5fd36e75e71dc44bfad660c7ee0496134cf9ee81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:06:21 +0000

    Add package composio-crewai/0.5.41/composio_crewai-0.5.41.tar.gz

---
+++ packages/composio-crewai/composio_crewai-0.5.41.tar.gz/composio_crewai-0.5.41/composio_crewai/__init__.py
@@ -0,0 +1,24 @@
+from composio import WorkspaceType, action
+
+from composio_langchain import Action, App
+from composio_langchain import ComposioToolSet as Base
+from composio_langchain import Tag, Trigger
+
+
+class ComposioToolSet(
+    Base,
+    runtime="crewai",
+    description_char_limit=1024,
+):
+    pass
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "Trigger",
+    "WorkspaceType",
+    "action",
+    "ComposioToolSet",
+)
