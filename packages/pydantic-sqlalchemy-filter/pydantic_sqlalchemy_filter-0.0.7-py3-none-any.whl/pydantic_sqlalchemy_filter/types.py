0496e65879ac42610ec85e1c02234d2cdc7f9bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 16:05:04 +0000

    Add package pydantic-sqlalchemy-filter/0.0.7/pydantic_sqlalchemy_filter-0.0.7-py3-none-any.whl

---
+++ packages/pydantic-sqlalchemy-filter/pydantic_sqlalchemy_filter-0.0.7-py3-none-any.whl/pydantic_sqlalchemy_filter/types.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column
+from sqlalchemy.orm import DeclarativeBase
+
+
+class DatabaseModel(DeclarativeBase):
+    """SQLAlchemy model.
+
+    Fields:
+
+        id - Primary key.
+
+        deleted_at - Datetime when row was deleted.
+
+    """
+
+    id: Column
+    deleted_at: Column
+
+    def field_by_name(self, name: str) -> Column:
+        """Returns sqlalchemy model Column by name."""
+
+        raise NotImplementedError
