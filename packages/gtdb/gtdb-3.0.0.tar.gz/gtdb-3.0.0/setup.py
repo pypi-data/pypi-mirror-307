5f7f7aaa50161b519d06dc1c1a01f2b71e6581e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:24 +0000

    Add package gtdb/3.0.0/gtdb-3.0.0.tar.gz

---
+++ packages/gtdb/gtdb-3.0.0.tar.gz/gtdb-3.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="gtdb",
+	version="3.0.0",
+	author="Asror bek",
+	author_email="minecraftakk2009beta@gmail.com",
+	description="It is more convenient to manage a JSON file",
+	url="https://github.com/Dearpixels/gtdb",
+	packages=find_packages(),
+    python_requires='>=3.9'
+)
