3761c67b26c237827cfb9db561a025b1ca938f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 14:04:55 +0000

    Add package python-dataservice/0.9.0/python_dataservice-0.9.0.tar.gz

---
+++ packages/python-dataservice/python_dataservice-0.9.0.tar.gz/python_dataservice-0.9.0/dataservice/cache.py
@@ -0,0 +1,143 @@
+"""Cache Module."""
+
+from __future__ import annotations
+
+import atexit
+import json
+import logging
+import time
+from abc import ABC
+from functools import wraps
+from pathlib import Path
+from typing import Any, Callable
+
+from asyncer import asyncify
+from pydantic import HttpUrl
+
+from dataservice.models import Request, Response
+
+logger = logging.getLogger(__name__)
+
+
+class Cache(ABC):
+    """Base class for cache implementations."""
+
+    def set(self, key: Any, value: Any) -> None:
+        """Set a value in the cache."""
+        raise NotImplementedError
+
+    def get(self, key: Any) -> Any:
+        """Get a value from the cache."""
+        raise NotImplementedError
+
+    def delete(self, key: Any) -> None:
+        """Delete a value from the cache."""
+        raise NotImplementedError
+
+    def clear(self) -> None:
+        """Clear the cache."""
+        raise NotImplementedError
+
+
+class AsyncJsonCache:
+    """Simple JSON disk based cache implementation."""
+
+    def __init__(self, path: Path):
+        """Initialize the DictCache."""
+        self.path = path
+        self.cache = self._init_cache()
+        self.start_time = time.time()
+        atexit.register(self.write)
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        atexit.unregister(self.write)
+        await self.write()
+
+    def _init_cache(self):
+        if self.path.exists():
+            with open(self.path, "r") as f:
+                return json.load(f)
+        return {}
+
+    def set(self, key, value):
+        self.cache[key] = value
+
+    def get(self, key):
+        return self.cache.get(key)
+
+    def delete(self, key):
+        del self.cache[key]
+
+    def clear(self):
+        self.cache.clear()
+
+    async def write(self):
+        """Write the cache to disk."""
+
+        def sync_write():
+            logger.info(f"Writing cache to {self.path}")
+            with open(self.path, "w") as f:
+                json.dump(self.cache, f)
+
+        return await asyncify(sync_write)()
+
+    async def write_periodically(self, interval: int):
+        """Write the cache to disk periodically.
+
+        :param interval: The interval in seconds to write the cache.
+        """
+        if time.time() - self.start_time >= interval:
+            await self.write()
+            self.start_time = time.time()
+
+    def __contains__(self, key):
+        return key in self.cache
+
+    def __len__(self):
+        return len(self.cache)
+
+    def __iter__(self):
+        return iter(self.cache)
+
+    def __repr__(self):
+        return repr(self.cache)
+
+    def __str__(self):
+        return str(self.cache)
+
+
+async def cache_request(cache: AsyncJsonCache) -> Callable:
+    """
+    Caches the raw values (text, data) of the Response object returned by the request function.
+
+    :param cache: The cache to use.
+    """
+
+    async def wrapper(req_func: Callable, request: Request) -> Response:
+        """
+        Wraps a function to cache its results.
+
+        :param req_func: The function to wrap.
+        :param request: The request to cache.
+        """
+
+        @wraps(req_func)
+        async def inner() -> Response:
+            key = str(request.url)
+            if key in cache:
+                logger.debug(f"Cache hit for {key}")
+                text, data = cache.get(key)
+                return Response(request=request, text=text, data=data, url=HttpUrl(key))
+            else:
+                logger.debug(f"Cache miss for {key}")
+                response = await req_func(request)
+                value = response.text, response.data
+                cache.set(key, value)
+                return response
+
+        return await inner()
+
+    return wrapper
---
+++ packages/python-dataservice/python_dataservice-0.9.0.tar.gz/python_dataservice-0.9.0/dataservice/config.py
@@ -0,0 +1,123 @@
+"""Config."""
+
+from __future__ import annotations
+
+from typing import Annotated, Any, Literal, NewType, Optional
+
+from annotated_types import Ge
+from pydantic import BaseModel, Field, FilePath, NewPath
+
+PositiveInt = Annotated[int, Ge(0)]
+Milliseconds = NewType("Milliseconds", PositiveInt)
+Seconds = NewType("Seconds", PositiveInt)
+
+
+class RetryConfig(BaseModel):
+    """Retry configuration for the service."""
+
+    max_attempts: PositiveInt = 3
+    wait_exp_max: PositiveInt = 10
+    wait_exp_min: PositiveInt = 4
+    wait_exp_mul: PositiveInt = 1
+
+
+class RateLimiterConfig(BaseModel):
+    """Retry configuration for the service."""
+
+    max_rate: PositiveInt = 10
+    time_period: Seconds = Seconds(60)
+
+
+class CacheConfig(BaseModel):
+    use: bool = Field(default=False, description="Whether to cache requests.")
+    path: FilePath | NewPath = Field(
+        default="cache.json",
+        description="The path of the file to use for the cache. Defaults to 'cache.json'.",
+    )
+    write_interval: PositiveInt = Field(
+        default=20 * 60,
+        description="The interval to write the cache in seconds.Defaults to 20 minutes.",
+    )
+
+
+class ServiceConfig(BaseModel):
+    """Global configuration for the service."""
+
+    retry: RetryConfig = Field(
+        default_factory=RetryConfig, description="The retry configuration."
+    )
+    deduplication: bool = Field(
+        default=True, description="Whether to deduplicate requests."
+    )
+    deduplication_keys: set[str] = Field(
+        default={
+            "url",
+            "params",
+            "method",
+            "form_data",
+            "json_data",
+            "content_type",
+            "headers",
+        },
+        description="A list of keys to use for deduplication.",
+    )
+    max_concurrency: PositiveInt = Field(
+        default=10, description="The maximum number of concurrent requests."
+    )
+    random_delay: Milliseconds = Field(
+        default=Milliseconds(0),
+        description="The maximum random delay between requests.",
+    )
+
+    constant_delay: Milliseconds = Field(
+        default=Milliseconds(0),
+        description="Constant delay between requests.",
+    )
+
+    limiter: RateLimiterConfig | None = Field(
+        description="The rate limiter configuration", default=None
+    )
+    cache: CacheConfig = Field(
+        description="The cache configuration", default_factory=CacheConfig
+    )
+
+
+class ProxyConfig(BaseModel):
+    """Proxy configuration for the service."""
+
+    host: str = Field(description="The proxy host.")
+    port: int = Field(description="The proxy port.")
+    username: Optional[str] = Field(description="The proxy username.", default=None)
+    password: Optional[str] = Field(description="The proxy password.", default=None)
+
+    @classmethod
+    def from_url(cls, url: str) -> ProxyConfig:
+        if "://" in url:
+            url = url.split("://")[1]
+        if "@" in url:
+            auth, url = url.split("@")
+            username, password = auth.split(":")
+        else:
+            username = None
+            password = None
+        host, port = url.split(":")
+        return cls(host=host, port=int(port), username=username, password=password)
+
+    @property
+    def url(self) -> str:
+        if self.username and self.password:
+            return f"http://{self.username}:{self.password}@{self.host}:{self.port}"
+        return f"http://{self.host}:{self.port}"
+
+
+class PlaywrightConfig(BaseModel):
+    browser: Literal["chromium", "firefox", "webkit"] = Field(
+        description="The browser to use.", default="chromium"
+    )
+    headless: bool = Field(description="Whether to run in headless mode.", default=True)
+    slow_mo: PositiveInt = Field(
+        description="The slow motion delay in milliseconds.", default=0
+    )
+    device: Optional[dict[str, Any]] = Field(
+        description="The devices to use.", default=None
+    )
