547a096eb564245482764274efb4123be231378e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:06:15 +0000

    Add package github-bird/0.0.0/github_bird-0.0.0.tar.gz

---
+++ packages/github-bird/github_bird-0.0.0.tar.gz/github_bird-0.0.0/README.md
@@ -0,0 +1,18 @@
+# Introduction
+## Install package
+```shell
+pip install github_bird
+```
+
+##  Example Usage:
+
+```python
+from github_bird.repos import GithubCrawler
+
+# Define the GitHub API tokens here
+tokens = ["your_token_1", "your_token_2"]  # Replace with actual tokens
+
+crawler = GithubCrawler(tokens)
+crawler.crawl_user_repos('milvus-io')
+
+```
---
+++ packages/github-bird/github_bird-0.0.0.tar.gz/github_bird-0.0.0/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+from setuptools import find_packages
+with open("README.md", "r",encoding='utf-8') as f:
+  long_description = f.read()
+setup(name='github_bird',  # 包名
+      version='0.0.0',  # 版本号
+      description='github tools',
+      long_description_content_type = 'text/markdown',
+      long_description=long_description,
+      author='chandler song',
+      author_email='275737875@qq.com',
+      url='https://www.linkedin.com/in/chandlersong/',
+      keywords = "github api tool by token",
+      license='MIT',
+      packages = find_packages(),
+      install_requires=[
+          "requests",
+      ],
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Topic :: Software Development :: Libraries'
+      ],
+
+      )
