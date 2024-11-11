86005d415b1e16970f98faddbc7cb2b1cc573e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:37 +0000

    Add package torchmeter/0.0.0/torchmeter-0.0.0-py3-none-any.whl

---
+++ packages/torchmeter/torchmeter-0.0.0-py3-none-any.whl/torchmeter/__main__.py
@@ -0,0 +1,4 @@
+from .main import welcome
+
+if __name__ == "__main__":
+    welcome()
---
+++ packages/torchmeter/torchmeter-0.0.0-py3-none-any.whl/torchmeter/main.py
@@ -0,0 +1,17 @@
+def welcome():
+    print('''
+Welcome to TorchMeter ⏲️ !!!
+
+Torchmeter is an all-in-one tool for Pytorch model analysis, 
+measuring Params, FLOPs/MACs(aka. MACC or MADD), Memory cost, Inference time and Throughput!!
+🚀🚀🚀
+
+-------------------------------- NOTE --------------------------------
+
+You will see this because this version is just a starting point for the project.
+Subscribe to repo below for more updates and new features!!
+
+https://github.com/Ahzyuan/torchmeter
+
+Looking forward to your star if this project does help you ⭐⭐⭐
+''')
