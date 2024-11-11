0b470c956f2788714efc6f9ffddf446d1c2b6ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 16:05:23 +0000

    Add package mssqlconnect/0.3/mssqlconnect-0.3.tar.gz

---
+++ packages/mssqlconnect/mssqlconnect-0.3.tar.gz/mssqlconnect-0.3/mssqlconnect/__init__.py
@@ -0,0 +1,1 @@
+from mssqlconnect.mssqlconnect import *
---
+++ packages/mssqlconnect/mssqlconnect-0.3.tar.gz/mssqlconnect-0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mssqlconnect',
+    version='0.3',
+    packages=find_packages(),
+    description='mssqlconnect',
+    # long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    # url='http://github.com/yourusername/my_package',
+    author='棽杓',
+    author_email='1047366140@qq,com',
+    license='MIT',
+    install_requires=[
+        "pymssql"
+    ],
+    classifiers=[
+        # 分类信息
+    ]
+)
