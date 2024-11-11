2eeee854dd2368b4d111b1efe23f36b3fcc4c1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:07:24 +0000

    Add package gzspidertools/0.0.21/gzspidertools-0.0.21-py3-none-any.whl

---
+++ packages/gzspidertools/gzspidertools-0.0.21-py3-none-any.whl/gzspidertools/scraper/http/__init__.py
@@ -0,0 +1,7 @@
+from gzspidertools.scraper.http.request import AiohttpRequest
+from gzspidertools.scraper.http.request.form import AiohttpFormRequest
+
+__all__ = [
+    "AiohttpRequest",
+    "AiohttpFormRequest",
+]
---
+++ packages/gzspidertools/gzspidertools-0.0.21-py3-none-any.whl/gzspidertools/scraper/pipelines/oracle/fantasy.py
@@ -0,0 +1,7 @@
+from gzspidertools.scraper.pipelines.oracle import AyuOraclePipeline
+
+__all__ = ["AyuFtyOraclePipeline"]
+
+
+class AyuFtyOraclePipeline(AyuOraclePipeline):
+    ...
