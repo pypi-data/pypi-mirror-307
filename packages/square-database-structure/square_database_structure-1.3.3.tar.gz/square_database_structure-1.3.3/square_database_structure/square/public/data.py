debcde69a30c46f0ae6f93a5d2542e321d52e880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:01 +0000

    Add package square-database-structure/1.3.3/square_database_structure-1.3.3.tar.gz

---
+++ packages/square-database-structure/square_database_structure-1.3.3.tar.gz/square_database_structure-1.3.3/square_database_structure/square/public/data.py
@@ -0,0 +1,8 @@
+from square_database_structure.square.public.tables import App
+
+data_to_insert = []
+data_to_insert.append(App(app_name="test"))
+data_to_insert.append(App(app_name="square_admin"))
+data_to_insert.append(App(app_name="ttthree"))
+data_to_insert.append(App(app_name="square_wallet"))
+data_to_insert.append(App(app_name="raspi_home"))
