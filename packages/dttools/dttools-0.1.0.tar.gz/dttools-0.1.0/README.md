ad459c2f6a089272a58c99f51e899f80245b6686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 05:05:47 +0000

    Add package dttools/0.1.0/dttools-0.1.0.tar.gz

---
+++ packages/dttools/dttools-0.1.0.tar.gz/dttools-0.1.0/README.md
@@ -0,0 +1,8 @@
+# datetimetools
+
+A simple library for common date and time manipulations.
+
+## Installation
+
+```bash
+pip install datetimetools
---
+++ packages/dttools/dttools-0.1.0.tar.gz/dttools-0.1.0/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dttools",
+    version="0.1.0",
+    description="A simple library for common date and time manipulations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Emmanuel Ademola",
+    author_email="ademola.emmanuel383@gmail.com",
+    url="https://github.com/l4christ/dttools",
+    packages=find_packages(),
+    install_requires=["pytz"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
