6ab5b6bb5dd829f2933d51d93e52805bee106bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 05:05:47 +0000

    Add package cs-ai-common/0.5.7/cs_ai_common-0.5.7.tar.gz

---
+++ packages/cs-ai-common/cs_ai_common-0.5.7.tar.gz/cs_ai_common-0.5.7/cs_ai_common/rds/entities/external_site_instance.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from cs_ai_common.rds.entities.base import Base
+
+
+class ExternalSiteInstance(Base):
+    __tablename__ = "external_site_instances"
+
+    id = Column(Integer(), primary_key=True)
+    auth_type = Column(String(50), nullable=False)
+    auth_creds = Column(String(200), nullable=False)
+
+    user_id = Column(Integer(), ForeignKey('users.id'))
+    user = relationship('User', back_populates='external_site_instances')
+    
+    external_site_id = Column(Integer(), ForeignKey('external_site.id'))
+    external_site = relationship('ExternalSite', back_populates='external_site_instances')
