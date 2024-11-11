0d8ac73e41ef5c2a75432ffcb722db728f8e6e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:25 +0000

    Add package cut-video123423/0.0.12/cut_video123423-0.0.12.tar.gz

---
+++ packages/cut-video123423/cut_video123423-0.0.12.tar.gz/cut_video123423-0.0.12/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.12'
+DESCRIPTION = 'Easily cut the video by moviepy'
+
+setup(
+    name="cut_video123423",
+    version=VERSION,
+    author="lqc1234",
+    author_email="948961528@qq.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md',encoding="UTF8").read(),
+    packages=find_packages(),
+    install_requires=['moviepy'],
+    keywords=['python', 'moviepy', 'cut video'],
+    data_files=[('cut_video', ['to_erase.json'])],
+    entry_points={
+    'console_scripts': [
+        'cut_video = cut_video.cut_video:run'
+    ]
+    },
+    license="MIT",
+    url="https://github.com/chunleili/cut_video",
+    scripts=['cut_video/cut_video.py'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
