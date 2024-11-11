46a3a1f20a57654aa6daf02994f16af0dcb0e183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:23 +0000

    Add package hidjango/0.0.2/hidjango-0.0.2.tar.gz

---
+++ packages/hidjango/hidjango-0.0.2.tar.gz/hidjango-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='hidjango',
+    version='0.0.2',
+    description='A placeholder package for HiDjango',
+    author='Parsa Rezaei',
+    packages=['hidjango'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+
