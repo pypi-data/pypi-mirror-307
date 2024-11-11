860e006dd91a4494042df51c6ed850523cce7acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:06:53 +0000

    Add package juju/3.5.2.1/juju-3.5.2.1.tar.gz

---
+++ packages/juju/juju-3.5.2.1.tar.gz/juju-3.5.2.1/juju/version.py
@@ -0,0 +1,8 @@
+# Copyright 2023 Canonical Ltd.
+# Licensed under the Apache V2, see LICENCE file for details.
+
+LTS_RELEASES = ["jammy", "focal", "bionic", "xenial", "trusty", "precise"]
+
+DEFAULT_ARCHITECTURE = 'amd64'
+
+CLIENT_VERSION = "3.5.2.1"
