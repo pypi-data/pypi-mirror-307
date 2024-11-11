f8c1a9a3fc643bb92b980304c75a382eddd11c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:06:24 +0000

    Add package pytempl-icons/0.0.1a5/pytempl_icons-0.0.1a5-py3-none-any.whl

---
+++ packages/pytempl-icons/pytempl_icons-0.0.1a5-py3-none-any.whl/pytempl_icons/__init__.py
@@ -0,0 +1,46 @@
+from .abstract import (
+    CheckCircleBigIcon,
+    CheckIcon,
+    CirclePlusIcon,
+    CrossIcon,
+    DotFilledIcon,
+    EllipsisIcon,
+    ExclamationTriangleIcon,
+    HamburgerMenuIcon,
+    PlusIcon,
+    RefreshCWIcon,
+)
+from .arrows import (
+    ArrowLeftIcon,
+    ArrowRightIcon,
+    ChevronDownIcon,
+    ChevronLeftIcon,
+    ChevronRightIcon,
+    ChevronUpIcon,
+)
+from .objects import BanknoteIcon, EyeIcon, EyeOffIcon, PencilIcon, TrashIcon
+
+__version__ = "0.0.1a5"
+__all__ = [
+    "ArrowRightIcon",
+    "ArrowLeftIcon",
+    "BanknoteIcon",
+    "CheckCircleBigIcon",
+    "CheckIcon",
+    "ChevronDownIcon",
+    "ChevronLeftIcon",
+    "ChevronRightIcon",
+    "ChevronUpIcon",
+    "CirclePlusIcon",
+    "CrossIcon",
+    "DotFilledIcon",
+    "EllipsisIcon",
+    "ExclamationTriangleIcon",
+    "EyeIcon",
+    "EyeOffIcon",
+    "HamburgerMenuIcon",
+    "PencilIcon",
+    "PlusIcon",
+    "RefreshCWIcon",
+    "TrashIcon",
+]
