3d09d1b3bba310c1f40fa1d0bba9a0be356c1798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:07:14 +0000

    Add package team3-chassis/1.1.5/team3_chassis-1.1.5.tar.gz

---
+++ packages/team3-chassis/team3_chassis-1.1.5.tar.gz/team3_chassis-1.1.5/team3_chassis/messaging/loggerprovider.py
@@ -0,0 +1,82 @@
+import asyncio
+import json
+import logging.config
+import os
+from dataclasses import dataclass
+
+import aio_pika
+from aio_pika import ExchangeType
+
+class LoggerProvider:
+
+    connection: aio_pika.robust_connection = None
+    channel : aio_pika.channel= None
+    exchange_name = None
+    exchange : aio_pika.robust_exchange = None
+    rabbitmq_host = None
+    rabbitmq_user = None
+    rabbitmq_password = None
+    logger : logging.Logger = None
+
+    @classmethod
+    async def create(cls,rabbitmq_host, rabbitmq_user, rabbitmq_password, logger: logging.Logger, exchange_name:str='events'):
+        self=LoggerProvider()
+        self.rabbitmq_host = rabbitmq_host
+        self.rabbitmq_user = rabbitmq_user
+        self.rabbitmq_password = rabbitmq_password
+        self.logger = logger
+        self.exchange_name = exchange_name
+        await self.subscribe_channel(exchange_name)
+        return self
+
+
+    async def subscribe_channel(self, exchange_name):
+        retries = 5
+        for attempt in range(retries):
+            try:
+                self.connection = await aio_pika.connect_robust(
+                    host=self.rabbitmq_host,
+                    virtualhost='/',
+                    login=self.rabbitmq_user,
+                    password=self.rabbitmq_password
+                )
+            except Exception as e:
+                self.logger.error(f"Connection failed: {e}")
+                if attempt < retries - 1:
+                    self.logger.info(f"Retrying in {5} seconds...")
+                    await asyncio.sleep(5)
+                else:
+                    self.logger.error("All retry attempts failed")
+                    raise e
+        self.logger.info("Connection established")
+        # Create a channel
+        self.logger.info("Connected to RabbitMQ")
+        self.channel = await self.connection.channel()
+        self.logger.info("Channel created")
+        self.logger.info("Exchange created")
+        self.exchange = await self.channel.declare_exchange(name=exchange_name, type=ExchangeType.TOPIC, durable=True)
+
+
+    async def send_log(self, key,message, routing_key):
+        data={
+            "key": key,
+            "message": message
+        }
+        message_body = json.dumps(data)
+        await self.publish(message_body, routing_key)
+
+    async def publish(self,message_body, routing_key):
+        # Publish the message to the exchange
+        self.logger.info("Publishing message to exchange %s with routing key %s", self.exchange_name, routing_key)
+        await self.exchange.publish(
+            aio_pika.Message(
+                body=message_body.encode(),
+                content_type="text/plain"
+            ),
+            routing_key=routing_key)
+        
+    async def suscribe_and_publish(self, message, routing_key, exchange_name):
+        await self.subscribe_channel(exchange_name)
+        self.logger.info("Sending log to exchange %s with routing key %s", exchange_name, routing_key)
+        await self.publish(message, routing_key)
+    
