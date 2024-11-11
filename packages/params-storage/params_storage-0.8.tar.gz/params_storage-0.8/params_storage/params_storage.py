4e6bd2b8f49c11acc281dce3ad31b022c329cb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 05:05:47 +0000

    Add package params-storage/0.8/params_storage-0.8.tar.gz

---
+++ packages/params-storage/params_storage-0.8.tar.gz/params_storage-0.8/params_storage/params_storage.py
@@ -0,0 +1,57 @@
+import typing as tp
+
+TParams = tp.Dict[str, tp.Union[str, int, float]]
+
+
+class ParamsStorage:
+    """
+    ParamsStorage is key-value storage to store hyperparameters for machine learning models as a key. The value of a key
+    can be anything, e.g. cross-validation score of the hyperparameters. All floating-point hyperparameters will store
+    with given desired precision. This can be useful for pruning optuna params suggestions.
+
+    Example:
+        ps = ParamsStorage(precision=1)
+
+        ps.add(params=dict(lr=0.17, mode="Bayesian"))
+        ps.add(params=dict(lr=0.16, mode="Bayesian"))
+        ps.add(params=dict(lr=0.1, bs=32), value=0.95)
+
+        ps.size()  # 2
+        ps.exist(dict(lr=0.16, mode="Bayesian"))  # True
+        ps.exist(dict(lr=0.2, mode="Bayesian"))  # True, since round(0.16, 1) = 0.2
+        ps.get(dict(lr=0.1, bs=32))  # 0.95
+    """
+
+    def __init__(self, precision: int):
+        self.precision = precision
+        self._memory: tp.Dict[tuple, tp.Optional[tp.Any]] = dict()
+
+    @staticmethod
+    def _round_dict(params: TParams, digits: int) -> TParams:
+        rounded_dict: TParams = dict()
+        for k, v in params.items():
+            if isinstance(v, float):
+                rounded_dict[k] = round(v, digits)
+            else:
+                rounded_dict[k] = v
+        return rounded_dict
+
+    def _get_key(self, params: TParams) -> tuple:
+        key = sorted(list(zip(self._round_dict(params, self.precision).items())))
+        return tuple(key)
+
+    def add(self, params: TParams, value: tp.Optional[tp.Any] = None):
+        key = self._get_key(params)
+        if key not in self._memory:
+            self._memory[key] = value
+
+    def get(self, params: TParams) -> tp.Any:
+        key = self._get_key(params)
+        return self._memory[key]
+
+    def exist(self, params: TParams) -> bool:
+        key = self._get_key(params)
+        return key in self._memory
+
+    def size(self) -> int:
+        return len(self._memory)
