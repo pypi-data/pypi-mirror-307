10287368e39e968eeef1df8170867869fafdd8be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:25 +0000

    Add package hidjango/0.0.3/hidjango-0.0.3.tar.gz

---
+++ packages/hidjango/hidjango-0.0.3.tar.gz/hidjango-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='hidjango',
+    version='0.0.3',
+    description='A placeholder package for HiDjango',
+    author='Parsa Rezaei',
+    packages=['hidjango'],
+    url= "https://github.com/parsarezaee/HiDjango",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+
