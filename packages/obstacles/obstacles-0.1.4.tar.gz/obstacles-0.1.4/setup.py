11e9b76acb5174470d2d03d8f9014a244357c325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 05:05:25 +0000

    Add package obstacles/0.1.4/obstacles-0.1.4.tar.gz

---
+++ packages/obstacles/obstacles-0.1.4.tar.gz/obstacles-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='obstacles',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[
+        # Dependencies
+    ],
+    entry_points={
+        "console_scripts": [
+            "obstacles-game = Obstacles:obstacles_game"
+        ]
+    }
+)
