b59bac956441fed516b7718afaa7083c9b518d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:06:43 +0000

    Add package notoken887/1.26.33/notoken887-1.26.33.tar.gz

---
+++ packages/notoken887/notoken887-1.26.33.tar.gz/notoken887-1.26.33/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='notoken887',
+    version='1.26.33',
+    packages=find_packages(), 
+    author='Yous',  
+    author_email='hitler@gmail.com', 
+    description='my obfuscation test lib',
+    long_description='This is a test library for token encryption.',
+    url='https://thugging.org',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'notoken887 = notoken887.main:main',
+        ],
+    },
+)
