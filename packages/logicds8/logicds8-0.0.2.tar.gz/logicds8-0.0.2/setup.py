83ea6c28d434ac70a409a588732c79f4f8a550fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 14:05:15 +0000

    Add package logicds8/0.0.2/logicds8-0.0.2.tar.gz

---
+++ packages/logicds8/logicds8-0.0.2.tar.gz/logicds8-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="logicds8",
+    version="0.0.2",
+    author="Barnas Monteith,LogicDS",
+    author_email="barnas@engagescience.org",
+    description="LogicDS reasoning-based universal language teaching library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/brn378/logicds",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",],
+)
