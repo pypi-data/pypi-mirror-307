0623b7baec3548a3583066bfe242a87a6819b071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 15:06:00 +0000

    Add package teyvatapi/1.11.0.dev0/teyvatapi-1.11.0.dev0.tar.gz

---
+++ packages/teyvatapi/teyvatapi-1.11.0.dev0.tar.gz/teyvatapi-1.11.0.dev0/teyvatapi/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from teyvatapi.api.admin_api import AdminApi
+from teyvatapi.api.book_api import BookApi
+from teyvatapi.api.discord_api import DiscordApi
+from teyvatapi.api.o_auth_api import OAuthApi
+from teyvatapi.api.paimon_api import PaimonApi
+from teyvatapi.api.user_api import UserApi
+
