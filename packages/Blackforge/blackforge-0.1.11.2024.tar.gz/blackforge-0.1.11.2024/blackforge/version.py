68e754b75c7cc80991cca73d38f9995ea009497f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 14:04:51 +0000

    Add package Blackforge/0.1.11.2024/blackforge-0.1.11.2024.tar.gz

---
+++ packages/Blackforge/blackforge-0.1.11.2024.tar.gz/blackforge-0.1.11.2024/blackforge/version.py
@@ -0,0 +1,6 @@
+from ..setup import version
+
+BLACKFORGE_MAJOR:int=version["major"]
+BLACKFORGE_MINOR:int=version["minor"]
+BLACKFORGE_PATCH:int=version["patch"]
+BLACKFORGE_YEAR:int=version["year"]
