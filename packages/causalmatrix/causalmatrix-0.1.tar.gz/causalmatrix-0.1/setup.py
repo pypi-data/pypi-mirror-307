d73dd0c37d04883270b014457395eba01cb46375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 03:09:13 +0000

    Add package causalmatrix/0.1/causalmatrix-0.1.tar.gz

---
+++ packages/causalmatrix/causalmatrix-0.1.tar.gz/causalmatrix-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="causalmatrix",
+    version="0.1",
+    description="A placeholder package to reserve the name CausalMatrix",
+    long_description="This is a placeholder package to reserve the name CausalMatrix. The full functionality will be added in future versions.",
+    long_description_content_type="text/markdown",
+    author="Awadelrahman M. A. Ahmed",
+    author_email="awadrahman@gmail.com",
+    url="https://github.com/Awadelrahman/causalmatrix",  # Link to the GitHub repo (optional)
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
