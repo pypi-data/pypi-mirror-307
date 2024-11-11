00eab1d423cff8343e2ea1a09b358867b62b9b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-11 04:05:15 +0000

    Add package rlogging/2.0.3/rlogging-2.0.3-py3-none-any.whl

---
+++ packages/rlogging/rlogging-2.0.3-py3-none-any.whl/rlogging/integration/django/adapters.py
@@ -0,0 +1,23 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+
+from rlogging import HttpLoggerAdapter
+
+
+class DjangoLoggerAdapter(HttpLoggerAdapter):
+    def request(self, request: WSGIRequest, *args, **kwargs):
+        kwargs.setdefault('stacklevel', 5)
+
+        kwargs['extra'] = kwargs.get('extra', {})
+        kwargs['extra'].update({'django': {'path': request.path, 'view_func': request.resolver_match}})
+
+        return super().request(
+            request.build_absolute_uri(),
+            request.method,
+            *args,
+            **kwargs,
+        )
+
+    def response(self, response: HttpResponse, *args, **kwargs):
+        kwargs.setdefault('stacklevel', 5)
+        return super().response(response.status_code, *args, **kwargs)
