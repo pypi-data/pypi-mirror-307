1d36e9f10ea138e408ce1593d4aaa5b07e3521ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 04:08:29 +0000

    Add package toggl-api-wrapper/1.4.0/toggl_api_wrapper-1.4.0.tar.gz

---
+++ packages/toggl-api-wrapper/toggl_api_wrapper-1.4.0.tar.gz/toggl_api_wrapper-1.4.0/toggl_api/project.py
@@ -0,0 +1,222 @@
+from __future__ import annotations
+
+import logging
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Any, Final, Optional
+
+from httpx import HTTPStatusError, codes
+
+from toggl_api._exceptions import NamingError
+from toggl_api.utility import format_iso
+
+from .meta import BaseBody, RequestMethod, TogglCachedEndpoint
+from .models import TogglProject
+
+if TYPE_CHECKING:
+    from datetime import date, datetime
+
+log = logging.getLogger("toggl-api-wrapper.endpoint")
+
+
+@dataclass
+class ProjectBody(BaseBody):
+    """JSON body dataclass for PUT, POST & PATCH requests."""
+
+    name: Optional[str] = field(default=None)
+    """Name of the project. Defaults to None. Will be required if its a POST request."""
+
+    active: bool = field(default=True)
+    """Whether the project is archived or active."""
+    is_private: Optional[bool] = field(default=True)
+    """Whether the project is private or not. Defaults to True."""
+
+    client_id: Optional[int] = field(default=None)
+    client_name: Optional[str] = field(default=None)
+    """Client name if client_id is not set. Defaults to None. If client_id is
+    set the client_name will be ignored."""
+
+    color: Optional[str] = field(default=None)
+
+    start_date: Optional[datetime | date] = field(default=None)
+    end_date: Optional[datetime | date] = field(default=None)
+    """Date to set the end of the project. If not set or start date is after
+    the end date the end date will be ignored."""
+
+    def format(self, endpoint: str, **body: Any) -> dict[str, Any]:
+        """Formats the body for JSON requests.
+
+        Gets called by the endpoint methods before requesting.
+
+        Args:
+            endpoint: Name of the endpoint for filtering purposes.
+            body: Additional arguments for the body.
+
+        Returns:
+            dict[str, Any]: JSON compatible formatted body.
+        """
+        body.update(
+            {
+                "active": self.active,
+                "is_private": self.is_private,
+            },
+        )
+
+        if self.client_id:
+            body["client_id"] = self.client_id
+        elif self.client_name:
+            body["client_name"] = self.client_name
+
+        if self.color:
+            color = ProjectEndpoint.get_color(self.color) if self.color in ProjectEndpoint.BASIC_COLORS else self.color
+            body["color"] = color
+
+        if self.start_date:
+            body["start_date"] = format_iso(self.start_date)
+            if self.end_date and self.end_date > self.start_date:
+                body["end_date"] = format_iso(self.end_date)
+
+        if self.name:
+            body["name"] = self.name
+
+        return body
+
+
+class ProjectEndpoint(TogglCachedEndpoint[TogglProject]):
+    """Specific endpoints for retrieving and modifying projects.
+
+    [Official Documentation](https://engineering.toggl.com/docs/api/projects)
+    """
+
+    BASIC_COLORS: Final[dict[str, str]] = {
+        "blue": "#0b83d9",
+        "violet": "#9e5bd9",
+        "pink": "#d94182",
+        "orange": "#e36a00",
+        "gold": "#bf7000",
+        "green": "#2da608",
+        "teal": "#06a893",
+        "beige": "#c9806b",
+        "dark-blue": "#465bb3",
+        "purple": "#990099",
+        "yellow": "#c7af14",
+        "dark-green": "#566614",
+        "red": "#d92b2b",
+        "gray": "#525266",
+    }
+    """Basic colors available for projects in order."""
+
+    def collect(self, *, refresh: bool = False) -> list[TogglProject]:
+        """Returns all cached or remote projects.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/projects#get-workspaceprojects)
+        """
+        return self.request("", refresh=refresh)
+
+    def get(self, project_id: int | TogglProject, *, refresh: bool = False) -> TogglProject | None:
+        """Request a projects based on its id.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/projects#get-workspaceproject)
+        """
+        if isinstance(project_id, TogglProject):
+            project_id = project_id.id
+
+        if not refresh:
+            return self.cache.find_entry({"id": project_id})  # type: ignore[return-value]
+
+        try:
+            response = self.request(
+                f"/{project_id}",
+                refresh=refresh,
+            )
+        except HTTPStatusError as err:
+            if err.response.status_code == codes.NOT_FOUND:
+                log.warning("Project with id %s was not found!", project_id)
+                return None
+            raise
+
+        return response or None
+
+    def delete(self, project: TogglProject | int) -> None:
+        """Deletes a project based on its id.
+
+        This endpoint always hit the external API in order to keep projects consistent.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/projects#delete-workspaceproject)
+        """
+
+        project_id = project if isinstance(project, int) else project.id
+        try:
+            self.request(
+                f"/{project_id}",
+                method=RequestMethod.DELETE,
+                refresh=True,
+            )
+        except HTTPStatusError as err:
+            if err.response.status_code != codes.NOT_FOUND:
+                raise
+            log.warning(
+                "Project with id %s was either already deleted or did not exist in the first place!",
+                project_id,
+            )
+
+        if isinstance(project, int):
+            proj = self.cache.find_entry({"id": project})
+            if not isinstance(proj, TogglProject):
+                return
+            project = proj
+
+        self.cache.delete_entries(project)
+        self.cache.commit()
+
+    def edit(self, project: TogglProject | int, body: ProjectBody) -> TogglProject | None:
+        """Edit a project based on its id with the parameters provided in the body.
+
+        This endpoint always hit the external API in order to keep projects consistent.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/projects#put-workspaceproject)
+        """
+        if isinstance(project, TogglProject):
+            project = project.id
+        return self.request(
+            f"/{project}",
+            method=RequestMethod.PUT,
+            body=body.format("edit", workspace_id=self.workspace_id),
+            refresh=True,
+        )
+
+    def add(self, body: ProjectBody) -> TogglProject | None:
+        """Create a new project based on the parameters provided in the body.
+
+        This endpoint always hit the external API in order to keep projects consistent.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/projects#post-workspaceprojects)
+        """
+        if body.name is None:
+            msg = "Name must be set in order to create a project!"
+            raise NamingError(msg)
+
+        return self.request(
+            "",
+            method=RequestMethod.POST,
+            body=body.format("add", workspace_id=self.workspace_id),
+            refresh=True,
+        )
+
+    @classmethod
+    def get_color(cls, color: str) -> str:
+        """Get a color by name. Defaults to gray."""
+        return cls.BASIC_COLORS.get(color, "#d80435")
+
+    @classmethod
+    def get_color_id(cls, color: str) -> int:
+        """Get a color id by name."""
+        colors = list(cls.BASIC_COLORS.values())
+        return colors.index(color)
+
+    @property
+    def endpoint(self) -> str:
+        return f"workspaces/{self.workspace_id}/projects"
+
+    @property
+    def model(self) -> type[TogglProject]:
+        return TogglProject
---
+++ packages/toggl-api-wrapper/toggl_api_wrapper-1.4.0.tar.gz/toggl_api_wrapper-1.4.0/toggl_api/user.py
@@ -0,0 +1,263 @@
+from __future__ import annotations
+
+import logging
+import warnings
+from datetime import date, datetime, timezone
+from typing import Any, Final, Optional
+
+import httpx
+from httpx import BasicAuth, HTTPStatusError, codes
+
+from toggl_api import Comparison, TogglQuery
+from toggl_api._exceptions import DateTimeError
+
+from .meta import TogglCachedEndpoint, TogglEndpoint
+from .models import TogglTracker
+from .utility import format_iso
+
+log = logging.getLogger("toggl-api-wrapper.endpoint")
+
+
+class UserEndpoint(TogglCachedEndpoint[TogglTracker]):
+    """Endpoint for retrieving and fetching trackers with GET requests.
+
+    See the [TrackerEndpoint][toggl_api.TrackerEndpoint] for modifying trackers.
+
+    [Official Documentation](https://engineering.toggl.com/docs/api/me)
+    """
+
+    TRACKER_NOT_RUNNING: Final[int] = codes.METHOD_NOT_ALLOWED
+
+    def current(self, *, refresh: bool = True) -> TogglTracker | None:
+        """Get current running tracker. Returns None if no tracker is running.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/time_entries#get-get-current-time-entry)
+        """
+
+        if not refresh:
+            query = list(self.cache.query(TogglQuery("stop", None)))
+            return query[0] if query else None  # type: ignore[return-value]
+
+        try:
+            response = self.request("/time_entries/current", refresh=refresh)
+        except HTTPStatusError as err:
+            if err.response.status_code == self.TRACKER_NOT_RUNNING:
+                log.warning("No tracker is currently running!")
+                return None
+            raise
+
+        return response if isinstance(response, TogglTracker) else None
+
+    def _collect_cache(
+        self,
+        since: Optional[int | datetime] = None,
+        before: Optional[date] = None,
+        start_date: Optional[date] = None,
+        end_date: Optional[date] = None,
+    ) -> list[TogglTracker]:
+        cache: list[TogglTracker] = []
+        if since or before:
+            queries: list[TogglQuery[date]] = []
+
+            if since:
+                since = datetime.fromtimestamp(since, tz=timezone.utc) if isinstance(since, int) else since
+                queries.append(TogglQuery("timestamp", since, Comparison.GREATER_THEN))
+
+            if before:
+                queries.append(TogglQuery("start", before, Comparison.LESS_THEN))
+
+            cache.extend(self.query(*queries))
+
+        elif start_date and end_date:
+            cache.extend(
+                self.query(
+                    TogglQuery("start", start_date, Comparison.GREATER_THEN_OR_EQUAL),
+                    TogglQuery("start", end_date, Comparison.LESS_THEN_OR_EQUAL),
+                ),
+            )
+        else:
+            cache.extend(self.load_cache())
+
+        return cache
+
+    def collect(
+        self,
+        since: Optional[int | datetime] = None,
+        before: Optional[date] = None,
+        start_date: Optional[date] = None,
+        end_date: Optional[date] = None,
+        *,
+        refresh: bool = False,
+    ) -> list[TogglTracker]:
+        """Get a set of trackers depending on specified parameters.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/time_entries#get-timeentries)
+
+        Missing meta and include_sharing query flags not supported by wrapper at
+        the moment.
+
+        Examples:
+            >>> collect(since=17300032362, before=date(2024, 11, 27))
+
+            >>> collect(refresh=True)
+
+            >>> collect(start_date=date(2024, 11, 27), end_date=date(2024, 12, 27))
+
+        Args:
+            since: Get entries modified since this date using UNIX timestamp.
+                Includes deleted ones if refreshing.
+            before: Get entries with start time, before given date (YYYY-MM-DD)
+                or with time in RFC3339 format.
+            start_date: Get entries with start time, from start_date YYYY-MM-DD
+                or with time in RFC3339 format. To be used with end_date.
+            end_date: Get entries with start time, until end_date YYYY-MM-DD or
+                with time in RFC3339 format. To be used with start_date.
+            refresh: Whether to refresh the cache or not.
+
+        Raises:
+            DateTimeError: If the dates are not in the correct ranges.
+
+        Returns:
+            list[TogglTracker]: List of TogglTracker objects that are within
+                specified parameters. Empty if none is matched.
+        """
+
+        if start_date and end_date:
+            if end_date < start_date:
+                msg = "end_date must be after the start_date!"
+                raise DateTimeError(msg)
+            if start_date > datetime.now(tz=timezone.utc):
+                msg = "start_date must not be earlier than the current date!"
+                raise DateTimeError(msg)
+
+        if not refresh:
+            return self._collect_cache(since, before, start_date, end_date)
+
+        params = "/time_entries"
+        if since or before:
+            if since:
+                format_since = int(since.timestamp()) if isinstance(since, datetime) else since
+                params += f"?since={format_since}"
+
+            if before:
+                params += "&" if since else "?"
+                params += f"before={format_iso(before)}"
+
+        elif start_date and end_date:
+            params += f"?start_date={format_iso(start_date)}&end_date={format_iso(end_date)}"
+
+        response = self.request(params, refresh=refresh)
+
+        return response if isinstance(response, list) else []
+
+    def get(
+        self,
+        tracker_id: int | TogglTracker,
+        *,
+        refresh: bool = False,
+    ) -> TogglTracker | None:
+        """Get a single tracker by ID.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/time_entries#get-get-a-time-entry-by-id)
+
+        Args:
+            tracker_id: ID of the tracker to get.
+            refresh: Whether to refresh the cache or not.
+
+        Returns:
+            TogglTracker | None: TogglTracker object or None if not found.
+        """
+        if isinstance(tracker_id, TogglTracker):
+            tracker_id = tracker_id.id
+
+        if not refresh:
+            return self.cache.find_entry({"id": tracker_id})  # type: ignore[return-value]
+
+        try:
+            response = self.request(
+                f"/time_entries/{tracker_id}",
+                refresh=refresh,
+            )
+        except HTTPStatusError as err:
+            if err.response.status_code == codes.NOT_FOUND:
+                log.warning("Tracker with id %s does not exist!", tracker_id)
+                return None
+            raise
+
+        return response
+
+    def check_authentication(self) -> bool:
+        """Check if user is correctly authenticated with the Toggl API.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/me#get-logged)
+        """
+        warnings.warn(
+            (
+                "DEPRECATED: 'check_authentication' is being removed. "
+                "Use the static method 'verify_authentication instead!"
+            ),
+            stacklevel=3,
+        )
+        try:
+            TogglEndpoint.request(self, "/logged")
+        except HTTPStatusError as err:
+            log.critical("Failed to verify authentication!")
+            log.exception("%s")
+            if err.response.status_code != codes.FORBIDDEN:
+                raise
+
+            return False
+
+        return True
+
+    @staticmethod
+    def verify_authentication(auth: BasicAuth) -> bool:
+        """Check if user is correctly authenticated with the Toggl API.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/me#get-logged)
+
+        Examples:
+            >>> UserEndpoint.verify_authentication(auth)
+            True
+
+            >>> auth = generate_authentication()
+            >>> UserEndpoint.verify_authentication(auth)
+            True
+
+        Args:
+            auth: Basic authentication object either created manually or one
+                of the provided authentication utilities.
+
+        Raises:
+            HTTPStatusError: If anything that an error that is not a FORBIDDEN code.
+
+        Returns:
+            bool: True if successfully verified authentication else False.
+
+        """
+        try:
+            httpx.get(TogglEndpoint.BASE_ENDPOINT + "me/logged", auth=auth).raise_for_status()
+        except HTTPStatusError as err:
+            log.critical("Failed to verify authentication!")
+            log.exception("%s")
+            if err.response.status_code != codes.FORBIDDEN:
+                raise
+
+            return False
+
+        return True
+
+    def get_details(self) -> dict[str, Any]:
+        """Returns details for the current user.
+
+        [Official Documentation](https://engineering.toggl.com/docs/api/me#get-me)
+        """
+        return TogglEndpoint.request(self, "", raw=True).json()
+
+    @property
+    def endpoint(self) -> str:
+        return "me"
+
+    @property
+    def model(self) -> type[TogglTracker]:
+        return TogglTracker
