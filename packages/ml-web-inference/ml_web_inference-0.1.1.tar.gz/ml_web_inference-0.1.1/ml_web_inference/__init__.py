140f5e2b2dcf9a2c4cd6b282276ca184d7cdd34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 04:07:52 +0000

    Add package ml-web-inference/0.1.1/ml_web_inference-0.1.1.tar.gz

---
+++ packages/ml-web-inference/ml_web_inference-0.1.1.tar.gz/ml_web_inference-0.1.1/ml_web_inference/__init__.py
@@ -0,0 +1,12 @@
+from .main import expose
+from .main import Request, Response
+from fastapi.responses import JSONResponse, StreamingResponse, FileResponse
+
+__all__ = [
+    "expose",
+    "Request",
+    "Response",
+    "StreamingResponse",
+    "FileResponse",
+    "JSONResponse",
+]
