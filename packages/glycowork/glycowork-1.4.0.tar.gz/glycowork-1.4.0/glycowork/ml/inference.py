58355124462d8dc7bafec9ddcc1314c59c12553e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:07:10 +0000

    Add package glycowork/1.4.0/glycowork-1.4.0.tar.gz

---
+++ packages/glycowork/glycowork-1.4.0.tar.gz/glycowork-1.4.0/glycowork/ml/inference.py
@@ -0,0 +1,232 @@
+import pandas as pd
+import numpy as np
+from importlib import resources
+import math
+try:
+    import torch
+    from torch.utils.data import Dataset
+    # Choosing the right computing architecture
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda:0"
+except ImportError:
+  raise ImportError("<torch missing; did you do 'pip install glycowork[ml]'?>")
+from glycowork.glycan_data.loader import lib, unwrap
+from glycowork.motif.tokenization import prot_to_coded
+from glycowork.ml.processing import dataset_to_dataloader
+
+
+class SimpleDataset(Dataset):
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+  def __len__(self):
+    return len(self.x)
+
+  def __getitem__(self, index):
+    inp = self.x[index]
+    out = self.y[index]
+    return torch.FloatTensor(inp), torch.FloatTensor([out])
+
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
+
+def glycans_to_emb(glycans, model, libr = None, batch_size = 32, rep = True,
+                   class_list = None):
+    """Returns a dataframe of learned representations for a list of glycans\n
+    | Arguments:
+    | :-
+    | glycans (list): list of glycans in IUPAC-condensed as strings
+    | model (PyTorch object): trained graph neural network (such as SweetNet) for analyzing glycans
+    | libr (dict): dictionary of form glycoletter:index
+    | batch_size (int): change to batch_size used during training; default:32
+    | rep (bool): True returns representations, False returns actual predicted labels; default is True
+    | class_list (list): list of unique classes to map predictions\n
+    | Returns:
+    | :-
+    | Returns dataframe of learned representations (columns) for each glycan (rows)
+    """
+    if libr is None:
+      libr = lib
+    # Preparing dataset for PyTorch
+    glycan_loader = dataset_to_dataloader(glycans, range(len(glycans)),
+                                          libr = libr, batch_size = batch_size,
+                                          shuffle = False)
+    res = []
+    model = model.eval()
+    # Get predictions for each mini-batch
+    for data in glycan_loader:
+        x, y, edge_index, batch = data.labels, data.y, data.edge_index, data.batch
+        x = x.to(device)
+        y = y.to(device)
+        edge_index = edge_index.to(device)
+        batch = batch.to(device)
+        pred, out = model(x, edge_index, batch, inference = True)
+        # Unpacking and combining predictions
+        if rep:
+            res.extend(out.detach().cpu().numpy())
+        else:
+            res.extend(pred.detach().cpu().numpy())
+    if rep:
+        return pd.DataFrame(res)
+    else:
+        preds = [class_list[k] for k in np.argmax(res, axis = 1)]
+        return preds
+
+
+def get_multi_pred(prot, glycans, model, prot_dic,
+                   background_correction = False, correction_df = None,
+                   batch_size = 128, libr = None, flex = False):
+  """Inner function to actually get predictions for lectin-glycan binding from LectinOracle-type model\n
+  | Arguments:
+  | :-
+  | prot (string): protein amino acid sequence
+  | glycans (list): list of glycans in IUPACcondensed
+  | model (PyTorch object): trained LectinOracle-type model
+  | prot_dic (dictionary): dictionary of type protein sequence:ESM1b representation
+  | background_correction (bool): whether to correct predictions for background; default:False
+  | correction_df (dataframe): background prediction for (ideally) all provided glycans; default:None
+  | batch_size (int): change to batch_size used during training; default:128
+  | libr (dict): dictionary of form glycoletter:index
+  | flex (bool): depends on whether you use LectinOracle (False) or LectinOracle_flex (True); default:False\n
+  | Returns:
+  | :-
+  | Returns dataframe of glycan sequences and predicted binding to prot
+  """
+  if libr is None:
+      libr = lib
+  # Preparing dataset for PyTorch
+  if flex:
+      prot = prot_to_coded([prot])
+      feature = prot * len(glycans)
+  else:
+      rep = prot_dic.get(prot, "new protein, no stored embedding")
+      feature = [rep] * len(glycans)
+  train_loader = dataset_to_dataloader(glycans, [0.99]*len(glycans),
+                                         libr = libr, batch_size = batch_size,
+                                         shuffle = False, extra_feature = feature)
+  model = model.eval()
+  res = []
+  # Get predictions for each mini-batch
+  for k in train_loader:
+    x, y, edge_index, prot, batch = k.labels, k.y, k.edge_index, k.train_idx, k.batch
+    x, y, edge_index, prot, batch = x.to(device), y.to(device), edge_index.to(device), prot.view(max(batch) + 1, -1).float().to(device), batch.to(device)
+    pred = model(prot, x, edge_index, batch)
+    res.extend(pred.detach().cpu().numpy())
+  # Applying background correction of predictions
+  if background_correction:
+    correction_df = pd.Series(correction_df.pred.values,
+                              index = correction_df.motif).to_dict()
+    bg_res = [correction_df.get(j, 0) for j in glycans]
+    if 0 in bg_res:
+      print("Warning: not all glycans are in the correction_df; consider adding their background to correction_df")
+    res = [a_i - b_i for a_i, b_i in zip(res, bg_res)]
+  return res
+
+
+def get_lectin_preds(prot, glycans, model, prot_dic = None, background_correction = False,
+                     correction_df = None, batch_size = 128, libr = None, sort = True,
+                     flex = False):
+  """Wrapper that uses LectinOracle-type model for predicting binding of protein to glycans\n
+  | Arguments:
+  | :-
+  | prot (string): protein amino acid sequence
+  | glycans (list): list of glycans in IUPACcondensed
+  | model (PyTorch object): trained LectinOracle-type model
+  | prot_dic (dictionary): dictionary of type protein sequence:ESM1b representation
+  | background_correction (bool): whether to correct predictions for background; default:False
+  | correction_df (dataframe): background prediction for (ideally) all provided glycans; default:V4 correction file
+  | batch_size (int): change to batch_size used during training; default:128
+  | libr (dict): dictionary of form glycoletter:index
+  | sort (bool): whether to sort prediction results descendingly; default:True
+  | flex (bool): depends on whether you use LectinOracle (False) or LectinOracle_flex (True); default:False\n
+  | Returns:
+  | :-
+  | Returns dataframe of glycan sequences and predicted binding to prot
+  """
+  if libr is None:
+    libr = lib
+  if correction_df is None:
+    with resources.open_text("glycowork.ml", "glycowork_lectinoracle_background_correction.csv") as f:
+        correction_df = pd.read_csv(f)
+  if prot_dic is None and not flex:
+    print("It seems you did not provide a dictionary of protein:ESM-1b representations. This is necessary.")
+  preds = unwrap(get_multi_pred(prot, glycans, model, prot_dic,
+                         batch_size = batch_size, libr = libr,
+                         flex = flex))
+  df_pred = pd.DataFrame({'motif': glycans, 'pred': preds})
+  if background_correction:
+    correction_dict = {motif: pred for motif, pred in zip(correction_df['motif'], correction_df['pred'])}
+    for idx, row in df_pred.iterrows():
+        motif = row['motif']
+        if motif in correction_dict:
+            df_pred.at[idx, 'pred'] -= correction_dict[motif]
+  if sort:
+    df_pred.sort_values('pred', ascending = True, inplace = True)
+  return df_pred
+
+
+def get_esm1b_representations(prots, model, alphabet):
+  """Retrieves ESM1b representations of protein for using them as input for LectinOracle\n
+  | Arguments:
+  | :-
+  | prots (list): list of protein sequences (strings) that should be converted
+  | model (ESM1b object): trained ESM1b model; from running esm.pretrained.esm1b_t33_650M_UR50S()
+  | alphabet (ESM1b object): used for converting sequences; from running esm.pretrained.esm1b_t33_650M_UR50S()\n
+  | Returns:
+  | :-
+  | Returns dictionary of the form protein sequence:ESM1b representation
+  """
+  # model, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
+  batch_converter = alphabet.get_batch_converter()
+  unique_prots = list(set(prots))
+  data_list = [
+        ('protein' + str(idx), prot[:min(len(prot), 1000)])
+        for idx, prot in enumerate(unique_prots)
+    ]
+  _, _, batch_tokens = batch_converter(data_list)
+  with torch.no_grad():
+      results = model(batch_tokens, repr_layers = [33], return_contacts = False)
+  token_representations = results["representations"][33]
+  sequence_representations = [
+        token_representations[i, 1:len(seq) + 1].mean(0)
+        for i, (_, seq) in enumerate(data_list)
+    ]
+  prot_dic = {
+        unique_prots[k]: s_rep.tolist()
+        for k, s_rep in enumerate(sequence_representations)
+    }
+  return prot_dic
+
+
+def get_Nsequon_preds(prots, model, prot_dic):
+  """Predicts whether an N-sequon will be glycosylated\n
+  | Arguments:
+  | :-
+  | prots (list): list of protein sequences (strings), in the form of 20 AA + N + 20 AA; replace missing sequence with corr. number of 'z'
+  | model (PyTorch object): trained NSequonPred-type model
+  | prot_dic (dictionary): dictionary of type protein sequence:ESM1b representation\n
+  | Returns:
+  | :-
+  | Returns dataframe of protein sequences and predicted likelihood of being an N-sequon
+  """
+  reps = [prot_dic[k] for k in prots]
+  # Preparing dataset for PyTorch
+  dataset = SimpleDataset(reps, [0]*len(reps))
+  loader = torch.utils.data.DataLoader(dataset, batch_size = 32, shuffle = False)
+  model = model.eval()
+  preds = []
+  # Get predictions for each mini-batch
+  for x, _ in loader:
+    x = x.to(device)
+    pred = model(x)
+    pred = [sigmoid(x) for x in pred.cpu().detach().numpy()]
+    preds.extend(pred)
+  df_pred = pd.DataFrame({
+        'seq': prots,
+        'glycosylated': preds
+    })
+  return df_pred
---
+++ packages/glycowork/glycowork-1.4.0.tar.gz/glycowork-1.4.0/glycowork/network/evolution.py
@@ -0,0 +1,211 @@
+import os
+import pickle
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cosine
+from scipy.cluster.hierarchy import dendrogram, linkage
+from glycowork.motif.graph import subgraph_isomorphism
+
+this_dir, this_filename = os.path.split(__file__)
+data_path = os.path.join(this_dir, 'milk_networks_exhaustive.pkl')
+net_dic = pickle.load(open(data_path, 'rb'))
+
+
+def calculate_distance_matrix(to_compare, dist_func, label_list = None):
+  """calculates pairwise distances based on objects and a metric\n
+  | Arguments:
+  | :-
+  | to_compare (dict or list): objects to calculate pairwise distances for, if dict then values have to be lists
+  | dist_func (function): function such as 'jaccard' or 'cosine' that calculates distance given two lists/elements
+  | label_list (list): column names for the resulting distance matrix; default:range(len(to_compare))\n
+  | Returns:
+  | :-
+  | Returns a len(to_compare) x len(to_compare) distance matrix
+  """
+  n = len(to_compare)
+  dm = np.zeros((n, n))
+  # Check whether comparison objects are in a dict or list
+  if isinstance(to_compare, dict):
+    items = list(to_compare.values())
+    if label_list is None:
+      label_list = list(to_compare.keys())
+  else:
+    items = to_compare
+    if label_list is None:
+      label_list = list(range(n))
+  # Calculate pairwise distances
+  for i in range(n):
+    for j in range(i+1, n):
+      distance = dist_func(items[i], items[j])
+      dm[i, j] = distance
+      dm[j, i] = distance
+  dm = pd.DataFrame(dm, columns = label_list, index = label_list)
+  return dm
+
+
+def distance_from_embeddings(df, embeddings, cut_off = 10, rank = 'Species',
+                             averaging = 'median'):
+  """calculates a cosine distance matrix from learned embeddings\n
+  | Arguments:
+  | :-
+  | df (dataframe): dataframe with glycans as rows and taxonomic information as columns
+  | embeddings (dataframe): dataframe with glycans as rows and learned embeddings as columns (e.g., from glycans_to_emb)
+  | cut_off (int): how many glycans a rank (e.g., species) needs to have at least to be included; default:10
+  | rank (string): which taxonomic rank to use for grouping organisms; default:'Species'
+  | averaging (string): how to average embeddings, by 'median' or 'mean'; default:'median'\n
+  | Returns:
+  | :-
+  | Returns a rank x rank distance matrix
+  """
+  if averaging not in ['mean', 'median']:
+    print("Only 'median' and 'mean' are permitted averaging choices.")
+    return
+  # Subset df to only contain ranks with a minimum number of data points
+  value_counts = df[rank].value_counts()
+  valid_ranks = value_counts.index[value_counts >= cut_off]
+  df_filtered = df[df[rank].isin(valid_ranks)]
+  # Retrieve and average embeddings for all glycans
+  embeddings_filtered = embeddings.loc[df_filtered.index]
+  grouped = df_filtered.groupby(rank)
+  avg_embeddings = grouped.apply(lambda g: embeddings_filtered.loc[g.index].agg(averaging)).reset_index()
+  avg_values = np.vstack(avg_embeddings[0].values)
+  # Get the distance matrix
+  return calculate_distance_matrix(avg_values, cosine, label_list = valid_ranks)
+
+
+def jaccard(list1, list2):
+  """calculates Jaccard distance from two networks\n
+  | Arguments:
+  | :-
+  | list1 (list or networkx graph): list containing objects to compare
+  | list2 (list or networkx graph): list containing objects to compare\n
+  | Returns:
+  | :-
+  | Returns Jaccard distance between list1 and list2
+  """
+  intersection = len(set(list1).intersection(list2))
+  union = (len(list1) + len(list2)) - intersection
+  return 1 - float(intersection) / union
+
+
+def distance_from_metric(df, networks, metric = "Jaccard", cut_off = 10, rank = "Species"):
+  """calculates a distance matrix of generated networks based on provided metric\n
+  | Arguments:
+  | :-
+  | df (dataframe): dataframe with glycans as rows and taxonomic information as columns
+  | networks (list): list of networks in networkx format
+  | metric (string): which metric to use, available: 'Jaccard'; default:'Jaccard'
+  | cut_off (int): how many glycans a rank (e.g., species) needs to have at least to be included; default:10
+  | rank (string): which taxonomic rank to use for grouping organisms; default:'Species'\n
+  | Returns:
+  | :-
+  | Returns a rank x rank distance matrix
+  """
+  # Choose distance metric
+  metric_funcs = {"Jaccard": jaccard}
+  dist_func = metric_funcs.get(metric)
+  if dist_func is None:
+    print("Not a defined metric. At the moment, only 'Jaccard' is available as a metric.")
+    return
+  # Get all objects to calculate distance between
+  value_counts = df[rank].value_counts()
+  valid_ranks = value_counts.index[value_counts >= cut_off]
+  valid_networks = [net for spec, net in zip(df[rank], networks) if spec in valid_ranks]
+  # Get distance matrix
+  return calculate_distance_matrix(valid_networks, dist_func, label_list = valid_ranks.tolist())
+
+
+def dendrogram_from_distance(dm, ylabel = 'Mammalia', filepath = ''):
+  """plots a dendrogram from distance matrix\n
+  | Arguments:
+  | :-
+  | dm (dataframe): a rank x rank distance matrix (e.g., from distance_from_embeddings)
+  | ylabel (string): how to label the y-axis of the dendrogram; default:'Mammalia'
+  | filepath (string): absolute path including full filename allows for saving the plot\n
+  """
+  # Hierarchical clustering on the distance matrix
+  Z = linkage(dm)
+  plt.figure(figsize = (10, 10))
+  plt.title('Hierarchical Clustering Dendrogram')
+  plt.xlabel('Distance')
+  plt.ylabel(ylabel)
+  dendrogram(
+      Z,
+      truncate_mode = 'lastp',  # Show only the last p merged clusters
+      orientation = 'left',
+      p = 300,  # Show only the last p merged clusters
+      show_leaf_counts = False,  # Otherwise numbers in brackets are counts
+      leaf_rotation = 0.,
+      labels = dm.columns.tolist(),
+      leaf_font_size = 11.,
+      show_contracted = True,  # To get a distribution impression in truncated branches
+      )
+  if len(filepath) > 1:
+    plt.savefig(filepath, format = filepath.split('.')[-1], dpi = 300,
+                bbox_inches = 'tight')
+
+
+def check_conservation(glycan, df, network_dic = None, rank = 'Order', threshold = 5, motif = False):
+  """estimates evolutionary conservation of glycans and glycan motifs via biosynthetic networks\n
+  | Arguments:
+  | :-
+  | glycan (string): full glycan or glycan motif in IUPAC-condensed nomenclature
+  | df (dataframe): dataframe in the style of df_species, each row one glycan and columns are the taxonomic levels
+  | network_dic (dict): dictionary of form species name : biosynthetic network (gained from construct_network); default:pre-computed milk networks
+  | rank (string): at which taxonomic level to assess conservation; default:Order
+  | threshold (int): threshold of how many glycans a species needs to have to consider the species;default:5
+  | motif (bool): whether glycan is a motif (True) or a full sequence (False); default:False\n
+  | Returns:
+  | :-
+  | Returns a dictionary of taxonomic group : degree of conservation
+  """
+  if network_dic is None:
+    network_dic = net_dic
+  # Subset species with at least the threshold-number of glycans
+  species_counts = df['Species'].value_counts()
+  valid_species = species_counts.index[species_counts >= threshold]
+  df_filtered = df[df['Species'].isin(valid_species)]
+  # Subset members of rank with at least two species
+  rank_counts = df_filtered.groupby(rank)['Species'].nunique()
+  valid_ranks = rank_counts.index[rank_counts > 1]
+  # Subset networks
+  filtered_network_dic = {k: v for k, v in network_dic.items() if k in valid_species}
+  # For each member of rank, get all species networks and search for motif
+  conserved = {}
+  for r in valid_ranks:
+    rank_df = df_filtered[df_filtered[rank] == r]
+    rank_species = rank_df['Species'].unique()
+    rank_networks = [filtered_network_dic[spec] for spec in rank_species]
+    rank_nodes = [list(net.nodes()) for net in rank_networks]
+    if motif:
+      if glycan[-1] == ')':
+        conserved[r] = sum(glycan in "".join(nodes) for nodes in rank_nodes) / len(rank_nodes)
+      else:
+        conserved[r] =  sum(any(subgraph_isomorphism(node, glycan) for node in nodes) for nodes in rank_nodes) / len(rank_nodes)
+    else:
+      conserved[r] = sum(glycan in nodes for nodes in rank_nodes) / len(rank_nodes)
+  return conserved
+
+
+def get_communities(network_list, label_list = None):
+  """Find communities for each graph in a list of graphs\n
+  | Arguments:
+  | :-
+  | network_list (list): list of undirected biosynthetic networks, in the form of networkx objects
+  | label_list (list): labels to create the community names, which are running_number + _ + label[k]  for graph_list[k]; default:range(len(graph_list))\n
+  | Returns:
+  | :-
+  | Returns a merged dictionary of community : glycans in that community
+  """
+  if label_list is None:
+    label_list = list(range(len(network_list)))
+  final_comm_dict = {}
+  # Label the communities by species name and running number to distinguish them afterwards
+  for i, network in enumerate(network_list):
+    communities = nx.algorithms.community.louvain.louvain_communities(network)
+    for comm_index, community in enumerate(communities):
+      comm_name = f"{comm_index}_{label_list[i]}"
+      final_comm_dict[comm_name] = list(community)
+  return final_comm_dict
