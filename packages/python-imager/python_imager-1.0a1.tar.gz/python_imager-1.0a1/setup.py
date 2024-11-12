1988190474dd8106e36e6550da0d8bfa707f761b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:07:17 +0000

    Add package python-imager/1.0a1/python_imager-1.0a1.tar.gz

---
+++ packages/python-imager/python_imager-1.0a1.tar.gz/python_imager-1.0a1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.a1'
+DESCRIPTION = 'Python image program'
+with open("README.md", "r", encoding="utf8") as file:
+    LONG_DESCRIPTION = file.read()
+
+setup(
+        name="python-imager",
+        version=VERSION,
+        author="T-Sana",
+        author_email="tsana.code@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["numpy", "opencv-python", "screeninfo"],
+        keywords=['python', 'image'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+        ],
+        license="LICENCE",
+)
