c3f1ec2ed743e95d745d9f8eb6a5a556140d9d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:07:18 +0000

    Add package iamai/0.1.7/iamai-0.1.7-cp39-abi3-win_amd64.whl

---
+++ packages/iamai/iamai-0.1.7-cp39-abi3-win_amd64.whl/iamai/__init__.py
@@ -0,0 +1,25 @@
+"""API Documentation
+
+Comprehensive AI Toolkit for Multi Modal Learning and Cross-Platform Robotics.
+
+This Module imports the following contents from the sub-module.
+"""
+
+from .adapter import Adapter
+from .bot import Bot
+from .config import ConfigModel
+from .dependencies import Depends
+from .event import Event, MessageEvent
+from .plugin import Plugin
+from . import libcore
+
+__all__ = [
+    "Adapter",
+    "Bot",
+    "ConfigModel",
+    "Depends",
+    "Event",
+    "MessageEvent",
+    "Plugin",
+    "libcore",
+]
