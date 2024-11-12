dbca35cfb949517cf552618318da03f02faa4de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:06:47 +0000

    Add package ca5e9fb055b0e9d98f2c2a6bee72c3f9/0.1.0/ca5e9fb055b0e9d98f2c2a6bee72c3f9-0.1.0.tar.gz

---
+++ packages/ca5e9fb055b0e9d98f2c2a6bee72c3f9/ca5e9fb055b0e9d98f2c2a6bee72c3f9-0.1.0.tar.gz/ca5e9fb055b0e9d98f2c2a6bee72c3f9-0.1.0/main.py
@@ -0,0 +1,2 @@
+def main():
+    print("This package is an example of a Dependency Hallucination. If you are trying to use it, you have most likely been given this package incorrectly from an AI coding assistant.")
