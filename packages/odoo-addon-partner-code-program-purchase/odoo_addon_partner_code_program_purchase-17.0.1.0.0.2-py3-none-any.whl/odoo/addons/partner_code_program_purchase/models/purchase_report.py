d715f4fc40b55dd01d9061014515428aa33da685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 04:07:05 +0000

    Add package odoo-addon-partner-code-program-purchase/17.0.1.0.0.2/odoo_addon_partner_code_program_purchase-17.0.1.0.0.2-py3-none-any.whl

---
+++ packages/odoo-addon-partner-code-program-purchase/odoo_addon_partner_code_program_purchase-17.0.1.0.0.2-py3-none-any.whl/odoo/addons/partner_code_program_purchase/models/purchase_report.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Alberto Martínez <alberto.martinez@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class PurchaseReport(models.Model):
+    _inherit = "purchase.report"
+
+    code_program_id = fields.Many2one(
+        string="Code Program",
+        comodel_name="res.partner.code.program",
+    )
+
+    def _select(self):
+        return super()._select() + ", partner.code_program_id as code_program_id"
+
+    def _group_by(self):
+        return super()._group_by() + ", partner.code_program_id"
