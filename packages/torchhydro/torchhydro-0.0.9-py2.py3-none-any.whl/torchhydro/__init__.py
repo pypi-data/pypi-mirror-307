2f2531805cf999026a3856a74fbd32f9eace31ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:07:28 +0000

    Add package torchhydro/0.0.9/torchhydro-0.0.9-py2.py3-none-any.whl

---
+++ packages/torchhydro/torchhydro-0.0.9-py2.py3-none-any.whl/torchhydro/__init__.py
@@ -0,0 +1,94 @@
+"""
+Author: Wenyu Ouyang
+Date: 2023-07-31 08:40:43
+LastEditTime: 2024-09-10 20:10:25
+LastEditors: Wenyu Ouyang
+Description: Top-level package for torchhydro
+FilePath: \torchhydro\torchhydro\__init__.py
+Copyright (c) 2023-2024 Wenyu Ouyang. All rights reserved.
+"""
+
+""""""
+
+__author__ = """Wenyu Ouyang"""
+__email__ = "wenyuouyang@outlook.com"
+__version__ = '0.0.9'
+
+from pathlib import Path
+import yaml
+import os
+
+from hydroutils import hydro_file
+
+CACHE_DIR = Path(hydro_file.get_cache_dir())
+SETTING_FILE = os.path.join(Path.home(), "hydro_setting.yml")
+
+
+def read_setting(setting_path):
+    if not os.path.exists(setting_path):
+        raise FileNotFoundError(f"Configuration file not found: {setting_path}")
+
+    with open(setting_path, "r", encoding="utf-8") as file:
+        setting = yaml.safe_load(file)
+
+    example_setting = (
+        "minio:\n"
+        "  server_url: 'http://minio.waterism.com:9090' # Update with your URL\n"
+        "  client_endpoint: 'http://minio.waterism.com:9000' # Update with your URL\n"
+        "  access_key: 'your minio access key'\n"
+        "  secret: 'your minio secret'\n\n"
+        "local_data_path:\n"
+        "  root: 'D:\\data\\waterism' # Update with your root data directory\n"
+        "  datasets-origin: 'D:\\data\\waterism\\datasets-origin'\n"
+        "  datasets-interim: 'D:\\data\\waterism\\datasets-interim'"
+        "postgres:\n"
+        "  server_url: 'your database server URL'"  # if you don't have a database, you can just write this key and leave the rest empty
+        "  port: your database port"
+        "  username: 'your database username'"
+        "  password: 'your database password'"
+        "  database: 'your database name'"
+    )
+
+    if setting is None:
+        raise ValueError(
+            f"Configuration file is empty or has invalid format.\n\nExample configuration:\n{example_setting}"
+        )
+
+    # Define the expected structure
+    expected_structure = {
+        "minio": ["server_url", "client_endpoint", "access_key", "secret"],
+        "local_data_path": ["root", "datasets-origin", "datasets-interim"],
+        "postgres": ["server_url", "port", "username", "password", "database"],
+    }
+
+    # Validate the structure
+    try:
+        for key, subkeys in expected_structure.items():
+            if key not in setting:
+                raise KeyError(f"Missing required key in config: {key}")
+
+            if isinstance(subkeys, list):
+                for subkey in subkeys:
+                    if subkey not in setting[key]:
+                        raise KeyError(f"Missing required subkey '{subkey}' in '{key}'")
+    except KeyError as e:
+        raise ValueError(
+            f"Incorrect configuration format: {e}\n\nExample configuration:\n{example_setting}"
+        ) from e
+
+    return setting
+
+
+try:
+    SETTING = read_setting(SETTING_FILE)
+except ValueError as e:
+    print(e)
+except Exception as e:
+    print(f"Unexpected error: {e}")
+
+
+from .configs import *
+from .datasets import *
+from .models import *
+from .trainers import *
+from .explainers import *
---
+++ packages/torchhydro/torchhydro-0.0.9-py2.py3-none-any.whl/torchhydro/models/model_dict_function.py
@@ -0,0 +1,89 @@
+"""
+Author: Wenyu Ouyang
+Date: 2021-12-31 11:08:29
+LastEditTime: 2024-10-29 11:43:09
+LastEditors: Wenyu Ouyang
+Description: Dicts including models (which are seq-first), losses, and optims
+FilePath: \torchhydro\torchhydro\models\model_dict_function.py
+Copyright (c) 2021-2022 Wenyu Ouyang. All rights reserved.
+"""
+
+from torchhydro.models.dpl4hbv import DplLstmHbv, DplAnnHbv
+from torchhydro.models.cudnnlstm import (
+    CudnnLstmModel,
+    LinearCudnnLstmModel,
+    CNN1dLCmodel,
+    CudnnLstmModelLstmKernel,
+    CudnnLstmModelMultiOutput,
+    CpuLstmModel,
+)
+
+from torchhydro.models.simple_lstm import SimpleLSTMForecast
+from torchhydro.models.seq2seq import (
+    GeneralSeq2Seq,
+    DataEnhancedModel,
+    DataFusionModel,
+    Transformer,
+)
+from torch.optim import Adam, SGD, Adadelta
+from torchhydro.models.crits import (
+    RMSELoss,
+    RmseLoss,
+    MultiOutLoss,
+    UncertaintyWeights,
+    DynamicTaskPrior,
+    MultiOutWaterBalanceLoss,
+    NSELoss,
+    MAPELoss,
+    MASELoss,
+    MAELoss,
+    QuantileLoss,
+)
+from torchhydro.models.dpl4xaj import DplLstmXaj, DplAnnXaj
+from torchhydro.models.dpl4xaj_nn4et import DplLstmNnModuleXaj
+from torchhydro.models.spplstm import SPP_LSTM_Model, SPP_LSTM_Model_2
+from torchhydro.models.dpl4hbv import DplLstmHbv, DplAnnHbv
+from torchhydro.models.dpl4gr4j import DplLstmGr4j, DplAnnGr4j
+
+"""
+Utility dictionaries to map a string to a class.
+"""
+pytorch_model_dict = {
+    "KuaiLSTM": CudnnLstmModel,
+    "CpuLSTM": CpuLstmModel,
+    "KaiLSTM": LinearCudnnLstmModel,
+    "DapengCNNLSTM": CNN1dLCmodel,
+    "LSTMKernel": CudnnLstmModelLstmKernel,
+    "KuaiLSTMMultiOut": CudnnLstmModelMultiOutput,
+    "DplLstmXaj": DplLstmXaj,
+    "DplAttrXaj": DplAnnXaj,
+    "SPPLSTM": SPP_LSTM_Model,
+    "SimpleLSTMForecast": SimpleLSTMForecast,
+    "SPPLSTM2": SPP_LSTM_Model_2,
+    "Seq2Seq": GeneralSeq2Seq,
+    "DataEnhanced": DataEnhancedModel,
+    "DataFusion": DataFusionModel,
+    "Transformer": Transformer,
+    "DplNnModuleXaj": DplLstmNnModuleXaj,
+    "DplLstmHbv": DplLstmHbv,
+    "DplAnnHbv": DplAnnHbv,
+    "DplLstmGr4j": DplLstmGr4j,
+    "DplAnnGr4j": DplAnnGr4j,
+}
+
+pytorch_criterion_dict = {
+    "RMSE": RMSELoss,
+    # xxxSum means that calculate the criterion for each "feature"(the final dim of output), then sum them up
+    "RMSESum": RmseLoss,
+    "MultiOutLoss": MultiOutLoss,
+    "UncertaintyWeights": UncertaintyWeights,
+    "DynamicTaskPrior": DynamicTaskPrior,
+    "MultiOutWaterBalanceLoss": MultiOutWaterBalanceLoss,
+    "NSELoss": NSELoss,
+    "MAPELoss": MAPELoss,
+    "MASELoss": MASELoss,
+    "MAELoss": MAELoss,
+    "QuantileLoss": QuantileLoss,
+}
+
+pytorch_opt_dict = {"Adam": Adam, "SGD": SGD, "Adadelta": Adadelta}
