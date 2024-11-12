8ae86327489222c6d091aba70078c9e8e763ef78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:06:45 +0000

    Add package sthg-fastapi-logs/0.1.13/sthg_fastapi_logs-0.1.13-py3-none-any.whl

---
+++ packages/sthg-fastapi-logs/sthg_fastapi_logs-0.1.13-py3-none-any.whl/sthg_fastapi_logs/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.13"
+
+from .exception import register_exception as register_exception
+from .log_wrapper import register_middleware as register_middleware
+from .log_wrapper import class_log as class_log
+from .log_util import acc_logger as acc_logger
