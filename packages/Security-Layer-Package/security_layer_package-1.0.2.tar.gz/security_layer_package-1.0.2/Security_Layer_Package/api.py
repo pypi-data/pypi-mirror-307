28297c633639b79c98529d478e1adae41633df49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:06:46 +0000

    Add package Security-Layer-Package/1.0.2/security_layer_package-1.0.2.tar.gz

---
+++ packages/Security-Layer-Package/security_layer_package-1.0.2.tar.gz/security_layer_package-1.0.2/Security_Layer_Package/api.py
@@ -0,0 +1,13 @@
+import requests
+
+# Define the API endpoint
+url = 'http://127.0.0.1:8888'
+# prompt='hello'
+# Make the API call
+
+
+
+# Display the response in the app
+def detection(prompt):
+    response = requests.get(url+'/english/'+prompt)
+    return(response.text[0])
