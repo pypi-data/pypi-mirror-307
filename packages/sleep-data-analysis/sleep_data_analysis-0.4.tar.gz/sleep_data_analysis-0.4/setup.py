93dabe75a15f4882b97a0fd1b07ee6d2776a3f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:07:14 +0000

    Add package sleep-data-analysis/0.4/sleep_data_analysis-0.4.tar.gz

---
+++ packages/sleep-data-analysis/sleep_data_analysis-0.4.tar.gz/sleep_data_analysis-0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name="sleep_data_analysis",
+    version="0.4",
+    py_modules=["health_sleep_analysis"],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "scikit-learn",
+        "scipy"
+    ],
+    author="Prithviraj K Tagadinamani",
+    author_email="prithvirajtagadinamani@gmail.com",
+    description="A package for analyzing health and sleep data",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/prithviraj-kt/sleep_data_analysis.git",  # Replace with your actual URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            'sleep-data-analysis = sleep_data_analysis.project:main',
+        ],
+    },
+)
