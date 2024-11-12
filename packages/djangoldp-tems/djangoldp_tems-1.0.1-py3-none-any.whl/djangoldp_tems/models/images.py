0b4ed01c3a4cf546585efab29cb2e74e105b720f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:06:56 +0000

    Add package djangoldp-tems/1.0.1/djangoldp_tems-1.0.1-py3-none-any.whl

---
+++ packages/djangoldp-tems/djangoldp_tems-1.0.1-py3-none-any.whl/djangoldp_tems/models/images.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from djangoldp_tems.models.__base_model import baseTEMSModel
+
+
+class TEMSImages(baseTEMSModel):
+    name = models.CharField(max_length=255, blank=True, null=True, default="")
+    url = models.CharField(max_length=2000, blank=True, null=True, default="")
+
+    def __str__(self):
+        return self.name or self.url or self.urlid
+
+    class Meta(baseTEMSModel.Meta):
+        container_path = "/objects/images/"
+        verbose_name = _("TEMS Image")
+        verbose_name_plural = _("TEMS Images")
+
+        serializer_fields = [
+            "@id",
+            "creation_date",
+            "update_date",
+            "name",
+            "url",
+        ]
+        nested_fields = []
+        rdf_type = "tems:Image"
