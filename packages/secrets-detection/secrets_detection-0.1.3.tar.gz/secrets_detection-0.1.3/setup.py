866bfb6d56daeb6562d928e638cb8abc06d9cd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 04:08:37 +0000

    Add package secrets-detection/0.1.3/secrets_detection-0.1.3.tar.gz

---
+++ packages/secrets-detection/secrets_detection-0.1.3.tar.gz/secrets_detection-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='secrets-detection',  
+    version='0.1.3',  
+    packages=find_packages(),  
+    entry_points={
+        'console_scripts': [
+            'scan-secrets = secret_detect.scanner:analyze_repository',
+        ],
+    },
+    author='boyinf',   
+    description='A package for use in detecting secrets leaks in the GitLab pipeline.',  
+    long_description="The secrets detection tool was developed with the aim of assisting in the analysis of secrets leaking through the pipeline. Created to assist in leak detection, the tool stands out for its simplicity and efficiency through regex and analysis of the entire repository, representing the result intuitively through JSON reports.",  
+    long_description_content_type='text/plain',
+    url='https://github.com/Boyinf/secrets-detection/',  
+    license='MIT',  
+    classifiers=[  
+        'Programming Language :: Python :: 3',  
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',  
+    ],
+)
