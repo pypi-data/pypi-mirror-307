f83e990f7bd5238f28b275c9c2d1493a3eb27765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:07:18 +0000

    Add package python-imager/1.0.18/python_imager-1.0.18.tar.gz

---
+++ packages/python-imager/python_imager-1.0.18.tar.gz/python_imager-1.0.18/README.md
@@ -0,0 +1,5 @@
+# Pyimager
+ Pyimager is a python package
+
+# Installation
+ pip install python-imager
---
+++ packages/python-imager/python_imager-1.0.18.tar.gz/python_imager-1.0.18/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.18'
+DESCRIPTION = 'Python image program'
+with open("README.md", "r", encoding="utf8") as file:
+    LONG_DESCRIPTION = file.read()
+
+setup(
+        name="python-imager",
+        version=VERSION,
+        author="T-Sana",
+        author_email="tsana.code@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=["numpy", "opencv-python", "screeninfo"],
+        keywords=['python', 'image'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+        ],
+        license="LICENCE",
+)
