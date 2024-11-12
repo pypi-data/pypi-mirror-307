fc320521d84fe3b0b2a212b8c1dad27e619ef482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 04:06:30 +0000

    Add package djangoldp-tems/1.0.3/djangoldp_tems-1.0.3.tar.gz

---
+++ packages/djangoldp-tems/djangoldp_tems-1.0.3.tar.gz/djangoldp_tems-1.0.3/djangoldp_tems_trial6/models/object.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from djangoldp_tems.models.__base_object import baseTEMSObject
+
+
+class Trial6Object(baseTEMSObject):
+    subtitle = models.CharField(max_length=255, blank=True, null=True, default="")
+    editor = models.CharField(max_length=255, blank=True, null=True, default="")
+    contributors = models.TextField(blank=True, null=True, default="")
+    author = models.CharField(max_length=254, blank=True, null=True, default="")
+    platform = models.CharField(max_length=254, blank=True, null=True, default="")
+
+    def __str__(self):
+        return self.title or self.urlid
+
+    class Meta(baseTEMSObject.Meta):
+        container_path = "/objects/trial6/"
+        verbose_name = _("TEMS Trial 6 Object")
+        verbose_name_plural = _("TEMS Trial 6 Objects")
+
+        serializer_fields = [
+            "@id",
+            "creation_date",
+            "update_date",
+            "title",
+            "description",
+            "copyright",
+            "website",
+            "licences",
+            "images",
+            "subtitle",
+            "editor",
+            "contributors",
+            "author",
+            "platform",
+            "assets",
+        ]
+        nested_fields = [
+            "licences",
+            "assets",
+            "images",
+        ]
+        rdf_type = "tems:Object"
