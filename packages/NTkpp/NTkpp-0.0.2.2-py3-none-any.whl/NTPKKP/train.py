22d50e5696216242c6a50ee7f344af71fa7bc63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 04:07:11 +0000

    Add package NTkpp/0.0.2.2/NTkpp-0.0.2.2-py3-none-any.whl

---
+++ packages/NTkpp/NTkpp-0.0.2.2-py3-none-any.whl/NTPKKP/train.py
@@ -0,0 +1,9 @@
+def train(network, x_train, y_train, loss_func, epochs, optimizer=None):
+    for epoch in range(epochs):
+        network.backward(x_train, y_train, loss_func)
+        for layer in network.layers:
+            if hasattr(layer, 'update_weights'):
+                layer.update_weights(network.learning_rate, optimizer=optimizer)
+        loss_value = (loss_func(y_train, network.forward(x_train)))/y_train.size
+
+        print(f"Эпоха {epoch + 1}/{epochs}, Потеря: {loss_value:.4f}")
