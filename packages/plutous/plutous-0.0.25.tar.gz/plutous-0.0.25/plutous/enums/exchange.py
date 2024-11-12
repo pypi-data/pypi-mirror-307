a231ccd066700fd2b3bc77683e9b534fb3308465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 04:07:16 +0000

    Add package plutous/0.0.25/plutous-0.0.25.tar.gz

---
+++ packages/plutous/plutous-0.0.25.tar.gz/plutous-0.0.25/plutous/enums/exchange.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class Exchange(Enum):
+    BINANCE = "Binance"
+    BINANCE_COINM = "BinanceCoinm"
+    BINANCE_USDM = "BinanceUsdm"
+    BITGET = "Bitget"
+    BYBIT = "Bybit"
+    GATEIO = "GateIO"
+    HUOBI = "Huobi"
+    HYPERLIQUID = "Hyperliquid"
+    KUCOIN = "Kucoin"
+    KUCOIN_FUTURES = "KucoinFutures"
+    OKX = "Okx"
+    PHEMEX = "Phemex"
+    UPBIT = "Upbit"
+    WOO = "Woo"
+    MEXC = "Mexc"
+    LBANK = "LBank"
