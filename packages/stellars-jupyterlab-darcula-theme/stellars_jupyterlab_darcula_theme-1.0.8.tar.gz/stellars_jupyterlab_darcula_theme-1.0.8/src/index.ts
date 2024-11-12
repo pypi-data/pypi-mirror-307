463a62108e9426ecfdd279d0dfb3c6349b284417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-11-12 03:07:18 +0000

    Add package stellars-jupyterlab-darcula-theme/1.0.8/stellars_jupyterlab_darcula_theme-1.0.8.tar.gz

---
+++ packages/stellars-jupyterlab-darcula-theme/stellars_jupyterlab_darcula_theme-1.0.8.tar.gz/stellars_jupyterlab_darcula_theme-1.0.8/src/index.ts
@@ -0,0 +1,30 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the @stellars/jupyterlab_darcula_theme extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@stellars/jupyterlab_darcula_theme:plugin',
+  description: 'IntelliJ Darcula Theme tweaked by Stellars, based on original Darcula theme',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension @stellars/jupyterlab_darcula_theme is activated!');
+    const style = '@stellars/jupyterlab_darcula_theme/index.css';
+
+    manager.register({
+      name: 'Stellars Darcula',
+      themeScrollbars: true,
+      isLight: false,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default plugin;
